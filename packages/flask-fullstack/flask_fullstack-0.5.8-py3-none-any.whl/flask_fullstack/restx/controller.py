2d5e02341c055edd1ecdfbdc941ffb5c193f0ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:43 +0000

    Add package flask-fullstack/0.5.8/flask_fullstack-0.5.8-py3-none-any.whl

---
+++ packages/flask-fullstack/flask_fullstack-0.5.8-py3-none-any.whl/flask_fullstack/restx/controller.py
@@ -0,0 +1,349 @@
+from __future__ import annotations
+
+from collections.abc import Callable, Sequence
+from functools import wraps
+from typing import Any
+
+from flask import jsonify
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    set_access_cookies,
+    unset_jwt_cookies,
+)
+from flask_restx import Model as BaseModel, Namespace, abort as default_abort
+from flask_restx.fields import (
+    Boolean as BoolField,
+    Integer as IntegerField,
+    List as ListField,
+    Nested,
+)
+from flask_restx.marshalling import marshal
+from flask_restx.reqparse import RequestParser
+from flask_restx.utils import merge, unpack
+from pydantic import BaseModel as BaseModelV2
+
+from .marshals import Model, ResponseDoc, v2_model_to_ffs
+from ..base import DatabaseSearcherMixin, JWTAuthorizerMixin, UserRole
+
+Undefined = object()
+
+
+class ResourceController(Namespace, DatabaseSearcherMixin, JWTAuthorizerMixin):
+    """
+    Expansion of :class:`Namespace`, which adds decorators for methods of :class:`Resource`.
+
+    Methods of this class (used as decorators) allow parsing request parameters,
+    modifying responses and automatic updates to the Swagger documentation where possible
+    """
+
+    def __init__(
+        self,
+        name: str,
+        *,
+        description: str = None,
+        path: str = None,
+        decorators=None,
+        validate=None,
+        authorizations=None,
+        ordered: bool = False,
+        **kwargs,
+    ):
+        super().__init__(
+            name,
+            description,
+            path,
+            decorators,
+            validate,
+            authorizations,
+            ordered,
+            **kwargs,
+        )
+
+    def abort(self, code: int, message: str = None, **kwargs):
+        default_abort(code, message, **kwargs)
+
+    def doc_abort(
+        self,
+        error_code: int | str,
+        description: str,
+    ):
+        response = ResponseDoc.error_response(error_code, description)
+        return self.response(*response.get_args())
+
+    def add_authorization(
+        self,
+        response,
+        auth_agent: UserRole,
+        auth_name: str = None,
+    ) -> None:
+        jwt = self._get_identity()
+        if jwt is None:
+            jwt = {}
+        jwt[auth_name or ""] = auth_agent.get_identity()
+        set_access_cookies(response, create_access_token(identity=jwt))
+
+    def remove_authorization(self, response, auth_name: str = None) -> None:
+        jwt = self._get_identity()
+        unset_jwt_cookies(response)
+        if jwt is not None:
+            jwt.pop(auth_name or "")
+            if jwt:
+                set_access_cookies(response, create_access_token(identity=jwt))
+
+    def adds_authorization(self, auth_name: str = None):
+        def adds_authorization_wrapper(function):
+            @wraps(function)
+            def adds_authorization_inner(*args, **kwargs):
+                response, result, headers = unpack(function(*args, **kwargs))
+                if isinstance(result, UserRole):  # TODO passthrough for headers
+                    response = jsonify(response)
+                    self.add_authorization(response, result, auth_name)
+                    return response
+                return response, result, headers
+
+            return adds_authorization_inner
+
+        return adds_authorization_wrapper
+
+    def removes_authorization(self, auth_name: str = None):
+        def removes_authorization_wrapper(function):
+            @wraps(function)
+            @jwt_required()
+            def removes_authorization_inner(*args, **kwargs):
+                response, code, headers = unpack(function(*args, **kwargs))
+                response = jsonify(response)  # TODO passthrough for headers & code
+                self.remove_authorization(response, auth_name)
+                return response
+
+            return removes_authorization_inner
+
+        return removes_authorization_wrapper
+
+    def argument_parser(self, parser: RequestParser, use_undefined: bool = False):
+        """
+        - Parses request parameters and adds them to kwargs used to call the decorated function.
+        - Automatically updates endpoint's parameters with arguments from the parser.
+        """
+
+        def argument_wrapper(function):
+            @self.expect(parser)
+            @wraps(function)
+            def argument_inner(*args, **kwargs):
+                kwargs.update(parser.parse_args())
+                if use_undefined:
+                    kwargs.update(
+                        {
+                            args.dest or args.name: Undefined
+                            for args in parser.args
+                            if (args.dest or args.name) not in kwargs.keys()
+                        }
+                    )
+                return function(*args, **kwargs)
+
+            return argument_inner
+
+        return argument_wrapper
+
+    def doc_file_param(self, field_name: str):  # redo...
+        def doc_file_param_wrapper(function):
+            return self.doc(
+                params={field_name: {"in": "formData", "type": "file"}},
+                consumes="multipart/form-data",
+            )(function)
+
+        return doc_file_param_wrapper
+
+    def doc_responses(self, *responses: ResponseDoc):
+        """
+        Adds responses to the documentation. **Affects docs only!**
+
+        :param responses: all responses to document. Models inside are registered automatically.
+        """
+
+        def doc_responses_wrapper(function):
+            for response in responses:
+                response.register_model(self)
+                function = self.response(*response.get_args())(function)
+            return function
+
+        return doc_responses_wrapper
+
+    def _marshal_result(self, result, fields: type[Model], as_list: bool, **context):
+        if as_list:
+            return [fields.convert(d, **context) for d in result]
+        return fields.convert(result, **context)
+
+    def marshal_with(
+        self,
+        fields: BaseModel | type[Model] | type[BaseModelV2],
+        as_list=False,
+        skip_none=True,
+        *args,
+        **kwargs,
+    ):
+        result = super().marshal_with
+
+        if self.is_registrable(fields):
+            if issubclass(fields, BaseModelV2):
+                fields = v2_model_to_ffs(fields)
+            model = self.models.get(fields.name, None) or self.model(model=fields)
+
+            def marshal_with_wrapper(function: Callable) -> Callable[..., Model]:
+                @wraps(function)
+                @result(model, as_list, *args, skip_none=skip_none, **kwargs)
+                def marshal_with_inner(*args, **kwargs):
+                    result = function(*args, **kwargs)
+                    if isinstance(result, tuple):
+                        result, code, headers = unpack(result)
+                        return (
+                            self._marshal_result(result, fields, as_list, **kwargs),
+                            code,
+                            headers,
+                        )
+                    return self._marshal_result(result, fields, as_list, **kwargs)
+
+                return marshal_with_inner
+
+            return marshal_with_wrapper
+
+        return result(fields, as_list, *args, skip_none=skip_none, **kwargs)
+
+    def marshal(self, data, fields: type[Model] | ..., context=None, *args, **kwargs):
+        if self.is_registrable(fields):
+            if issubclass(fields, BaseModelV2):
+                fields = v2_model_to_ffs(fields)
+            if isinstance(data, Sequence):
+                data = [fields.convert(d, **context or {}) for d in data]
+            else:
+                data = fields.convert(data, **context or {})
+            fields = self.models.get(fields.name, None) or self.model(model=fields)
+        return marshal(data, fields, *args, **kwargs)
+
+    def marshal_with_authorization(
+        self,
+        fields: BaseModel | type[Model] | type[BaseModelV2],
+        as_list: bool = False,
+        auth_name: str = None,
+        **kwargs,
+    ):
+        model = self.models.get(fields.name, None) or self.model(model=fields)
+
+        def marshal_with_authorization_wrapper(function):
+            doc = {
+                "responses": {
+                    "200": (None, [model], kwargs) if as_list else (None, model, kwargs)
+                },
+                "__mask__": kwargs.get(
+                    "mask", True
+                ),  # Mask values can't be determined outside app context
+            }
+            function.__apidoc__ = merge(getattr(function, "__apidoc__", {}), doc)
+
+            @wraps(function)
+            def marshal_with_authorization_inner(*args, **kwargs):
+                response, result, headers = unpack(function(*args, **kwargs))
+                if isinstance(result, UserRole):  # TODO passthrough for headers
+                    response = jsonify(
+                        self.marshal(response, fields, skip_none=True, context=kwargs)
+                    )
+                    self.add_authorization(response, result, auth_name)
+                    return response
+                return response, result, headers
+
+            return marshal_with_authorization_inner
+
+        return marshal_with_authorization_wrapper
+
+    def lister(
+        self,
+        per_request: int,
+        marshal_model: BaseModel | type[Model] | type[BaseModelV2],
+        skip_none: bool = True,
+        count_all: Callable[..., int] | None = None,
+        provided_total: bool = False,
+    ):
+        """
+        - Used for organising pagination.
+        - Uses `counter` form incoming arguments for the decorated function and `per_request` argument
+          to define start and finish indexes, passed as keyword arguments to the decorated function.
+        - Marshals the return of the decorated function as a list with `marshal_model`.
+        - Adds information on the response to documentation automatically.
+
+        :param per_request:
+        :param marshal_model:
+        :param skip_none:
+        :param count_all:
+        :param provided_total:
+        :return:
+        """
+        if self.is_registrable(marshal_model):
+            name = getattr(marshal_model, "name", marshal_model.__name__)
+            model = self.models.get(name, None) or self.model(model=marshal_model)
+        else:
+            name = marshal_model.name
+            model = marshal_model
+
+        response = {
+            "results": ListField(Nested(model), max_items=per_request),
+            "has-next": BoolField,
+        }
+        if count_all is not None or provided_total:
+            response["total"] = IntegerField
+        response = BaseModel(f"List{name}", response)
+        response = ResponseDoc(200, f"Max size of results: {per_request}", response)
+
+        def lister_wrapper(function):
+            @self.doc_responses(response)
+            @wraps(function)
+            def lister_inner(*args, **kwargs):
+                offset: int = kwargs.pop("offset", None)
+                counter: int = kwargs.pop("counter", None)
+                if offset is None:
+                    if counter is None:
+                        self.abort(400, "Neither counter nor offset are provided")
+                    offset = counter * per_request
+
+                kwargs["start"] = offset
+                kwargs["finish"] = offset + per_request + 1
+                result_list = function(*args, **kwargs)
+                if provided_total:
+                    total = result_list[1]
+                    result_list = result_list[0]
+
+                if has_next := len(result_list) > per_request:
+                    result_list.pop()
+
+                if isinstance(marshal_model, type) and issubclass(marshal_model, Model):
+                    result_list = [
+                        marshal_model.convert(result, **kwargs)
+                        for result in result_list
+                    ]
+                result = {
+                    "results": marshal(result_list, model, skip_none=skip_none),
+                    "has-next": has_next,
+                }
+                if count_all is not None:
+                    result["total"] = count_all(*args, **kwargs)
+                if provided_total:
+                    result["total"] = total
+                return result
+
+            return lister_inner
+
+        return lister_wrapper
+
+    def is_registrable(self, model: Any) -> bool:
+        return isinstance(model, type) and (
+            issubclass(model, Model) or issubclass(model, BaseModelV2)
+        )
+
+    def model(self, name: str = None, model=None, **kwargs):
+        # TODO recursive registration
+        if self.is_registrable(model):
+            if issubclass(model, BaseModelV2):
+                model = v2_model_to_ffs(model)
+            if model.name is None:
+                model.name = name or model.__qualname__
+            return super().model(name or model.name, model.model(), **kwargs)
+        return super().model(name, model, **kwargs)
---
+++ packages/flask-fullstack/flask_fullstack-0.5.8-py3-none-any.whl/flask_fullstack/siox/structures.py
@@ -0,0 +1,174 @@
+from __future__ import annotations
+
+from collections import OrderedDict
+from collections.abc import Callable
+from logging import Filter, getLogger
+from typing import Protocol
+
+from flask_restx import Model
+from flask_socketio import Namespace as _Namespace, SocketIO as _SocketIO, disconnect
+
+from .errors import EventException
+from .interfaces import EventGroupBase
+from ..utils import render_packed, restx_model_to_message
+
+
+class BeforeEventProtocol(Protocol):
+    def __call__(self, *args):
+        pass
+
+
+class AfterEventProtocol(Protocol):
+    def __call__(self, result):
+        pass
+
+
+class Namespace(_Namespace):
+    def __init__(self, namespace: str = None, use_kebab_case: bool = False):
+        super().__init__(namespace)
+        self.doc_channels = OrderedDict()
+        self.doc_messages = OrderedDict()
+        self.use_kebab_case = use_kebab_case
+
+    def on_event(self, name: str):
+        def on_event_wrapper(function: Callable[[...], None]):
+            setattr(self, f"on_{name}", function)
+
+        return on_event_wrapper
+
+    def on_connect(self, function: Callable[[...], None] | None = None):
+        def on_connect_wrapper(function: Callable[[...], None]):
+            self.on_connect = function
+
+        if function is None:
+            return on_connect_wrapper
+        return on_connect_wrapper(function)
+
+    def on_disconnect(self, function: Callable[[...], None] = None):
+        def on_disconnect_wrapper(function: Callable[[...], None]):
+            self.on_disconnect = function
+
+        if function is None:
+            return on_disconnect_wrapper
+        return on_disconnect_wrapper(function)
+
+    def attach_event_group(self, event_group: EventGroupBase):
+        event_group.attach_namespace(self.namespace)
+        self.doc_channels.update(event_group.extract_doc_channels())
+        self.doc_messages.update(event_group.extract_doc_messages())
+        for name, handler in event_group.extract_handlers():
+            self.on_event(name)(handler)
+
+    def handle_exception(
+        self,
+        data: dict | list | str | int | None = None,
+        code: int | None = None,
+        message: str | None = None,
+    ) -> dict | None:
+        return render_packed(data=data, code=code, message=message)
+
+    def trigger_event(self, event, *args):
+        try:
+            return super().trigger_event(event, *args)
+        except EventException as e:
+            if e.critical:
+                disconnect()
+            return self.handle_exception(code=e.code, message=e.message, data=e.data)
+
+
+class NoPingPongFilter(Filter):
+    def filter(self, record):
+        return not (
+            "Received packet PONG" in record.getMessage()
+            or "Sending packet PING" in record.getMessage()
+        )
+
+
+class SocketIO(_SocketIO):
+    default_namespace_class: type[Namespace] = Namespace
+
+    def __init__(
+        self,
+        app=None,
+        title: str = "SIO",
+        version: str = "1.0.0",
+        doc_path: str = "/sio-doc/",
+        remove_ping_pong_logs: bool = False,
+        use_kebab_case: bool = False,
+        namespace_class: type[Namespace] = None,
+        restx_models: dict[str, Model] = None,  # TODO get rid of restx
+        **kwargs,
+    ):
+        self.doc_path = doc_path
+        self.use_kebab_case = use_kebab_case
+        self.namespace_class = (
+            self.default_namespace_class if namespace_class is None else namespace_class
+        )
+
+        self.async_api = {
+            "asyncapi": "2.2.0",
+            "info": {"title": title, "version": version},
+            "channels": OrderedDict(),
+            "components": {
+                "messages": OrderedDict(
+                    (name, restx_model_to_message(name, model))
+                    for name, model in (restx_models or {}).items()
+                )
+            },
+        }
+
+        self.before_event_funcs: list[BeforeEventProtocol] = []
+        self.after_event_funcs: list[AfterEventProtocol] = []
+
+        if remove_ping_pong_logs:
+            getLogger("engineio.server").addFilter(NoPingPongFilter())
+
+        super().__init__(app, **kwargs)
+
+    def docs(self):
+        return self.async_api
+
+    def init_app(self, app, **kwargs):
+        app.config["JSON_SORT_KEYS"] = False  # TODO kinda bad for a library
+
+        @app.route(self.doc_path)
+        def documentation():
+            return self.docs()
+
+        return super().init_app(app, **kwargs)
+
+    def on_namespace(self, namespace_handler):
+        if isinstance(namespace_handler, Namespace):
+            self.async_api["channels"].update(namespace_handler.doc_channels)
+            self.async_api["components"]["messages"].update(
+                namespace_handler.doc_messages
+            )
+        return super().on_namespace(namespace_handler)
+
+    def _handle_event(self, handler, message, namespace, sid, *args):
+        def new_handler(*args):
+            for function in self.before_event_funcs:
+                args = function(*args)
+            result = handler(*args)
+            for function in self.after_event_funcs:
+                result = function(result)
+            return result
+
+        return super()._handle_event(new_handler, message, namespace, sid, *args)
+
+    def _add_namespace(self, namespace: Namespace, *event_groups: EventGroupBase):
+        for event_group in event_groups:
+            namespace.attach_event_group(event_group)
+        self.on_namespace(namespace)
+
+    def add_namespace(self, name: str = None, *event_groups: EventGroupBase, **_):
+        self._add_namespace(
+            self.namespace_class(name, self.use_kebab_case),
+            *event_groups,
+        )
+
+    def before_event(self, function: BeforeEventProtocol):
+        self.before_event_funcs.append(function)
+
+    def after_event(self, function: AfterEventProtocol):
+        self.after_event_funcs.append(function)
