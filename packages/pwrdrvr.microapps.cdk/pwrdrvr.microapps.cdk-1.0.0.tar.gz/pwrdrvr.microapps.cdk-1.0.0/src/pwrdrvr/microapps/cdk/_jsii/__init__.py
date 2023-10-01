071ebf52988e4feed8ef2dfff99edb2a14967f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:09 +0000

    Add package pwrdrvr.microapps.cdk/1.0.0/pwrdrvr.microapps.cdk-1.0.0.tar.gz

---
+++ packages/pwrdrvr.microapps.cdk/pwrdrvr.microapps.cdk-1.0.0.tar.gz/pwrdrvr.microapps.cdk-1.0.0/src/pwrdrvr/microapps/cdk/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_apigatewayv2_alpha._jsii
+import aws_cdk.aws_apigatewayv2_authorizers_alpha._jsii
+import aws_cdk.aws_apigatewayv2_integrations_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pwrdrvr/microapps-cdk", "1.0.0", __name__[0:-6], "microapps-cdk@1.0.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
