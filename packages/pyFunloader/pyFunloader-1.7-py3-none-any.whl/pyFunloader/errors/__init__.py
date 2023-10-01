d1cccf59767bd31027c32fdffef615d204f1cffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:29 +0000

    Add package pyFunloader/1.7/pyFunloader-1.7-py3-none-any.whl

---
+++ packages/pyFunloader/pyFunloader-1.7-py3-none-any.whl/pyFunloader/errors/__init__.py
@@ -0,0 +1,17 @@
+from sys import stderr
+
+class EmptyResponse(BaseException):
+    
+    def __init__(self, msg) -> None:
+        print(msg, file=stderr)
+
+class InvalidOperation(BaseException):
+    
+    def __init__(self, msg) -> None:
+        print(msg, file=stderr)
+
+
+class GenericError(BaseException):
+    
+    def __init__(self, msg) -> None:
+        print(msg, file=stderr)
