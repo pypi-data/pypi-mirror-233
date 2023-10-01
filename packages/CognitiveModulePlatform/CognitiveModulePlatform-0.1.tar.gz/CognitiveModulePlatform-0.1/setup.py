b8f7e57d1aed5411addf52e9de49bf440152b166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:42 +0000

    Add package CognitiveModulePlatform/0.1/CognitiveModulePlatform-0.1.tar.gz

---
+++ packages/CognitiveModulePlatform/CognitiveModulePlatform-0.1.tar.gz/CognitiveModulePlatform-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CognitiveModulePlatform',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # list your project dependencies here
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A description of your library',
+)
