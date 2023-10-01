fda0c1c3025082b4819b610449355768090a67e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 15:05:32 +0000

    Add package xlogger-lob/0.0.2/xlogger-lob-0.0.2.tar.gz

---
+++ packages/xlogger-lob/xlogger-lob-0.0.2.tar.gz/xlogger-lob-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='xlogger-lob',  # 对外模块的名字
+    version='0.0.2',  # 版本号
+    description='',  # 描述
+    author='lobsterb',  # 作者
+    author_email='57263573@qq.com',
+    py_modules=['logger'],  # 要发布的模块
+)
