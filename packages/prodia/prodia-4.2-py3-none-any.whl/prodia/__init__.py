8a6b23801ba962a39d535b16d1acb390103aeaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:15 +0000

    Add package prodia/4.2/prodia-4.2-py3-none-any.whl

---
+++ packages/prodia/prodia-4.2-py3-none-any.whl/prodia/__init__.py
@@ -0,0 +1,14 @@
+from prodia.resources import (
+    StableDiffusion,
+    AsyncStableDiffusion,
+    StableDiffusionXL,
+    AsyncStableDiffusionXL,
+    Upscaler,
+    AsyncUpscaler
+)
+from prodia.util import (
+    request,
+    arequest,
+    load,
+    aload
+)
