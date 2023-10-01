5f47945227ee98744b321c0dbc1a77261c0c210f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:40 +0000

    Add package dimstack/0.3.1/dimstack-0.3.1.tar.gz

---
+++ packages/dimstack/dimstack-0.3.1.tar.gz/dimstack-0.3.1/src/dimstack/display.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+DISPLAY_MODE = "text"
+FIGSIZE = (6, 3)
+
+
+def mode(dispmode: str):
+    """Set the display mode for the stack.
+
+    Args:
+        mode (str): "text", "str"/"string", "plot", or "df"
+    """
+    global DISPLAY_MODE
+    DISPLAY_MODE = dispmode
+
+
+def display_df(data: dict, title: str = "", dispmode=None):
+    """Display a dataframe.
+
+    Args:
+        df (pd.DataFrame): _description_
+    """
+    if dispmode is None:
+        dispmode = DISPLAY_MODE
+
+    df = pd.DataFrame(data).astype(str)
+
+    if dispmode == "text":
+        if title:
+            print(f"{title}")
+        print(df.to_string(index=False))
+        print()
+    if dispmode == "string" or dispmode == "str":
+        return df.to_string(index=False)
+    elif dispmode == "plot":
+        return df.style.hide(axis="index").set_caption(title)
+    elif dispmode == "df":
+        return df
+    else:
+        return data
