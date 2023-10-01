9f73b1e56660e9dad5440b9ca68a2676652a268c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:10 +0000

    Add package data-transformation-tc/0.1/data_transformation_tc-0.1.tar.gz

---
+++ packages/data-transformation-tc/data_transformation_tc-0.1.tar.gz/data_transformation_tc-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data_transformation_tc",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+)
