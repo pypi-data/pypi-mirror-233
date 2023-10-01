393566112eefae56b759dce617c29b4ab9479ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:14 +0000

    Add package physicslab/1.3.5/physicslab-1.3.5-py3-none-any.whl

---
+++ packages/physicslab/physicslab-1.3.5-py3-none-any.whl/physicsLab/electricity/unionElements/__init__.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+# 模块化逻辑电路
+from physicsLab.electricity.unionElements.unionLogic import *
+# 模块化音乐电路
+from physicsLab.electricity.unionElements.unionMusic import *
+# 模块化电路引脚连接
+from physicsLab.electricity.unionElements.wires import *
