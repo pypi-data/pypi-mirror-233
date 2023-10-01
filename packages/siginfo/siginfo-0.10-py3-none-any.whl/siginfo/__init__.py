1c17535c1fb626fc41710a631aa9a3921fca0268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:07 +0000

    Add package siginfo/0.10/siginfo-0.10-py3-none-any.whl

---
+++ packages/siginfo/siginfo-0.10-py3-none-any.whl/siginfo/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""siginfo: A Python package to help debugging and monitoring python script"""
+
+from siginfo.siginfoclass import SiginfoBasic, SigInfoPDB, SigInfoSingle
+
+
+__version__ = '0.10'
+__author__ = 'Jonas Marcello'
+
+__all__ = (
+    "SiginfoBasic",
+    "SigInfoPDB",
+    "SigInfoSingle"
+)
