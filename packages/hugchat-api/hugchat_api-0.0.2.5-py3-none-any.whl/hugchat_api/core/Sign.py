93ce21279e9f9c9370334141316cb9ba0534c0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:40 +0000

    Add package hugchat-api/0.0.2.5/hugchat_api-0.0.2.5-py3-none-any.whl

---
+++ packages/hugchat-api/hugchat_api-0.0.2.5-py3-none-any.whl/hugchat_api/core/Sign.py
@@ -0,0 +1,224 @@
+from http.cookies import SimpleCookie
+import logging
+import os
+import re
+import json
+import time
+from typing import Union
+
+from aiohttp import ClientResponse
+from hugchat_api.core.Workflow import Workflow
+from hugchat_api.core.Customflow import Customflow
+from hugchat_api.utils import Request
+from .CorotineLoop import CorotineLoop
+
+
+class Sign(Workflow):
+    def __init__(
+        self,
+        email: str,
+        loop: CorotineLoop,
+        passwd: Union[str, None] = None,
+    ) -> None:
+        self.loop = loop
+        self.email: str = email
+        self.passwd: Union[str, None] = passwd
+        self.headers = {
+            "Referer": "https://huggingface.co/",
+            # "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64",
+        }
+        self.cookies: SimpleCookie[str] = SimpleCookie()
+
+        # cookie dir and file
+        self.DEFAULT_PATH_DIR = (
+            os.path.dirname(os.path.abspath(__file__)) + "/usercookies"
+        )
+        self.DEFAULT_COOKIE_PATH = self.DEFAULT_PATH_DIR + f"/{email}.json"
+
+    async def _requestsGet(
+        self, url: str, params=None, allow_redirects=True
+    ) -> ClientResponse:
+        res = await Request.Get(
+            url,
+            params=params,
+            headers=self.headers,
+            cookies=self.cookies,
+            # proxies=self.proxies,
+            allow_redirects=allow_redirects,
+        )
+        self._refreshCookies(res.cookies)
+        return res
+
+    async def _requestsPost(
+        self,
+        url: str,
+        headers=None,
+        params=None,
+        data=None,
+        allow_redirects=True,
+    ) -> ClientResponse:
+        res = await Request.Post(
+            url,
+            params=params,
+            data=data,
+            headers=self.headers if headers is None else headers,
+            cookies=self.cookies,
+            allow_redirects=allow_redirects,
+        )
+        self._refreshCookies(res.cookies)
+        return res
+
+    def _refreshCookies(self, cookies: SimpleCookie[str]):
+        self.cookies.update(cookies)
+
+    async def _SigninWithEmail(self):
+        url = "https://huggingface.co/login"
+        data = {
+            "username": self.email,
+            "password": self.passwd,
+        }
+        res = await self._requestsPost(url=url, data=data, allow_redirects=False)
+        if res.status == 400:
+            res.close()
+            raise Exception("Incorrect username or password")
+
+    async def _getAuthURL(self) -> str:
+        url = "https://huggingface.co/chat/login"
+        headers = {
+            "Referer": "https://huggingface.co/chat/login",
+            "User-Agent": self.headers["User-Agent"],
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+        res = await self._requestsPost(url, headers=headers, allow_redirects=False)
+        if res.status == 200:
+            # location = res.headers.get("Location", None)
+            # js = await res.json()
+            data = json.loads(await res.text())
+            location = data["location"]
+            res.close()
+            if location:
+                return location
+            else:
+                raise Exception("No authorize url!")
+        else:
+            res.close()
+            raise Exception("Something went wrong!")
+
+    async def _grantAuth(self, url: str) -> int:
+        res = await self._requestsGet(url, allow_redirects=False)
+        if res.headers.__contains__("location"):
+            location = res.headers["location"]
+            res = await self._requestsGet(location, allow_redirects=False)
+            if res.cookies.__contains__("hf-chat"):
+                res.close()
+                return 1
+        # raise Exception("grantAuth fatal")
+        if res.status != 200:
+            raise Exception("grant auth fatal!")
+        csrf = re.findall(
+            '/oauth/authorize.*?name="csrf" value="(.*?)"', await res.text()
+        )
+        if len(csrf) == 0:
+            raise Exception("No csrf found!")
+        data = {"csrf": csrf[0]}
+        res.close()
+
+        res = await self._requestsPost(url, data=data, allow_redirects=False)
+        if res.status != 303:
+            raise Exception(f"get hf-chat cookies fatal! - {res.status}")
+        else:
+            location = res.headers.get("Location")
+        res.close()
+        res = await self._requestsGet(location, allow_redirects=False)
+        if res.status != 302:
+            res.close()
+            raise Exception(f"get hf-chat cookie fatal! - {res.status}")
+        else:
+            res.close()
+            return 1
+
+    def saveCookiesToDir(self, cookie_dir_path: Union[str, None] = None) -> str:
+
+        # path
+        cookie_dir_path = (
+            self.DEFAULT_PATH_DIR if not cookie_dir_path else cookie_dir_path
+        )
+        if not cookie_dir_path.endswith("/"):
+            cookie_dir_path += "/"
+        cookie_path = cookie_dir_path + f"{self.email}.json"
+        if not os.path.exists(cookie_dir_path):
+            logging.info("Cookie directory not exist, creating...")
+            os.makedirs(cookie_dir_path)
+        logging.info(f"Cookie store path: {cookie_path}")
+
+        # save
+        d = {}
+        for cookie in self.cookies.items():
+            d[cookie[0]] = cookie[1].value
+        with open(cookie_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(d))
+        return cookie_path
+
+    def _getCookiePath(self, cookie_dir_path: str) -> str:
+        if not cookie_dir_path.endswith("/"):
+            cookie_dir_path += "/"
+        if not os.path.exists(cookie_dir_path):
+            return ""
+        files = os.listdir(cookie_dir_path)
+        for i in files:
+            if i == f"{self.email}.json":
+                return cookie_dir_path + i
+        return ""
+
+    def loadCookiesFromDir(
+        self, cookie_dir_path: Union[str, None] = None
+    ) -> SimpleCookie[str]:
+        
+        # path
+        cookie_dir_path = (
+            self.DEFAULT_PATH_DIR if not cookie_dir_path else cookie_dir_path
+        )
+        cookie_path = self._getCookiePath(cookie_dir_path)
+        if not cookie_path:
+            raise Exception(
+                f"Cookie not found. please check the path given: {cookie_dir_path}.\n"
+                + f"Cookie file must be named like this: 'your_email'+'.json': '{self.email}.json'"
+            )
+
+        #load
+        with open(cookie_path, "r", encoding="utf-8") as f:
+            try:
+                js = json.loads(f.read())
+                self.cookies.load(js)
+                return self.cookies
+            except Exception:
+                raise Exception(
+                    "load cookies from files fatal. Please check the format"
+                )
+
+    async def run(self):
+        await self._SigninWithEmail()
+        location = await self._getAuthURL()
+        if await self._grantAuth(location):
+            pass
+        if self.save:
+            self.saveCookiesToDir(self.cookie_dir_path)
+        return self.cookies
+
+    def login(
+        self, save: bool = False, cookie_dir_path: Union[str, None] = None
+    ) -> SimpleCookie[str]:
+        self.save = save
+        self.cookie_dir_path = cookie_dir_path
+        fut = self.loop.submit(self)
+        return Customflow.wait(fut)
+
+
+if __name__ == "__main__":
+    email = os.getenv("EMAIL")
+    passwd = os.getenv("PASSWD")
+    sign = Sign(email, passwd)
+    cookies = sign.login()
+    sign.saveCookiesToDir()
+    print(cookies)
---
+++ packages/hugchat-api/hugchat_api-0.0.2.5-py3-none-any.whl/hugchat_api/terminal_cli.py
@@ -0,0 +1,268 @@
+import gc, os, argparse
+import getpass
+import logging
+import traceback
+import asyncio
+from typing import Union
+
+from hugchat_api.utils.PrintWeb import PrintWeb
+
+from .utils.PrintLoop import PrintLoop
+
+
+from .core import HuggingChat
+from .core.Message import Message
+from .core.Sign import Sign
+from .core.Bot import Bot
+from .utils import (
+    color,
+    formatHistory,
+    formatConversations,
+    getTextFromInput,
+    getIdByIndex,
+)
+from .core import ListBots
+
+hug = HuggingChat()
+
+WEB_SEARCH = False
+NEW_CONVERSATION = False
+
+
+def checkCookies(u):
+    login = Sign(u, None)
+    try:
+        login.loadCookiesFromDir()
+        return True
+    except Exception:
+        # print(e)
+        return False
+
+
+def login(u, p=None, force=False):
+    if not p:
+        logging.info("No Password input, trying to load it from mysql or files")
+        login = Sign(u, p)
+        cookies = login.loadCookiesFromDir()
+    else:
+        login = Sign(u, p)
+        if force:
+            cookies = login.login(save=True)
+        else:
+            try:
+                cookies = login.loadCookiesFromDir()
+            except Exception:
+                cookies = login.login(save=True)
+    return cookies
+
+
+# def printWait() -> typing.Callable:
+#     tick = time.time()
+#     index = 0
+#     count = 3
+#     dots = "·.."
+#
+#     def c() -> str:
+#         nonlocal tick, index, dots
+#         if time.time() - tick >= 0.5:
+#             tick = time.time()
+#             content = ["."] * count
+#             content[index] = "·"
+#             index = (index + 1) % count
+#             dots = "".join(content)
+#             # print(f"\r{dots}", flush=True, end="")
+#         return dots
+#
+#     return c
+
+
+# async def waitAndPrint(message: Message):
+#     print_wait = printWait()
+#     if message.web_search_enabled:
+#         while not message.web_search_done:
+#             print_wait()
+#             await asyncio.sleep(0.01)
+#         print(f"\r{message.web_search_steps}", flush=True)
+#     while not message.isDone():
+#         if message.error is not None:
+#             raise message.error
+#         content = "\r" + "".join(message.getText())
+#         content += print_wait()
+#         print(content, flush=True, end="")
+#         await asyncio.sleep(0.01)
+#     else:
+#         content = "".join(message.getText())
+#         string = f"\r({color('HFBot', 'blue')}): {content}"
+#         print(string, flush=True)
+#     return
+
+
+def printloop(message: Message):
+    printweb = PrintWeb(message)
+    printweb.update()
+    print()
+    loop = PrintLoop(message)
+    string = f"\r({color('HFBot', 'blue')}): "
+    print(string)
+    loop.main()
+    print()
+    printweb.sources()
+    print()
+
+
+def changeWeb_search():
+    global WEB_SEARCH
+    WEB_SEARCH = True if not WEB_SEARCH else False
+    print(f"WEB_SEARCH is set to `{WEB_SEARCH}`")
+    return
+
+
+# def getPromptFalcon() -> ListBots.PromptLlama:
+#     return ListBots.PromptLlama(
+#         system=input("system prompt: "),
+#         user=input("user prompt: ")
+#     )
+
+
+def chat(bot: Bot, text: str | ListBots.Prompt) -> None:
+    if not bot.current_conversation:
+        print(
+            "Please select or create a conversation using '/ls' and '/cd <int>' or '/new'."
+        )
+        return
+
+    message = bot.chat(
+        text,
+        conversation_id=bot.current_conversation,
+        web_search=WEB_SEARCH,
+    )
+    if message is None:
+        return
+    # await waitAndPrint(message)
+    printloop(message)
+    return
+
+
+
+async def main(EMAIL: str, PASSWD: Union[str, None]):
+    # global FLAG
+    global WEB_SEARCH
+    global NEW_CONVERSATION
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-u", type=str, help="email to be sign in.(sign up before this)"
+    )
+    parser.add_argument(
+        "-p",
+        action="store_true",
+        help="再终端中输入密码(每账号请先注册一个) - input password in terminal(sign up before this)",
+    )
+    parser.add_argument(
+        "-f",
+        action="store_true",
+        help="忽视已保存信息强制登录 - ignore the stored cookies and login",
+    )
+    parser.add_argument("--debug", action="store_true", default=False)
+    args = parser.parse_args()
+    level = logging.DEBUG if args.debug else logging.WARNING
+    logging.basicConfig(
+        format="%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s",
+        encoding="utf-8",
+        level=level,
+        filename="terminal_cli.log",
+        filemode="w",
+    )
+    force = args.f
+    u = EMAIL if not args.u else args.u
+    print(f"Login in as <{u}>")
+    if args.p:
+        p = getpass.getpass()
+    elif not checkCookies(u):
+        p = getpass.getpass() if not PASSWD else PASSWD
+    else:
+        p = None
+
+    cookies = login(u, p, force)
+    print(f"You are now logged in as <{u}>")
+    # bot: Bot = hug.getBot(u, cookies=cookies, model=ListBots.FALCON_180B)
+    bot: Bot = hug.getBot(u, cookies=cookies, model=ListBots.META_70B_HF)
+    gc.collect()
+    while 1:
+        try:
+            gc.collect()
+            text = getTextFromInput(bot.current_conversation)
+            if text is None:
+                continue
+            command = text.strip()
+            if command[0] == "/":
+                command = command[1:].split(" ")
+                if command[0] == "exit" or command[0] == "q":
+                    os._exit(0)
+                elif command[0] == "new":
+                    bot.createConversation()
+                    NEW_CONVERSATION = True
+                elif command[0] == "ls":
+                    print(formatConversations(bot.getConversations()))
+                elif command[0] == "cd":
+                    try:
+                        tmp_id = getIdByIndex(bot.conversations, int(command[1]))
+                        bot.switchConversation(tmp_id)
+                    except Exception:
+                        print("cd fatal")
+                elif command[0] == "rm":
+                    try:
+                        tmp_id = getIdByIndex(bot.conversations, int(command[1]))
+                        bot.removeConversation(tmp_id)
+                    except Exception as e:
+                        print(f"remove conversation fatal: {e}")
+                elif command[0] == "old":
+                    print(formatHistory(bot.getHistoriesByID()))
+                elif command[0] == "web":
+                    changeWeb_search()
+                elif command[0] == "reload":
+                    print("Reloading conversations...")
+                    bot.fetchConversations()
+                    # for i in bot.conversations:
+                    #     bot.updateTitle(i["id"])
+                    print("done.")
+                    print(formatConversations(bot.getConversations()))
+                elif command[0] == "pro":
+                    bot.setSystemPrompt(bot.model, input("System prompt:"))
+                else:
+                    print("wrong command。")
+                    continue
+            else:
+                chat(bot, text)
+                # if not bot.current_conversation:
+                #     print(
+                #         "Please select or create a conversation using '/ls' and '/cd <int>' or '/new'."
+                #     )
+                #     continue
+                #
+                # message = bot.chat(
+                #     text,
+                #     conversation_id=bot.current_conversation,
+                #     web_search=WEB_SEARCH,
+                # )
+                # if message is None:
+                #     continue
+                # # await waitAndPrint(message)
+                # printloop(message)
+        except Exception:
+            traceback.print_exc()
+
+
+if __name__ == "__main__":
+    EMAIL = os.getenv("EMAIL")
+    if not EMAIL:
+        print("please provide email")
+        os._exit(0)
+    PASSWD = os.getenv("PASSWD")
+    event_loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(event_loop)
+    try:
+        event_loop.run_until_complete(main(EMAIL, PASSWD))
+    except Exception:
+        traceback.print_exc()
+        event_loop.close()
