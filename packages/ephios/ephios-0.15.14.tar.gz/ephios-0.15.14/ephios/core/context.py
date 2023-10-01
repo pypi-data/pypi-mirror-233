b0bb427340c019b16c9324565fdaebcbdeab8309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:42 +0000

    Add package ephios/0.15.14/ephios-0.15.14.tar.gz

---
+++ packages/ephios/ephios-0.15.14.tar.gz/ephios-0.15.14/ephios/core/context.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.utils.translation import get_language
+
+from ephios.core.models import AbstractParticipation
+from ephios.core.signals import footer_link, nav_link
+
+
+def ephios_base_context(request):
+    footer = {}
+    for _, result in footer_link.send(None, request=request):
+        for label, url in result.items():
+            footer[label] = url
+
+    nav = []
+    for _, result in nav_link.send(None, request=request):
+        nav += result
+
+    return {
+        "ParticipationStates": AbstractParticipation.States,
+        "nav": nav,
+        "footer": footer,
+        "LANGUAGE_CODE": get_language(),
+        "ephios_version": settings.EPHIOS_VERSION,
+        "PWA_APP_ICONS": settings.PWA_APP_ICONS,
+    }
