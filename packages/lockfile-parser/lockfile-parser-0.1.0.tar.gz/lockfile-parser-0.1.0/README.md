46e7a949d53ea2039b971857360cb2e6fea167e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:05:01 +0000

    Add package lockfile-parser/0.1.0/lockfile-parser-0.1.0.tar.gz

---
+++ packages/lockfile-parser/lockfile-parser-0.1.0.tar.gz/lockfile-parser-0.1.0/README.md
@@ -0,0 +1,3 @@
+# cargo-lock-parser-py
+
+A small no dependancy (except `toml`) lockfile parser in python. Currently implements parsing `Cargo.lock` and `poetry.lock`. Intended for use with larger projects for static lockfile introspection.
---
+++ packages/lockfile-parser/lockfile-parser-0.1.0.tar.gz/lockfile-parser-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lockfile-parser",  # Replace with your desired package name
+    version="0.1.0",
+    author="Nikhil",
+    author_email="nm2868@columbia.edu",
+    description="A mininal lockfile parser in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nikhilmitrax/lockfile-parser-py",  # Replace with your repo URL
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "toml>=0.10.2",  # Adjust the version as needed
+    ],
+)
