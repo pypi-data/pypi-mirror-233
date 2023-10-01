de2d5a6b924c500dcd9591c9f2af81e422a2a2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:37 +0000

    Add package sharingiscaring/0.3.8/sharingiscaring-0.3.8-py3-none-any.whl

---
+++ packages/sharingiscaring/sharingiscaring-0.3.8-py3-none-any.whl/sharingiscaring/GRPCClient/queries/_GetConsensusInfo.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from sharingiscaring.GRPCClient.types_pb2 import *
+from sharingiscaring.enums import NET
+from sharingiscaring.GRPCClient.service_pb2_grpc import QueriesStub
+from sharingiscaring.GRPCClient.queries._SharedConverters import (
+    Mixin as _SharedConverters,
+)
+from typing import Iterator
+from typing import TYPE_CHECKING
+import sys
+
+if TYPE_CHECKING:
+    from sharingiscaring.GRPCClient import GRPCClient
+from sharingiscaring.GRPCClient.CCD_Types import *
+from rich import print
+
+
+class Mixin(_SharedConverters):
+    def get_consensus_info(
+        self: GRPCClient,
+        net: Enum = NET.MAINNET,
+    ) -> CCD_ConsensusInfo:
+        self.check_connection(net, sys._getframe().f_code.co_name)
+        if net == NET.MAINNET:
+            grpc_return_value: BlockInfo = self.stub_mainnet.GetConsensusInfo(
+                request=Empty()
+            )
+        else:
+            grpc_return_value: BlockInfo = self.stub_testnet.GetConsensusInfo(
+                request=Empty()
+            )
+
+        result = {}
+
+        for descriptor in grpc_return_value.DESCRIPTOR.fields:
+            key, value = self.get_key_value_from_descriptor(
+                descriptor, grpc_return_value
+            )
+
+            if key == "protocol_version":
+                result[key] = ProtocolVersions(value).name
+
+            elif type(value) in self.simple_types:
+                result[key] = self.convertType(value)
+
+        return CCD_ConsensusInfo(**result)
