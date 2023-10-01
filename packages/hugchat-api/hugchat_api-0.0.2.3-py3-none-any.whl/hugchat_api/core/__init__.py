4cc810278af5f2eea082668752ba4c352dcd0a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:20 +0000

    Add package hugchat-api/0.0.2.3/hugchat_api-0.0.2.3-py3-none-any.whl

---
+++ packages/hugchat-api/hugchat_api-0.0.2.3-py3-none-any.whl/hugchat_api/core/__init__.py
@@ -0,0 +1,35 @@
+from http.cookies import SimpleCookie
+import asyncio
+
+from hugchat_api.core.CorotineLoop import CorotineLoop
+from .Bot import Bot
+from .Sign import Sign
+from . import ListBots
+from hugchat_api.utils import Request
+
+
+class HuggingChat:
+    def __init__(self, *args):
+        self.loop = CorotineLoop()
+        asyncio.run(Request.init(self.loop.loop))
+
+    def getBot(
+        self,
+        email: str,
+        cookies: SimpleCookie[str],
+        model: str = ListBots.META_70B_HF,
+    ) -> Bot:
+        bot = Bot(
+            email=email,
+            cookies=cookies,
+            model=model,
+            loop=self.loop,
+        )
+        return bot
+
+    def getSign(self, email, passwd = None) -> Sign:
+        return Sign(
+            email=email,
+            passwd=passwd,
+            loop=self.loop,
+        )
