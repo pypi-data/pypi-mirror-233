1836b4add3667ee11fc5beef2906eb0275dc6fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:54 +0000

    Add package rookiepy/0.1.5/rookiepy-0.1.5.tar.gz

---
+++ packages/rookiepy/rookiepy-0.1.5.tar.gz/rookiepy-0.1.5/src/lib.rs
@@ -0,0 +1,130 @@
+
+use std::{time::SystemTime, path::PathBuf};
+use rookie::{self,Cookie};
+
+use pyo3::prelude::*;
+
+
+// Wrapper struct for Cookie
+#[pyclass]
+pub struct PyCookie {
+    pub inner: Cookie,
+}
+
+
+#[pymethods]
+impl PyCookie {
+    #[getter]
+    fn host(&self) -> &str {
+        &self.inner.host
+    }
+
+    #[getter]
+    fn path(&self) -> &str {
+        &self.inner.path
+    }
+
+    #[getter]
+    fn secure(&self) -> bool {
+        self.inner.secure
+    }
+
+    #[getter]
+    fn expires(&self) -> PyResult<u128> {
+        match self.inner.expires.duration_since(SystemTime::UNIX_EPOCH) {
+            Ok(duration) => {
+                let ms = duration.as_millis();
+                Ok(ms)
+            }
+            Err(_) => Ok(0)
+        }
+    }
+    
+
+    #[getter]
+    fn name(&self) -> &str {
+        &self.inner.name
+    }
+
+    #[getter]
+    fn value(&self) -> &str {
+        &self.inner.value
+    }
+
+    #[getter]
+    fn http_only(&self) -> bool {
+        self.inner.http_only
+    }
+
+    #[getter]
+    fn same_site(&self) -> i64 {
+        self.inner.same_site
+    }
+}
+
+#[pyfunction]
+fn firefox(_py: Python, domains: Option<Vec<&str>>) -> PyResult<Vec<PyCookie>> {
+    let cookies = rookie::firefox(domains).unwrap();
+    
+    let py_cookies: Vec<PyCookie> = cookies.into_iter().map(|cookie| PyCookie { inner: cookie }).collect();
+
+    Ok(py_cookies)
+}
+
+#[pyfunction]
+fn chrome(_py: Python, domains: Option<Vec<&str>>) -> PyResult<Vec<PyCookie>> {
+    let cookies = rookie::chrome(domains).unwrap();
+    
+    let py_cookies: Vec<PyCookie> = cookies.into_iter().map(|cookie| PyCookie { inner: cookie }).collect();
+
+    Ok(py_cookies)
+}
+
+
+#[pyfunction]
+fn brave(_py: Python, domains: Option<Vec<&str>>) -> PyResult<Vec<PyCookie>> {
+    let cookies = rookie::brave(domains).unwrap();
+    
+    let py_cookies: Vec<PyCookie> = cookies.into_iter().map(|cookie| PyCookie { inner: cookie }).collect();
+
+    Ok(py_cookies)
+}
+
+#[pyfunction]
+fn edge(_py: Python, domains: Option<Vec<&str>>) -> PyResult<Vec<PyCookie>> {
+    let cookies = rookie::edge(domains).unwrap();
+    
+    let py_cookies: Vec<PyCookie> = cookies.into_iter().map(|cookie| PyCookie { inner: cookie }).collect();
+
+    Ok(py_cookies)
+}
+
+
+#[pyfunction]
+fn chromium_based(_py: Python, key_path: String, db_path: String, domains: Option<Vec<&str>>) -> PyResult<Vec<PyCookie>> {
+    let cookies = rookie::chromium_based(PathBuf::from(key_path), PathBuf::from(db_path), domains).unwrap();
+    
+    let py_cookies: Vec<PyCookie> = cookies.into_iter().map(|cookie| PyCookie { inner: cookie }).collect();
+
+    Ok(py_cookies)
+}
+
+#[pyfunction]
+fn firefox_based(_py: Python, db_path: String, domains: Option<Vec<&str>>) -> PyResult<Vec<PyCookie>> {
+    let cookies = rookie::firefox_based(PathBuf::from(db_path), domains).unwrap();
+    
+    let py_cookies: Vec<PyCookie> = cookies.into_iter().map(|cookie| PyCookie { inner: cookie }).collect();
+
+    Ok(py_cookies)
+}
+
+#[pymodule]
+fn rookiepy(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(firefox, m)?)?;
+    m.add_function(wrap_pyfunction!(chrome, m)?)?;
+    m.add_function(wrap_pyfunction!(brave, m)?)?;
+    m.add_function(wrap_pyfunction!(edge, m)?)?;
+    m.add_function(wrap_pyfunction!(chromium_based, m)?)?;
+    m.add_function(wrap_pyfunction!(firefox_based, m)?)?;
+    Ok(())
+}
