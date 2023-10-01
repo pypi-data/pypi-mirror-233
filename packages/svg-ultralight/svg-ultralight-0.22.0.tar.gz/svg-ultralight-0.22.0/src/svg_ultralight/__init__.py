787c25723f2fd81d1b2962a16ed7171df430b663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:36 +0000

    Add package svg-ultralight/0.22.0/svg-ultralight-0.22.0.tar.gz

---
+++ packages/svg-ultralight/svg-ultralight-0.22.0.tar.gz/svg-ultralight-0.22.0/src/svg_ultralight/__init__.py
@@ -0,0 +1,50 @@
+"""Bring many of the svg_ultralight functions into the svg_ultralight namespace.
+
+:author: Shay Hill
+:created: 12/22/2019.
+"""
+from svg_ultralight.bounding_boxes.supports_bounds import SupportsBounds
+from svg_ultralight.bounding_boxes.type_bound_element import BoundElement
+from svg_ultralight.bounding_boxes.type_bounding_box import BoundingBox
+from svg_ultralight.bounding_boxes.type_padded_text import PaddedText
+from svg_ultralight.constructors.new_element import (
+    deepcopy_element,
+    new_element,
+    new_sub_element,
+    update_element,
+)
+from svg_ultralight.inkscape import (
+    write_pdf,
+    write_pdf_from_svg,
+    write_png,
+    write_png_from_svg,
+    write_root,
+)
+from svg_ultralight.main import new_svg_root, write_svg
+from svg_ultralight.nsmap import NSMAP
+from svg_ultralight.query import pad_text
+from svg_ultralight.root_elements import new_svg_root_around_bounds
+from svg_ultralight.string_conversion import format_number, format_numbers
+
+__all__ = [
+    "BoundElement",
+    "BoundingBox",
+    "PaddedText",
+    "SupportsBounds",
+    "new_element",
+    "new_sub_element",
+    "update_element",
+    "deepcopy_element",
+    "new_svg_root",
+    "pad_text",
+    "write_svg",
+    "write_png_from_svg",
+    "write_png",
+    "write_pdf_from_svg",
+    "write_pdf",
+    "write_root",
+    "NSMAP",
+    "format_number",
+    "format_numbers",
+    "new_svg_root_around_bounds",
+]
