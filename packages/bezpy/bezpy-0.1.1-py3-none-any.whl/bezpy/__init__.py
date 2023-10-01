d3b519214b099a4b5c6b7a949cf84498affecdb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:40 +0000

    Add package bezpy/0.1.1/bezpy-0.1.1-py3-none-any.whl

---
+++ packages/bezpy/bezpy-0.1.1-py3-none-any.whl/bezpy/__init__.py
@@ -0,0 +1,9 @@
+"""
+bezpy: Python module for processing and analyzing magnetic field (B),
+       electric field (E), and impedance (Z) data.
+"""
+from . import mt
+from . import tl
+from . import mag
+
+__version__ = '0.1.1'
