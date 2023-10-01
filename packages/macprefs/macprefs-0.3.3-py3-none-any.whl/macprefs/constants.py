b823c76e2409802254248f323113e0ae906ec5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:16 +0000

    Add package macprefs/0.3.3/macprefs-0.3.3-py3-none-any.whl

---
+++ packages/macprefs/macprefs-0.3.3-py3-none-any.whl/macprefs/constants.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+__all__ = ('GLOBAL_DOMAIN_ARG', 'MAX_CONCURRENT_EXPORT_TASKS')
+
+GLOBAL_DOMAIN_ARG: Final[str] = '-globalDomain'
+MAX_CONCURRENT_EXPORT_TASKS: Final[int] = 40
---
+++ packages/macprefs/macprefs-0.3.3-py3-none-any.whl/macprefs/utils.py
@@ -0,0 +1,81 @@
+from collections.abc import ValuesView
+from datetime import datetime
+from types import FrameType
+from typing import Any, AnyStr, Mapping, Sequence
+import logging
+import sys
+
+from loguru import logger
+
+from .mp_typing import ComplexInnerTypes
+
+__all__ = ('is_simple', 'setup_logging', 'to_str')
+
+
+class InterceptHandler(logging.Handler):  # pragma: no cover
+    """Intercept handler taken from Loguru's documentation."""
+    def emit(self, record: logging.LogRecord) -> None:
+        level: str | int
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+        # Find caller from where originated the logged message
+        frame: FrameType | None = logging.currentframe()
+        depth = 2
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+def setup_log_intercept_handler() -> None:  # pragma: no cover
+    """Sets up Loguru to intercept records from the logging module."""
+    logging.basicConfig(handlers=(InterceptHandler(),), level=0)
+
+
+def setup_logging(debug: bool | None = False) -> None:
+    """Shared function to enable logging."""
+    if debug:  # pragma: no cover
+        setup_log_intercept_handler()
+        logger.enable('')
+    else:
+        logger.configure(handlers=(dict(
+            format='<level>{message}</level>',
+            level='INFO',
+            sink=sys.stderr,
+        ),))
+
+
+async def _can_decode_unicode(x: bytes) -> bool:
+    try:
+        x.decode('utf-8')
+    except UnicodeDecodeError:
+        return False
+    return True
+
+
+SimpleArg = Mapping[Any, ComplexInnerTypes] | Sequence[ComplexInnerTypes] | ValuesView[str]
+
+
+async def is_simple(x: SimpleArg) -> bool:
+    """Check if a value is a simple type of value."""
+    if isinstance(x, dict):
+        x = x.values()
+    for y in x:
+        if (isinstance(y, (datetime, list, dict))
+                or (isinstance(y, bytes) and not await _can_decode_unicode(y))):
+            return False
+    return True
+
+
+def to_str(x: AnyStr) -> str:
+    """Convert a value to a string for shell."""
+    if isinstance(x, bytes):
+        try:
+            return x.decode('utf-8')
+        except UnicodeDecodeError:
+            return ''.join(hex(y)[2:] for y in x)
+    ret = str(x)
+    return ret.lower() if ret in ('True', 'False') else ret
