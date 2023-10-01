e46291247154d4ee2602d1a36da40bba37ef3929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:04 +0000

    Add package robo-boards/0.1.1a0/robo-boards-0.1.1a0.tar.gz

---
+++ packages/robo-boards/robo-boards-0.1.1a0.tar.gz/robo-boards-0.1.1a0/README.md
@@ -0,0 +1,12 @@
+**robo-boards**
+
+
+Mobile robotics 
+modules for I/O and control of microprocessor boards 
+
+
+see https://robo-boards.readthedocs.io/en/latest/
+
+
+
+
---
+++ packages/robo-boards/robo-boards-0.1.1a0.tar.gz/robo-boards-0.1.1a0/roboboards/__init__.py
@@ -0,0 +1,18 @@
+"""
+robo-boards - 
+"""
+
+__version__ = 'v0.01.01a'
+
+from . import roboboards
+
+# import acts to export 
+
+#from .object import Object
+#from .system import System
+
+#
+
+
+
+
