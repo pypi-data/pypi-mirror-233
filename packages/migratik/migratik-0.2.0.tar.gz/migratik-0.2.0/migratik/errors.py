de3abf3f7abc22fbac31a4fc31f03e82b31a856f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:10 +0000

    Add package migratik/0.2.0/migratik-0.2.0.tar.gz

---
+++ packages/migratik/migratik-0.2.0.tar.gz/migratik-0.2.0/migratik/errors.py
@@ -0,0 +1,28 @@
+
+
+class MigratikError(Exception):
+    pass
+
+
+class MigrationError(MigratikError):
+    pass
+
+
+class MigrationPathError(MigratikError):
+    pass
+
+
+class MigrationFileError(MigratikError):
+    pass
+
+
+class MigrationParsingError(MigratikError):
+    pass
+
+
+class MigrationTableError(MigratikError):
+    pass
+
+
+class ConfigError(MigratikError):
+    pass
