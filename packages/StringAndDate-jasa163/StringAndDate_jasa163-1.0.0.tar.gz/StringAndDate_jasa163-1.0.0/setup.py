649b6764acb46e7d2e044207e86ee017a5dd361a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:20 +0000

    Add package StringAndDate-jasa163/1.0.0/StringAndDate_jasa163-1.0.0.tar.gz

---
+++ packages/StringAndDate-jasa163/StringAndDate_jasa163-1.0.0.tar.gz/StringAndDate_jasa163-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='StringAndDate_jasa163',
+    version='1.0.0',
+    description='Your module description',
+    author='jasa163',
+    url='https://github.com/hnust-xijing/python-games',
+    author_email='jasa163@163.com'
+)
+
+
