f58362ff6b415311b15a8da48136f0eae192ff93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:54 +0000

    Add package qrlocator/0.0.2/qrlocator-0.0.2-py3-none-any.whl

---
+++ packages/qrlocator/qrlocator-0.0.2-py3-none-any.whl/qrlocator/qrlocator.py
@@ -0,0 +1,19 @@
+import cv2
+import math
+import matplotlib.pyplot as plt
+from pyzbar.pyzbar import decode
+
+class QRlocator:
+    ##holding package name. Pakcage is testing at https://test.pypi.org/project/qrlocator/
+
+
+
+
+
+
+
+
+
+
+
+
