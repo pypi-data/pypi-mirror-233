5e491eae1fe35428bee2d66c44841238a018d136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:39 +0000

    Add package octasonic/1.0.2/octasonic-1.0.2.tar.gz

---
+++ packages/octasonic/octasonic-1.0.2.tar.gz/octasonic-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='octasonic',
+    packages=['octasonic'],
+    version='1.0.2',
+    description='Python library for the Octasonic breakout board',
+    long_description='Python library for the Octasonic breakout board',
+    author='Andy Grove',
+    author_email='andygrove73@gmail.com',
+    url='https://github.com/andygrove/octasonic-python',
+    download_url='https://github.com/andygrove/octasonic-python/archive/1.0.2.tar.gz',
+    keywords=['octasonic', 'ultrasonic'],
+    classifiers=[],
+)
