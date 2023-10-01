1e3e5c4af1d47dc48ce849eeccb943c128107ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:57 +0000

    Add package weblodge/0.5.0/weblodge-0.5.0-py3-none-any.whl

---
+++ packages/weblodge/weblodge-0.5.0-py3-none-any.whl/weblodge/web_app/exceptions.py
@@ -0,0 +1,38 @@
+"""
+Web App exceptions.
+"""
+class BuildException(Exception):
+    """
+    Exception relative to the build.
+    """
+
+
+class RequirementsFileNotFound(BuildException):
+    """
+    The requirements file was not found.
+    """
+
+
+class EntryPointFileNotFound(BuildException):
+    """
+    The entry point file was not found.
+    """
+
+
+class FlaskAppNotFound(BuildException):
+    """
+    Can not find the Flask application object in the entry point file.
+    """
+
+
+class DeploymentException(Exception):
+    """
+    Exception relative to the deployment.
+    """
+
+
+class NoMoreFreeApplicationAvailable(DeploymentException):
+    """
+    A free application is already deployed.
+    Contains the name of the free application.
+    """
