35d95b3b4727ebee5c9cbfe004c7e25110237a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:03 +0000

    Add package httpheaders/2023.10.1/httpheaders-2023.10.1-py3-none-any.whl

---
+++ packages/httpheaders/httpheaders-2023.10.1-py3-none-any.whl/headers/__init__.py
@@ -0,0 +1,8 @@
+"""HTTP Headers
+
+see: https://www.iana.org/assignments/http-fields/http-fields.xhtml
+generated at: 2023-10-01T02:21:21.364377
+"""
+from .string import *
+
+__version__ = "2023.10.01"
