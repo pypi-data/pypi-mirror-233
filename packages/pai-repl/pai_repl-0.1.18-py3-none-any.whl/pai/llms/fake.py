99519bc0cd50ea6ca4424901ce3343d4f79308cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:18 +0000

    Add package pai-repl/0.1.18/pai_repl-0.1.18-py3-none-any.whl

---
+++ packages/pai-repl/pai_repl-0.1.18-py3-none-any.whl/pai/llms/fake.py
@@ -0,0 +1,41 @@
+import time
+from typing import Any, Generator, List
+from pai.history import HistoryNode
+from pai.llms.llm_protocol import (
+    LLM,
+    LLMError,
+    LLMResponse,
+    LLMResponseCode,
+    LLMResponseMessage,
+    LLMStreamChunk,
+)
+
+
+def chunk_string(s, n):
+    """Divide the string s into chunks of size n."""
+    return [s[i : i + n] for i in range(0, len(s), n)]
+
+
+class FakeLLM(LLM):
+    def prompt(self, history: List[HistoryNode], prompt: str) -> Any:
+        return prompt
+
+    def description(self) -> str:
+        return "FakeLLM"
+
+    def call(
+        self, history: List[HistoryNode], prompt: str
+    ) -> Generator[LLMStreamChunk, None, LLMResponse]:
+        message = "This code will list the files\nin the current directory. \n```python\nimport os\nos.listdir()\n```\n"
+
+        # chuck the message by 7 characters
+        for s in chunk_string(message, 7):
+            yield LLMStreamChunk(s)
+            time.sleep(0.2)
+
+        return LLMResponseCode(
+            prompt=prompt,
+            code="import os\nos.listdir()",
+            message="This code will list the files\nin the current directory.",
+            raw=None,
+        )
