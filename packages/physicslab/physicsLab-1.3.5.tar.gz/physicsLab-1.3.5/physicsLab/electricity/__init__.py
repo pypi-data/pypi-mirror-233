e69c387321512e741200dca59a4d28d2e1bb8cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:05:14 +0000

    Add package physicslab/1.3.5/physicsLab-1.3.5.tar.gz

---
+++ packages/physicslab/physicsLab-1.3.5.tar.gz/physicsLab-1.3.5/physicsLab/electricity/__init__.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+# 元件引脚类
+from physicsLab.electricity.elementPin import *
+# 操作导线
+from physicsLab.electricity.wire import *
+# 元件类
+from physicsLab.electricity.elementsClass import *
+# 元件坐标系
+from physicsLab.electricity.elementXYZ import *
+# 模块化电路
+from physicsLab.electricity.unionElements import *
---
+++ packages/physicslab/physicsLab-1.3.5.tar.gz/physicsLab-1.3.5/physicsLab/electricity/wire.py
@@ -0,0 +1,66 @@
+#coding=utf-8
+import physicsLab.errors as errors
+import physicsLab._fileGlobals as _fileGlobals
+import physicsLab.electricity.elementPin as _elementPin
+
+# 检查函数参数是否是导线
+def _check_typeWire(func):
+    def result(SourcePin: "_elementPin.element_Pin", TargetPin: "_elementPin.element_Pin", color: str = '蓝') -> None:
+        if (
+                isinstance(SourcePin, _elementPin.element_Pin) and
+                isinstance(TargetPin, _elementPin.element_Pin)
+        ):
+            # 将英文的color转换为中文
+            if color in ("black", "blue", "red", "green", "yellow"):
+                color = {"black": "黑", "blue": "蓝", "red": "红", "green": "绿", "yellow": "黄"}[color]
+
+            if (color not in ("黑", "蓝", "红", "绿", "黄")):
+                raise errors.wireColorError
+
+            _fileGlobals.check_ExperimentType(_fileGlobals.experimentType.Circuit)
+
+            func(SourcePin, TargetPin, color)
+
+    return result
+
+# 新版连接导线
+@_check_typeWire
+def crt_Wire(SourcePin: "_elementPin.element_Pin", TargetPin: "_elementPin.element_Pin", color: str = '蓝') -> None:
+    _fileGlobals.Wires.append({"Source": SourcePin.element_self._arguments["Identifier"], "SourcePin": SourcePin.pinLabel,
+                   "Target": TargetPin.element_self._arguments["Identifier"], "TargetPin": TargetPin.pinLabel,
+                   "ColorName": f"{color}色导线"})
+
+# 删除导线
+@_check_typeWire
+def del_Wire(SourcePin: "_elementPin.element_Pin", TargetPin: "_elementPin.element_Pin", color: str = '蓝') -> None:
+    a_wire = {
+        "Source": SourcePin.element_self._arguments["Identifier"], "SourcePin": SourcePin.pinLabel,
+        "Target": TargetPin.element_self._arguments["Identifier"], "TargetPin": TargetPin.pinLabel,
+        "ColorName": f"{color}色导线"
+    }
+    if a_wire in _fileGlobals.Wires:
+        _fileGlobals.Wires.remove(a_wire)
+    else:
+        a_wire = {
+            "Source": TargetPin.element_self._arguments["Identifier"], "SourcePin": TargetPin.pinLabel,
+            "Target": SourcePin.element_self._arguments["Identifier"], "TargetPin": SourcePin.pinLabel,
+            "ColorName": f"{color}色导线"
+        }
+        if a_wire in _fileGlobals.Wires:
+            _fileGlobals.Wires.remove(a_wire)
+        else:
+            raise errors.wireNotFoundError
+
+# 删除所有导线
+def clear_Wires() -> None:
+    _fileGlobals.check_ExperimentType(_fileGlobals.experimentType.Circuit)
+    _fileGlobals.Wires.clear()
+
+# 获取当前导线数
+def count_Wires() -> int:
+    _fileGlobals.check_ExperimentType(_fileGlobals.experimentType.Circuit)
+    return len(_fileGlobals.Wires)
+
+# 打印导线的json
+def print_Wires() -> None:
+    print(_fileGlobals.Wires)
