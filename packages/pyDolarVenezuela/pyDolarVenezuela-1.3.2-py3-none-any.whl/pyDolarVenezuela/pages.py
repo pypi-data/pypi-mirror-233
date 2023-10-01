a84644732942b7a2689f011c7547ff6d586a26eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:51 +0000

    Add package pyDolarVenezuela/1.3.2/pyDolarVenezuela-1.3.2-py3-none-any.whl

---
+++ packages/pyDolarVenezuela/pyDolarVenezuela-1.3.2-py3-none-any.whl/pyDolarVenezuela/pages.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class Monitor:
+    name: str
+    provider: str
+
+BCV = Monitor(
+    name="Banco Central de Venezuela",
+    provider="http://www.bcv.org.ve/"
+)
+
+CriptoDolar = Monitor(
+    name="Cripto Dolar",
+    provider="https://exchange.vcoud.com/"
+)
+
+ExchangeMonitor = Monitor(
+    name="Exchange Monitor",
+    provider="https://exchangemonitor.net/"
+)
+
+iVenezuela = Monitor(
+    name="iVenezuela",
+    provider="https://www.ivenezuela.travel/"
+)
