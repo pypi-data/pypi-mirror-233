cee654170f79aade8f4cccbf43b671a38e7751a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:04 +0000

    Add package cfbd-json-py/0.0.3/cfbd_json_py-0.0.3.tar.gz

---
+++ packages/cfbd-json-py/cfbd_json_py-0.0.3.tar.gz/cfbd_json_py-0.0.3/cfbd_json_py/draft.py
@@ -0,0 +1,42 @@
+def get_cfbd_nfl_teams(
+        api_key: str = None,
+        api_key_dir: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_nfl_positions(
+        api_key: str = None,
+        api_key_dir: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_nfl_draft_info(
+        year: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+        nfl_team: str = None,
+        college: str = None,
+        conference: str = None,
+        position: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
