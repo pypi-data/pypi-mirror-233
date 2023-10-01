003c4e4ebe250c25b4bdb00b3af12f2d0058ee1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:40 +0000

    Add package srcpy/0.1.7/srcpy-0.1.7.tar.gz

---
+++ packages/srcpy/srcpy-0.1.7.tar.gz/srcpy-0.1.7/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# .+
+# .context    : SRCpy, RAI coded signal (SRC)
+# .title      : package setup
+# .kind       : python script
+# .author     : Fabrizio Pollastri <mxgbot@gmail.com>
+# .site       : Revello - Italy
+# .creation   : 30-Mar-2021
+# .copyright  : (c) 2021 Fabrizio Pollastri
+# .license    : GNU Lesser General Public License
+# .-
+
+import os
+from setuptools import setup
+import subprocess
+
+VERSION = subprocess.run(['git','describe','--abbrev=0','--tags'],
+    stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+
+
+setup(
+    name = 'srcpy',
+    long_description = 'Encoder/decoder for the SRC time signal',
+    long_description_content_type = 'text/x-rst',
+    url = 'https://github.com/fabriziop/SRCpy',
+    packages = ['srcpy'],
+    package_dir = {'srcpy':'src'},
+    include_package_data = True,
+    install_requires = ['dateutils','numpy','scipy'],
+    scripts = ['examples/time_to_src.py','examples/src_to_time.py',
+        'examples/play_wav.py'],
+    version = VERSION)
+
+# define global variables
+__script__ = os.path.basename(__file__)
+__author__ = 'Fabrizio Pollastri <mxgbot@gmail.com>'
+
+#### END
