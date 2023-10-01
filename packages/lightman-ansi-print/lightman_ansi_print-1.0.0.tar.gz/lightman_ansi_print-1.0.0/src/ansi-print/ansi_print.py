6f1256b25d7a7b0887745c437dda7a5a801701ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 14:04:10 +0000

    Add package lightman-ansi-print/1.0.0/lightman_ansi_print-1.0.0.tar.gz

---
+++ packages/lightman-ansi-print/lightman_ansi_print-1.0.0.tar.gz/lightman_ansi_print-1.0.0/src/ansi-print/ansi_print.py
@@ -0,0 +1,25 @@
+class colours:
+    fgBlack = "\033[0;30m"
+    fgRed = "\033[0;31m"
+    fgGreen = "\033[0;32m"
+    fgYellow = "\033[0;33m"
+    fgBlue = "\033[0;34m"
+    fgPurple = "\033[0;35m"
+    fgCyan = "\033[0;36m"
+    fgWhite = "\033[0;37m"
+
+    bgBlack = "\033[0;;40m"
+    bgRed = "\033[0;;41m"
+    bgGreen = "\033[0;;42m"
+    bgYellow = "\033[0;;43m"
+    bgBlue = "\033[0;;44m"
+    bgPurple = "\033[0;;45m"
+    bgCyan = "\033[0;;46m"
+    bgWhite = "\033[0;;47m"
+
+class styles:
+    normal = "\033[0m"
+    bold = "\033[1m"
+    light = "\033[2m"
+    italic = "\033[3m"
+    underline = "\033[4m"
