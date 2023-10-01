b9a05df82cd9a8317c4f79136a98ed961b495af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 03:04:52 +0000

    Add package chasten/0.2.0/chasten-0.2.0.tar.gz

---
+++ packages/chasten/chasten-0.2.0.tar.gz/chasten-0.2.0/chasten/configuration.py
@@ -0,0 +1,85 @@
+"""Configuration for Chasten."""
+
+import logging
+import logging.config
+import logging.handlers
+import sys
+from typing import Tuple
+
+import platformdirs
+from rich.logging import RichHandler
+from rich.traceback import install
+
+from chasten import constants
+
+
+def configure_tracebacks() -> None:
+    """Configure stack tracebacks arising from a crash to use rich."""
+    install()
+
+
+def user_config_dir(application_name: str, application_author: str) -> str:
+    """Return the user's configuration directory using platformdirs."""
+    # access the directory and then return it based on the
+    # provided name of the application and the application's author
+    chasten_user_config_dir_str = platformdirs.user_config_dir(
+        appname=application_name,
+        appauthor=application_author,
+    )
+    return chasten_user_config_dir_str
+
+
+def configure_logging(
+    debug_level: str = constants.logging.Default_Logging_Level,
+    debug_dest: str = constants.logging.Default_Logging_Destination,
+) -> Tuple[logging.Logger, bool]:
+    """Configure standard Python logging package."""
+    # use the specified logger with the specified destination
+    # by dynamically constructing the function to call and then
+    # invoking it with the provided debug_dest parameter
+    debug_dest = debug_dest.lower()
+    function_name = constants.logger.Function_Prefix + debug_dest
+    configure_module = sys.modules[__name__]
+    # it was possible to create the requested logger, so return it
+    try:
+        return (getattr(configure_module, function_name)(debug_level), True)
+    # it was not possible to create the requested logger, so
+    # return the default console logger as a safe alternative
+    except AttributeError:
+        return (configure_logging_console(debug_level), False)
+
+
+def configure_logging_console(
+    debug_level: str = constants.logging.Default_Logging_Level,
+) -> logging.Logger:
+    """Configure standard Python logging package to use rich."""
+    # use the RichHandler to provide formatted
+    # debugging output in the console
+    logging.basicConfig(
+        level=debug_level,
+        format=constants.logging.Format,
+        datefmt="[%X]",
+        handlers=[RichHandler()],
+    )
+    # create a logger and then return it
+    logger = logging.getLogger()
+    return logger
+
+
+def configure_logging_syslog(
+    debug_level: str = constants.logging.Default_Logging_Level,
+) -> logging.Logger:
+    """Configure standard Python logging package to use syslog."""
+    # use the SysLogHandler to send output to a localhost on a port
+    syslog_handler = logging.handlers.SysLogHandler(
+        address=(constants.server.Localhost, constants.server.Port)
+    )
+    logging.basicConfig(
+        level=debug_level,
+        format=constants.logging.Format,
+        datefmt="[%X]",
+        handlers=[syslog_handler],
+    )
+    # create a logger and then return it
+    logger = logging.getLogger()
+    return logger
---
+++ packages/chasten/chasten-0.2.0.tar.gz/chasten-0.2.0/chasten/validate.py
@@ -0,0 +1,120 @@
+"""Validate various aspects of configurations and command-line arguments."""
+
+from typing import Any, Dict, List, Tuple
+
+import jsonschema
+from jsonschema.exceptions import ValidationError
+
+from chasten import constants
+
+# intuitive description:
+# a configuration file links to one or more checks files
+# see ./chasten in the root of the repository for the config.yml file
+JSON_SCHEMA_CONFIG = {
+    "type": "object",
+    "required": [],
+    "properties": {
+        "chasten": {
+            "type": "object",
+            "properties": {
+                "checks-file": {
+                    "type": "array",
+                    "items": {"type": "string"},
+                    "required": [],
+                },
+            },
+            "additionalProperties": False,
+        },
+    },
+}
+
+# intutive description:
+# a checks file describes all of the details for one or more checks
+# see ./chasten in the root of the repository for the checks.yml file
+JSON_SCHEMA_CHECKS = {
+    "type": "object",
+    "properties": {
+        "checks": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string"},
+                    "id": {"type": "string"},
+                    "description": {"type": "string"},
+                    "pattern": {"type": "string"},
+                    "code": {"type": "string"},
+                    "count": {
+                        "anyOf": [
+                            {
+                                "type": "object",
+                                "properties": {"min": {"type": "integer"}},
+                                "required": ["min"],
+                            },
+                            {
+                                "type": "object",
+                                "properties": {"max": {"type": "integer"}},
+                                "required": ["max"],
+                            },
+                            {
+                                "type": "object",
+                                "properties": {
+                                    "min": {"type": "integer"},
+                                    "max": {"type": "integer"},
+                                },
+                                "required": ["min", "max"],
+                            },
+                        ]
+                    },
+                },
+                "required": ["name", "id", "pattern", "code"],
+                "additionalProperties": False,
+            },
+        }
+    },
+}
+
+
+def extract_checks_file_name(
+    configuration: Dict[str, Dict[str, Any]]
+) -> Tuple[bool, List[str]]:
+    """Validate the checks configuration."""
+    # there is a main "chasten" key
+    if constants.checks.Check_Chasten in configuration.keys():
+        # there is a "checks-file" key
+        if constants.checks.Check_File in configuration[constants.checks.Check_Chasten]:
+            # extract the name of the checks-files
+            # and return them in a list with a boolean
+            # indicate to show that checks files were found
+            checks_file_name_list = configuration[constants.checks.Check_Chasten][
+                constants.checks.Check_File
+            ]
+            return (True, checks_file_name_list)
+    # contents were not found and thus returen no filenames
+    return (False, [constants.markers.Empty_String])
+
+
+def validate_configuration(
+    configuration: Dict[str, Dict[str, Any]],
+    schema: Dict[str, Any] = JSON_SCHEMA_CONFIG,
+) -> Tuple[bool, str]:
+    """Validate the main configuration."""
+    # indicate that validation passed; since there
+    # were no validation errors, return an empty string
+    try:
+        jsonschema.validate(configuration, schema)
+        return (True, constants.markers.Empty_String)
+    # indicate that validation failed;
+    # since validation errors exist, package them up
+    # and return them along with the indication
+    except ValidationError as validation_error:
+        error_message = str(validation_error)
+        error_message = error_message.lstrip()
+        return (False, error_message)
+
+
+def validate_checks_configuration(
+    configuration: Dict[str, Dict[str, Any]]
+) -> Tuple[bool, str]:
+    """Validate the checks configuration."""
+    return validate_configuration(configuration, JSON_SCHEMA_CHECKS)
