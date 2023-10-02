fc00288a9cc6dac337c2c86407756b2b35ccd590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:27 +0000

    Add package maglnuse-logging/1.2.0/maglnuse_logging-1.2.0-py3-none-any.whl

---
+++ packages/maglnuse-logging/maglnuse_logging-1.2.0-py3-none-any.whl/logging_library2/logger.py
@@ -0,0 +1,21 @@
+import logging
+import coloredlogs 
+
+def setup_logger(name, log_to_file=False, log_level=logging.DEBUG): 
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    if log_to_file:
+        file_handler = logging.FileHandler('app.log')
+        file_handler.setLevel(log_level)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    coloredlogs.install(level=log_level, logger=logger)
+    return logger
