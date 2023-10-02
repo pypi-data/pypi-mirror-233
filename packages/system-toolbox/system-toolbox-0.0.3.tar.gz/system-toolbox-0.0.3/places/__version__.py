8da69528f2e69b845032944ee3687b2f43fd9aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:30 +0000

    Add package system-toolbox/0.0.3/system-toolbox-0.0.3.tar.gz

---
+++ packages/system-toolbox/system-toolbox-0.0.3.tar.gz/system-toolbox-0.0.3/places/__version__.py
@@ -0,0 +1,12 @@
+"""
+System-toolbox
+"""
+
+__title__ = "system-toolbox"
+__description__ = "Helper scripts and library for calendar"
+__version__ = "0.0.3"
+__dependencies__ = [
+    "requests==2.31.0",
+    "msgspec==0.18.2",
+    "python-dateutil==2.8.2",
+]
