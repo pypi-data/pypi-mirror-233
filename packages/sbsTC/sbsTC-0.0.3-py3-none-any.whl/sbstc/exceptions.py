8a07352545f45a09972a270e96616f5a9bb7c852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:28 +0000

    Add package sbsTC/0.0.3/sbsTC-0.0.3-py3-none-any.whl

---
+++ packages/sbsTC/sbsTC-0.0.3-py3-none-any.whl/sbstc/exceptions.py
@@ -0,0 +1,14 @@
+class InvalidCurrency(Exception):
+    pass
+
+class RequestsError(Exception):
+    pass
+
+class InvalidYearException(Exception):
+    pass
+
+class InvalidFormatDate(Exception):
+    pass
+
+class DataNotFound(Exception):
+    pass
