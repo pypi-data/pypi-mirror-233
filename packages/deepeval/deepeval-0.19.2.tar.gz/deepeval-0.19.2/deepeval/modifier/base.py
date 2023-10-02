91d18374a2baea41c210b17e7a4535c01467b9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:06:02 +0000

    Add package deepeval/0.19.2/deepeval-0.19.2.tar.gz

---
+++ packages/deepeval/deepeval-0.19.2.tar.gz/deepeval-0.19.2/deepeval/modifier/base.py
@@ -0,0 +1,22 @@
+"""Providing modifier abstractions
+"""
+from typing import Any
+from abc import abstractmethod
+
+
+class Modifier:
+    """Providing modifiers."""
+
+    @abstractmethod
+    def modify(self, input: Any):
+        self.input = input
+
+
+class ClipInterrogatorModifier(Modifier):
+    def modify(self, image_path: str, model_name="ViT-L-14/openai"):
+        from PIL import Image
+        from clip_interrogator import Config, Interrogator
+
+        image = Image.open(image_path).convert("RGB")
+        ci = Interrogator(Config(clip_model_name=model_name))
+        return ci.interrogate(image)
