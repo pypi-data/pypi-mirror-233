6981e4b359c42695936dfb2e495a3be1cadfcc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:23 +0000

    Add package openapi2jsonschema_ng/0.9.0/openapi2jsonschema_ng-0.9.0.tar.gz

---
+++ packages/openapi2jsonschema_ng/openapi2jsonschema_ng-0.9.0.tar.gz/openapi2jsonschema_ng-0.9.0/openapi2jsonschema_ng/test_command.py
@@ -0,0 +1,21 @@
+import os
+import pytest  # type: ignore
+from click.testing import CliRunner
+
+from openapi2jsonschema.command import default
+
+FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../fixtures")
+
+
+@pytest.mark.datafiles(os.path.join(FIXTURE_DIR, "petstore.yaml"))
+def test_command(datafiles):
+    runner = CliRunner()
+    for spec in os.listdir(datafiles):
+        result = runner.invoke(default, ["file://{}/{}".format(datafiles, spec)])
+        assert result.exit_code == 0
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(default, ["--help"])
+    assert result.exit_code == 0
