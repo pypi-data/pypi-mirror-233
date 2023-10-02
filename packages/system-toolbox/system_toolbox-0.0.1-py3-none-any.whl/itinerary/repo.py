b2b8fb81909781ed43f8e89cd8dd0466c59ca74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:22 +0000

    Add package system-toolbox/0.0.1/system_toolbox-0.0.1-py3-none-any.whl

---
+++ packages/system-toolbox/system_toolbox-0.0.1-py3-none-any.whl/itinerary/repo.py
@@ -0,0 +1,72 @@
+"""
+/itinerary
+/locations/nozawa
+/current
+"""
+
+import json
+import time
+import urllib.parse
+from datetime import datetime
+
+import msgspec
+from functools import cached_property
+
+from places import client, models
+
+from itinerary import utils
+
+
+ROOT_DIR = "./data"
+
+
+class Destination(msgspec.Struct):
+    date: datetime
+    name: str
+
+    @property
+    def location(self) -> models.Location | None:
+        filepath = f"{ROOT_DIR}/locations/{self.safe_name}"
+        try:
+            payload = utils.load_json(filepath)
+            return msgspec.convert(payload, models.Location)
+        except FileNotFoundError:
+            # We don't want to spam the API
+            time.sleep(2)
+            location = client.search(self.name)
+            utils.write_json(filepath, msgspec.to_builtins(location))
+            return location
+
+    @property
+    def safe_name(self):
+        return urllib.parse.quote(self.name)
+
+
+class Itinerary:
+    path = f"{ROOT_DIR}/itinerary"
+
+    def __init__(self, data):
+        self.data = data
+
+    @classmethod
+    def load(cls, rootdir):
+        data = utils.load_json(self.path)
+        return cls(data, rootdir=rootdir)
+
+    def save(self):
+        utils.write_json(self.path, msgspec.to_builtins(self.data))
+
+    def from_current(self, date: datetime):
+        curr = None
+        for d in self:
+            if d.date > date:
+                if curr:
+                    yield curr
+                    curr = None
+                yield d
+            else:
+                curr = d
+
+    def __iter__(self):
+        for d in self.data:
+            yield d
---
+++ packages/system-toolbox/system_toolbox-0.0.1-py3-none-any.whl/places/__version__.py
@@ -0,0 +1,7 @@
+"""
+System-toolbox
+"""
+
+__title__ = "system-toolbox"
+__description__ = "Helper scripts and library for calendar"
+__version__ = "0.0.1"
