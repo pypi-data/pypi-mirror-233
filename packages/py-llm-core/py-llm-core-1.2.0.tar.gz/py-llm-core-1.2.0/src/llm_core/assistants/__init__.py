b836abdcff6bfbcd445656caeef9e2877927b86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:05:01 +0000

    Add package py-llm-core/1.2.0/py-llm-core-1.2.0.tar.gz

---
+++ packages/py-llm-core/py-llm-core-1.2.0.tar.gz/py-llm-core-1.2.0/src/llm_core/assistants/__init__.py
@@ -0,0 +1,9 @@
+from .base import OpenAIAssistant
+from .chain_of_verification import COVQuestionAnswering
+from .chain_of_density import DenserSummaryCollection
+
+__all__ = [
+    "OpenAIAssistant",
+    "COVQuestionAnswering",
+    "DenserSummaryCollection",
+]
