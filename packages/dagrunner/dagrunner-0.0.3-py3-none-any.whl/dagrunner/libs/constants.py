0884114f81121d459634c31d46b6662121edf45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:07 +0000

    Add package dagrunner/0.0.3/dagrunner-0.0.3-py3-none-any.whl

---
+++ packages/dagrunner/dagrunner-0.0.3-py3-none-any.whl/dagrunner/libs/constants.py
@@ -0,0 +1,9 @@
+# Variables for storing laboratory execution libs
+COMPLETED_TASKS = "completed_tasks"
+FAILED_TASKS = "failed_tasks"
+TRIGGERED_TASKS = "triggered_tasks"
+ABORTED_TASKS = "aborted_tasks"
+
+# Variables for storing signal parameter
+SUCCESS = "success"
+FAILED = "failure"
