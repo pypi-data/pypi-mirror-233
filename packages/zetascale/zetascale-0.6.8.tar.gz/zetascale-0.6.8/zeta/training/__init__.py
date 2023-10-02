c30b565a676a4bcc6dbf38e472d107e44adc9145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:21 +0000

    Add package zetascale/0.6.8/zetascale-0.6.8.tar.gz

---
+++ packages/zetascale/zetascale-0.6.8.tar.gz/zetascale-0.6.8/zeta/training/__init__.py
@@ -0,0 +1,7 @@
+
+#training
+from zeta.training.train import Trainer, train
+from zeta.training.loss.nebula import Nebula
+from zeta.training.dataloader import *
+from zeta.training.fsdp import fsdp
+from zeta.training.scheduler import get_lr_scheduler_with_warmup
