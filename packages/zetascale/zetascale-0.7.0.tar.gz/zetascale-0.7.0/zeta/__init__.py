54e79531a2b2e4bc3ec8eed9e2f8b1cfe91a4703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:28 +0000

    Add package zetascale/0.7.0/zetascale-0.7.0.tar.gz

---
+++ packages/zetascale/zetascale-0.7.0.tar.gz/zetascale-0.7.0/zeta/__init__.py
@@ -0,0 +1,31 @@
+from zeta.zeta import zeta
+print(zeta)
+
+
+#nn
+from zeta import nn
+from zeta.nn.architecture.transformer import FeedForward
+from zeta.nn.modules.layernorm import LayerNorm
+
+#models
+from zeta import models
+
+
+#utils
+from zeta import utils
+
+
+#training
+from zeta import training
+
+#tokenizers
+from zeta import tokenizers
+
+#rl
+from zeta import rl
+
+#optim
+from zeta import optim
+
+#ops
+from zeta import ops
