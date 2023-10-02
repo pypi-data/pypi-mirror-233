8528a4e2bdab84673070755b6a3d57c12f5fbddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:04:55 +0000

    Add package duckduckgo-search/3.9.1/duckduckgo_search-3.9.1.tar.gz

---
+++ packages/duckduckgo-search/duckduckgo_search-3.9.1.tar.gz/duckduckgo_search-3.9.1/duckduckgo_search/utils.py
@@ -0,0 +1,47 @@
+import re
+from html import unescape
+from typing import Optional
+from urllib.parse import unquote
+
+REGEX_500_IN_URL = re.compile(r"(?:\d{3}-\d{2}\.js)")
+REGEX_STRIP_TAGS = re.compile("<.*?>")
+
+USERAGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+]
+
+
+def _extract_vqd(html_bytes: bytes) -> Optional[str]:
+    for c1, c2 in (
+        (b'vqd="', b'"'),
+        (b"vqd=", b"&"),
+        (b"vqd='", b"'"),
+    ):
+        try:
+            start = html_bytes.index(c1) + len(c1)
+            end = html_bytes.index(c2, start)
+            return html_bytes[start:end].decode()
+        except ValueError:
+            pass
+
+
+def _is_500_in_url(url: str) -> bool:
+    """something like '506-00.js' inside the url"""
+    return bool(REGEX_500_IN_URL.search(url))
+
+
+def _normalize(raw_html: str) -> str:
+    """strip HTML tags"""
+    if raw_html:
+        return unescape(re.sub(REGEX_STRIP_TAGS, "", raw_html))
+    return ""
+
+
+def _normalize_url(url: str) -> str:
+    """unquote url and replace spaces with '+'"""
+    if url:
+        return unquote(url).replace(" ", "+")
+    return ""
