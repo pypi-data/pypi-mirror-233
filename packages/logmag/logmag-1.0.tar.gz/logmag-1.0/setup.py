e49bace4c36d718c2f60cede782ec7a0733db036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 04:05:27 +0000

    Add package logmag/1.0/logmag-1.0.tar.gz

---
+++ packages/logmag/logmag-1.0.tar.gz/logmag-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='logmag',
+      version='1.0',
+      description='Logging',
+      packages=['logmag'],
+      author_email='zhassulanissayev@gmail.com',
+      zip_safe=False)
