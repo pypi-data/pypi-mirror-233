bb1d2676b0c2fa000ff8da38fb6bdf1819842ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:06:27 +0000

    Add package uva/0.3.6/uva-0.3.6.tar.gz

---
+++ packages/uva/uva-0.3.6.tar.gz/uva-0.3.6/README.md
@@ -0,0 +1,3 @@
+# Uva Command Line Tool
+
+This is an Uva Command line tool that can help with Uva online judge problem submissions.
---
+++ packages/uva/uva-0.3.6.tar.gz/uva-0.3.6/uva/commands/__init__.py
@@ -0,0 +1,5 @@
+from .commands import login, logout
+from .commands import submit
+from .commands import get_latest_subs
+from .commands import get_pdf_file
+from .commands import initialize_uva_dir
