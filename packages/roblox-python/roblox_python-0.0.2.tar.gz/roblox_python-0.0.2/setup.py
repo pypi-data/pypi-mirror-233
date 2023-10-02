5a1feeb5af0db243e0eea18a20740878c5876871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:41 +0000

    Add package roblox-python/0.0.2/roblox_python-0.0.2.tar.gz

---
+++ packages/roblox-python/roblox_python-0.0.2.tar.gz/roblox_python-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='roblox_python',
+    version='0.0.2',
+
+    description='Library Python Roblox',
+
+    author='TV_Kirill',
+    author_email='kirill.shishkov2007@gmail.com',
+
+    packages=['roblox_py'],
+    install_requires=["httpx"],
+)
