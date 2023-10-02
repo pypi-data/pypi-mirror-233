d13c5711bdd7ca493c446760c8e3a859663072c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:06:02 +0000

    Add package deepeval/0.19.1/deepeval-0.19.1.tar.gz

---
+++ packages/deepeval/deepeval-0.19.1.tar.gz/deepeval-0.19.1/tests/test_quickstart.py
@@ -0,0 +1,40 @@
+"""This is the ideal user flow
+"""
+import os
+
+import pytest
+
+from deepeval.api import Api
+from deepeval.metrics.factual_consistency import assert_factual_consistency
+
+IMPLEMENTATION_NAME = "Quickstart Example 2"
+os.environ["CONFIDENT_AI_IMP_NAME"] = IMPLEMENTATION_NAME
+
+
+def generate_llm_output(query: str):
+    expected_output = "Our customer success phone line is 1200-231-231."
+    return expected_output
+
+
+def test_llm_output():
+    query = "What is the customer success phone line?"
+    expected_output = "Our customer success phone line is 1200-231-231."
+    output = generate_llm_output(query)
+    assert_factual_consistency(output, expected_output)
+
+
+def test_llm_output_custom():
+    query = "What is the customer success phone line?"
+    expected_output = "Dogs and cats love to walk around the beach."
+    with pytest.raises(AssertionError):
+        assert_factual_consistency(query, expected_output)
+
+
+def test_implementation_inside_quickstart():
+    client = Api()
+    imps = client.list_implementations()
+    FOUND = False
+    for imp in imps:
+        if imp["name"] == IMPLEMENTATION_NAME:
+            FOUND = True
+    assert FOUND, f"Not found in {imps}"
