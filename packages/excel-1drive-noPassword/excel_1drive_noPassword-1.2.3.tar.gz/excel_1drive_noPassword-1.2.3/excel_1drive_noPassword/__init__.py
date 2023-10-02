1fb949d4c906422cde9d20c00567ee95caac7497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 04:05:12 +0000

    Add package excel-1drive-noPassword/1.2.3/excel_1drive_noPassword-1.2.3.tar.gz

---
+++ packages/excel-1drive-noPassword/excel_1drive_noPassword-1.2.3.tar.gz/excel_1drive_noPassword-1.2.3/excel_1drive_noPassword/__init__.py
@@ -0,0 +1,4 @@
+#Importing the necessary methods.
+from excel_1drive_noPassword.excel_1drive_noPassword import excel_1drive_toDict
+from excel_1drive_noPassword.excel_1drive_noPassword import excel_1drive_sheetnames
+from excel_1drive_noPassword.excel_1drive_noPassword import excel_1dr_SingleSheet_toDF
