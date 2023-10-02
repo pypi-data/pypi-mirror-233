a91438552c32b889198d3d31815aef7d6a52d11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:27 +0000

    Add package maglnuse-logging/1.2.0/maglnuse_logging-1.2.0.tar.gz

---
+++ packages/maglnuse-logging/maglnuse_logging-1.2.0.tar.gz/maglnuse_logging-1.2.0/README.md
@@ -0,0 +1,5 @@
+In this library, our team constructed logging system which has several functions to configure logging.
+Configurations:
+addHandler;
+setLevel;
+setFormatter;
---
+++ packages/maglnuse-logging/maglnuse_logging-1.2.0.tar.gz/maglnuse_logging-1.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='maglnuse_logging',
+    version='1.2.0',
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=[],
+    entry_points={},
+    url='https://gitlab.com/adv_python_maglnuse/assignment3',
+    license='MIT',
+    author='maglnuse',
+    author_email='n_maulen@kbtu.kz',
+    description='A custom logging library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
