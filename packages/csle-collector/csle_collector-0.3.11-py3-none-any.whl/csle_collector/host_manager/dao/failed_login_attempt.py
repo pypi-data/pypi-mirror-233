677f73ca4a256eac253b0fe4a44166a973eadd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:54 +0000

    Add package csle-collector/0.3.11/csle_collector-0.3.11-py3-none-any.whl

---
+++ packages/csle-collector/csle_collector-0.3.11-py3-none-any.whl/csle_collector/host_manager/dao/failed_login_attempt.py
@@ -0,0 +1,32 @@
+import datetime
+
+
+class FailedLoginAttempt:
+    """
+    Class representing a failed login event on some server in the emulation
+    """
+
+    def __init__(self):
+        """
+        Initializes the object
+        """
+        self.timestamp = None
+
+    @staticmethod
+    def parse_from_str(login_attempt_str: str) -> "FailedLoginAttempt":
+        """
+        Parses a failed login event DTO from a string
+
+        :param login_attempt_str: the string to parse
+        :return: the parsed DTO
+        """
+        failed_login_attempt_dto = FailedLoginAttempt()
+        failed_login_attempt_dto.timestamp = \
+            datetime.datetime.strptime(login_attempt_str, '%Y %b %d %H:%M:%S').timestamp()
+        return failed_login_attempt_dto
+
+    def __str__(self) -> str:
+        """
+        :return: a string representation of the object
+        """
+        return "timestamp:{}".format(self.timestamp)
