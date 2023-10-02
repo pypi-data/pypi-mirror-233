cb1611677c5ae526f1226910e8f160ad4f52d734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:06:31 +0000

    Add package xlogger-lob/0.0.3/xlogger-lob-0.0.3.tar.gz

---
+++ packages/xlogger-lob/xlogger-lob-0.0.3.tar.gz/xlogger-lob-0.0.3/logger.py
@@ -0,0 +1,24 @@
+import loguru
+import sys
+
+logger = loguru.logger
+
+
+def setup_logger(log_path="logs/"):
+    config = {
+        "handlers": [
+            {
+                "sink": log_path + "_{time:YYYY-MM-DD}.log",
+                "rotation": "5 MB",
+                "retention": "3 days",
+                "format": "{time:YYYY-MM-DD HH:mm:ss} - {level} - {file}:{line} - {message}",
+                "level": "INFO",
+            },
+            {
+                "sink": sys.stdout,  # <-- Change here
+                "level": "INFO",
+            },
+        ]
+    }
+
+    logger.configure(**config)
