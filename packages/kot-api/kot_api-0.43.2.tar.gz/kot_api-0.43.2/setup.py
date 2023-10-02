71049904dbd78bee515a845683b4fd9ed5395db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:55 +0000

    Add package kot-api/0.43.2/kot_api-0.43.2.tar.gz

---
+++ packages/kot-api/kot_api-0.43.2.tar.gz/kot_api-0.43.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+
+setup(
+    name="kot_api",
+    version="0.43.2",
+    description="""This is API package for KOT""",
+    url="https://github.com/KOT-database/KOT",
+    author='Onur Atakan ULUSOY',
+    author_email='atadogan06@gmail.com',
+    license='MIT',
+    install_requires=install_requires,
+    python_requires=">= 3",
+    zip_safe=False,
+)
+
+
