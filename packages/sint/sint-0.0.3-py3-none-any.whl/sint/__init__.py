72a01f3404f12de1fdc0d9f042a0cf9d1d9baf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:04:57 +0000

    Add package sint/0.0.3/sint-0.0.3-py3-none-any.whl

---
+++ packages/sint/sint-0.0.3-py3-none-any.whl/sint/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.3"
+from .sint import sint
+from .types import SInt, NumStr
+from .ntups import sign, psign, zsign, nsign
+from .enums import Sign
+from .utils import tern2x, tosint
+
+__all__ = ["sint", "SInt", "NumStr", "sign", "psign", "zsign", "nsign", "Sign", "tern2x", "tosint"]
