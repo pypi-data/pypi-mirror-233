069d4fa803e3f25789b387d69e158d5c6c79d18e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 04:05:54 +0000

    Add package godot-parser/0.1.7/godot_parser-0.1.7.tar.gz

---
+++ packages/godot-parser/godot_parser-0.1.7.tar.gz/godot_parser-0.1.7/CHANGES.md
@@ -0,0 +1,36 @@
+# Changelog
+
+## 0.1.7
+
+- Add py.typed file to provide type information [\#8](https://github.com/stevearc/godot_parser/issues/8), [\#10](https://github.com/stevearc/godot_parser/issues/10)
+- Allow colon in key names [\#9](https://github.com/stevearc/godot_parser/issues/9)
+- Drop support for python 3.6
+
+## 0.1.6
+
+- Fix errors when using new version of pyparsing
+
+## 0.1.5
+
+- Fix quote escaping during serialization
+  [\#7](https://github.com/stevearc/godot_parser/issues/7)
+- Better error message for binary scenes
+  [\#6](https://github.com/stevearc/godot_parser/issues/6)
+
+## 0.1.4
+
+- Supports node groups
+  [\#5](https://github.com/stevearc/godot_parser/pull/5)
+
+## 0.1.3
+
+- Supports trailing commas in list definitions.
+- Supports quoted keys in resources.
+
+## 0.1.2
+
+- Better support for inherited scenes
+
+## 0.1.1
+
+- Support for `use_tree()` with inherited scenes
---
+++ packages/godot-parser/godot_parser-0.1.7.tar.gz/godot_parser-0.1.7/godot_parser/structure.py
@@ -0,0 +1,53 @@
+""" The grammar of the larger structures in the GD file format """
+
+from pyparsing import (
+    DelimitedList,
+    Empty,
+    Group,
+    LineEnd,
+    Opt,
+    QuotedString,
+    Suppress,
+    Word,
+    alphanums,
+)
+
+from .sections import GDSection, GDSectionHeader
+from .values import value
+
+key = QuotedString('"', escChar="\\", multiline=False).set_name("key") | Word(
+    alphanums + "_/:"
+).set_name("key")
+var = Word(alphanums + "_").set_name("variable")
+attribute = Group(var + Suppress("=") + value)
+
+# [node name="Node2D"]
+section_header = (
+    (
+        Suppress("[")
+        + var.set_results_name("section_type")
+        + Opt(DelimitedList(attribute, Empty()))
+        + Suppress("]")
+        + Suppress(LineEnd())
+    )
+    .set_name("section_header")
+    .set_parse_action(GDSectionHeader.from_parser)
+)
+
+# texture = ExtResource( 1 )
+section_entry = Group(key + Suppress("=") + value + Suppress(LineEnd())).set_name(
+    "section_entry"
+)
+section_contents = DelimitedList(section_entry, Empty()).set_name("section_contents")
+
+# [node name="Sprite" type="Sprite"]
+# texture = ExtResource( 1 )
+section = (
+    (section_header + Opt(section_contents))
+    .set_name("section")
+    .set_parse_action(GDSection.from_parser)
+)
+
+# Exports
+
+scene_file = DelimitedList(section, Empty())
