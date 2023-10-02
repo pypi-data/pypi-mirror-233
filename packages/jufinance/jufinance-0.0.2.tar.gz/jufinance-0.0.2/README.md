65d43bfda20c119ebfdd260c0c8aaaaa12a7ffc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:03 +0000

    Add package jufinance/0.0.2/jufinance-0.0.2.tar.gz

---
+++ packages/jufinance/jufinance-0.0.2.tar.gz/jufinance-0.0.2/README.md
@@ -0,0 +1,2 @@
+# JUFinance
+### All-in-one Python Package for all stock market data.
---
+++ packages/jufinance/jufinance-0.0.2.tar.gz/jufinance-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+DESCRIPTION = "Python Package to access Indian Stock market data."
+LONG_DESCRIPTION = "Python Package to access Indian Stock market data. This is for educational purposes only."
+
+# Setting up
+setup(
+    name="jufinance",
+    version=VERSION,
+    author="Prannay Kedia & Sparsh Gupta",
+    author_email="<prannaykedia1@gmail.com>, <sparshgupta2407@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "beautifulsoup4",
+        "bs4",
+        "pandas",
+        "requests",
+        "selenium",
+    ],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=["python", "finance", "stock market"],
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+    ],
+)
