f02262c7aaaa00636b49a699e7fae46bce3bb54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:51 +0000

    Add package techdocs/0.1.2/techdocs-0.1.2.tar.gz

---
+++ packages/techdocs/techdocs-0.1.2.tar.gz/techdocs-0.1.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import setup
+
+setup(
+	name='techdocs',
+	version='0.1.2',
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'techdocs=techdocs.cli:main'
+        ]
+    },
+
+    # data_files=[('techdocs', ['techdocs/utils/*.json'])],
+
+    packages=setuptools.find_packages(),
+)
