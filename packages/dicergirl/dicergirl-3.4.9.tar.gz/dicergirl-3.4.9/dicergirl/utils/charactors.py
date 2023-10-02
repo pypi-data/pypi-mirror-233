1fdc5f9debdace54433bdb82b33c669b3e7412a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:51 +0000

    Add package dicergirl/3.4.9/dicergirl-3.4.9.tar.gz

---
+++ packages/dicergirl/dicergirl-3.4.9.tar.gz/dicergirl-3.4.9/dicergirl/utils/charactors.py
@@ -0,0 +1,46 @@
+from ..common.const import EMPTY_CHAR, EMPTY_DICT
+
+class BaseCharactor:
+    def __init__(self) -> None:
+        self.name = EMPTY_CHAR
+        self.sex = "女"
+        self.age = 18
+        self.hp = 0
+        self.hp_max = 0
+        self.tools = EMPTY_DICT
+        self.skills = EMPTY_DICT
+
+    def load(self, data: dict):
+        self.__dict__.update(data)
+        return self
+
+class Character(BaseCharactor):
+    """ 人物卡模板 """
+    def __init__(self) -> None:
+        super().__init__()
+
+    def init(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        data = "姓名: %s\n" % self.name
+        data += "性别: %s 年龄: %d\n" % (self.sex, self.age)
+        data += "生命值: %d/%d" % (self.hp, self.hp_max)
+        return data
+
+    def skills_output(self) -> str:
+        if not self.skills:
+            return "%s 当前无任何技能数据。" % self.name
+        r = "%s技能数据: " % self.name
+        for k, v in self.skills.items():
+            r += "\n%s: %d" % (k, v)
+        return r
+
+    def output(self) -> str:
+        return self.__repr__()
+
+    def rollcount(self) -> tuple:
+        return (self.__count(), self.__count() + self.luc)
+
+    def __count(self):
+        return 0
