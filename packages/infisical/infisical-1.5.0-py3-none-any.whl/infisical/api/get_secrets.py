2dbff60a7c84d7b125493dffe26e8b454c2e160e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:52 +0000

    Add package infisical/1.5.0/infisical-1.5.0-py3-none-any.whl

---
+++ packages/infisical/infisical-1.5.0-py3-none-any.whl/infisical/api/get_secrets.py
@@ -0,0 +1,27 @@
+from infisical.models.api import GetSecretsDTO, SecretsResponse
+from requests import Session
+
+
+def get_secrets_req(api_request: Session, options: GetSecretsDTO) -> SecretsResponse:
+    """Send request again Infisical API to fetch secrets.
+    See more information on https://infisical.com/docs/api-reference/endpoints/secrets/read
+
+    :param api_request: The :class:`requests.Session` instance used to perform the request
+    :param workspace_id: The ID of the workspace
+    :param environment: The environment
+    :return: Returns the API response as-is
+    """
+
+    response = api_request.get(
+        "/api/v3/secrets",
+        params={
+            "environment": options.environment,
+            "workspaceId": options.workspace_id,
+            "secretPath": options.path,
+            "include_imports": str(options.include_imports).lower()
+        },
+    )
+    data = SecretsResponse.parse_obj(response.json())
+
+    return (data.secrets if data.secrets else [], data.imports if data.imports else [])
+    
---
+++ packages/infisical/infisical-1.5.0-py3-none-any.whl/infisical/api/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class GetServiceTokenDetailsUserResponse(BaseModel):
+    id: str = Field(..., alias="_id")
+    email: str
+    created_at: datetime = Field(..., alias="createdAt")
+    updated_at: datetime = Field(..., alias="updatedAt")
+    v: int = Field(..., alias="__v")
+    first_name: str = Field(..., alias="firstName")
+    last_name: str = Field(..., alias="lastName")
+
+
+class GetServiceTokenDetailsResponse(BaseModel):
+    id: str = Field(..., alias="_id")
+    name: str
+    workspace: str
+    user: GetServiceTokenDetailsUserResponse
+    expires_at: Optional[datetime] = Field(None, alias="expiresAt")
+    encrypted_key: str = Field(..., alias="encryptedKey")
+    iv: str
+    tag: str
+    created_at: datetime = Field(..., alias="createdAt")
+    updated_at: datetime = Field(..., alias="updatedAt")
+    v: int = Field(..., alias="__v")
+
+class KeyData(BaseModel):
+    id: str = Field(..., alias="_id")
+    workspace: str
+    encrypted_key: str = Field(..., alias="encryptedKey")
+    public_key: str = Field(..., alias="publicKey")
+    nonce: str
+
+class GetServiceTokenKeyResponse(BaseModel):
+    key: KeyData
