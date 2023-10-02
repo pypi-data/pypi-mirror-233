e2b27a4fcb141a16551398bc0169851e25ef4e1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:06:58 +0000

    Add package vizchain/0.1.0/vizchain-0.1.0.tar.gz

---
+++ packages/vizchain/vizchain-0.1.0.tar.gz/vizchain-0.1.0/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='vizchain',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='astelmach20@gmail.com',
+    description='A simple visionchain example',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/astelmach01/visionchain',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
