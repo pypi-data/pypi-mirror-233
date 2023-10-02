e83a34147121131262ba242d83aa34ca311ef9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 04:05:59 +0000

    Add package heahmund/0.1.2/heahmund-0.1.2.tar.gz

---
+++ packages/heahmund/heahmund-0.1.2.tar.gz/heahmund-0.1.2/src/heahmund/tcp.py
@@ -0,0 +1,81 @@
+# MIT License
+#
+# Copyright (c) 2023 Clivern
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import socket
+
+from heahmund.logger import Logger
+from heahmund.status import Status
+
+
+class TCP:
+    """TCP Check"""
+
+    def __init__(self, name, hostname="example.com", port=443, timeout=30):
+        self.name = name
+        self.hostname = hostname
+        self.port = port
+        self.timeout = timeout
+        self.logger = Logger().get_logger(__name__)
+
+    def run(self):
+        """
+        Run The Check
+
+        Returns:
+            The Check Result
+        """
+
+        status = Status.OK
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(self.timeout)
+
+        try:
+            sock.connect((self.hostname, self.port))
+
+            self.logger.debug(
+                "Server with hostname {} with port {}.".format(self.hostname, self.port)
+            )
+
+            status = Status.OK
+
+        except socket.timeout:
+            self.logger.debug(
+                "Server with hostname {} with port {} timeout error.".format(
+                    self.hostname, self.port
+                )
+            )
+
+            status = Status.NOT_OK
+
+        except socket.error as e:
+            self.logger.debug(
+                "Server with hostname {} with port {} error raised {}.".format(
+                    self.hostname, self.port, str(e)
+                )
+            )
+
+            status = Status.ERROR
+
+        finally:
+            sock.close()
+
+        return {"name": self.name, "status": status}
---
+++ packages/heahmund/heahmund-0.1.2.tar.gz/heahmund-0.1.2/tests/test_udp.py
@@ -0,0 +1,36 @@
+# MIT License
+#
+# Copyright (c) 2023 Clivern
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import pytest
+import uuid
+
+from heahmund.udp import UDP
+
+
+def test_udp():
+    """UDP Tests"""
+    name = str(uuid.uuid4())
+
+    udp = UDP(name, "8.8.8.8", 443, 10)
+    result = udp.run()
+
+    assert result == {"name": name, "status": "OK"}
