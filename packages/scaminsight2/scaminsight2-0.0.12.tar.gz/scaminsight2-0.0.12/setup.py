ef1e5d0b2374745298578b7d83a9a11a89b29ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:12 +0000

    Add package scaminsight2/0.0.12/scaminsight2-0.0.12.tar.gz

---
+++ packages/scaminsight2/scaminsight2-0.0.12.tar.gz/scaminsight2-0.0.12/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scaminsight2',
+    version='0.0.12',
+    description='PYPI tutorial package creation written by',
+    author='tester',
+    license="MIT",
+    author_email='tester@gmail.com',
+    url='https://github.com/test',
+    install_requires=['python-whois',  'bs4', 'selenium','webdriver-manager'],
+    packages=find_packages(exclude=[]),
+    keywords=[''],
+    python_requires='>=3.6',
+    scripts=['scaminsight2/main.py'],
+    entry_points={
+        'console_scripts': [
+            'scaminsight2test=scaminsight2.main:main'
+        ]
+    },
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
