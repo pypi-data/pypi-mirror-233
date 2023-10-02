508d039539d7afa59706601ad55c7fbdfa893a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:28 +0000

    Add package fractal-noise/0.0.3/fractal_noise-0.0.3.tar.gz

---
+++ packages/fractal-noise/fractal_noise-0.0.3.tar.gz/fractal_noise-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="fractal_noise",
+    version="0.0.3",
+    author="Sophia Shovkovy",
+    author_email="sophiashovkovy@gmail.com",
+    description="Python implementation for Fractal Noise, ideal for creating realistic terrain, cloud-like textures, and intricate visual effects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["fractal_noise"]),
+    install_requires=["numpy", "perlin-noise"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest==4.4.1"],
+    test_suite="tests",
+)
