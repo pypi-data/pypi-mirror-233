2a7b6ca2e8f5682a3cdc534804e054353db296c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:06:04 +0000

    Add package bg-pyclient/0.0.2/bg_pyclient-0.0.2-py3-none-any.whl

---
+++ packages/bg-pyclient/bg_pyclient-0.0.2-py3-none-any.whl/bg_pyclient/corefile.py
@@ -0,0 +1,14 @@
+# this will be the core file where we will be keeping the content 
+
+
+
+#doing just hello world as of now 
+class structure:
+    def __init__(self, name, age):
+        self.functionality = " test structure"
+        
+
+    def functionality(self):
+        print(f"Hello, my functionality  is to {self.name}!")
+
+   
