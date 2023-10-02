544e8a2e3a2b5c66ed738e84f1b6bbb86992b03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:21 +0000

    Add package zetascale/0.6.9/zetascale-0.6.9.tar.gz

---
+++ packages/zetascale/zetascale-0.6.9.tar.gz/zetascale-0.6.9/zeta/nn/attention/__init__.py
@@ -0,0 +1,15 @@
+"""Zeta Halo"""
+
+#attentions
+from zeta.nn.attention.flash_attention2 import FlashAttentionTwo
+from zeta.nn.attention.local_attention import LocalAttention
+from zeta.nn.attention.local_attention_mha import LocalMHA
+from zeta.nn.attention.multihead_attention import MultiheadAttention
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.nn.attention.cross_attention import CrossAttention
+from zeta.nn.attention.flash_attention import FlashAttention
+# from zeta.nn.attention.spatial_linear_attention import SpatialLinearAttention
+
+from zeta.nn.attention.mixture_attention import MixtureOfAttention, MixtureOfAutoregressiveAttention
+from zeta.nn.attention.attend import Attend
+from zeta.nn.attention.mgqa import MGQA
