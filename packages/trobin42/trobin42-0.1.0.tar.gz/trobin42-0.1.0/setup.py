4e28f9fa72ddc62592da8924c801d3c1634c51de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:04 +0000

    Add package trobin42/0.1.0/trobin42-0.1.0.tar.gz

---
+++ packages/trobin42/trobin42-0.1.0.tar.gz/trobin42-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trobin42',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here
+    ],
+)
