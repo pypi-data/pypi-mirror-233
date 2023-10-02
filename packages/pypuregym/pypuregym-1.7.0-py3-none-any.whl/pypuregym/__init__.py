20192d76f0a034d91fc898521b842e5874ff6808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:04:39 +0000

    Add package pypuregym/1.7.0/pypuregym-1.7.0-py3-none-any.whl

---
+++ packages/pypuregym/pypuregym-1.7.0-py3-none-any.whl/pypuregym/__init__.py
@@ -0,0 +1,23 @@
+# pylint: disable=unused-import
+import enum
+
+from pypuregym.pure_api import PureAPI  # noqa: F401
+
+
+class GymType(enum.Enum):
+    """An enum to represent a Pure gym type."""
+
+    YOGA = "Y"
+    FITNESS = "F"
+
+
+# pylint: disable=invalid-name
+class Region(enum.Enum):
+    """An enum to represent a Pure region location."""
+
+    HK = 1
+    SG = 2
+    CN = 4
+
+
+__version__ = "1.7.0"
