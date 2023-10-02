860e485a44a87710d586fa3d3d55a58a1a9316fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:04:37 +0000

    Add package table-transformer/1.0.2/table_transformer-1.0.2-py3-none-any.whl

---
+++ packages/table-transformer/table_transformer-1.0.2-py3-none-any.whl/table_transformer/__init__.py
@@ -0,0 +1,8 @@
+import sys
+from .main import (TableExtractionPipeline,
+                           get_structure_class_thresholds,
+                           visualize_cells)
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+
