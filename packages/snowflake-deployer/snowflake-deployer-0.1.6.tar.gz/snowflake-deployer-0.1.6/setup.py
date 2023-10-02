e389a68db471a7ae6932a7801de8f5a83dfd2138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:04:39 +0000

    Add package snowflake-deployer/0.1.6/snowflake-deployer-0.1.6.tar.gz

---
+++ packages/snowflake-deployer/snowflake-deployer-0.1.6.tar.gz/snowflake-deployer-0.1.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+#import pathlib
+#import pkg_resources
+
+#with pathlib.Path('requirements.txt').open() as requirements_txt:
+#    install_requires = [
+#        str(requirement)
+#        for requirement
+#        in pkg_resources.parse_requirements(requirements_txt)
+#    ]
+
+
+setup(
+    name='snowflake-deployer',
+    version='0.1.6',
+    description='Snowflake state based deployer',
+    author='Justin Hanson, Jernej Plankelj',
+    entry_points = {
+        'console_scripts': ['snowflake-deployer=src.cli:cli'],
+        #'console_scripts': ['snowflake_deployer=src.command_line:cli'],
+    },
+    #install_requires=install_requires,
+    install_requires=['cryptography','snowflake-connector-python','pyyaml','schema','jinja2'],
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    keywords = ['SNOWFLAKE','DATAOPS','DEVOPS','DATA','DEPLOYMENT'],
+    project_urls = {
+        'Project Docs': 'https://metaopslabs.github.io/snowflake_deployer/'
+    }
+
+) 
