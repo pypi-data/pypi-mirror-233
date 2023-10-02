942647880c5a9ac4314fd9ba67dd60162e300242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:04:50 +0000

    Add package promplate/0.2.2.5/promplate-0.2.2.5.tar.gz

---
+++ packages/promplate/promplate-0.2.2.5.tar.gz/promplate-0.2.2.5/promplate/prompt/chat.py
@@ -0,0 +1,63 @@
+from sys import version_info
+from typing import Literal, TypedDict
+
+from .utils import is_message_start
+
+if version_info >= (3, 11):
+    from typing import NotRequired
+
+    class Message(TypedDict):
+        role: Literal["user", "assistant", "system"]
+        content: str
+        name: NotRequired[str]
+
+else:
+
+    class MessageWithoutName(TypedDict):
+        role: Literal["user", "assistant", "system"]
+        content: str
+
+    class MessageWithName(MessageWithoutName):
+        name: str
+
+    Message = MessageWithoutName | MessageWithName  # type: ignore
+
+    del MessageWithoutName, MessageWithName
+
+
+def ensure(text_or_list: list[Message] | str) -> list[Message]:
+    return (
+        parse_chat_markup(text_or_list)
+        if isinstance(text_or_list, str)
+        else text_or_list
+    )
+
+
+def parse_chat_markup(text: str) -> list[Message]:
+    messages = []
+    current_message = None
+    buffer = []
+
+    for line in text.splitlines():
+        match = is_message_start.match(line)
+        if match:
+            role, name = match.group(1), match.group(2)
+
+            if current_message:
+                current_message["content"] = "\n".join(buffer)
+                messages.append(current_message)
+                buffer.clear()
+
+            current_message = {"role": role, "content": ""}
+
+            if name:
+                current_message["name"] = name
+
+        elif current_message:
+            buffer.append(line)
+
+    if current_message:
+        current_message["content"] = "\n".join(buffer)
+        messages.append(current_message)
+
+    return messages or [{"role": "user", "content": text}]
