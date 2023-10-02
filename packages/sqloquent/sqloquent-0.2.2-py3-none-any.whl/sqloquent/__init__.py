d03ed71b902dfce7bbe10ed97127a110d42af978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:12 +0000

    Add package sqloquent/0.2.2/sqloquent-0.2.2-py3-none-any.whl

---
+++ packages/sqloquent/sqloquent-0.2.2-py3-none-any.whl/sqloquent/__init__.py
@@ -0,0 +1,46 @@
+"""
+    The majority of useful features are exposed from the root level of
+    the package, and the rest from either sqloquent.tools or from
+    invoking the tools through the CLI.
+"""
+
+from sqloquent.classes import (
+    SqlModel,
+    SqlQueryBuilder,
+    SqliteContext,
+    DeletedModel,
+    HashedModel,
+    Attachment,
+    Row,
+    JoinedModel,
+    JoinSpec,
+    dynamic_sqlmodel,
+)
+from sqloquent.interfaces import (
+    CursorProtocol,
+    DBContextProtocol,
+    ModelProtocol,
+    QueryBuilderProtocol,
+    JoinedModelProtocol,
+    RowProtocol,
+    RelationProtocol,
+    RelatedModel,
+    RelatedCollection,
+)
+from sqloquent.relations import (
+    Relation,
+    HasOne,
+    HasMany,
+    BelongsTo,
+    BelongsToMany,
+    has_one,
+    has_many,
+    belongs_to,
+    belongs_to_many
+)
+from sqloquent.migration import (
+    Column,
+    Table,
+    Migration,
+    get_index_name,
+)
