c98b54d8ebe1552f9ec83885cbb028f90e022fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:14 +0000

    Add package wordtm/0.1.0/wordtm-0.1.0.tar.gz

---
+++ packages/wordtm/wordtm-0.1.0.tar.gz/wordtm-0.1.0/wordtm_0/add.py
@@ -0,0 +1,7 @@
+# add.py
+#   Description
+#   By Johnny Cheng
+#   Updated: 2 October 2023
+
+def add(x, y):
+	return x+y
---
+++ packages/wordtm/wordtm-0.1.0.tar.gz/wordtm-0.1.0/wordtm_0/extras/divide.py
@@ -0,0 +1,7 @@
+# divide.py
+#   Description
+#   By Johnny Cheng
+#   Updated: 2 October 2023
+
+def divide(x, y):
+	return x/y
