cd81064f65bb716925999691b3731615360ef785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:04:38 +0000

    Add package line-ex-forex-next3/1.25/line_ex_forex_next3-1.25.tar.gz

---
+++ packages/line-ex-forex-next3/line_ex_forex_next3-1.25.tar.gz/line_ex_forex_next3-1.25/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name='line_ex_forex_next3' ,
+       version = '1.25' ,
+       description = 'line_ex_forex_next3' ,
+       author = 'omid' ,
+       packages =['line_ex_forex_next3'] ,
+       zip_safe = False
+       
+       )
