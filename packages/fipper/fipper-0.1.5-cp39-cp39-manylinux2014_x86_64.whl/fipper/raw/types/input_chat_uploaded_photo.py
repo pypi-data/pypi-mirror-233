49baa5c3af97d260326b78dc1bc478c542bf584d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 04:05:26 +0000

    Add package fipper/0.1.5/fipper-0.1.5-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/fipper/fipper-0.1.5-cp39-cp39-manylinux2014_x86_64.whl/fipper/raw/types/input_chat_uploaded_photo.py
@@ -0,0 +1,95 @@
+#  Fipper - Telegram MTProto API Client Library for Python.
+#  Copyright (C) 2022-2023 AyiinXd <https://github.com/AyiinXd>
+#
+#  This file is part of Fipper.
+#
+#  Fipper is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Fipper is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Fipper.  If not, see <http://www.gnu.org/licenses/>.
+
+from io import BytesIO
+
+from fipper.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from fipper.raw.core import TLObject
+from fipper import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class InputChatUploadedPhoto(TLObject):  # type: ignore
+    """Telegram API type.
+
+    Constructor of :obj:`~fipper.raw.base.InputChatPhoto`.
+
+    Details:
+        - Layer: ``148``
+        - ID: ``C642724E``
+
+    Parameters:
+        file (:obj:`InputFile <fipper.raw.base.InputFile>`, *optional*):
+            N/A
+
+        video (:obj:`InputFile <fipper.raw.base.InputFile>`, *optional*):
+            N/A
+
+        video_start_ts (``float`` ``64-bit``, *optional*):
+            N/A
+
+    """
+
+    __slots__: List[str] = ["file", "video", "video_start_ts"]
+
+    ID = 0xc642724e
+    QUALNAME = "types.InputChatUploadedPhoto"
+
+    def __init__(self, *, file: "raw.base.InputFile" = None, video: "raw.base.InputFile" = None, video_start_ts: Optional[float] = None) -> None:
+        self.file = file  # flags.0?InputFile
+        self.video = video  # flags.1?InputFile
+        self.video_start_ts = video_start_ts  # flags.2?double
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "InputChatUploadedPhoto":
+        
+        flags = Int.read(b)
+        
+        file = TLObject.read(b) if flags & (1 << 0) else None
+        
+        video = TLObject.read(b) if flags & (1 << 1) else None
+        
+        video_start_ts = Double.read(b) if flags & (1 << 2) else None
+        return InputChatUploadedPhoto(file=file, video=video, video_start_ts=video_start_ts)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        flags = 0
+        flags |= (1 << 0) if self.file is not None else 0
+        flags |= (1 << 1) if self.video is not None else 0
+        flags |= (1 << 2) if self.video_start_ts is not None else 0
+        b.write(Int(flags))
+        
+        if self.file is not None:
+            b.write(self.file.write())
+        
+        if self.video is not None:
+            b.write(self.video.write())
+        
+        if self.video_start_ts is not None:
+            b.write(Double(self.video_start_ts))
+        
+        return b.getvalue()
