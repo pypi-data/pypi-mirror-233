7d1a7868fbe15e3ede36aeb8cc4c485736fd861f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:29 +0000

    Add package GlobalKit/1.0.6/GlobalKit-1.0.6.tar.gz

---
+++ packages/GlobalKit/GlobalKit-1.0.6.tar.gz/GlobalKit-1.0.6/GlobalKit/alphabets.py
@@ -0,0 +1,119 @@
+# English
+english_lowercase: str = 'abcdefghijklmnopqrstuvwxyz'
+english_uppercase: str = english_lowercase.upper()
+english: str = english_lowercase + english_uppercase
+
+english_vowels_lowercase: str = 'aeiouy'
+english_vowels_uppercase: str = english_vowels_lowercase.upper()
+english_vowels: str = english_vowels_lowercase + english_vowels_uppercase
+
+english_consonants_lowercase: str = 'bcdfghjklmnpqrstvwxz'
+english_consonants_uppercase: str = english_consonants_lowercase.upper()
+english_consonants: str = english_consonants_lowercase + english_consonants_uppercase
+
+# Spanish
+spanish_lowercase: str = 'abcdefghijklmnñopqrstuvwxyz'
+spanish_uppercase: str = spanish_lowercase.upper()
+spanish: str = spanish_lowercase + spanish_uppercase
+
+spanish_vowels_lowercase: str = 'aeiouáéíóúü'
+spanish_vowels_uppercase: str = spanish_vowels_lowercase.upper()
+spanish_vowels: str = spanish_vowels_lowercase + spanish_vowels_uppercase
+
+spanish_consonants_lowercase: str = 'bcdfghjklmnñpqrstvwxyz'
+spanish_consonants_uppercase: str = spanish_consonants_lowercase.upper()
+spanish_consonants: str = spanish_consonants_lowercase + spanish_consonants_uppercase
+
+# French
+french_lowercase: str = 'abcdefghijklmnopqrstuvwxyzàâçéèêëîïôùûüœ'
+french_uppercase: str = french_lowercase.upper()
+french: str = french_lowercase + french_uppercase
+
+french_vowels_lowercase: str = 'aeiouàâéèêëîïôùûü'
+french_vowels_uppercase: str = french_vowels_lowercase.upper()
+french_vowels: str = french_vowels_lowercase + french_vowels_uppercase
+
+french_consonants_lowercase: str = 'bcçdfghjklmnpqrstvwxyz'
+french_consonants_uppercase: str = french_consonants_lowercase.upper()
+french_consonants: str = french_consonants_lowercase + french_consonants_uppercase
+
+# German
+german_lowercase: str = 'abcdefghijklmnopqrstuvwxyzäöüß'
+german_uppercase: str = german_lowercase.upper()
+german: str = german_lowercase + german_uppercase
+
+german_vowels_lowercase: str = 'aeiouäöü'
+german_vowels_uppercase: str = german_vowels_lowercase.upper()
+german_vowels: str = german_vowels_lowercase + german_vowels_uppercase
+
+german_consonants_lowercase: str = 'bcdfghjklmnpqrstvwxyzß'
+german_consonants_uppercase: str = german_consonants_lowercase.upper()
+german_consonants: str = german_consonants_lowercase + german_consonants_uppercase
+
+# Italian
+italian_lowercase: str = 'abcdefghijklmnopqrstuvwxyzàèéìòù'
+italian_uppercase: str = italian_lowercase.upper()
+italian: str = italian_lowercase + italian_uppercase
+
+italian_vowels_lowercase: str = 'aeiouàèéìòù'
+italian_vowels_uppercase: str = italian_vowels_lowercase.upper()
+italian_vowels: str = italian_vowels_lowercase + italian_vowels_uppercase
+
+italian_consonants_lowercase: str = 'bcdfghjklmnpqrstvwxyz'
+italian_consonants_uppercase: str = italian_consonants_lowercase.upper()
+italian_consonants: str = italian_consonants_lowercase + italian_consonants_uppercase
+
+# Portuguese
+portuguese_lowercase: str = 'abcdefghijklmnopqrstuvwxyzáâãàçéêíóôõú'
+portuguese_uppercase: str = portuguese_lowercase.upper()
+portuguese: str = portuguese_lowercase + portuguese_uppercase
+
+portuguese_vowels_lowercase: str = 'aeiouáâãàéêíóôõú'
+portuguese_vowels_uppercase: str = portuguese_vowels_lowercase.upper()
+portuguese_vowels: str = portuguese_vowels_lowercase + portuguese_vowels_uppercase
+
+portuguese_consonants_lowercase: str = 'bcdfghjklmnpqrstvwxyzç'
+portuguese_consonants_uppercase: str = portuguese_consonants_lowercase.upper()
+portuguese_consonants: str = portuguese_consonants_lowercase + portuguese_consonants_uppercase
+
+# Dutch
+dutch_lowercase: str = 'abcdefghijklmnopqrstuvwxyz'
+dutch_uppercase: str = dutch_lowercase.upper()
+dutch: str = dutch_lowercase + dutch_uppercase
+
+dutch_vowels_lowercase: str = 'aeiou'
+dutch_vowels_uppercase: str = dutch_vowels_lowercase.upper()
+dutch_vowels: str = dutch_vowels_lowercase + dutch_vowels_uppercase
+
+dutch_consonants_lowercase: str = 'bcdfghjklmnpqrstvwxyz'
+dutch_consonants_uppercase: str = dutch_consonants_lowercase.upper()
+dutch_consonants: str = dutch_consonants_lowercase + dutch_consonants_uppercase
+
+# Russian
+russian_lowercase: str = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+russian_uppercase: str = russian_lowercase.upper()
+russian: str = russian_lowercase + russian_uppercase
+
+russian_vowels_lowercase: str = 'аеёиоуэыюя'
+russian_vowels_uppercase: str = russian_vowels_lowercase.upper()
+russian_vowels: str = russian_vowels_lowercase + russian_vowels_uppercase
+
+russian_consonants_lowercase: str = 'бвгджзйклмнпрстфхцчшщ'
+russian_consonants_uppercase: str = russian_consonants_lowercase.upper()
+russian_consonants: str = russian_consonants_lowercase + russian_consonants_uppercase
+
+# Ukrainian
+ukrainian_lowercase: str = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
+ukrainian_uppercase: str = ukrainian_lowercase.upper()
+ukrainian: str = ukrainian_lowercase + ukrainian_uppercase
+
+ukrainian_vowels_lowercase: str = 'аеєиіїоуюя'
+ukrainian_vowels_uppercase: str = ukrainian_vowels_lowercase.upper()
+ukrainian_vowels: str = ukrainian_vowels_lowercase + ukrainian_vowels_uppercase
+
+ukrainian_consonants_lowercase: str = 'бвгґджзйклмнпрстфхцчшщ'
+ukrainian_consonants_uppercase: str = ukrainian_consonants_lowercase.upper()
+ukrainian_consonants: str = ukrainian_consonants_lowercase + ukrainian_consonants_uppercase
+
+# Other
+numbers: str = '0123456789'
