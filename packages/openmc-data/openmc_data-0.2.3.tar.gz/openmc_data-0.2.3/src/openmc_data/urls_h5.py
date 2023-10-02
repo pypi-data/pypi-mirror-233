09fcd6f1d119d058a7a55c77e88cc63f9696e022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:07:10 +0000

    Add package openmc-data/0.2.3/openmc_data-0.2.3.tar.gz

---
+++ packages/openmc-data/openmc_data-0.2.3.tar.gz/openmc_data-0.2.3/src/openmc_data/urls_h5.py
@@ -0,0 +1,20 @@
+all_h5_release_details = {
+    "endf": {
+        "b7.1": {
+            "neutron-photon": {
+                "base_url": "https://anl.box.com/shared/static/",
+                "compressed_files": ["9igk353zpy8fn9ttvtrqgzvw1vtejoz6.xz"],
+                "compressed_file_size": 1.6,
+                "uncompressed_file_size": 6.2,
+            }
+        },
+        "b8.0": {
+            "neutron-photon": {
+                "base_url": "https://anl.box.com/shared/static/",
+                "compressed_files": ["uhbxlrx7hvxqw27psymfbhi7bx7s6u6a.xz"],
+                "compressed_file_size": 3.4,
+                "uncompressed_file_size": 13.7,
+            }
+        },
+    }
+}
