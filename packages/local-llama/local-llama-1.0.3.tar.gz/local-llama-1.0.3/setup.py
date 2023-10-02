b7a729a57d6e5c05b8bb1692dacd8c0668fde14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:27 +0000

    Add package local-llama/1.0.3/local-llama-1.0.3.tar.gz

---
+++ packages/local-llama/local-llama-1.0.3.tar.gz/local-llama-1.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='local-llama',  # Change this to a suitable name for your script
+    version='1.0.3',
+    description='A flask server for a chrome extension that querys llm models',
+    long_description='A flask server for a chrome extension that querys llm models for use with this chrome extention: https://chrome.google.com/webstore/detail/local-llama-llm-ai-chat-q/ekobbgdgkocdnnjoahoojakmoimfjlbm',
+    url='https://github.com/mrdiamonddirt/local-llama-chrome-extension',
+    author='Rowan Wood',
+    author_email='mrdiamonddirt@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'flask-cors',
+        'rich',
+        'llama-cpp-python',
+        'typing-extensions',
+    ],
+    entry_points={
+        'console_scripts': [
+            'local-llama = Cli.cli:main',
+        ],
+    },
+)
