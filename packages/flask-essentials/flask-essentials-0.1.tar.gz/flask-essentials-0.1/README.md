c5c8130c1cd2baaa2d3de8b785abc4a40a59c81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:05:01 +0000

    Add package flask-essentials/0.1/flask-essentials-0.1.tar.gz

---
+++ packages/flask-essentials/flask-essentials-0.1.tar.gz/flask-essentials-0.1/README.md
@@ -0,0 +1,1 @@
+### flask-essentials docs
---
+++ packages/flask-essentials/flask-essentials-0.1.tar.gz/flask-essentials-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-essentials',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Jasir Fetai',
+    author_email='jasirfetai@gmail.com',
+    description='A short description of your package',
+    url='https://github.com/jasir99/flask-essentials.git',
+    license='MIT',
+)
