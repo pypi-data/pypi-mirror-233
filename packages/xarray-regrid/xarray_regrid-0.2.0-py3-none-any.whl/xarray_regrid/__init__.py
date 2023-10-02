4b555bc31f8ffdfe94c557dc67a7701abeaac0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:04:39 +0000

    Add package xarray-regrid/0.2.0/xarray_regrid-0.2.0-py3-none-any.whl

---
+++ packages/xarray-regrid/xarray_regrid-0.2.0-py3-none-any.whl/xarray_regrid/__init__.py
@@ -0,0 +1,8 @@
+from xarray_regrid.regrid import Regridder
+from xarray_regrid.utils import Grid, create_regridding_dataset
+
+__all__ = [
+    "Grid",
+    "Regridder",
+    "create_regridding_dataset",
+]
