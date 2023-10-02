0b136953045fdcbd78d2c93c3559daf961a057b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:06:04 +0000

    Add package argdantic/1.0.0/argdantic-1.0.0-py2.py3-none-any.whl

---
+++ packages/argdantic/argdantic-1.0.0-py2.py3-none-any.whl/argdantic/__init__.py
@@ -0,0 +1,10 @@
+"""Typed command line interfaces with argparse and pydantic
+"""
+from argdantic.core import ArgParser
+from argdantic.fields import ArgField
+
+__version__ = "1.0.0"
+__all__ = [
+    "ArgParser",
+    "ArgField",
+]
