18f49ce945d24ed352969b259e12ed000bcd26e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:04:53 +0000

    Add package encode-utils-cli/1.0.1a0/encode_utils_cli-1.0.1a0-py3-none-any.whl

---
+++ packages/encode-utils-cli/encode_utils_cli-1.0.1a0-py3-none-any.whl/encode_utils_cli/util/source.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from vapoursynth import VideoNode, core
+
+
+def source(video: Path) -> VideoNode:
+    """Load video source."""
+    return (
+        core.lsmas.LibavSMASHSource(source=video)
+        if video.suffix == ".mp4"
+        else core.lsmas.LWLibavSource(source=video)
+    )
