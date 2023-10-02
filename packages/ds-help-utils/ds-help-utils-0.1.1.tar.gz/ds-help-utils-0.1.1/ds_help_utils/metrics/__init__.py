8e8e2663c4fae40d56b117ab1c5b933237e13a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 15:04:46 +0000

    Add package ds-help-utils/0.1.1/ds-help-utils-0.1.1.tar.gz

---
+++ packages/ds-help-utils/ds-help-utils-0.1.1.tar.gz/ds-help-utils-0.1.1/ds_help_utils/metrics/__init__.py
@@ -0,0 +1,5 @@
+from ._ranking import (precision_at_k, 
+                       recall_at_k, 
+                       average_precision_at_k, 
+                       discounted_cumulative_gain_at_k, 
+                       normalized_discounted_cumulative_gain_at_k)
---
+++ packages/ds-help-utils/ds-help-utils-0.1.1.tar.gz/ds-help-utils-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ds-help-utils",
+    version="0.1.1",
+    description="A package to help do DS tasks more easily",
+    author="Dmitriy Emelianov",
+    author_email="asdqwer92@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.23.1",
+        "pandas>=2.0.0",
+        "scikit-learn>=1.1.1"
+    ]
+)
