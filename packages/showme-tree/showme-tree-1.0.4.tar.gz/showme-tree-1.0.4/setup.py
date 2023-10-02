5caa15c8820fe66f74c04a8990e54fc708801360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 14:07:20 +0000

    Add package showme-tree/1.0.4/showme-tree-1.0.4.tar.gz

---
+++ packages/showme-tree/showme-tree-1.0.4.tar.gz/showme-tree-1.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="showme-tree",
+    version="1.0.4",
+    author="Sophia Shovkovy",
+    packages=find_packages(),
+    url="https://github.com/sshovkov/showme-folder-structure",
+    entry_points={
+        "console_scripts": [
+            "showme-tree = showme_tree.main:main",
+        ],
+    },
+    install_requires=[
+        "gitignore-parser",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
