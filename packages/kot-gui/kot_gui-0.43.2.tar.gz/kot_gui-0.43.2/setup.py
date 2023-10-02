89d12f6da470b748f741ab4b1a3d5cf9f107a7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:05:56 +0000

    Add package kot-gui/0.43.2/kot_gui-0.43.2.tar.gz

---
+++ packages/kot-gui/kot_gui-0.43.2.tar.gz/kot_gui-0.43.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setup(
+    name="kot_gui",
+    version="0.43.2",
+    description="""This is GUI package for KOT""",
+    url="https://github.com/KOT-database/KOT",
+    author='Onur Atakan ULUSOY',
+    author_email='atadogan06@gmail.com',
+    license='MIT',
+    install_requires=install_requires,
+    python_requires=">= 3",
+    zip_safe=False,
+)
+
+
