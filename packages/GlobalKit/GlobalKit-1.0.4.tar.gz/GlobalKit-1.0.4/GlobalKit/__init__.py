ab3c9e330bcbb4bf2fb5b2de7fad78f069e619d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-02 03:04:55 +0000

    Add package GlobalKit/1.0.4/GlobalKit-1.0.4.tar.gz

---
+++ packages/GlobalKit/GlobalKit-1.0.4.tar.gz/GlobalKit-1.0.4/GlobalKit/__init__.py
@@ -0,0 +1,14 @@
+# Import functions
+from .functions import *
+
+# Import alphabets
+from .Alphabets.english import *
+from .Alphabets.spanish import *
+from .Alphabets.french import *
+from .Alphabets.german import *
+from .Alphabets.dutch import *
+from .Alphabets.italian import *
+from .Alphabets.portuguese import *
+from .Alphabets.russian import *
+from .Alphabets.ukrainian import *
+from .Alphabets.other import *
