498e88f1ff532ef4c7eae04cd16b8248d6b6f7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:21 +0000

    Add package goodgit/0.1.6/goodgit-0.1.6.tar.gz

---
+++ packages/goodgit/goodgit-0.1.6.tar.gz/goodgit-0.1.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='goodgit',
+    version='0.1.6',
+    packages=find_packages(),
+    description='Git; for humans',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Shubham Gupta • Saket Gupta',
+    author_email='shubhastro2@gmail.com',
+    install_requires=[
+        'requests',
+        'GitPython',
+        'questionary',
+        'rich',
+        'click',  # Add click here
+    ],
+    entry_points={
+        'console_scripts': [
+            'gg = goodgit.goodgit:cli',
+        ],
+    },
+)
