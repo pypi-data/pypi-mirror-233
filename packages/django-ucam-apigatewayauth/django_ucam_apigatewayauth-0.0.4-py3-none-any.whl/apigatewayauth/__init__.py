5ec1c1ab5a4b2d9d2a674a23c1694c80f0f2a005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:04:44 +0000

    Add package django-ucam-apigatewayauth/0.0.4/django_ucam_apigatewayauth-0.0.4-py3-none-any.whl

---
+++ packages/django-ucam-apigatewayauth/django_ucam_apigatewayauth-0.0.4-py3-none-any.whl/apigatewayauth/__init__.py
@@ -0,0 +1,4 @@
+from .api_gateway_auth import APIGatewayAuthentication  # noqa: F401
+from .api_gateway_auth import APIGatewayAuthenticationDetails  # noqa: F401
+
+default_app_config = "apigatewayauth.apps.APIGatewayAuthConfig"
