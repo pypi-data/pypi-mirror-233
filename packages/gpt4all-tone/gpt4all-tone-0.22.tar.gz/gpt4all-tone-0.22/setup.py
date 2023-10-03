cc89cd4a340a5634c789133688df678e41af8f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:06:40 +0000

    Add package gpt4all-tone/0.22/gpt4all-tone-0.22.tar.gz

---
+++ packages/gpt4all-tone/gpt4all-tone-0.22.tar.gz/gpt4all-tone-0.22/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gpt4all-tone',
+    version='0.22',
+    packages=find_packages(),
+    install_requires=[
+        "argparse",
+        "gpt4all",
+    ],
+    author='Evgenii Evstafev',
+    author_email='chigwel@gmail.com',
+    description='A comprehensive tool for sentiment analysis and tone review, empowered by GPT4ALL.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chigwell/gpt4all-tone',
+    entry_points={
+        'console_scripts': [
+            'gpt4all_tone = gpt4all_tone.gpt4all_tone:main',
+        ],
+    },
+)
