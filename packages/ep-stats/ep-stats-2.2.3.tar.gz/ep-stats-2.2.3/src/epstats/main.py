f1a4a4d8620261f4363c55a2ed09b99d1ffa5974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:05:30 +0000

    Add package ep-stats/2.2.3/ep-stats-2.2.3.tar.gz

---
+++ packages/ep-stats/ep-stats-2.2.3.tar.gz/ep-stats-2.2.3/src/epstats/main.py
@@ -0,0 +1,53 @@
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from pydantic import BaseSettings
+
+from .toolkit.testing import TestDaoFactory, TestData
+from .server import get_api, serve, ApiSettings
+from prometheus_client import make_asgi_app
+
+
+class Settings(BaseSettings):
+    api: ApiSettings = ApiSettings()
+
+    evaluation_pool_size: int = 5
+
+
+settings = Settings()
+
+
+def get_dao_factory():
+    return TestDaoFactory(TestData())
+
+
+def get_dao():
+    try:
+        dao = get_dao_factory().get_dao()
+        yield dao
+    finally:
+        dao.close()
+
+
+def get_executor_pool():
+    try:
+        evaluation_pool_size = settings.evaluation_pool_size
+        evaluation_pool = ThreadPoolExecutor(max_workers=evaluation_pool_size)
+        yield evaluation_pool
+    finally:
+        pass
+
+
+api = get_api(settings.api, get_dao, get_executor_pool)
+metrics_app = make_asgi_app()
+api.mount("/metrics", metrics_app)
+
+
+def main():
+    from .config import config
+
+    logging.config.dictConfig(config["logging"])
+    serve("epstats:api", settings.api, config["logging"])
+
+
+if __name__ == "__main__":
+    main()
