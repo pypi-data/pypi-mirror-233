f0a86c890f6a521d795f24c42f57d476f3d71809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:04:32 +0000

    Add package PyPushBullet/0.2.6/PyPushBullet-0.2.6.tar.gz

---
+++ packages/PyPushBullet/PyPushBullet-0.2.6.tar.gz/PyPushBullet-0.2.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from pathlib import Path
+
+setup(
+    name='PyPushBullet',
+    version='0.2.6',
+    description='A simple library to push notification using PushBullet. You need a pushbullet API key for this',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    author='Balla Vamsi Srinivas',
+    author_email='ballavamsisrinivas@gmail.com',
+    url='https://github.com/ballavamsi/PyPushBullet',
+    packages=['PyPushBullet'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
