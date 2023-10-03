c99c43dcded2f37475a5bbaed1ade0d665fde2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:06 +0000

    Add package geometriccalculatelib/1.0.0/geometriccalculatelib-1.0.0.tar.gz

---
+++ packages/geometriccalculatelib/geometriccalculatelib-1.0.0.tar.gz/geometriccalculatelib-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="geometriccalculatelib",
+    version="1.0.0",
+    description="A library for geometric calculations",
+    author="Alexandr M",
+    author_email="alex.mmdvdvv@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
