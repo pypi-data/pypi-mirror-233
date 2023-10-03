2e1f29a9199ce093ebce5c75f5082701e26ddea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:52 +0000

    Add package orchestrator-core/1.3.2/orchestrator_core-1.3.2-py3-none-any.whl

---
+++ packages/orchestrator-core/orchestrator_core-1.3.2-py3-none-any.whl/orchestrator/db/sorting/product.py
@@ -0,0 +1,11 @@
+from sqlalchemy.inspection import inspect
+
+from orchestrator.db import ProductTable
+from orchestrator.db.sorting.sorting import generic_column_sort, generic_sort
+from orchestrator.utils.helpers import to_camel
+
+PRODUCT_SORT_FUNCTIONS_BY_COLUMN = {
+    to_camel(key): generic_column_sort(value) for [key, value] in inspect(ProductTable).columns.items()
+}
+
+sort_products = generic_sort(PRODUCT_SORT_FUNCTIONS_BY_COLUMN)
