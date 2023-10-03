53639c7ae9feb243304c3890a7cdde487f056cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:50 +0000

    Add package ifgen/2.4.0/ifgen-2.4.0-py3-none-any.whl

---
+++ packages/ifgen/ifgen-2.4.0-py3-none-any.whl/ifgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=50538057667f048e4e1c2e1232e77e1b
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "An interface generator for distributed computing."
+PKG_NAME = "ifgen"
+VERSION = "2.4.0"
---
+++ packages/ifgen/ifgen-2.4.0-py3-none-any.whl/ifgen/commands/gen.py
@@ -0,0 +1,43 @@
+"""
+An entry-point for the 'gen' command.
+"""
+
+# built-in
+from argparse import ArgumentParser as _ArgumentParser
+from argparse import Namespace as _Namespace
+
+# third-party
+from vcorelib.args import CommandFunction as _CommandFunction
+from vcorelib.paths import normalize
+
+# internal
+from ifgen import PKG_NAME
+from ifgen.config import load
+from ifgen.generation import generate
+from ifgen.paths import combine_if_not_absolute
+
+DEFAULT_CONFIG = f"{PKG_NAME}.yaml"
+
+
+def gen_cmd(args: _Namespace) -> int:
+    """Execute the gen command."""
+
+    root = normalize(args.root)
+
+    generate(
+        root.resolve(), load(combine_if_not_absolute(root, DEFAULT_CONFIG))
+    )
+
+    return 0
+
+
+def add_gen_cmd(parser: _ArgumentParser) -> _CommandFunction:
+    """Add gen-command arguments to its parser."""
+
+    parser.add_argument(
+        "-r",
+        "--root",
+        default=".",
+        help="root directory to use for relative paths",
+    )
+    return gen_cmd
