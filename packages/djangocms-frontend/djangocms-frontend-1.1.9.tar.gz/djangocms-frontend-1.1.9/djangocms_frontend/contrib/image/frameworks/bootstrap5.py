473fb9803a16370a9d2538438a3676609a0804a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:07:34 +0000

    Add package djangocms-frontend/1.1.9/djangocms-frontend-1.1.9.tar.gz

---
+++ packages/djangocms-frontend/djangocms-frontend-1.1.9.tar.gz/djangocms-frontend-1.1.9/djangocms_frontend/contrib/image/frameworks/bootstrap5.py
@@ -0,0 +1,24 @@
+class ImageRenderMixin:
+    def render(self, context, instance, placeholder):
+        # assign link to a context variable to be performant
+        context["picture_link"] = instance.get_link()
+        context["picture_size"] = instance.get_size(
+            width=context.get("width", 0),
+            height=context.get("height", 0),
+        )
+        context["img_srcset_data"] = instance.img_srcset_data
+        if instance.alignment:
+            instance.add_classes(f"align-{instance.alignment}")
+        if instance.picture_fluid:
+            instance.add_classes("img-fluid")
+        if instance.picture_rounded:
+            instance.add_classes("rounded")
+        if instance.picture_thumbnail:
+            instance.add_classes("img-thumbnail")
+        if instance.parent and instance.parent.plugin_type == "CardPlugin":
+            instance.add_classes(
+                "card-img-top" if instance.position == 0 else "card-img-bottom"
+            )
+        elif instance.parent and instance.parent.plugin_type == "FigurePlugin":
+            instance.add_classes("figure-img")
+        return super().render(context, instance, placeholder)
