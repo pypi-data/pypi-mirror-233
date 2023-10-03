0ffa611ee157a38fe7ae06dc2d95dbd074a3efb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:52 +0000

    Add package triplea/0.0.3/triplea-0.0.3-py3-none-any.whl

---
+++ packages/triplea/triplea-0.0.3-py3-none-any.whl/triplea/cli/pipeline.py
@@ -0,0 +1,21 @@
+import click
+from triplea.service.repository.pipeline_flag import (
+    go_extract_triple,
+    go_affiliation_mining,
+    go_extract_topic,
+)
+from triplea.cli.main import cli
+
+
+@cli.command("pipeline", help="Run Custom PipeLine in arepo.")
+@click.option("--name", "-n", "name", help="Name of pipeline")
+def pipeline(name: str):
+    if name == "FlagExtractKG":
+        go_extract_triple()
+    elif name == "FlagExtractTopic":
+        go_extract_topic()
+    elif name == "FlagAffiliationMining":
+        go_affiliation_mining()
+
+    else:
+        raise NotImplementedError
