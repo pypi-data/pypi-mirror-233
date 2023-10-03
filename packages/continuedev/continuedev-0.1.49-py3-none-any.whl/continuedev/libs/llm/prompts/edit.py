615fef7709664ffd595365643f0ff8bc3a7bb760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:07:39 +0000

    Add package continuedev/0.1.49/continuedev-0.1.49-py3-none-any.whl

---
+++ packages/continuedev/continuedev-0.1.49-py3-none-any.whl/continuedev/libs/llm/prompts/edit.py
@@ -0,0 +1,27 @@
+from textwrap import dedent
+
+simplified_edit_prompt = dedent(
+    """\
+            Consider the following code:
+            ```
+            {{{code_to_edit}}}
+            ```
+            Edit the code to perfectly satisfy the following user request:
+            {{{user_input}}}
+            Output nothing except for the code. No code block, no English explanation, no start/end tags."""
+)
+
+simplest_edit_prompt = dedent(
+    """\
+            Here is the code before editing:
+            ```
+            {{{code_to_edit}}}
+            ```
+
+            Here is the edit requested:
+            "{{{user_input}}}"
+            
+            Here is the code after editing:"""
+)
+
+codellama_infill_edit_prompt = "{{file_prefix}}<FILL>{{file_suffix}}"
