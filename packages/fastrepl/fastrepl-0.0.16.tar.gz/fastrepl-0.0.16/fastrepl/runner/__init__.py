d1a79de1b95fee3180b6dd7f3e40827a6f24c805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:04:32 +0000

    Add package fastrepl/0.0.16/fastrepl-0.0.16.tar.gz

---
+++ packages/fastrepl/fastrepl-0.0.16.tar.gz/fastrepl-0.0.16/fastrepl/runner/__init__.py
@@ -0,0 +1,66 @@
+from typing import Union, Callable, overload
+
+from fastrepl.dataset import Dataset
+
+import fastrepl
+from fastrepl.runner.evaluator import LocalEvaluatorRunner, RemoteEvaluatorRunner
+from fastrepl.runner.generator import LocalGeneratorRunner, RemoteGeneratorRunner
+from fastrepl.runner.others import LocalCustomRunner
+
+
+@overload
+def local_runner(*, fn: Callable) -> LocalCustomRunner:
+    ...
+
+
+@overload
+def local_runner(*, generator: fastrepl.Generator) -> LocalGeneratorRunner:
+    ...
+
+
+@overload
+def local_runner(
+    *, evaluator: fastrepl.Evaluator, dataset: Dataset, output_feature="result"
+) -> LocalEvaluatorRunner:
+    ...
+
+
+def local_runner(
+    **kwargs,
+) -> Union[LocalGeneratorRunner, LocalEvaluatorRunner, LocalCustomRunner]:
+    if "generator" in kwargs:
+        return LocalGeneratorRunner(generator=kwargs["generator"])
+
+    if "evaluator" in kwargs:
+        return LocalEvaluatorRunner(
+            evaluator=kwargs["evaluator"],
+            dataset=kwargs["dataset"],
+            output_feature=kwargs.get("output_feature", "result"),
+        )
+
+    if "fn" in kwargs:
+        return LocalCustomRunner(fn=kwargs["fn"])
+
+    raise ValueError
+
+
+@overload
+def remote_runner(*, generator: fastrepl.Generator) -> RemoteGeneratorRunner:
+    return RemoteGeneratorRunner(generator)
+
+
+@overload
+def remote_runner(
+    *, evaluator: fastrepl.Evaluator, dataset: Dataset, output_feature="result"
+) -> RemoteEvaluatorRunner:
+    return RemoteEvaluatorRunner(evaluator, dataset, output_feature)
+
+
+def remote_runner(**kwargs) -> Union[RemoteEvaluatorRunner, RemoteGeneratorRunner]:
+    if "generator" in kwargs:
+        return RemoteGeneratorRunner(generator=kwargs["generator"])
+
+    return RemoteEvaluatorRunner(**kwargs)
+
+
+__all__ = ["local_runner", "remote_runner"]
