8022153ed0d19502d8784cc4b69692f777889820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:07:52 +0000

    Add package k-lights-interface/0.1.21/k_lights_interface-0.1.21.tar.gz

---
+++ packages/k-lights-interface/k_lights_interface-0.1.21.tar.gz/k_lights_interface-0.1.21/examples/epos600_driver_test.py
@@ -0,0 +1,27 @@
+from k_lights_interface.k_serial_manager import KSerialManager
+from k_lights_interface.k_logging import set_log_level, logging
+import k_lights_interface.proto_protocol as kprot
+from time import sleep
+
+def test_set_channels():
+    dev_manager = KSerialManager()
+    all_connected_devices = dev_manager.connect_to_all()
+    [print(dev) for dev in all_connected_devices]
+    assert(len(all_connected_devices) > 0)
+    device = all_connected_devices[0]
+    print(f"Chosen device for tests: {device}")
+    ret = device.set_emitter_output_type(kprot.EmitterOutputType.DAC_OUTPUT)
+    if not ret:
+        print("Couldnt set emitter output type")
+
+    for i in range(1):
+        test_list = [0]*6
+        test_list[i] = 100
+        device.set_rgbacl_emitter_channels_without_compensation_unsafe(test_list)
+        print(f"THIS IS CHANNEL {i}")
+        sleep(10)
+    #sleep(0.4) 
+    device.set_rgbacl_emitter_channels_without_compensation_unsafe([0,0,0,0,0,0])
+
+if __name__ == "__main__":
+    test_set_channels()
