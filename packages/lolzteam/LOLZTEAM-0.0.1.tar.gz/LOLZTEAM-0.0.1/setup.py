d3fa38e6cf3db08184a64361bf4f94edb252ead1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:06:52 +0000

    Add package lolzteam/0.0.1/LOLZTEAM-0.0.1.tar.gz

---
+++ packages/lolzteam/LOLZTEAM-0.0.1.tar.gz/LOLZTEAM-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+long_description = "Placeholder for lib transfer"
+
+requirements = []
+
+setuptools.setup(
+    name="LOLZTEAM",
+    version="0.0.1",
+    author="AS7RID",
+    author_email="as7ridwork@gmail.com",
+    description="Placeholder for lib transfer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/AS7RIDENIED/Lolzteam_Python_Api",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=["Programming Language :: Python :: 3.11"],
+    python_requires='>=3.9'
+)
