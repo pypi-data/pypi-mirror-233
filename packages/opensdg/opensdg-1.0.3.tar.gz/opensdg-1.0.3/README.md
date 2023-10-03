942372c8d212c3ccb651b9279c2d5918689433b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:07:16 +0000

    Add package opensdg/1.0.3/opensdg-1.0.3.tar.gz

---
+++ packages/opensdg/opensdg-1.0.3.tar.gz/opensdg-1.0.3/README.md
@@ -0,0 +1,1 @@
+Python ctypes bindings for opensdg library.
---
+++ packages/opensdg/opensdg-1.0.3.tar.gz/opensdg-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='opensdg',
+    version='1.0.3',
+    author='Josip Janzic',
+    author_email='hi@josip.dev',
+    description='Python binding for opensdg library',
+    url='https://github.com/janza/opensdg-py',
+    packages=['opensdg'],
+    install_requires=[],
+)
