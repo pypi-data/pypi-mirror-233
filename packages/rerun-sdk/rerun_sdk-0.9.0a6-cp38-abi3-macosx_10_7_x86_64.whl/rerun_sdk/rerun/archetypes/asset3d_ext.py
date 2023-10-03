f75906d6156ca4cb9a64253759607312d630618c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:11 +0000

    Add package rerun-sdk/0.9.0a6/rerun_sdk-0.9.0a6-cp38-abi3-macosx_10_7_x86_64.whl

---
+++ packages/rerun-sdk/rerun_sdk-0.9.0a6-cp38-abi3-macosx_10_7_x86_64.whl/rerun_sdk/rerun/archetypes/asset3d_ext.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from ..error_utils import catch_and_log_exceptions
+
+if TYPE_CHECKING:
+    from ..components import MediaType
+    from . import Asset3D
+
+
+def guess_media_type(path: str) -> MediaType | None:
+    from pathlib import Path
+
+    from ..components import MediaType
+
+    ext = Path(path).suffix
+    if ext == ".glb":
+        return MediaType.GLB
+    elif ext == ".gltf":
+        return MediaType.GLTF
+    elif ext == ".obj":
+        return MediaType.OBJ
+    else:
+        return None
+
+
+class Asset3DExt:
+    @staticmethod
+    def from_file(path: str) -> Asset3D:
+        """
+        Creates a new [`Asset3D`] from the file contents at `path`.
+
+        The [`MediaType`] will be guessed from the file extension.
+
+        If no [`MediaType`] can be guessed at the moment, the Rerun Viewer will try to guess one
+        from the data at render-time. If it can't, rendering will fail with an error.
+        """
+        from . import Asset3D
+
+        with catch_and_log_exceptions(context="Asset3D.from_file"):
+            with open(path, "rb") as file:
+                return Asset3D.from_bytes(file.read(), guess_media_type(path))
+        return Asset3D._clear()
+
+    @staticmethod
+    def from_bytes(blob: bytes, media_type: MediaType | None) -> Asset3D:
+        """
+        Creates a new [`Asset3D`] from the given `bytes`.
+
+        If no [`MediaType`] is specified, the Rerun Viewer will try to guess one from the data
+        at render-time. If it can't, rendering will fail with an error.
+        """
+        from . import Asset3D
+
+        # TODO(cmc): we could try and guess using magic bytes here, like rust does.
+        with catch_and_log_exceptions(context="Asset3D.from_file"):
+            return Asset3D(blob=blob, media_type=media_type)
+        return Asset3D._clear()
