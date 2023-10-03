ff0109da4d567b4a9821566fa541455d3789f437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:48 +0000

    Add package fastapi-microservice-helper/0.0.7/fastapi_microservice_helper-0.0.7-py3-none-any.whl

---
+++ packages/fastapi-microservice-helper/fastapi_microservice_helper-0.0.7-py3-none-any.whl/fastapi_microservice_helper/models/generation_detail.py
@@ -0,0 +1,34 @@
+import typing
+from dataclasses import field, dataclass
+
+
+@dataclass
+class GenerationImportModuleDetail:
+    module: str
+    type: str
+
+
+@dataclass
+class GenerationPropertyDetail:
+    name: str
+    type: typing.Optional[str] = None
+    default_value: typing.Optional[any] = None
+    default_factory: typing.Optional[any] = None
+    allow_none: bool = False
+
+
+@dataclass
+class GenerationMethodDetail:
+    name: str
+    params: list[GenerationPropertyDetail] = field(default_factory=list)
+    body: str = None
+    response: str = None
+
+
+@dataclass
+class GenerationClassDetail:
+    class_name: str
+    parent_name: str = None
+    methods: list[GenerationMethodDetail] = field(default_factory=list)
+    properties: list[GenerationPropertyDetail] = field(default_factory=list)
+    is_dataclass: bool = False
