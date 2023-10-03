34a958d62811bc42ffc488a760e2f967c68e55fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:05:49 +0000

    Add package jointml-client/0.1.97/jointml_client-0.1.97-py3-none-any.whl

---
+++ packages/jointml-client/jointml_client-0.1.97-py3-none-any.whl/joint_ml/__init__.py
@@ -0,0 +1,20 @@
+"""Base path is the client_package, therefore set imports accordingly."""
+
+from joint_ml._base_client import Client
+from joint_ml._metric import Metric
+from joint_ml._client_abstract_methods import load_model, get_dataset, train, test
+from joint_ml._utils import save_output, save_csv_in_zip, save_weights_in_zip, save_metric_in_zip
+
+
+__all__ = [
+    "Client",
+    "Metric",
+    "load_model",
+    "get_dataset",
+    "train",
+    "test",
+    "save_metric_in_zip",
+    "save_csv_in_zip",
+    "save_weights_in_zip",
+    "save_output",
+]
