96e6a9da05c9ef263d0ece1c0f380b702b620789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:06 +0000

    Add package podme-api/0.1.4/podme_api-0.1.4-py3-none-any.whl

---
+++ packages/podme-api/podme_api-0.1.4-py3-none-any.whl/podme_api/__init__.py
@@ -0,0 +1,7 @@
+"""Init file for podme_api."""
+
+import pkg_resources  # part of setuptools
+
+__version__ = pkg_resources.get_distribution("podme_api").version
+
+from podme_api.client import PodMeClient, PodMeSchibstedClient
