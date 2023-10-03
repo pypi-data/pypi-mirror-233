27f1978c96dbeae9133fd1a5b6e666787ecdd672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:08:02 +0000

    Add package plone.rest/3.0.2/plone.rest-3.0.2.tar.gz

---
+++ packages/plone.rest/plone.rest-3.0.2.tar.gz/plone.rest-3.0.2/setup.py
@@ -0,0 +1,78 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+version = "3.0.2"
+
+long_description = read("README.rst") + "\n\n" + read("CHANGES.rst") + "\n\n"
+
+
+setup(
+    name="plone.rest",
+    version=version,
+    description="Plone support for HTTP verbs.",
+    long_description=long_description,
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope2",
+        "Framework :: Zope :: 4",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="rest http",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.rest/",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["plone"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=2.7",
+    extras_require=dict(
+        test=[
+            "plone.app.testing[robot]>=4.2.2",
+            "plone.app.robotframework",
+            "plone.dexterity",
+            "Products.CMFCore",
+            "requests",
+        ]
+    ),
+    install_requires=[
+        "setuptools",
+        "collective.monkeypatcher",
+        "zope.component",
+        "zope.interface",
+        "zope.publisher",
+        "zope.traversing",
+        "Products.CMFCore",
+        "Zope2",
+        "six",
+    ],
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
