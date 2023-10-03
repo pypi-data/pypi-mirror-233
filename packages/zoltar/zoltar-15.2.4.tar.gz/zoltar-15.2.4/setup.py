9d63dc20b5bc88a6845c8a5a39a983c075400f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:22 +0000

    Add package zoltar/15.2.4/zoltar-15.2.4.tar.gz

---
+++ packages/zoltar/zoltar-15.2.4.tar.gz/zoltar-15.2.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='zoltar',
+    version='15.2.4',
+    author='whybee',
+    author_email='whybee@wearehackerone.com',
+    description='Security research package',
+    long_description='This package is meant for security research purposes and does not contain any useful code.',
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+    ]
+    )
