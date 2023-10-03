11c6f30dec3b1749c2040bf25f3054a29304ecc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:29 +0000

    Add package pyVLMP/1.0.33/pyVLMP-1.0.33-py3-none-any.whl

---
+++ packages/pyVLMP/pyVLMP-1.0.33-py3-none-any.whl/VLMP/utils/selections/__init__.py
@@ -0,0 +1,92 @@
+import logging
+import copy
+
+def getSelections(models,selectionsList,**param):
+
+    logger = logging.getLogger("VLMP")
+
+    #Check params
+    for p in param.keys():
+        if p not in selectionsList:
+            continue
+        else:
+            for k in param[p].keys():
+                if k not in ["models","expression"]:
+                    logger.error(f"[getSelections] Parameter {k} not recognized for selection {p},"
+                                  " available parameters are: models, expression")
+                    raise Exception("Unknown parameter")
+
+    selections = {}
+
+    for sel in [s for s in param.keys() if s in selectionsList]:
+        selections[sel] = []
+
+        if "models" in param[sel]:
+            selectedModels = [ m for m in models if m.getName() in param[sel]["models"]]
+        else:
+            selectedModels = models
+
+        for mdl in selectedModels:
+            if "expression" in param[sel]:
+                mdlSelIds = mdl.getSelection(**param[sel]["expression"])
+            else:
+                mdlSelIds = mdl.getSelection()
+
+            offSet    = mdl.getIdOffset()
+            mdlSelIds = [i+offSet for i in mdlSelIds]
+
+            selections[sel] += mdlSelIds
+        selections[sel] = list(set(selections[sel]))
+
+    #Selection is a set of global ids
+    return copy.deepcopy(selections)
+
+def splitStateAccordingStructure(state,structure):
+
+    logger = logging.getLogger("VLMP")
+
+    #Check state and structure have the same length
+    if len(state) != len(structure):
+        logger.error("[splitStateAccordingStructure] State and structure have different lengths")
+        raise Exception("State and structure have different lengths")
+
+    # Structure has to have the following format:
+    # [A,A,A,...,B,B,B,...,C,C,C,...,...]
+    # Each letter represents a different structure
+    # Check structure is correct
+    appearedStructures = []
+    for s in structure:
+        if s not in appearedStructures:
+            appearedStructures.append(s)
+        elif s != appearedStructures[-1]:
+            logger.error("[splitStateAccordingStructure] Structure is not correct")
+            raise Exception("Structure is not correct")
+        else:
+            continue
+
+    # Split pos according the different models
+    splittedState = []
+
+    currentStruct      = structure[0]
+    currentStructState = []
+    for i in range(len(structure)):
+        if structure[i] == currentStruct:
+            currentStructState.append(state[i])
+        else:
+            splittedState.append(currentStructState)
+            currentStruct      = structure[i]
+            currentStructState = [state[i]]
+
+    splittedState.append(currentStructState)
+
+    # At this point, splittedState is a list of lists,
+    # each list contains the state of a model
+    #                       struct[0]          struct[1]   ...
+    # splittedState = [[state1_0,state2_0,...],[state1_0,state2_0,...],...]
+    # It is ensured that the order is kept.
+    # splittedState is the state list splitted according the structure
+
+    return splittedState
+
+
+
