6df2edba99e365077e6ef2f100a544bd0b37fdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:32 +0000

    Add package nibe/2.4.0/nibe-2.4.0-py3-none-any.whl

---
+++ packages/nibe/nibe-2.4.0-py3-none-any.whl/nibe/exceptions.py
@@ -0,0 +1,79 @@
+from exceptiongroup import ExceptionGroup
+
+
+class NibeException(Exception):
+    pass
+
+
+class AddressInUseException(NibeException):
+    pass
+
+
+class CoilNotFoundException(NibeException):
+    pass
+
+
+class DecodeException(NibeException):
+    pass
+
+
+class ValidationError(NibeException):
+    pass
+
+
+class NoMappingException(ValidationError):
+    pass
+
+
+class EncodeException(NibeException):
+    pass
+
+
+class WriteException(NibeException):
+    pass
+
+
+class WriteIOException(WriteException):
+    pass
+
+
+class CoilWriteSendException(WriteIOException):
+    pass
+
+
+class WriteTimeoutException(WriteIOException):
+    pass
+
+
+class ReadException(NibeException):
+    pass
+
+
+class ReadIOException(ReadException):
+    pass
+
+
+class ReadExceptionGroup(ExceptionGroup, ReadException):
+    def __str__(self) -> str:
+        messages = ", ".join(str(exception) for exception in self.exceptions)
+        return f"{self.message} ({messages})"
+
+
+class ReadSendException(ReadIOException):
+    pass
+
+
+class ReadTimeoutException(ReadIOException):
+    pass
+
+
+class ProductInfoReadTimeoutException(ReadIOException):
+    pass
+
+
+class ModelIdentificationFailed(NibeException):
+    pass
+
+
+class ModbusUrlException(NibeException, ValueError):
+    pass
