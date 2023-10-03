22e15b9446f66ff167d4c62cfa926bb3ad6b90df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:26 +0000

    Add package dagster-webserver/1.5.0/dagster_webserver-1.5.0-py3-none-any.whl

---
+++ packages/dagster-webserver/dagster_webserver-1.5.0-py3-none-any.whl/dagster_webserver/webapp/build/_next/static/XFzXQoNQjTTRKtW9E6Nli/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6dd23449a452d234.js"],"/_error":["static/chunks/pages/_error-d9106668104f511c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ packages/dagster-webserver/dagster_webserver-1.5.0-py3-none-any.whl/dagster_webserver/webapp/build/_next/static/chunks/pages/_error-d9106668104f511c.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{50341:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(61360)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=50341)}),_N_E=n.O()}]);
+//# sourceMappingURL=_error-d9106668104f511c.js.map
