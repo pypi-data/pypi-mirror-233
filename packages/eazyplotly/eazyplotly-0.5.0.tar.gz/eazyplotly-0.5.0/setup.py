9e502aab271037650c9c4a930367d5cb3d7019ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:14 +0000

    Add package eazyplotly/0.5.0/eazyplotly-0.5.0.tar.gz

---
+++ packages/eazyplotly/eazyplotly-0.5.0.tar.gz/eazyplotly-0.5.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eazyplotly",
+    version="0.5.0",
+    description="Package to make the use of Plotly library easier",
+    author="Saeid Alizadeh",
+    author_email="saeidalz96@gmail.com",
+    url="https://github.com/saeidalz13/eazyplotly",
+    packages=find_packages(),
+    install_requires=[
+        "plotly",
+        "pandas"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization"
+    ]
+)
