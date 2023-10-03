97021519af9702ea8bc78f2fcf6e2f5a33a34578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:53 +0000

    Add package elementary-data/0.11.0/elementary_data-0.11.0-py3-none-any.whl

---
+++ packages/elementary-data/elementary_data-0.11.0-py3-none-any.whl/elementary/monitor/dbt_project/macros/get_models.sql
@@ -0,0 +1,30 @@
+{% macro get_models(exclude_elementary=false) %}
+    {% set dbt_models_relation = ref('elementary', 'dbt_models') %}
+    {%- if elementary.relation_exists(dbt_models_relation) -%}
+        --{# TODO: should we group by #}
+        {% set get_models_query %}
+              with dbt_artifacts_models as (
+                select
+                  name,
+                  unique_id,
+                  database_name,
+                  schema_name,
+                  case when alias is not null then alias
+                  else name end as table_name,
+                  owner as owners,
+                  tags,
+                  package_name,
+                  description,
+                  original_path as full_path
+                from {{ dbt_models_relation }}
+                {% if exclude_elementary %}
+                  where package_name != 'elementary'
+                {% endif %}
+              )
+
+             select * from dbt_artifacts_models
+        {% endset %}
+        {% set models_agate = run_query(get_models_query) %}
+        {% do return(elementary.agate_to_dicts(models_agate)) %}
+    {%- endif -%}
+{% endmacro %}
