875415d7b4678326fd268e0bbc30132465095080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:07:21 +0000

    Add package lamindb_setup/0.54.0/lamindb_setup-0.54.0-py2.py3-none-any.whl

---
+++ packages/lamindb_setup/lamindb_setup-0.54.0-py2.py3-none-any.whl/lamindb_setup/_silence_loggers.py
@@ -0,0 +1,26 @@
+import logging
+
+
+# from https://github.com/boto/boto3/blob/8c6e641bed8130a9d8cb4d97b4acbe7aa0d0657a/boto3/__init__.py#L37  # noqa
+def set_stream_logger(name, level):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    logger.addHandler(handler)
+
+
+def silence_loggers():
+    # this gets logged 6 times
+    set_stream_logger(name="botocore.credentials", level=logging.WARNING)
+    set_stream_logger(name="botocore.hooks", level=logging.WARNING)
+    set_stream_logger(name="botocore.utils", level=logging.WARNING)
+    set_stream_logger(name="botocore.auth", level=logging.WARNING)
+    set_stream_logger(name="botocore.endpoint", level=logging.WARNING)
+    try:
+        import aibotocore  # noqa
+
+        # the 7th logging message of credentials came from aiobotocore
+        set_stream_logger(name="aiobotocore.credentials", level=logging.WARNING)
+    except ImportError:
+        pass
