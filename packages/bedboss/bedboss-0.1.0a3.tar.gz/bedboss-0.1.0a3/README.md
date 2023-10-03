ad0b36132c38bb05766f1dbbafe172367a57e873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:06:40 +0000

    Add package bedboss/0.1.0a3/bedboss-0.1.0a3.tar.gz

---
+++ packages/bedboss/bedboss-0.1.0a3.tar.gz/bedboss-0.1.0a3/README.md
@@ -0,0 +1,33 @@
+# bedboss
+
+---
+![Run pytests](https://github.com/bedbase/bedboss/workflows/Run%20instalation%20test/badge.svg)
+[![docs-badge](https://readthedocs.org/projects/bedboss/badge/?version=latest)](https://bedboss.databio.org/en/latest/)
+[![pypi-badge](https://img.shields.io/pypi/v/bedboss)](https://pypi.org/project/bedboss)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+bedboss is a command-line pipeline that standardizes and calculates statistics for genomic interval data, and enters the results into a BEDbase database. It has 3 components: 1) bedmaker (`bedboss make`); 2) bedqc (`bedboss qc`); and 3) bedstat `bedboss stat`. You may run all 3 pipelines separately, together (`bedbase all`).
+
+## 1) bedmaker
+
+Converts supported file types into BED and bigBed format. Currently supported formats:
+   - bedGraph
+   - bigBed
+   - bigWig
+   - wig
+
+## 2) bedqc
+
+Assess QC of BED files and flag potential problems for further evaluation so you can determine whether they should be included in downstream analysis. 
+Currently, it flags BED files that are larger than 2 GB, have over 5 milliom regions, or have mean region width less than 10 bp.
+These thresholds can be changed with pipeline arguments.
+
+## bedstat
+
+Calculates statistics about BED files.
+
+# Documentation
+
+Detailed information about each pipeline can be found in the [bedboss Readme](./docs/README.md).
+
+For the specific bedbase.org instance, see instructions in the bedbase.org repo.
---
+++ packages/bedboss/bedboss-0.1.0a3.tar.gz/bedboss-0.1.0a3/bedboss/utils.py
@@ -0,0 +1,82 @@
+import os
+import logging
+import requests
+import urllib
+from bbconf import BedBaseConf
+from typing import NoReturn
+
+
+_LOGGER = logging.getLogger("bedboss")
+
+
+def extract_file_name(file_path: str) -> str:
+    """
+    Extraction file name from file path
+
+    :param file_path: full file path
+    :return: file name without extension
+    """
+    file_name = os.path.basename(file_path)
+    file_name = file_name.split(".")[0]
+    return file_name
+
+
+def standardize_genome_name(input_genome: str) -> str:
+    """
+    Standardizing user provided genome
+
+    :param input_genome: standardize user provided genome, so bedboss know what genome
+    we should use
+    :return: genome name string
+    """
+    input_genome = input_genome.strip()
+    # TODO: we have to add more genome options and preprocessing of the string
+    if input_genome == "hg38" or input_genome == "GRCh38":
+        return "hg38"
+    elif input_genome == "hg19" or input_genome == "GRCh37":
+        return "hg19"
+    elif input_genome == "mm10":
+        return "mm10"
+    # else:
+    #     raise GenomeException("Incorrect genome assembly was provided")
+    else:
+        return input_genome
+
+
+def download_file(url: str, path: str) -> NoReturn:
+    """
+    Download file from the url to specific location
+
+    :param url: URL of the file
+    :param path: Local path with filename
+    :return: NoReturn
+    """
+    _LOGGER.info(f"Downloading remote file: {url}")
+    _LOGGER.info(f"Local path: {path}")
+    try:
+        urllib.request.urlretrieve(url, path)
+        _LOGGER.info(f"File downloaded successfully!")
+    except Exception as e:
+        _LOGGER.error(f"File download failed.")
+        raise e
+
+
+def check_db_connection(bedbase_config: str) -> bool:
+    """
+    Check if the database connection is working
+
+    :param bedbase_config: path to the bedbase config file
+    :return: True if connection is successful, False otherwise
+    """
+    _LOGGER.info(f"Checking database connection...")
+    if not os.path.exists(bedbase_config):
+        raise FileNotFoundError(f"Bedbase config file {bedbase_config} was not found.")
+    else:
+        _LOGGER.info(f"Bedbase config file {bedbase_config} was found.")
+    try:
+        BedBaseConf(bedbase_config)
+        _LOGGER.info(f"Database connection is successful.")
+        return True
+    except Exception as e:
+        _LOGGER.error(f"Database connection failed. Error: {e}")
+        return False
