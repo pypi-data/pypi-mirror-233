df9e7d3712d3d37cfe300013cb97c5169f2b6492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:50 +0000

    Add package mpai-cae-arp/0.5.3/mpai_cae_arp-0.5.3-py3-none-any.whl

---
+++ packages/mpai-cae-arp/mpai_cae_arp-0.5.3-py3-none-any.whl/mpai_cae_arp/network/arp_pb2_grpc.pyi
@@ -0,0 +1,44 @@
+from abc import abstractmethod
+
+from grpc import Channel, Server
+
+from .arp_pb2 import InfoRequest, InfoResponse, JobRequest, JobResponse
+
+class AIMStub:
+    def __init__(self, channel: Channel) -> None: ...
+
+class AIMServicer:
+    @abstractmethod
+    def getInfo(self, request: InfoRequest, context) -> InfoResponse: ...
+    @abstractmethod
+    def work(self, request: JobRequest, context) -> JobResponse: ...
+
+class AIM:
+    @staticmethod
+    def getInfo(
+        request: InfoRequest,
+        target,
+        options=(),
+        channel_credentials=None,
+        call_credentials=None,
+        insecure: bool = False,
+        compression=None,
+        wait_for_ready=None,
+        timeout=None,
+        metadata=None,
+    ) -> InfoResponse: ...
+    @staticmethod
+    def work(
+        request: JobRequest,
+        target,
+        options=(),
+        channel_credentials=None,
+        call_credentials=None,
+        insecure: bool = False,
+        compression=None,
+        wait_for_ready=None,
+        timeout=None,
+        metadata=None,
+    ) -> JobResponse: ...
+
+def add_AIMServicer_to_server(servicer: AIMServicer, server: Server) -> None: ...
