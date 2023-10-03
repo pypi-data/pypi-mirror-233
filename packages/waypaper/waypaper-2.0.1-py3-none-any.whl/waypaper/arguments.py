1d6182977dd29e8fb0a8bb2ae21346d62305159f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:07:00 +0000

    Add package waypaper/2.0.1/waypaper-2.0.1-py3-none-any.whl

---
+++ packages/waypaper/waypaper-2.0.1-py3-none-any.whl/waypaper/arguments.py
@@ -0,0 +1,15 @@
+"""Module that parses user arguments and returns them in args object"""
+
+import argparse
+
+from waypaper.options import FILL_OPTIONS, BACKEND_OPTIONS
+from waypaper.translation_en import *
+
+
+parser = argparse.ArgumentParser(prog = "waypaper", description = MSG_DESC, epilog = MSG_INFO)
+parser.add_argument("-v", "--version", help=MSG_ARG_HELP, action="store_true")
+parser.add_argument("--restore", help=MSG_ARG_REST, action="store_true")
+parser.add_argument("--random", help=MSG_ARG_RAND, action="store_true")
+parser.add_argument("--fill", help=MSG_ARG_FILL, action="store_true")
+parser.add_argument("--backend", help=MSG_ARG_BACK, choices=BACKEND_OPTIONS)
+args = parser.parse_args()
