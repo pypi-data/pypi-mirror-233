44232ea1957bf29c4b9c87bea107c8dd41682328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:07:09 +0000

    Add package tomodachi-testcontainers/0.8.0/tomodachi_testcontainers-0.8.0-py3-none-any.whl

---
+++ packages/tomodachi-testcontainers/tomodachi_testcontainers-0.8.0-py3-none-any.whl/tomodachi_testcontainers/pytest/async_probes.py
@@ -0,0 +1,41 @@
+import contextlib
+from asyncio import iscoroutinefunction
+from typing import Any, Callable
+
+from tenacity import AsyncRetrying, RetryError, retry_unless_exception_type
+from tenacity.stop import stop_after_delay
+from tenacity.wait import wait_fixed
+
+
+async def probe_until(func: Callable, probe_interval: float = 0.1, stop_after: float = 3.0) -> Any:
+    """Run given function until it finishes without exceptions."""
+    result: Any = None
+    async for attempt in AsyncRetrying(
+        wait=wait_fixed(probe_interval),
+        stop=stop_after_delay(stop_after),
+        reraise=True,
+    ):
+        with attempt:
+            if iscoroutinefunction(func):
+                result = await func()
+            else:
+                result = func()
+    return result
+
+
+async def probe_during_interval(func: Callable, probe_interval: float = 0.1, stop_after: float = 3.0) -> Any:
+    """Run given function until timeout is reached and the function always finishes without exceptions."""
+    result: Any = None
+    with contextlib.suppress(RetryError):
+        async for attempt in AsyncRetrying(
+            wait=wait_fixed(probe_interval),
+            stop=stop_after_delay(stop_after),
+            retry=retry_unless_exception_type(BaseException),
+            reraise=True,
+        ):
+            with attempt:
+                if iscoroutinefunction(func):
+                    result = await func()
+                else:
+                    result = func()
+    return result
