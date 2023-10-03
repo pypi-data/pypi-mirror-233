6f8ef2e4436a2b6682368599eace435df0379670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:44 +0000

    Add package chartmogul/1.5.1/chartmogul-1.5.1.tar.gz

---
+++ packages/chartmogul/chartmogul-1.5.1.tar.gz/chartmogul-1.5.1/chartmogul/api/plan.py
@@ -0,0 +1,30 @@
+from marshmallow import Schema, fields, post_load
+from ..resource import Resource
+from collections import namedtuple
+
+
+class Plan(Resource):
+    """
+    https://dev.chartmogul.com/v1.0/reference#plans
+    """
+    _path = "/plans{/uuid}"
+    _root_key = 'plans'
+    _many = namedtuple(
+        'Plans',
+        [_root_key, 'current_page', 'total_pages', 'has_more', 'cursor'],
+        defaults=[None, None, None, None]
+    )
+
+    class _Schema(Schema):
+        uuid = fields.String()
+        data_source_uuid = fields.String()
+        name = fields.String()
+        interval_count = fields.Int()
+        interval_unit = fields.String()
+        external_id = fields.String()
+
+        @post_load
+        def make(self, data, **kwargs):
+            return Plan(**data)
+
+    _schema = _Schema()
