ac6df04e9050d88a54a4db33de183c7faa42edc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:07:06 +0000

    Add package hexcover/0.4.1/hexcover-0.4.1.tar.gz

---
+++ packages/hexcover/hexcover-0.4.1.tar.gz/hexcover-0.4.1/setup.py
@@ -0,0 +1,17 @@
+"""
+setup.py
+
+Created by Stephan Hügel on 2019-06-29
+
+This file is part of hexcover.
+
+Copyright (c) 2019 Stephan Hügel
+
+"""
+
+
+from setuptools import setup
+
+
+if __name__ == "__main__":
+    setup()
