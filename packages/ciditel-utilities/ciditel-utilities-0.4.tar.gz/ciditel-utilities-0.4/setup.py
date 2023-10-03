a0e3c802b8b3ffa9d61fedd51c0bd305f10c0128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:07:13 +0000

    Add package ciditel-utilities/0.4/ciditel-utilities-0.4.tar.gz

---
+++ packages/ciditel-utilities/ciditel-utilities-0.4.tar.gz/ciditel-utilities-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ciditel-utilities',
+  packages = find_packages(), 
+  version = '0.4',
+  description = 'Verificaciones al usuario',
+  
+)
