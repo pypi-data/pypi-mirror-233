2522a77663af0968eb7d036fed921bce9a540e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:48 +0000

    Add package ccxtools/0.0.95/ccxtools-0.0.95.tar.gz

---
+++ packages/ccxtools/ccxtools-0.0.95.tar.gz/ccxtools-0.0.95/src/ccxtools/base/FutureExchange.py
@@ -0,0 +1,25 @@
+from ccxtools.base.Exchange import Exchange
+
+
+class FutureExchange(Exchange):
+
+    def __init__(self):
+        self.contract_sizes = None
+
+    def get_contract_sizes(self):
+        raise NotImplementedError
+
+    def get_funding_rates(self, ticker):
+        raise NotImplementedError
+
+    def get_balance(self, ticker):
+        raise NotImplementedError
+
+    def get_position(self, ticker):
+        raise NotImplementedError
+
+    def post_market_order(self, ticker, side, open_close, amount):
+        raise NotImplementedError
+
+    def get_precise_order_amount(self, ticker, ticker_amount):
+        raise NotImplementedError
