61e94ca4d90e20610d8d859ad86123e3f4e1e118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:07:59 +0000

    Add package strada/0.1.13/strada-0.1.13.tar.gz

---
+++ packages/strada/strada-0.1.13.tar.gz/strada-0.1.13/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="strada",
+    version="0.1.13",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "google-api-python-client",
+        "google-auth",
+        "oauth2client",
+        "google-auth-oauthlib",
+        "google-auth-httplib2",
+        "requests",
+    ],
+    author="Strada",
+    description="Strada SDK",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
