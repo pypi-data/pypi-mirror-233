4296dcdb790323b984782cf1e0b1891214fc5674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:44 +0000

    Add package pylitterbot/2023.4.9/pylitterbot-2023.4.9.tar.gz

---
+++ packages/pylitterbot/pylitterbot-2023.4.9.tar.gz/pylitterbot-2023.4.9/pylitterbot/__init__.py
@@ -0,0 +1,18 @@
+"""pylitterbot module."""
+__version__ = "2023.4.9"
+
+from .account import Account
+from .robot import Robot
+from .robot.feederrobot import FeederRobot
+from .robot.litterrobot import LitterRobot
+from .robot.litterrobot3 import LitterRobot3
+from .robot.litterrobot4 import LitterRobot4
+
+__all__ = [
+    "Account",
+    "Robot",
+    "LitterRobot",
+    "LitterRobot3",
+    "LitterRobot4",
+    "FeederRobot",
+]
