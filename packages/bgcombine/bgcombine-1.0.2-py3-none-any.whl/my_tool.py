b8fba9c22875310f46e2708cc0d972ded3a428a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:08:04 +0000

    Add package bgcombine/1.0.2/bgcombine-1.0.2-py3-none-any.whl

---
+++ packages/bgcombine/bgcombine-1.0.2-py3-none-any.whl/my_tool.py
@@ -0,0 +1,17 @@
+import click
+import app.attendance as attendance
+import app.personal_info_all_data as info
+
+@click.command()
+@click.argument('mode')
+@click.argument('pth_out')
+@click.option('--c')
+def main(mode, pth_out, c):
+    if mode == 'info':
+        pth_in = click.prompt("\n--------------------------------------------------\nenter path to child and family data input folder")
+        info.main(pth_in, pth_out)
+    elif mode == 'attendance':
+        pth_in = click.prompt("\n--------------------------------------------------\nenter path to attendance data input folder")
+        attendance.main(pth_in, pth_out, c)
+    else:
+        click.echo(f'{mode} is not a supported mode. Type --help for accepted commands.')
