e122811328346bc87424319ba2e7d64da6828ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:12 +0000

    Add package python-cdd/0.0.99rc14/python-cdd-0.0.99rc14.tar.gz

---
+++ packages/python-cdd/python-cdd-0.0.99rc14.tar.gz/python-cdd-0.0.99rc14/cdd/shared/pkg_utils.py
@@ -0,0 +1,29 @@
+"""
+pkg_utils
+"""
+
+from distutils.sysconfig import get_python_lib
+
+
+def relative_filename(filename, remove_hints=tuple()):
+    """
+    Remove all the paths which are not relevant
+
+    :param filename: Filename
+    :type filename: ```str```
+
+    :param remove_hints: Hints as to what can be removed
+    :type remove_hints: ```Tuple[str]```
+
+    :return: Relative path (if derived) else original
+    :rtype: ```str```
+    """
+    _filename = filename.casefold()
+    lib = get_python_lib(), get_python_lib(prefix="")
+    for elem in remove_hints + lib:
+        if _filename.startswith(elem.casefold()):
+            return filename[len(elem) + 1 :]
+    return filename
+
+
+__all__ = ["relative_filename"]
