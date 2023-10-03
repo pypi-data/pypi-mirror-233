6a8bf77ab62f4ebcdb86022d966af1097cdde35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:24 +0000

    Add package wavefront-api-client/2.202.2/wavefront_api_client-2.202.2-py3-none-any.whl

---
+++ packages/wavefront-api-client/wavefront_api_client-2.202.2-py3-none-any.whl/wavefront_api_client/api/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from wavefront_api_client.api.access_policy_api import AccessPolicyApi
+from wavefront_api_client.api.account__user_and_service_account_api import AccountUserAndServiceAccountApi
+from wavefront_api_client.api.alert_api import AlertApi
+from wavefront_api_client.api.api_token_api import ApiTokenApi
+from wavefront_api_client.api.cloud_integration_api import CloudIntegrationApi
+from wavefront_api_client.api.dashboard_api import DashboardApi
+from wavefront_api_client.api.derived_metric_api import DerivedMetricApi
+from wavefront_api_client.api.direct_ingestion_api import DirectIngestionApi
+from wavefront_api_client.api.event_api import EventApi
+from wavefront_api_client.api.external_link_api import ExternalLinkApi
+from wavefront_api_client.api.ingestion_spy_api import IngestionSpyApi
+from wavefront_api_client.api.integration_api import IntegrationApi
+from wavefront_api_client.api.maintenance_window_api import MaintenanceWindowApi
+from wavefront_api_client.api.message_api import MessageApi
+from wavefront_api_client.api.metric_api import MetricApi
+from wavefront_api_client.api.metrics_policy_api import MetricsPolicyApi
+from wavefront_api_client.api.monitored_application_api import MonitoredApplicationApi
+from wavefront_api_client.api.monitored_service_api import MonitoredServiceApi
+from wavefront_api_client.api.notificant_api import NotificantApi
+from wavefront_api_client.api.proxy_api import ProxyApi
+from wavefront_api_client.api.query_api import QueryApi
+from wavefront_api_client.api.recent_app_map_search_api import RecentAppMapSearchApi
+from wavefront_api_client.api.recent_traces_search_api import RecentTracesSearchApi
+from wavefront_api_client.api.role_api import RoleApi
+from wavefront_api_client.api.saved_app_map_search_api import SavedAppMapSearchApi
+from wavefront_api_client.api.saved_app_map_search_group_api import SavedAppMapSearchGroupApi
+from wavefront_api_client.api.saved_search_api import SavedSearchApi
+from wavefront_api_client.api.saved_traces_search_api import SavedTracesSearchApi
+from wavefront_api_client.api.saved_traces_search_group_api import SavedTracesSearchGroupApi
+from wavefront_api_client.api.search_api import SearchApi
+from wavefront_api_client.api.source_api import SourceApi
+from wavefront_api_client.api.span_sampling_policy_api import SpanSamplingPolicyApi
+from wavefront_api_client.api.usage_api import UsageApi
+from wavefront_api_client.api.user_api import UserApi
+from wavefront_api_client.api.user_group_api import UserGroupApi
+from wavefront_api_client.api.wavefront_api import WavefrontApi
+from wavefront_api_client.api.webhook_api import WebhookApi
