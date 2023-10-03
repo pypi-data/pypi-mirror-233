c89ed561af746d01475c76c797982729e5710fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:03 +0000

    Add package psalpsdtools/1.5/psalpsdtools-1.5.tar.gz

---
+++ packages/psalpsdtools/psalpsdtools-1.5.tar.gz/psalpsdtools-1.5/psalpsdtools/usage.py
@@ -0,0 +1,18 @@
+#from psalpsdtools import Edrw
+from Edrw import Edrw
+
+# SET REGION TO PROCESS
+regName = "Central Visayas"
+# SET QUARTER, ALSO USED IN READING THE S-D FILE
+qtr = "Q2"
+# SET FOLDER OF SOURCES/FINAL FILES INCLUDING S-D FILE
+baseFolder = "C:/EDRW/Q2"
+# SET S-D FILE NAME
+sdFile = 'Duck_SD Q2 2023.xlsm'
+# COMMODITY 08=chicken, 09=duck, etc.
+commcode = '09'
+# Year
+yr = '23'
+
+myedrw = Edrw()
+myedrw.update_sources(regName,qtr,baseFolder,sdFile,commcode,yr)
---
+++ packages/psalpsdtools/psalpsdtools-1.5.tar.gz/psalpsdtools-1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='psalpsdtools',
+      version='1.5',
+      description='PSA-LPSD Tools',
+      packages=['psalpsdtools'],
+      install_requires=[
+          'pip',
+          'openpyxl',
+      ],
+      author_email='a.diaziii@psa.gov.ph',
+      long_description=open('README.md', 'r', encoding='utf-8').read(),
+      long_description_content_type='text/markdown',
+      license='MIT',
+      url='https://github.com/tondiaz/psalpsdtools',
+      zip_safe=False)
