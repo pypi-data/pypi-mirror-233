c57fa034ef0eba0d6c4e8f1a0da8be1bb45493c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:13 +0000

    Add package rerun-sdk/0.9.0a6/rerun_sdk-0.9.0a6-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/rerun-sdk/rerun_sdk-0.9.0a6-cp38-abi3-macosx_11_0_arm64.whl/rerun_sdk/rerun/datatypes/translation_rotation_scale3d_ext.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Iterable, cast
+
+if TYPE_CHECKING:
+    from .._log import ComponentBatchLike
+    from . import Rotation3DLike, Scale3DLike, Vec3D, Vec3DLike
+
+
+class TranslationRotationScale3DExt:
+    # TODO(#2641): this is needed until we support default value for params
+    def __init__(
+        self: Any,
+        translation: Vec3DLike | None = None,
+        rotation: Rotation3DLike | None = None,
+        scale: Scale3DLike | None = None,
+        *,
+        from_parent: bool = False,
+    ) -> None:
+        self.__attrs_init__(  # pyright: ignore[reportGeneralTypeIssues]
+            translation=translation, rotation=rotation, scale=scale, from_parent=from_parent
+        )
+
+    @staticmethod
+    def translation__field_converter_override(data: Vec3DLike | None) -> Vec3D | None:
+        if data is None:
+            return None
+        else:
+            from . import Vec3D
+
+            return Vec3D(data)
+
+    # Implement the AsComponents protocol
+    def as_component_batches(self) -> Iterable[ComponentBatchLike]:
+        from ..archetypes import Transform3D
+        from ..datatypes import TranslationRotationScale3D
+
+        return Transform3D(cast(TranslationRotationScale3D, self)).as_component_batches()
+
+    def num_instances(self) -> int:
+        # Always a mono-component
+        return 1
