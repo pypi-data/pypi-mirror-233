026ba9f715b8f80aa7c7de8cf84339e8793551ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:07:43 +0000

    Add package invenio-rdm-records/4.29.0/invenio_rdm_records-4.29.0-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-records/invenio_rdm_records-4.29.0-py2.py3-none-any.whl/invenio_rdm_records/services/tasks.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Celery tasks."""
+
+from celery import shared_task
+from flask import current_app
+from invenio_access.permissions import system_identity
+
+from ..proxies import current_rdm_records
+from .errors import EmbargoNotLiftedError
+
+
+@shared_task(ignore_result=True)
+def update_expired_embargos():
+    """Lift expired embargos."""
+    service = current_rdm_records.records_service
+
+    records = service.scan_expired_embargos(system_identity)
+    for record in records.hits:
+        try:
+            service.lift_embargo(_id=record["id"], identity=system_identity)
+        except EmbargoNotLiftedError as ex:
+            current_app.logger.warning(ex.description)
+            continue
