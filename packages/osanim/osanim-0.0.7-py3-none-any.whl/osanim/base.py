12a7ebca1e0f431942a70b0ae88124dd0d1c5475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:47 +0000

    Add package osanim/0.0.7/osanim-0.0.7-py3-none-any.whl

---
+++ packages/osanim/osanim-0.0.7-py3-none-any.whl/osanim/base.py
@@ -0,0 +1,53 @@
+import requests
+
+class _BaseAPI:
+    _base_url = 'api.osanim.com'
+    _uri = ''
+   
+    def __init__(self, token: str):
+        self._token = token
+
+    def _api_call(self, method='GET', payload=None, headers: dict = None) -> dict:
+        """
+        Calls Endpoint with appropriate payload
+        :param method:
+        :param payload:
+        :return:
+        """
+        response: dict = {}
+
+        try:
+            endpoint = f'{self._base_url}/{self._uri}'
+            if headers is not None:
+                headers['Content-Type'] = 'application/json'
+                headers['Authorization'] = f'Bearer {self._token}'
+
+            req = requests.request(url=endpoint, method=method, json=payload, headers=headers)
+            req.text
+            if req.ok:
+                try:
+                    response = req.json()
+                except requests.exceptions.JSONDecodeError:
+                    response = {'status': True}
+            else:
+                response = req.json()
+
+        except requests.exceptions.ConnectionError:
+            response['status'] = "Server Error"
+            response['message'] = "Network Problem: Unable to reach server"
+
+        except requests.exceptions.Timeout:
+            response['status'] = "Server Error"
+            response['message'] = "Request Timeout"
+
+        except requests.exceptions.RequestException as e:
+
+            response['status'] = "System Error"
+            response['message'] = e
+
+        return response
+
+
+
+
+
