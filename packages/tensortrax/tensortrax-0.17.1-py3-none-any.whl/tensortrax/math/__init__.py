00994992818a46ecffbfc4bccddd6fb80af5bfd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:00 +0000

    Add package tensortrax/0.17.1/tensortrax-0.17.1-py3-none-any.whl

---
+++ packages/tensortrax/tensortrax-0.17.1-py3-none-any.whl/tensortrax/math/__init__.py
@@ -0,0 +1,83 @@
+r"""
+ _
+| |                          ████████╗██████╗  █████╗ ██╗  ██╗
+| |_ ___ _ __  ___  ___  _ __╚══██╔══╝██╔══██╗██╔══██╗╚██╗██╔╝
+| __/ _ \ '_ \/ __|/ _ \| '__|  ██║   ██████╔╝███████║ ╚███╔╝
+| ||  __/ | | \__ \ (_) | |     ██║   ██╔══██╗██╔══██║ ██╔██╗
+ \__\___|_| |_|___/\___/|_|     ██║   ██║  ██║██║  ██║██╔╝ ██╗
+                                ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+"""
+
+from .._tensor import broadcast_to, dual2real, ravel, reshape, squeeze
+from . import _linalg as linalg
+from . import _math_array as base
+from . import _special as special
+from ._math_tensor import (
+    abs,
+    array,
+    cos,
+    cosh,
+    diagonal,
+    dot,
+    einsum,
+    exp,
+    external,
+    hstack,
+    if_else,
+    log,
+    log10,
+    matmul,
+    repeat,
+    sign,
+    sin,
+    sinh,
+    split,
+    sqrt,
+    stack,
+    sum,
+    tan,
+    tanh,
+    tile,
+    trace,
+    transpose,
+    vstack,
+)
+
+__all__ = [
+    "linalg",
+    "base",
+    "special",
+    "abs",
+    "array",
+    "broadcast_to",
+    "cos",
+    "cosh",
+    "diagonal",
+    "dot",
+    "dual2real",
+    "einsum",
+    "exp",
+    "external",
+    "hstack",
+    "log",
+    "log10",
+    "matmul",
+    "ravel",
+    "repeat",
+    "reshape",
+    "sign",
+    "sin",
+    "sinh",
+    "split",
+    "sqrt",
+    "squeeze",
+    "stack",
+    "sum",
+    "tan",
+    "tanh",
+    "tile",
+    "trace",
+    "transpose",
+    "vstack",
+    "if_else",
+]
