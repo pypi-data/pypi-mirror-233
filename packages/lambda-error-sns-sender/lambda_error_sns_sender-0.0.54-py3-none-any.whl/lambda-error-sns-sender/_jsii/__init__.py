5edd3f081dbed9baf6c9e621d791feac8f08f167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:14 +0000

    Add package lambda-error-sns-sender/0.0.54/lambda_error_sns_sender-0.0.54-py3-none-any.whl

---
+++ packages/lambda-error-sns-sender/lambda_error_sns_sender-0.0.54-py3-none-any.whl/lambda-error-sns-sender/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "lambda-error-sns-sender",
+    "0.0.54",
+    __name__[0:-6],
+    "lambda-error-sns-sender@0.0.54.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
