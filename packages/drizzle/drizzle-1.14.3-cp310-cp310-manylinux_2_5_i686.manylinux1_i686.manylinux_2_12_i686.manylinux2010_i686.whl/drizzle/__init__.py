d40aadf648ff4113c600f874b4b4c62e897d758e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:21 +0000

    Add package drizzle/1.14.3/drizzle-1.14.3-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/drizzle/drizzle-1.14.3-cp310-cp310-manylinux_2_5_i686.manylinux1_i686.manylinux_2_12_i686.manylinux2010_i686.whl/drizzle/__init__.py
@@ -0,0 +1,11 @@
+"""
+A package for combining dithered images into a single image
+"""
+
+
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = 'unknown'
