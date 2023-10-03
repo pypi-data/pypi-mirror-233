34773f368f704e4276ae01602fb06900231b24fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:07:27 +0000

    Add package rpa-cooperativa/1.0.89/rpa_cooperativa-1.0.89.tar.gz

---
+++ packages/rpa-cooperativa/rpa_cooperativa-1.0.89.tar.gz/rpa_cooperativa-1.0.89/rpa_coop/__init__.py
@@ -0,0 +1,23 @@
+from rpa_coop.rpa_coop import *
+fluid = Fluid()
+whatsapp = Whatsapp()
+sms = Sms()
+send_sms = sms.enviar_SMS
+dados = Dados()
+criar_conexao_bd = dados.criar_engine
+query_sql_fn = Dados()
+sql_query_fn = Dados()
+acc = Acc()
+monitorar = Monitorar()
+monitor_rpa = Monitorar()
+escrever_por_extenso = Escrever_por_extenso()
+numero_por_extenso = escrever_por_extenso.numero_por_extenso
+taxa_percentual_por_extenso = escrever_por_extenso.taxa_por_extenso
+gerador_de_codigo = Gerador_de_codigo()
+selenium = Selenium()
+mail = Emails()
+dia_util = Dia_util()
+adobe = Adobe()
+get_errors =  Get_errors()
+
+
