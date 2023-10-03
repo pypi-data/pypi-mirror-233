0d4e1bc9c03b2ce026e78bc04257e11f93b79df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:46 +0000

    Add package viNtik123Vita/1.0.0/viNtik123Vita-1.0.0.tar.gz

---
+++ packages/viNtik123Vita/viNtik123Vita-1.0.0.tar.gz/viNtik123Vita-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viNtik123Vita',
+    version='1.0.0',
+    author='viNtik123Vita',
+    author_email='vitalik.kovalenko.2104@gmail.com',
+    description='--',
+    long_description='Readme content goes here',
+    long_description_content_type='text/markdown',
+    url='https://github.com/viNtik123Vita/Work',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+)
