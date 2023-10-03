d56c6f8a1ee9dd10ca4a459fb04ac7bda7f5b034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:03 +0000

    Add package spatialdata/0.0.13/spatialdata-0.0.13.tar.gz

---
+++ packages/spatialdata/spatialdata-0.0.13.tar.gz/spatialdata-0.0.13/src/spatialdata/_core/data_extent.py
@@ -0,0 +1,36 @@
+"""Functions to compute the bounding box describing the extent of a SpatialElement or region."""
+import numpy as np
+from geopandas import GeoDataFrame
+from shapely import Point
+
+from spatialdata._types import ArrayLike
+from spatialdata.models import get_axes_names
+
+
+def _get_bounding_box_of_circle_elements(shapes: GeoDataFrame) -> tuple[ArrayLike, ArrayLike, tuple[str, ...]]:
+    """Get the coordinates for the corners of the bounding box of that encompasses a circle element.
+
+    This computes the bounding box around all circles in the element.
+
+    Returns
+    -------
+    min_coordinate
+        The minimum coordinate of the bounding box.
+    max_coordinate
+        The maximum coordinate of the bounding box.
+    """
+    circle_element = shapes
+    if not isinstance(circle_element.geometry.iloc[0], Point):
+        raise NotImplementedError("Only circles (shapely Point) are currently supported (not Polygon or MultiPolygon).")
+    circle_dims = get_axes_names(circle_element)
+
+    centroids = []
+    for dim_name in circle_dims:
+        centroids.append(getattr(circle_element["geometry"], dim_name).to_numpy())
+    centroids_array = np.column_stack(centroids)
+    radius = np.expand_dims(circle_element["radius"].to_numpy(), axis=1)
+
+    min_coordinates = (centroids_array - radius).astype(int)
+    max_coordinates = (centroids_array + radius).astype(int)
+
+    return min_coordinates, max_coordinates, circle_dims
