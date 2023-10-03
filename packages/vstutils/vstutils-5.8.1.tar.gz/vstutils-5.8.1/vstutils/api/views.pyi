2118c9c41e70569fb3a24a9a9f972bc43842fc1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:03 +0000

    Add package vstutils/5.8.1/vstutils-5.8.1.tar.gz

---
+++ packages/vstutils/vstutils-5.8.1.tar.gz/vstutils-5.8.1/vstutils/api/views.pyi
@@ -0,0 +1,15 @@
+from typing import List, Dict
+from . import base
+
+
+class BulkViewSet(base.rvs.APIView):
+    client_environ_keys_copy: List
+    type_to_bulk: Dict
+
+
+class HealthView(base.ListNonModelViewSet):
+    ...
+
+
+class MetricsView(base.ListNonModelViewSet):
+    ...
