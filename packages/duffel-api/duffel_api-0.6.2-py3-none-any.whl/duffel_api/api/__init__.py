deb199dda114090d75851c0b1414a33cc02eaab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:07:48 +0000

    Add package duffel-api/0.6.2/duffel_api-0.6.2-py3-none-any.whl

---
+++ packages/duffel-api/duffel_api-0.6.2-py3-none-any.whl/duffel_api/api/__init__.py
@@ -0,0 +1,44 @@
+from .booking.offer_requests import OfferRequestClient, OfferRequestCreate
+from .booking.offers import OfferClient
+from .booking.orders import OrderClient, OrderCreate, OrderUpdate
+from .booking.order_cancellations import OrderCancellationClient
+from .booking.order_changes import OrderChangeClient
+from .booking.order_change_offers import OrderChangeOfferClient
+from .booking.order_change_requests import OrderChangeRequestClient
+from .booking.partial_offer_requests import (
+    PartialOfferRequestClient,
+    PartialOfferRequestCreate,
+)
+from .booking.payments import PaymentClient
+from .booking.seat_maps import SeatMapClient
+from .duffel_payments.payment_intents import PaymentIntentClient, PaymentIntentCreate
+from .links.sessions import LinksSessionClient, LinksSessionCreate
+from .notifications.webhooks import WebhookClient
+from .supporting.aircraft import AircraftClient
+from .supporting.airports import AirportClient
+from .supporting.airlines import AirlineClient
+
+__all__ = [
+    "AircraftClient",
+    "AirportClient",
+    "AirlineClient",
+    "LinksSessionClient",
+    "LinksSessionCreate",
+    "OfferRequestClient",
+    "OfferRequestCreate",
+    "OfferClient",
+    "OrderChangeClient",
+    "OrderChangeOfferClient",
+    "OrderChangeRequestClient",
+    "OrderClient",
+    "OrderCreate",
+    "OrderUpdate",
+    "OrderCancellationClient",
+    "PartialOfferRequestClient",
+    "PartialOfferRequestCreate",
+    "PaymentClient",
+    "PaymentIntentClient",
+    "PaymentIntentCreate",
+    "SeatMapClient",
+    "WebhookClient",
+]
