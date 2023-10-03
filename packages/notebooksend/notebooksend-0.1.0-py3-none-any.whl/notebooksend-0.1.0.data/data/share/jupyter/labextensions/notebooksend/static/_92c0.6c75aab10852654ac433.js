83546a8c300e90d84a0f6cfb51ae13c2e806fde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:07:27 +0000

    Add package notebooksend/0.1.0/notebooksend-0.1.0-py3-none-any.whl

---
+++ packages/notebooksend/notebooksend-0.1.0-py3-none-any.whl/notebooksend-0.1.0.data/data/share/jupyter/labextensions/notebooksend/static/_92c0.6c75aab10852654ac433.js
@@ -0,0 +1,14 @@
+(self["webpackChunknotebooksend"] = self["webpackChunknotebooksend"] || []).push([["_92c0"],{
+
+/***/ "?92c0":
+/*!************************!*\
+  !*** crypto (ignored) ***!
+  \************************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=_92c0.6c75aab10852654ac433.js.map
