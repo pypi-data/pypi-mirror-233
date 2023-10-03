c04b93002be390b53f996b90b5f5a0833b4ba2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:06:47 +0000

    Add package noveldown/1.2.1/noveldown-1.2.1.tar.gz

---
+++ packages/noveldown/noveldown-1.2.1.tar.gz/noveldown-1.2.1/noveldown/__init__.py
@@ -0,0 +1,16 @@
+from .api import (  # isort: skip
+    get_available_ids,
+    download,
+    download_progress,
+    query,
+)
+
+(  # screw you pylance # pylint: disable=pointless-statement
+    get_available_ids,
+    download,
+    download_progress,
+    query,
+)
+
+__version__ = (1, 2, 0)
+__version_str__ = ".".join(map(str, __version__))
