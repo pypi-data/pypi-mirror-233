a83362b51b6b4098a349c4e5e49fe71b397aafe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:33 +0000

    Add package pyais/2.5.8/pyais-2.5.8-py3-none-any.whl

---
+++ packages/pyais/pyais-2.5.8-py3-none-any.whl/tests/utils/generate_msg_interface.py
@@ -0,0 +1,20 @@
+from pyais.messages import MSG_CLASS
+
+for typ, cls in MSG_CLASS.items():
+    if not cls.fields():
+        continue
+    print(cls.__name__, cls.__doc__)
+    print()
+    print("\tAttributes:")
+    for field in cls.fields():
+
+        print("\t\t*", f"`{field.name}`")
+        if field.name == 'radio':
+            print('\t\t\t* Further decoded by `.get_communication_state()` ')
+
+        if 'mmsi' in field.name:
+            print("\t\t\t*", "type:", f"({int}, {str})")
+        else:
+            print("\t\t\t*", "type:", field.metadata['d_type'])
+        print("\t\t\t*", "bit-width:", field.metadata['width'])
+        print("\t\t\t*", "default:", field.metadata['default'])
