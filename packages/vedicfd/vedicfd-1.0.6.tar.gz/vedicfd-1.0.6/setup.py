7c298a17247811e7e18e8fccf4d523c5c15574c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:30 +0000

    Add package vedicfd/1.0.6/vedicfd-1.0.6.tar.gz

---
+++ packages/vedicfd/vedicfd-1.0.6.tar.gz/vedicfd-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vedicfd',
+    version='1.0.6',
+    author='USP',
+    author_email='pathakumashankar@hotmail.com',
+    description='detect faces',
+    # long_description='A longer description of your package',
+    # url='https://github.com/yourusername/your_package_name',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python-headless', 'requests' , 'face_recognition', 'pickle4', 'pytube'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
