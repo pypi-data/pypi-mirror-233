70fe3d040e382ef41d31f86bc921646db553ae9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:05:52 +0000

    Add package xtenors/1.0.1/xtenors-1.0.1-py3-none-any.whl

---
+++ packages/xtenors/xtenors-1.0.1-py3-none-any.whl/xtenors/__init__.py
@@ -0,0 +1,30 @@
+
+
+import os
+import sys
+import pathlib
+
+# if pathlib.Path(os.getcwd()).parts[-1] == "xtenors":
+#     sys.path.append("./__local__")
+
+#     import PATHS
+
+#     if PATHS.XTUPLES not in sys.path:
+#         sys.path.append(PATHS.XTUPLES)
+#         print(sys.path)
+
+# import all modules, so can load individually
+from . import dates
+from . import units
+from . import conventions
+from . import iterators
+from . import calendars
+from . import arithmetic
+from . import adjustments
+
+# also convenience import other specific commonly used items
+from .dates import *
+from .units import *
+from .iterators import Iterator
+from .adjustments import adjust
+from .xtenors import Tenor, add
---
+++ packages/xtenors/xtenors-1.0.1-py3-none-any.whl/xtenors/adjustments.py
@@ -0,0 +1,122 @@
+
+
+
+from __future__ import annotations
+
+import abc
+import typing
+
+import operator
+import itertools
+import functools
+import datetime
+import calendar
+
+import xtuples as xt
+
+from .dates import *
+from .units import *
+
+from . import conventions
+from . import iterators
+from . import calendars
+
+# ---------------------------------------------------------------
+
+# TODO: check signs
+
+# TODO: check unmodified and m != m(valid) is error?
+
+# ---------------------------------------------------------------
+
+def adjust_date_forward(d, first_valid, iterator, modified = None):
+
+    if d.month == first_valid.month:
+        return first_valid
+    
+    elif (
+        first_valid.month > d.month 
+        and modified is conventions.Modified.MODIFIED
+    ):
+        _, gen = iterator.update(start=d, step = days(-1))
+        _, prev_valid = next(gen)
+        return prev_valid
+
+    else:
+        assert False, dict(
+            d=d,
+            first_valid=first_valid,
+            modified=modified,
+        )
+
+def adjust_date_backward(d, first_valid, iterator, modified = None):
+
+    if d.month == first_valid.month:
+        return first_valid
+    
+    elif (
+        first_valid.month < d.month 
+        and modified is conventions.Modified.MODIFIED
+    ):
+        _, gen = iterator.update(start=d, step = days(1))
+        _, next_valid = next(gen)
+        return next_valid
+
+    else:
+        assert False, dict(
+            d=d,
+            first_valid=first_valid,
+            modified=modified,
+        )
+
+# ---------------------------------------------------------------
+
+def adjust(
+    ddt,
+    iterator,
+    roll=None,
+    modified = None,
+):
+
+    d = (
+        ddt if not isinstance(ddt, datetime.datetime)
+        else ddt.date()
+    )
+
+    _, gen = iterator.update(start=d, step = days(
+        1 if roll is conventions.Roll.FOLLOWING
+        else -1 if roll is conventions.Roll.PRECEDING
+        else 0
+    ))
+
+    accept, first_valid = next(gen)
+
+    if roll is None or roll is conventions.Roll.ERROR:
+        assert accept and first_valid == d, dict(
+            accept=accept,
+            first_valid=first_valid,
+            d=d,
+        )
+        res = d
+
+    elif roll is conventions.Roll.PRECEDING:
+        res = adjust_date_backward(
+            d, first_valid, iterator, modified=modified
+            #
+        )
+
+    elif roll is conventions.Roll.FOLLOWING:
+        res = adjust_date_forward(
+            d, first_valid, iterator, modified=modified
+            #
+        )
+
+    else:
+        assert False, roll
+
+    if isinstance(ddt, datetime.datetime):
+        return datetime.combine(res, ddt.time())
+
+    return res
+
+# ---------------------------------------------------------------
