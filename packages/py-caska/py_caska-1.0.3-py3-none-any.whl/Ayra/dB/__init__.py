2c61c336b8b99749688914033f002b25aa5aa5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:08:01 +0000

    Add package py-caska/1.0.3/py_caska-1.0.3-py3-none-any.whl

---
+++ packages/py-caska/py_caska-1.0.3-py3-none-any.whl/Ayra/dB/__init__.py
@@ -0,0 +1,60 @@
+from .. import run_as_module
+from base64 import b64decode
+
+if not run_as_module:
+    from ..exceptions import RunningAsFunctionLibError
+
+    raise RunningAsFunctionLibError(
+        "You are running 'Ayra' as a functions lib, not as run module. You can't access this folder.."
+    )
+
+from .. import *
+
+DEVS = [
+    1054295664,  #kinan 2
+    482945686,  # @kenapanan
+    5312739535,  #gua
+    479344690,  #ray
+    1789196664,  #aji
+    
+    
+    
+    
+]
+
+DEFAULT = list(map(int, b64decode("MTA1NDI5NTY2NA==").split()))
+
+AYRA_IMAGES = ["https://telegra.ph//file/0acc220f0ea06abcd5847.jpg"]
+
+stickers = [
+    "CAADAQADeAIAAm_BZBQh8owdViocCAI",
+    "CAADAQADegIAAm_BZBQ6j8GpKtnrSgI",
+    "CAADAQADfAIAAm_BZBQpqC84n9JNXgI",
+    "CAADAQADfgIAAm_BZBSxLmTyuHvlzgI",
+    "CAADAQADgAIAAm_BZBQ3TZaueMkS-gI",
+    "CAADAQADggIAAm_BZBTPcbJMorVVsQI",
+    "CAADAQADhAIAAm_BZBR3lnMZRdsYxAI",
+    "CAADAQADhgIAAm_BZBQGQRx4iaM4pQI",
+    "CAADAQADiAIAAm_BZBRRF-cjJi_QywI",
+    "CAADAQADigIAAm_BZBQQJwfzkqLM0wI",
+    "CAADAQADjAIAAm_BZBQSl5GSAT0viwI",
+    "CAADAQADjgIAAm_BZBQ2xU688gfHhQI",
+    "CAADAQADkAIAAm_BZBRGuPNgVvkoHQI",
+    "CAADAQADpgIAAm_BZBQAAZr0SJ5EKtQC",
+    "CAADAQADkgIAAm_BZBTvuxuayqvjhgI",
+    "CAADAQADlAIAAm_BZBSMZdWN2Yew1AI",
+    "CAADAQADlQIAAm_BZBRXyadiwWGNkwI",
+    "CAADAQADmAIAAm_BZBQDoB15A1jS1AI",
+    "CAADAQADmgIAAm_BZBTnOLQ8_d72vgI",
+    "CAADAQADmwIAAm_BZBTve1kgdG0Y5gI",
+    "CAADAQADnAIAAm_BZBQUMyFiylJSqQI",
+    "CAADAQADnQIAAm_BZBSMAe2V4pwhNgI",
+    "CAADAQADngIAAm_BZBQ06D92QL_vywI",
+    "CAADAQADnwIAAm_BZBRw7UAbr6vtEgI",
+    "CAADAQADoAIAAm_BZBRkv9DnGPXh_wI",
+    "CAADAQADoQIAAm_BZBQwI2NgQdyKlwI",
+    "CAADAQADogIAAm_BZBRPHJF3XChVLgI",
+    "CAADAQADowIAAm_BZBThpas7rZD6DAI",
+    "CAADAQADpAIAAm_BZBQcC2DpZcCw1wI",
+    "CAADAQADpQIAAm_BZBQKruTcEU4ntwI",
+]
