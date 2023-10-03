1989d8257f1b084c9aabcec15ede94c759ece634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:15 +0000

    Add package rerun-sdk/0.9.0a6/rerun_sdk-0.9.0a6-cp38-abi3-manylinux_2_31_x86_64.whl

---
+++ packages/rerun-sdk/rerun_sdk-0.9.0a6-cp38-abi3-manylinux_2_31_x86_64.whl/rerun_sdk/rerun/datatypes/translation_and_mat3x3_ext.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Iterable, cast
+
+if TYPE_CHECKING:
+    from .._log import ComponentBatchLike
+    from . import Mat3x3Like, Vec3DLike
+
+
+class TranslationAndMat3x3Ext:
+    # TODO(#2641): this is needed until we support default value for from_parent
+    def __init__(
+        self: Any,
+        translation: Vec3DLike | None = None,
+        matrix: Mat3x3Like | None = None,
+        *,
+        from_parent: bool = False,
+    ) -> None:
+        """
+        Create a new instance of the TranslationAndMat3x3 datatype.
+
+        Parameters
+        ----------
+        translation:
+             3D translation, applied after the matrix.
+        matrix:
+             3x3 matrix for scale, rotation & shear.
+        from_parent:
+             If true, the transform maps from the parent space to the space where the transform was logged.
+             Otherwise, the transform maps from the space to its parent.
+        """
+
+        self.__attrs_init__(  # pyright: ignore[reportGeneralTypeIssues]
+            translation=translation, matrix=matrix, from_parent=from_parent
+        )
+
+    # Implement the AsComponents
+    def as_component_batches(self) -> Iterable[ComponentBatchLike]:
+        from ..archetypes import Transform3D
+        from ..datatypes import TranslationAndMat3x3
+
+        return Transform3D(cast(TranslationAndMat3x3, self)).as_component_batches()
+
+    def num_instances(self) -> int:
+        # Always a mono-component
+        return 1
