566c89567511e75435d3e234fa8e261491d2f454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:34 +0000

    Add package filter-stations/0.4.3/filter_stations-0.4.3.tar.gz

---
+++ packages/filter-stations/filter_stations-0.4.3.tar.gz/filter_stations-0.4.3/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='filter_stations',
+    version='0.4.3',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Making it easier to navigate and clean station data',
+    author='Austin Kaburia',
+    author_email='kaburiaaustin1@gmail.com',
+    url='https://github.com/kaburia/Packaging/tree/main/filter_stations',
+    install_requires=[
+        'pandas',
+        'requests',
+        'python-dateutil',
+        'argparse',
+        'haversine',
+        'matplotlib',
+        'numpy',
+        'IPython',
+        'folium',
+        'datetime',
+        'statsmodels',
+        'tqdm'
+    ],
+    entry_points={
+        'console_scripts': [
+            'my-script=filter_stations.filter_stations:main'
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
