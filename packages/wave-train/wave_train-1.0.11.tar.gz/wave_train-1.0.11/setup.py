9219f23449f0b5c88bb5774bc918ee7ff77c90a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:29 +0000

    Add package wave-train/1.0.11/wave_train-1.0.11.tar.gz

---
+++ packages/wave-train/wave_train-1.0.11.tar.gz/wave_train-1.0.11/setup.py
@@ -0,0 +1,7 @@
+# To install a development version run: pip install -e .
+# in the root directory
+# Import command : import wave_train.foo
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
