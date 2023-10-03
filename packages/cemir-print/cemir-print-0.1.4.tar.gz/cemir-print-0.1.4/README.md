1bb68e1b546f4e4acac331cf781d8a024f07b4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:06 +0000

    Add package cemir-print/0.1.4/cemir-print-0.1.4.tar.gz

---
+++ packages/cemir-print/cemir-print-0.1.4.tar.gz/cemir-print-0.1.4/README.md
@@ -0,0 +1,9 @@
+`from cemir_print.ccprint import ccprint`
+
+
+```
+ccprint("Hello, World!")
+ccprint([1, 2, 3, 4, 5, 6,{"name": "İzmir", "plaka": 35}])
+ccprint(42)
+ccprint(False)
+```
---
+++ packages/cemir-print/cemir-print-0.1.4.tar.gz/cemir-print-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='cemir-print',
+    version='0.1.4',
+    description='A colorful printing library for Python',
+    author='muslu yüksektepe',
+    author_email='musluyuksektepe@gmail.com',
+    url='https://github.com/muslu/cemir-print',
+    packages=['cemir_print'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
