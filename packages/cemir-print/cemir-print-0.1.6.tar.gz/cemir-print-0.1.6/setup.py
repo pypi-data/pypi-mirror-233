fcf04c1a7837b954534351213babafa94ba73ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:08:07 +0000

    Add package cemir-print/0.1.6/cemir-print-0.1.6.tar.gz

---
+++ packages/cemir-print/cemir-print-0.1.6.tar.gz/cemir-print-0.1.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cemir-print',
+    version='0.1.6',
+    description='A colorful printing library for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='muslu yüksektepe',
+    author_email='musluyuksektepe@gmail.com',
+    url='https://github.com/muslu/cemir-print',
+    packages=['cemir_print'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
