1c8c765faa0d149ffc1f6db1d8ffd3540d933775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:17 +0000

    Add package shauntal-tracker/1.0.2/shauntal_tracker-1.0.2.tar.gz

---
+++ packages/shauntal-tracker/shauntal_tracker-1.0.2.tar.gz/shauntal_tracker-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='shauntal_tracker',
+    version='1.0.2',  # Your unique version number
+    description='Track your consistency on a line graph',
+    author='Jamangi',
+    author_email='jamangi.jmg@gmail.com',
+    packages=['shauntal_tracker'],
+    install_requires=['matplotlib'],
+)
