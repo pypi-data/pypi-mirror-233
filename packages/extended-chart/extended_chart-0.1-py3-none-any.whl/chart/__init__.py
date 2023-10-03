7139964ee66c7222e9d400094655f28cc953d13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:30 +0000

    Add package extended-chart/0.1/extended_chart-0.1-py3-none-any.whl

---
+++ packages/extended-chart/extended_chart-0.1-py3-none-any.whl/chart/__init__.py
@@ -0,0 +1,8 @@
+from chart import style
+from chart.add_overlay import add_overlay
+from chart.add_marker import add_marker
+
+from chart.ExtendedChart import ExtendedChart
+from chart.add_trade_marker import add_trade_marker
+from chart.add_histogram import add_histogram
+from chart.add_trade_marker import trade_colors
---
+++ packages/extended-chart/extended_chart-0.1-py3-none-any.whl/extended_chart/add_histogram.py
@@ -0,0 +1,40 @@
+from types import NoneType
+from pandas import DataFrame, Series
+from extended_chart.add_overlay import add_overlay
+
+
+# TODO: I would like to use histograms into my charts for rendering equity curves and indicator histograms
+#  but tradingview lightweight chart does not support y-axis for histograms so I can't use this method
+
+def add_histogram(chart, data, label: str | None = None, color='#2596be', price_line: bool = False, price_label: bool = False):
+
+    if isinstance(data, Series):
+        data = data.to_frame()
+
+    if len(data.columns) == 1:
+        if isinstance(label, NoneType):
+            rename_label = data.columns[0]
+        else:
+            rename_label = label
+        data.columns = [rename_label]
+        data['color'] = color
+
+    elif len(data.columns) == 2 and 'color' in data.columns:
+        rename_label = label
+        label = set(data.columns) - set(['color'])
+        label = list(label)[0]
+
+        if not rename_label:
+            rename_label = label
+
+        data = data.rename(columns={label: rename_label})
+
+    else:
+        raise Exception('Histogram only accepts one value; and optional column color')
+
+    # TODO: Not possible by tradingview lightweight api - https://github.com/louisnw01/lightweight-charts-python/issues/124
+    # add_overlay(chart, data=data.drop('color', axis=1, errors='ignore'), color='rgba(0,0,0,0)')
+    hist_chart = chart.create_histogram(name=rename_label, price_label=price_label, price_line=price_line)
+    hist_chart.set(data)
+
+    return hist_chart
