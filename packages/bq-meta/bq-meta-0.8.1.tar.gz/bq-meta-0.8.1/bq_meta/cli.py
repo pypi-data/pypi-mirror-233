4192254b8fc2491892de1145258142d90474fd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:08:09 +0000

    Add package bq-meta/0.8.1/bq-meta-0.8.1.tar.gz

---
+++ packages/bq-meta/bq-meta-0.8.1.tar.gz/bq-meta-0.8.1/bq_meta/cli.py
@@ -0,0 +1,100 @@
+import os
+from typing import Optional
+
+import click
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
+from google.cloud.bigquery.table import TableReference
+
+from bq_meta import const, output
+from bq_meta.client import Client
+from bq_meta.config import Config
+from bq_meta.initialize import initialize
+from bq_meta.service.history_service import HistoryService
+from bq_meta.service.project_service import ProjectService
+from bq_meta.service.table_service import TableService
+from bq_meta.service.snippet_service import SnippetService
+from bq_meta.service.version_service import VersionService
+from bq_meta.util import table_utils
+from bq_meta.window import Window
+from loguru import logger
+import os
+
+
+@click.command()
+@click.argument("full_table_id", required=False)
+@click.option("--raw", help="View raw response from the BigQuery for specific 'FULL_TABLE_ID'", is_flag=True)
+@click.option("--init", help="Initialize 'bq-meta' configuration", is_flag=True)
+@click.option("--info", help="Print info of currently used account", is_flag=True)
+@click.option("--fetch-projects", help="Fetch available google projects", is_flag=True)
+@click.option("--debug", help="Log debug messages into BQ_META_HOME/debug.log", is_flag=True)
+@click.option("--trace", help="Log tace messages into BQ_META_HOME/trace.log", is_flag=True)
+@click.version_option()
+def cli(
+    full_table_id: Optional[str],
+    raw: bool,
+    init: bool,
+    info: bool,
+    fetch_projects: bool,
+    debug: bool,
+    trace: bool,
+):
+    """BiqQuery metadata"""
+
+    ctx = click.get_current_context()
+    console = Console(theme=const.theme, soft_wrap=True, force_interactive=True)
+    config = Config()
+    client = Client(console, config)
+    project_service = ProjectService(console, config, client)
+    table_service = TableService(console, config, client, project_service)
+    snippet_service = SnippetService()
+    history_service = HistoryService(console, config, table_service)
+    window = Window(console, config, history_service, table_service, snippet_service)
+    table = None
+
+    logger.remove(0) # disable stdout handler
+    if debug:
+        if os.path.exists(const.BQ_META_DEBUG):
+            os.remove(const.BQ_META_DEBUG)
+        logger.add(const.BQ_META_DEBUG, colorize=True, level="DEBUG")
+
+    if trace:
+        if os.path.exists(const.BQ_META_TRACE):
+            os.remove(const.BQ_META_TRACE)
+        logger.add(const.BQ_META_TRACE, colorize=True, level="TRACE")
+
+    if os.path.exists(const.BQ_META_HOME):
+        version_service = VersionService()
+        version_service.update_config(config)
+
+    if init:
+        initialize(config, console, project_service)
+        ctx.exit()
+    elif not os.path.exists(const.BQ_META_HOME):
+        panel = Panel(
+            title="Not initialized, run",
+            renderable=Text("bq-meta --init"),
+            expand=False,
+            padding=(1, 3),
+            border_style=const.request_style,
+        )
+        console.print(panel)
+        ctx.exit()
+    elif info:
+        console.print(output.get_config_info(config))
+        ctx.exit()
+    elif fetch_projects:
+        project_service.fetch_projects()
+        ctx.exit()
+    elif full_table_id:
+        table_ref = TableReference.from_string(full_table_id.replace(":", "."))
+        project_id = table_ref.project
+        dataset_id = table_ref.dataset_id
+        table_id = table_ref.table_id
+        table = table_service.get_table(project_id, dataset_id, table_id)
+        if raw:
+            console.print_json(table_utils.get_properties(table))
+            ctx.exit()
+
+    window.live_window(table)
---
+++ packages/bq-meta/bq-meta-0.8.1.tar.gz/bq-meta-0.8.1/bq_meta/service/history_service.py
@@ -0,0 +1,46 @@
+from typing import List, Optional
+
+from bq_meta import const
+from bq_meta.config import Config
+from bq_meta.service.table_service import TableService
+from bq_meta.util import bash_util
+from google.cloud.bigquery.table import Table, TableReference
+from rich.console import Console
+from rich.live import Live
+from loguru import logger
+
+
+class HistoryService:
+    def __init__(self, console: Console, config: Config, table_service: TableService) -> None:
+        self.console = console
+        self.config = config
+        self.table_service = table_service
+        self.history_path = const.BQ_META_HISTORY
+
+    def list_tables(self) -> List[str]:
+        projects = open(self.history_path, "r").read().splitlines()
+        return projects
+
+    def save_table(self, table: Table):
+        history = self.list_tables()
+        try:
+            history.remove(table.full_table_id)
+        except ValueError:
+            pass
+        history.append(table.full_table_id)
+        logger.debug(f"Saved table: {table}")
+        with open(self.history_path, "w") as f:
+            f.write("\n".join(history))
+
+    def pick_table(self, live: Optional[Live]) -> Optional[Table]:
+        history = self.list_tables()
+        from_history = bash_util.pick_one(history, live)
+        table = None
+        if from_history:
+            table_ref = TableReference.from_string(from_history.replace(":", "."))
+            project_id = table_ref.project
+            dataset_id = table_ref.dataset_id
+            table_id = table_ref.table_id
+            table = self.table_service.get_table(project_id, dataset_id, table_id, live)
+        logger.debug(f"Picked table: {table}")
+        return table
