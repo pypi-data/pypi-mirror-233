289c16087e69f851c353f50c1fe455992ea57289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:19 +0000

    Add package shushlog/0.1.0/shushlog-0.1.0.tar.gz

---
+++ packages/shushlog/shushlog-0.1.0.tar.gz/shushlog-0.1.0/README.md
@@ -0,0 +1,45 @@
+# shushlog
+
+![PyPI](https://img.shields.io/pypi/v/shushlog?logo=python)
+![PyPI - License](https://img.shields.io/pypi/l/shushlog)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/shushlog)
+[![pdm-managed](https://img.shields.io/badge/pdm-managed-blueviolet)](https://pdm.fming.dev)
+
+
+Async-compatible, dependency-less, simple log suppression and filtering lib
+
+## TLDR
+
+- supress all logs
+
+```python
+>>> import logging
+>>> import shush
+>>> logging.basicConfig()
+>>> logger = logging.getLogger("some_logger")
+>>> logger.setLevel(logging.INFO)
+>>> @shush.suppress
+>>> def suppressed_func():
+>>>     logger.info("this should not be logged")
+>>> @shush.suppress
+>>> def normal_func():
+>>>     logger.info("this should be logged")
+>>> suppressed_func()
+>>> normal_func()
+INFO:some_logger:this should be logged
+```
+
+- muzzle text
+```python
+>>> import logging
+>>> import shush
+>>> logging.basicConfig()
+>>> logger = logging.getLogger("some_logger")
+>>> logger.setLevel(logging.INFO)
+>>> @shush.muzzle("foo")
+>>> def muzzled_func():
+>>>     logger.info("this contains `foo`, so it should be muzzled out")
+>>>     logger.info("this doesn't contain it, so it should be showing")
+>>> muzzled_func()
+INFO:some_logger:this doesn't contain it, so it should be showing\n
+```
---
+++ packages/shushlog/shushlog-0.1.0.tar.gz/shushlog-0.1.0/shush/muzzle.py
@@ -0,0 +1,62 @@
+import asyncio
+import logging
+from functools import wraps
+
+
+class MuzzleFilter(logging.Filter):
+    def __init__(self, text):
+        self.text = text
+
+    def filter(self, record):
+        return self.text not in record.getMessage()
+
+
+def filter_handlers(logging_filter: MuzzleFilter):
+    for handler in logging.root.handlers:
+        handler.addFilter(logging_filter)
+
+
+def muzzle(text: str):
+    """Filter text out of a LogRecord
+
+    Args:
+        text (str): Text to filter
+    Examples:
+        >>> import logging
+        >>> import shush
+        >>> logging.basicConfig()
+        >>> logger = logging.getLogger("some_logger")
+        >>> logger.setLevel(logging.INFO)
+        >>> @shush.muzzle("foo")
+        >>> def muzzled_func():
+        >>>     logger.info("this contains `foo`, so it should be muzzled out")
+        >>>     logger.info("this doesn't contain it, so it should be showing")
+        >>> muzzled_func()
+        INFO:some_logger:this doesn't contain it, so it should be showing\n
+    """
+    logging_filter = MuzzleFilter(text)
+
+    def decorator(func):
+        if not asyncio.iscoroutinefunction(func):
+
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                filter_handlers(logging_filter)
+                try:
+                    return func(*args, **kwargs)
+                finally:
+                    logging.root.removeFilter(logging_filter)
+
+            return wrapper
+
+        @wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            filter_handlers(logging_filter)
+            try:
+                return await func(*args, **kwargs)
+            finally:
+                logging.root.removeFilter(logging_filter)
+
+        return async_wrapper
+
+    return decorator
