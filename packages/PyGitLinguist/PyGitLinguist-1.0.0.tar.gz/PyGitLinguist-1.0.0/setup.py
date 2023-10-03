f91f9521a194c87c41604059da9becbf1b016c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 14:06:33 +0000

    Add package PyGitLinguist/1.0.0/PyGitLinguist-1.0.0.tar.gz

---
+++ packages/PyGitLinguist/PyGitLinguist-1.0.0.tar.gz/PyGitLinguist-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyGitLinguist',
+    version='1.0.0',
+    description='Python version of github\'s Linguist',
+    author='Guy Nachshon',
+    author_email='guy.na8@gmail.com',
+    url='https://github.com/guy-nachshon/pylinguist',
+    packages=find_packages()
+)
