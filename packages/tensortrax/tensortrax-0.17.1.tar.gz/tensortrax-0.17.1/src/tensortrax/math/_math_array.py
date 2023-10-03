11a3c71340a4bdede3741cf6ff7983f0062ed6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:00 +0000

    Add package tensortrax/0.17.1/tensortrax-0.17.1.tar.gz

---
+++ packages/tensortrax/tensortrax-0.17.1.tar.gz/tensortrax-0.17.1/src/tensortrax/math/_math_array.py
@@ -0,0 +1,30 @@
+r"""
+ _
+| |                          ████████╗██████╗  █████╗ ██╗  ██╗
+| |_ ___ _ __  ___  ___  _ __╚══██╔══╝██╔══██╗██╔══██╗╚██╗██╔╝
+| __/ _ \ '_ \/ __|/ _ \| '__|  ██║   ██████╔╝███████║ ╚███╔╝
+| ||  __/ | | \__ \ (_) | |     ██║   ██╔══██╗██╔══██║ ██╔██╗
+ \__\___|_| |_|___/\___/|_|     ██║   ██║  ██║██║  ██║██╔╝ ██╗
+                                ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+"""
+
+import numpy as np
+
+from .._tensor import Tensor, f
+
+
+def eye(A):
+    "Identity (Eye) of a Tensor."
+    if isinstance(A, Tensor):
+        B = np.zeros_like(f(A))
+    else:
+        B = np.zeros_like(A)
+    B[np.diag_indices(B.shape[0])] = 1
+    return B
+
+
+def cross(a, b):
+    "Cross product of two vectors a and b."
+    return np.einsum(
+        "...i->i...", np.cross(np.einsum("i...->...i", a), np.einsum("i...->...i", b))
+    )
