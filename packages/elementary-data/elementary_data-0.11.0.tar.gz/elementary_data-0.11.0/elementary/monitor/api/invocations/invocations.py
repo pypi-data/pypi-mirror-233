861e1314953b0d010d29bcf78f6d33bd47a6f2f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:07:53 +0000

    Add package elementary-data/0.11.0/elementary_data-0.11.0.tar.gz

---
+++ packages/elementary-data/elementary_data-0.11.0.tar.gz/elementary_data-0.11.0/elementary/monitor/api/invocations/invocations.py
@@ -0,0 +1,47 @@
+from typing import Dict, List
+
+from elementary.clients.api.api_client import APIClient
+from elementary.clients.dbt.base_dbt_runner import BaseDbtRunner
+from elementary.monitor.fetchers.invocations.invocations import InvocationsFetcher
+from elementary.monitor.fetchers.invocations.schema import DbtInvocationSchema
+from elementary.utils.log import get_logger
+
+logger = get_logger(__name__)
+
+
+class InvocationsAPI(APIClient):
+    def __init__(self, dbt_runner: BaseDbtRunner):
+        super().__init__(dbt_runner)
+        self.invocations_fetcher = InvocationsFetcher(dbt_runner=self.dbt_runner)
+
+    def get_last_invocation(self, type: str) -> DbtInvocationSchema:
+        if type == "test":
+            return self.invocations_fetcher.get_test_last_invocation()
+        else:
+            raise NotImplementedError
+
+    def get_invocation_by_time(
+        self, type: str, invocation_max_time: str
+    ) -> DbtInvocationSchema:
+        if type == "test":
+            return self.invocations_fetcher.get_test_last_invocation(
+                macro_args=dict(invocation_max_time=invocation_max_time)
+            )
+        else:
+            raise NotImplementedError
+
+    def get_invocation_by_id(
+        self, type: str, invocation_id: str
+    ) -> DbtInvocationSchema:
+        if type == "test":
+            return self.invocations_fetcher.get_test_last_invocation(
+                macro_args=dict(invocation_id=invocation_id)
+            )
+        else:
+            raise NotImplementedError
+
+    def get_models_latest_invocations_data(self) -> List[DbtInvocationSchema]:
+        return self.invocations_fetcher.get_models_latest_invocations_data()
+
+    def get_models_latest_invocation(self) -> Dict[str, str]:
+        return self.invocations_fetcher.get_models_latest_invocation()
---
+++ packages/elementary-data/elementary_data-0.11.0.tar.gz/elementary_data-0.11.0/elementary/monitor/dbt_project/macros/get_nodes_depends_on_nodes.sql
@@ -0,0 +1,38 @@
+{% macro get_nodes_depends_on_nodes(exclude_elementary=false) %}
+    {% set models_depends_on_nodes_query %}
+        with dbt_models as (
+            select * from {{ ref('elementary', 'dbt_models') }}
+            {% if exclude_elementary %}
+              where package_name != 'elementary'
+            {% endif %}
+        ),
+
+        dbt_sources as (
+            select * from {{ ref('elementary', 'dbt_sources') }}
+        ),
+
+        dbt_exposures as (
+            select * from {{ ref('elementary', 'dbt_exposures') }}
+        )
+
+        select
+            unique_id,
+            depends_on_nodes,
+            'model' as type
+        from dbt_models 
+        union all
+        select
+            unique_id,
+            null as depends_on_nodes,
+            'source' as type
+        from dbt_sources
+        union all
+        select
+            unique_id,
+            depends_on_nodes,
+            'exposure' as type
+        from dbt_exposures   
+    {% endset %}
+    {% set models_depends_on_agate = run_query(models_depends_on_nodes_query) %}
+    {% do return(elementary.agate_to_dicts(models_depends_on_agate)) %}
+{% endmacro %}
