20c65ab724c7a971027faed4764bc1c01cf86323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:07:46 +0000

    Add package file-and-string-encryption/0.0.7/file_and_string_encryption-0.0.7-py3-none-any.whl

---
+++ packages/file-and-string-encryption/file_and_string_encryption-0.0.7-py3-none-any.whl/file_and_string_encryption/__init__.py
@@ -0,0 +1,26 @@
+from .file_and_string_encryption import (
+    bytes_to_data_using_pickle,
+    data_to_bytes_using_pickle,
+    decrypt_data,
+    decrypt_directory,
+    decrypt_file,
+    decrypt_file_with_password,
+    decrypt_hidden_directory,
+    decrypt_string_with_password,
+    derive_key_from_password,
+    encrypt_and_hide_directory,
+    encrypt_data,
+    encrypt_directory,
+    encrypt_file,
+    encrypt_file_with_password,
+    encrypt_string_with_password,
+    generate_password,
+    generate_semi_random_alphanumeric_string,
+    get_random_key,
+    hash_password_with_argon2,
+    load_password_encrypted_key,
+    save_password_encrypted_key,
+    verfiy_hashed_password_with_argon2,
+)
+
+__version__ = "0.0.7"
