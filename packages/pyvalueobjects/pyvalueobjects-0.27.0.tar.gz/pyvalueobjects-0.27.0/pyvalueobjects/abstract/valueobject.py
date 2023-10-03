95146815e055b1bdae745aa0109283fe799d3e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:33 +0000

    Add package pyvalueobjects/0.27.0/pyvalueobjects-0.27.0.tar.gz

---
+++ packages/pyvalueobjects/pyvalueobjects-0.27.0.tar.gz/pyvalueobjects-0.27.0/pyvalueobjects/abstract/valueobject.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+
+class ValueObject(ABC):
+
+    def __init__(self, value):
+        self._validate(value)
+        self._value = value
+
+    def value(self):
+        return self._value
+
+    def str_value(self) -> str:
+        return str(self._value)
+
+    def _validate(self, value):
+        pass
