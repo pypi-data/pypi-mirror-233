e7b686b235d5ba15e8d5befddf96116e6d83ae69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 17:06:34 +0000

    Add package emb3d/0.1.103/emb3d-0.1.103.tar.gz

---
+++ packages/emb3d/emb3d-0.1.103.tar.gz/emb3d-0.1.103/emb3d/reader.py
@@ -0,0 +1,15 @@
+"""
+Readers
+"""
+import json
+from pathlib import Path
+
+
+def line(fname: Path):
+    with fname.open() as f:
+        for line in f:
+            yield line.strip()
+
+def jsonl(fname: Path):
+    for nxt_line in line(fname):
+        yield json.loads(nxt_line)
