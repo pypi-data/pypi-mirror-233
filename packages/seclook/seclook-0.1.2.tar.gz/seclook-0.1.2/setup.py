04fa46171644d8cba2a710875b57cc0c1878f643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:22 +0000

    Add package seclook/0.1.2/seclook-0.1.2.tar.gz

---
+++ packages/seclook/seclook-0.1.2.tar.gz/seclook-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="seclook",
+    version="0.1.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "requests",
+        "configparser",
+    ],
+    entry_points={
+        "console_scripts": [
+            "seclook=seclook.cli:main",
+        ],
+    },
+    description="Simple security lookups via CLI",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ackatz/seclook",
+)
