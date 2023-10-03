e7897850d5180484a2f82a24f75f341b208147b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 15:06:27 +0000

    Add package my-package-packagingcon23/1.0.0/my-package-packagingcon23-1.0.0.tar.gz

---
+++ packages/my-package-packagingcon23/my-package-packagingcon23-1.0.0.tar.gz/my-package-packagingcon23-1.0.0/README.md
@@ -0,0 +1,1 @@
+# packagingCon23-SLSA
---
+++ packages/my-package-packagingcon23/my-package-packagingcon23-1.0.0.tar.gz/my-package-packagingcon23-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='my-package-packagingcon23',
+    version="1.0.0",
+    description='my-package-packagingcon23',
+    long_description="",
+    long_description_content_type='text/markdown',
+)
