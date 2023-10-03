9898f549c2aebcb661162e36a4840292ec1ecda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-03 16:08:43 +0000

    Add package fake-useragent/1.3.0/fake-useragent-1.3.0.tar.gz

---
+++ packages/fake-useragent/fake-useragent-1.3.0.tar.gz/fake-useragent-1.3.0/tests/test_utils.py
@@ -0,0 +1,39 @@
+import sys
+
+if sys.version_info >= (3, 10):
+    import importlib.resources as ilr
+else:
+    import importlib_resources as ilr
+
+import unittest
+from unittest.mock import patch
+
+from fake_useragent import utils
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_utils_load(self):
+        data = utils.load()
+
+        self.assertIsInstance(data, list)
+        self.assertGreater(len(data), 10)
+        self.assertIsInstance(data[0], object)
+        self.assertIsInstance(data[0]["useragent"], str)
+
+    def test_utils_load_pkg_resource_fallback(self):
+        # By default use_local_file is also True during production
+        # We will not allow the default importlib resources to be used, by triggering an Exception
+        with patch.object(ilr, "files") as mocked_importlib_resources_files:
+            # This exception should trigger the alternative path, trying to use pkg_resource as fallback
+            mocked_importlib_resources_files.side_effect = Exception("Error")
+            data = utils.load()
+
+        self.assertIsInstance(data, list)
+        self.assertIsInstance(data[0], object)
+        self.assertIsInstance(data[0]["useragent"], str)
