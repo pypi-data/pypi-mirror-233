d52105784c730e2471a209a16755faff7013f9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:07:39 +0000

    Add package jbturtle/0.2.1/jbturtle-0.2.1.tar.gz

---
+++ packages/jbturtle/jbturtle-0.2.1.tar.gz/jbturtle-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'jbturtle',
+    version = '0.2.1',
+    packages = find_packages(),
+    install_requires = [
+        'Pillow',
+    ],
+)
+
