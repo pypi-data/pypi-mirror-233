f1a81750196287143a46500c8a4c9442e9c0f8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:08:41 +0000

    Add package rank-llm/0.0.7/rank_llm-0.0.7-py3-none-any.whl

---
+++ packages/rank-llm/rank_llm-0.0.7-py3-none-any.whl/rank_llm/rank_vicuna.py
@@ -0,0 +1,109 @@
+import re
+from typing import Tuple, List, Union, Dict, Any
+
+from fastchat.model import load_model, get_conversation_template, add_model_args
+from ftfy import fix_text
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers.generation import GenerationConfig
+
+from rank_llm.rankllm import RankLLM, PromptMode
+
+
+def replace_number(s):
+    return re.sub(r"\[(\d+)\]", r"(\1)", s)
+
+
+class RankVicuna(RankLLM):
+    def __init__(
+        self,
+        model: str,
+        context_size: int,
+        top_k_candidates: int,
+        dataset: str,
+        prompt_mode: PromptMode,
+        device: str,
+        num_gpus: int,
+    ) -> None:
+        super().__init__(model, context_size, top_k_candidates, dataset, prompt_mode)
+        self._device = device
+        if self._device == "cuda":
+            assert torch.cuda.is_available()
+        if prompt_mode != PromptMode.RANK_GPT:
+            raise ValueError(
+                f"Unsuported prompt mode: {prompt_mode}. The only prompt mode cuurently supported by vicuna is a slight variation of Rank_GPT prompt."
+            )
+        # ToDo: Make repetition_penalty configurable
+        self._llm, self._tokenizer = load_model(model, device=device, num_gpus=num_gpus)
+
+    def run_llm(self, prompt: str) -> Tuple[str, int]:
+        inputs = self._tokenizer([prompt])
+        inputs = {k: torch.tensor(v).to(self._device) for k, v in inputs.items()}
+        gen_cfg = GenerationConfig.from_model_config(self._llm.config)
+        gen_cfg.max_new_tokens = self.num_output_tokens()
+        gen_cfg.min_length = 1
+        # gen_cfg.temperature = 0
+        gen_cfg.do_sample = False
+        output_ids = self._llm.generate(**inputs, generation_config=gen_cfg)
+
+        if self._llm.config.is_encoder_decoder:
+            output_ids = output_ids[0]
+        else:
+            output_ids = output_ids[0][len(inputs["input_ids"][0]) :]
+        outputs = self._tokenizer.decode(
+            output_ids, skip_special_tokens=True, spaces_between_special_tokens=False
+        )
+        return outputs, output_ids.size(0)
+
+    def num_output_tokens(self) -> int:
+        return 200
+
+    def _add_prefix_prompt(self, query: str, num: int) -> str:
+        return f"I will provide you with {num} passages, each indicated by a numerical identifier []. Rank the passages based on their relevance to the search query: {query}.\n"
+
+    def _add_post_prompt(self, query: str, num: int) -> str:
+        return f"Search Query: {query}.\nRank the {num} passages above based on their relevance to the search query. All the passages should be included and listed using identifiers, in descending order of relevance. The output format should be [] > [], e.g., [4] > [2], Only respond with the ranking results, do not say any word or explain."
+
+    def create_prompt(
+        self, retrieved_result: Dict[str, Any], rank_start: int, rank_end: int
+    ) -> Tuple[str, int]:
+        query = retrieved_result["query"]
+        num = len(retrieved_result["hits"][rank_start:rank_end])
+        max_length = 300
+        while True:
+            conv = get_conversation_template(self._model)
+            # conv.set_system_message(
+            #     "You are RankVicuna, an intelligent assistant that can rank passages based on their relevancy to the query."
+            # )
+            prefix = self._add_prefix_prompt(query, num)
+            rank = 0
+            input_context = f"{prefix}\n"
+            for hit in retrieved_result["hits"][rank_start:rank_end]:
+                rank += 1
+                content = hit["content"]
+                content = content.replace("Title: Content: ", "")
+                content = content.strip()
+                # For Japanese should cut by character: content = content[:int(max_length)]
+                content = " ".join(content.split()[: int(max_length)])
+                input_context += f"[{rank}] {replace_number(content)}\n"
+
+            input_context += self._add_post_prompt(query, num)
+            conv.append_message(conv.roles[0], input_context)
+            prompt = conv.get_prompt() + " ASSISTANT:"
+            prompt = fix_text(prompt)
+            num_tokens = self.get_num_tokens(prompt)
+            if num_tokens <= self.max_tokens() - self.num_output_tokens():
+                break
+            else:
+                max_length -= max(
+                    1,
+                    (num_tokens - self.max_tokens() + self.num_output_tokens())
+                    // (rank_end - rank_start),
+                )
+        return prompt, self.get_num_tokens(prompt)
+
+    def get_num_tokens(self, prompt: str) -> int:
+        return len(self._tokenizer.encode(prompt))
+
+    def cost_per_1k_token(self, input_token: bool) -> float:
+        return 0
---
+++ packages/rank-llm/rank_llm-0.0.7-py3-none-any.whl/rank_llm/run_rank_llm.py
@@ -0,0 +1,168 @@
+import argparse
+import json
+from pathlib import Path
+
+import torch
+from tqdm import tqdm
+
+import sys
+import os
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from rank_llm.pyserini_retriever import PyseriniRetriever, RetrievalMethod
+from rank_llm.rank_gpt import SafeOpenai
+from rank_llm.rankllm import PromptMode
+from rank_llm.rank_vicuna import RankVicuna
+from rank_llm.topics_dict import TOPICS
+from rank_llm.trec_eval import EvalFunction
+
+
+def get_api_key() -> str:
+    from dotenv import dotenv_values, load_dotenv
+    import os
+
+    load_dotenv(dotenv_path=f"../.env.local")
+    return os.getenv("OPEN_AI_API_KEY")
+
+
+def main(args):
+    model_path = args.model_path
+    context_size = args.context_size
+    top_k_candidates = args.top_k_candidates
+    dataset = args.dataset
+    num_gpus = args.num_gpus
+    retrieval_method = args.retrieval_method
+    prompt_mode = args.prompt_mode
+    shuffle_candidates = args.shuffle_candidates
+    print_prompts_responses = args.print_prompts_responses
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if "gpt" in model_path:
+        openai_keys = get_api_key()
+        agent = SafeOpenai(
+            model=model_path,
+            context_size=context_size,
+            top_k_candidates=top_k_candidates,
+            dataset=dataset,
+            prompt_mode=prompt_mode,
+            keys=openai_keys,
+        )
+    else:
+        agent = RankVicuna(
+            model=model_path,
+            context_size=context_size,
+            top_k_candidates=top_k_candidates,
+            dataset=dataset,
+            prompt_mode=prompt_mode,
+            device=device,
+            num_gpus=num_gpus,
+        )
+    candidates_file = Path(
+        f"../retrieve_results/{retrieval_method.name}/retrieve_results_{dataset}.json"
+    )
+    if not candidates_file.is_file():
+        print("Retrieving:")
+        retriever = PyseriniRetriever(dataset, retrieval_method)
+        # Always retrieve top 100 so that results are reusable for all top_k_candidates values.
+        retriever.retrieve_and_store(k=100)
+    else:
+        print("Reusing existing retrieved results.")
+
+    with open(candidates_file, "r") as f:
+        retrieved_results = json.load(f)
+
+    print("\nReranking:")
+    rerank_results = []
+    input_token_counts = []
+    output_token_counts = []
+    aggregated_prompts = []
+    aggregated_responses = []
+    for result in tqdm(retrieved_results):
+        (
+            rerank_result,
+            in_token_count,
+            out_token_count,
+            prompts,
+            responses,
+        ) = agent.sliding_windows(
+            result,
+            rank_start=0,
+            rank_end=top_k_candidates,
+            window_size=20,
+            step=10,
+            shuffle_candidates=shuffle_candidates,
+            logging=print_prompts_responses,
+        )
+        rerank_results.append(rerank_result)
+        input_token_counts.append(in_token_count)
+        output_token_counts.append(out_token_count)
+        aggregated_prompts.extend(prompts)
+        aggregated_responses.extend(responses)
+    print(f"input_tokens_counts={input_token_counts}")
+    print(f"total input token count={sum(input_token_counts)}")
+    print(f"output_token_counts={output_token_counts}")
+    print(f"total output token count={sum(output_token_counts)}")
+    file_name = agent.write_rerank_results(
+        retrieval_method.name,
+        rerank_results,
+        input_token_counts,
+        output_token_counts,
+        aggregated_prompts,
+        aggregated_responses,
+        shuffle_candidates,
+    )
+    EvalFunction.eval(["-c", "-m", "ndcg_cut.1", TOPICS[dataset], file_name])
+    EvalFunction.eval(["-c", "-m", "ndcg_cut.5", TOPICS[dataset], file_name])
+    EvalFunction.eval(["-c", "-m", "ndcg_cut.10", TOPICS[dataset], file_name])
+
+
+""" sample run:
+python rank_vicuna.py --model_path=../checkpoints/vicuna/vicuna-7b-checkpoint-800 --dataset=dl19 --retrieval_method=bm25
+"""
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path", type=str, required=True, help="Path to the model"
+    )
+    parser.add_argument(
+        "--context_size", type=int, default=4096, help="context size used for model"
+    )
+    parser.add_argument(
+        "--top_k_candidates",
+        type=int,
+        default=100,
+        help="the number of top candidates to rerank",
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        required=True,
+        help=f"dataset name, must be in {TOPICS.keys()}",
+    )
+    parser.add_argument(
+        "--num_gpus", type=int, default=1, help="the number of GPUs to use"
+    )
+    parser.add_argument(
+        "--retrieval_method",
+        type=RetrievalMethod,
+        required=True,
+        choices=list(RetrievalMethod),
+    )
+    parser.add_argument(
+        "--prompt_mode",
+        type=PromptMode,
+        required=True,
+        choices=list(PromptMode),
+    )
+    parser.add_argument(
+        "--shuffle_candidates",
+        action="store_true",
+        help="whether to shuffle the candidates before reranking",
+    )
+    parser.add_argument(
+        "--print_prompts_responses",
+        action="store_true",
+        help="whether to print promps and responses",
+    )
+    args = parser.parse_args()
+    main(args)
