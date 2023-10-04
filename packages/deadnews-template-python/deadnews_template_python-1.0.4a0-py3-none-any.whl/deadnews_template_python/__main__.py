1c1ce1caba46dc96c76ad1a6609394985d168c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:11 +0000

    Add package deadnews-template-python/1.0.4a0/deadnews_template_python-1.0.4a0-py3-none-any.whl

---
+++ packages/deadnews-template-python/deadnews_template_python-1.0.4a0-py3-none-any.whl/deadnews_template_python/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""Entrypoint for app, enables execution with `python -m deadnews_template_python`."""
+from deadnews_template_python._main import main
+
+if __name__ == "__main__":
+    main()
