aa0bec15d89dc4abe97a96cdca3cc91f508422b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:07:00 +0000

    Add package elemeno-ai-sdk/0.6.3/elemeno_ai_sdk-0.6.3-py2.py3-none-any.whl

---
+++ packages/elemeno-ai-sdk/elemeno_ai_sdk-0.6.3-py2.py3-none-any.whl/elemeno_ai_sdk/ml/automl/client.py
@@ -0,0 +1,43 @@
+from typing import Dict, Optional
+
+from elemeno_ai_sdk.ml.mlhub_client import MLHubRemote
+
+
+class AutoMLClient(MLHubRemote):
+    def __init__(self, env: Optional[str] = None) -> None:
+        super().__init__(env=env)
+
+    def list_jobs(self):
+        return self.get(url=f"{self.base_url}/automl")
+
+    def get_job(self, job_id: str):
+        return self.get(url=f"{self.base_url}/automl/{job_id}")
+
+    def run_job(
+        self,
+        feature_table_name: str,
+        features_selected: str,
+        id_column: str,
+        target_name: str,
+        start_date: str,
+        end_date: str,
+        task: str,
+        scoring: str,
+        num_features: int,
+        generations: int,
+    ) -> Dict[str, str]:
+        body = {
+            "featureTableName": feature_table_name,
+            "idColumn": id_column,
+            "targetName": target_name,
+            "startDate": start_date,
+            "endDate": end_date,
+            "task": task,
+            "scoring": scoring,
+            "numFeatures": num_features,
+            "generations": generations,
+        }
+        if features_selected != "":
+            body["featuresSelected"] = features_selected
+
+        return self.post(url=f"{self.base_url}/automl", body=body)
