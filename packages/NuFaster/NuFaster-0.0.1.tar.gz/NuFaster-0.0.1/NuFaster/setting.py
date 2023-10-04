fca419fed5d1bdddc5ef37d632a037e4e6275fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:05:30 +0000

    Add package NuFaster/0.0.1/NuFaster-0.0.1.tar.gz

---
+++ packages/NuFaster/NuFaster-0.0.1.tar.gz/NuFaster-0.0.1/NuFaster/setting.py
@@ -0,0 +1,17 @@
+pack_root = f'{__file__}/../pack_dir/Record'
+
+
+class Dir(str):
+    
+    def __init__(self, sub_dir):
+        self.sub_dir = sub_dir
+    
+    def str(self):
+        return self.__str__()
+    
+    def __str__(self):
+        return fr'{pack_root}\{self.sub_dir}'
+
+
+Default_build_dir = Dir('Build')  # 默认打包路径
+Default_module_dir = Dir('Module')  # 默认打包路径
---
+++ packages/NuFaster/NuFaster-0.0.1.tar.gz/NuFaster-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='NuFaster',  # 包的名称
+        version='0.0.1',  # 版本号
+        author='toca-Y',  # 作者名字
+        author_email='',  # 作者邮箱
+        description='借助nuitka更快的打包(分模块打包)',  # 包的简短描述
+        long_description='分模块打包',  # 包的详细描述
+        url='https://github.com/toca-Y/NuitkaPackV2',  # 包的主页或仓库链接
+        packages=find_packages(),  # 寻找和包含所有的 Python 包
+        install_requires=[
+            'nuitka==1.5.8'
+        ],  # 依赖项列表，如果有的话
+        classifiers=[  # 包的分类
+        ],
+)
