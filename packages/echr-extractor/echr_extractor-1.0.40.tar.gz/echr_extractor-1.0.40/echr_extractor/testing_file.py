41b3a0d30ce51787ddaf346344f0d80e5dfe25e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:07:42 +0000

    Add package echr-extractor/1.0.40/echr_extractor-1.0.40.tar.gz

---
+++ packages/echr-extractor/echr_extractor-1.0.40.tar.gz/echr_extractor-1.0.40/echr_extractor/testing_file.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+from os.path import abspath
+
+import echr_extractor
+
+current_dir = (abspath(__file__))
+correct_dir = '\\'.join(current_dir.replace('\\', '/').split('/')[:-2])
+sys.path.append(correct_dir)
+# print(sys.path)
+
+
+if __name__ == '__main__':
+    link = 'https://hudoc.echr.coe.int/eng#{"fulltext":["(NOT%20\"has%20been%20a%20violation%20of%20Article%206\")%20AND%20(\"has%20been%20no%20violation%20of%20Article%206\")"],"documentcollectionid2":["GRANDCHAMBER","CHAMBER"]}'
+    logging.info('hey')
+
+
+    df = echr_extractor.get_echr(link=link)
+    b = 2
