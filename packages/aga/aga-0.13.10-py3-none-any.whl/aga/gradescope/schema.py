cbacd7a98e1bfb08fd224f94c9fec39569c9bca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:08:22 +0000

    Add package aga/0.13.10/aga-0.13.10-py3-none-any.whl

---
+++ packages/aga/aga-0.13.10-py3-none-any.whl/aga/gradescope/schema.py
@@ -0,0 +1,105 @@
+"""Contains a TestResult and TestRunner for producing gradescope's results.json file.
+
+The design of this module is inspire by gradescope_utils's JSONTestRunner, but this is a
+complete rewrite.
+"""
+
+from dataclasses import dataclass, field
+from typing import Literal, Optional, TextIO
+
+from dataclasses_json import dataclass_json
+
+from ..runner import ProblemOutput, TcOutput
+
+
+# pylint: disable=R0902
+@dataclass_json
+@dataclass
+class GradescopeTestJson:
+    """The JSON schema for a single Test.
+
+    Attributes
+    ----------
+    score : Optional[float]
+        The test's score. Required if no top-level score is set.
+    max_score : Optional[float]
+        The max score for the test.
+    name : Optional[str]
+        The test's name.
+    output : Optional[str]
+        Human-readable text output of the test.
+    tags : Optional[List[str]]
+        Tags for the test.
+    visibility : str
+        The test's visibility. "hidden", "visible", "after_due_date", "after_published"
+    """
+
+    score: Optional[float] = None
+    max_score: Optional[float] = None
+    status: Optional[Literal["passed", "failed"]] = None
+    name: Optional[str] = None
+    number: Optional[float] = None
+    output: Optional[str] = None
+    tags: Optional[str] = None
+    visibility: Optional[str] = None
+
+    @classmethod
+    def from_test_case_output(cls, output: TcOutput) -> "GradescopeTestJson":
+        """Create a test json from a test case output."""
+        return cls(
+            score=output.score,
+            max_score=output.max_score,
+            status=output.status,
+            name=output.name,
+            output=output.rich_output,
+            visibility="hidden" if output.hidden else "visible",
+        )
+
+
+@dataclass_json
+@dataclass
+class GradescopeJson:
+    """The JSON schema for Gradescope.
+
+    We currently don't support the leaderboard and extra_data features of the gradescope
+    schema. Those are documented on the autograder documentation, here:
+    <https://gradescope-autograders.readthedocs.io/en/latest/specs/>.
+
+    Attributes
+    ----------
+    tests : List[_GradescopeJsonTest]
+        The tests for the problem. Required if no global score provided.
+    score : Optional[float]
+        The overall score. Required if any test has no set score.
+    execution_time : Optional[int]
+        The execution time of all the tests, in seconds.
+    output : Optional[str]
+        The top-level, human-readable text output for all the problems.
+    visibility : Optional[str]
+        The default visibility for each test. Overridden by test-specific settings.
+    stdout_visibility : Optional[str]
+        Whether to show stdout for the tests. Same options as for visibility.
+    """
+
+    tests: list[GradescopeTestJson] = field(default_factory=list)
+    score: Optional[float] = None
+    execution_time: Optional[int] = None
+    output: Optional[str] = None
+    visibility: Optional[str] = None
+    stdout_visibility: Optional[str] = None
+
+    @classmethod
+    def from_problem_output(cls, output: ProblemOutput) -> "GradescopeJson":
+        """Create a gradescop json from a problem output."""
+        return cls(
+            tests=[GradescopeTestJson.from_test_case_output(tc) for tc in output.tests],
+            score=output.score,
+            output=output.output,
+        )
+
+
+def write_to(output: ProblemOutput, stream: TextIO) -> None:
+    """Write the data from output as gradescope json to a text stream."""
+    # pylint: disable=no-member
+    json = GradescopeJson.from_problem_output(output).to_json()  # type: ignore
+    stream.write(json)
---
+++ packages/aga/aga-0.13.10-py3-none-any.whl/aga/score.py
@@ -0,0 +1,179 @@
+"""Utilities for easily computing problem score."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from .core import Problem, SubmissionMetadata
+    from .core.problem import ProblemOutputType, ProblemParamSpec
+    from .runner import TcOutput
+
+
+PrizeCriteria = Callable[[list["TcOutput"], "SubmissionMetadata"], tuple[float, str]]
+
+
+@dataclass(frozen=True)
+class ScoreInfo:
+    """Info to help compute a score.
+
+    Attributes
+    ----------
+    value : float
+        The object's absolute point value.
+    weight : int
+        The relative of the remaining score this object will claim.
+    extra_credit : float
+        The absolute extra-credit point value.
+    """
+
+    weight: int
+    value: float
+    extra_credit: float
+
+
+def compute_scores(score_infos: list[ScoreInfo], total_score: float) -> list[float]:
+    """Compute the scores of a list of scorable objects.
+
+    Returns their scores in the same order as the initial list.
+    """
+    out = []
+    total_weight = 0
+    for s in score_infos:
+        total_score -= s.value
+        total_weight += s.weight
+
+    for s in score_infos:
+        if total_weight > 0:
+            out.append(
+                s.value + (total_score * s.weight) / total_weight + s.extra_credit
+            )
+        else:
+            out.append(s.value + s.extra_credit)
+
+    return out
+
+
+@dataclass(frozen=True)
+class Prize:
+    """A points prize, which can be earned by meeting certain problem-wide criteria."""
+
+    name: str
+    criteria: PrizeCriteria
+    score_info: ScoreInfo
+    hidden: bool
+
+
+@dataclass(frozen=True)
+class ScoredPrize:
+    """A prize with attached score."""
+
+    prize: Prize
+    max_score: float
+
+
+# This is an external API and users need all these arguments.
+# pylint: disable=too-many-arguments
+def prize(
+    criteria: PrizeCriteria,
+    name: str = "Prize",
+    weight: int = 1,
+    value: float = 0.0,
+    extra_credit: float = 0.0,
+    hidden: bool = False,
+) -> Callable[
+    [Problem[ProblemParamSpec, ProblemOutputType]],
+    Problem[ProblemParamSpec, ProblemOutputType],
+]:
+    """Add a points prize to the problem.
+
+    Parameters
+    ----------
+    criteria : Callable[[list[TcOutput], SubmissionMetadata], tuple[float, str]
+        The criteria for awarding the prize's points. The first returned value should be
+        a float from 0 to 1 which determines the fraction of points to assign. The
+        second should be a string which will be displayed to the student.
+    name : str
+        The name of the prize, to be displayed to the student.
+    weight : int
+        The prize's weight. See :ref:`Determining Score` for details.
+    value : int
+        The prize's absolute score. See :ref:`Determining Score` for details.
+    extra_credit : int
+        The prize's extra credit. See :ref:`Determining Score` for details.
+    hidden : bool
+        Whether the prize should be hidden from the student.
+
+    Returns
+    -------
+    Callable[[Problem[T]], Problem[T]]
+        A decorator which adds the prize to a problem.
+    """
+    to_add = Prize(name, criteria, ScoreInfo(weight, value, extra_credit), hidden)
+
+    def inner(
+        problem: Problem[ProblemParamSpec, ProblemOutputType]
+    ) -> Problem[ProblemParamSpec, ProblemOutputType]:
+        problem.add_prize(to_add)
+        return problem
+
+    return inner
+
+
+def all_correct(tests: list["TcOutput"], _: "SubmissionMetadata") -> tuple[float, str]:
+    """1.0 if all tests passed, 0.0 otherwise.
+
+    For use as a prize.
+    """
+    if all(t.is_correct() for t in tests):
+        return 1.0, "Good work! You earned these points since all tests passed."
+    else:
+        return 0.0, "To earn these points, make sure all tests pass."
+
+
+def on_time(_: list["TcOutput"], metadata: "SubmissionMetadata") -> tuple[float, str]:
+    """1.0 if the submission was on time, 0.0 otherwise.
+
+    For use as a prize.
+    """
+    if metadata.is_on_time():
+        return (
+            1.0,
+            "Good work! You earned these points by turning the assignment in on time.",
+        )
+    else:
+        return (
+            0.0,
+            "To earn this next time, make sure to turn the assignment in on time!",
+        )
+
+
+def correct_and_on_time(
+    tests: list["TcOutput"], metadata: "SubmissionMetadata"
+) -> tuple[float, str]:
+    """1.0 if the submission was correct and passed all tests, 0.0 otherwise.
+
+    For use as a prize.
+    """
+    match (bool(all_correct(tests, metadata)[0]), bool(on_time(tests, metadata)[0])):
+        case (True, True):
+            return (
+                1.0,
+                "Good work! You earned these points since all tests passed and "
+                "you turned in the assignment on time.",
+            )
+        case (False, True):
+            return 0.0, "To earn these points, make sure all tests pass."
+        case (True, False):
+            return (
+                0.0,
+                "To earn these points next time, "
+                "make sure to turn the assignment in on time.",
+            )
+        case _:
+            return (
+                0.0,
+                "To earn these points next time, "
+                "make sure to turn the assignment in on time, and that all tests pass.",
+            )
