1ff73e291710d11e8f6394de8476f5f5273e1a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:45 +0000

    Add package GausBinom-probability-Dist/0.1/GausBinom_probability_Dist-0.1.tar.gz

---
+++ packages/GausBinom-probability-Dist/GausBinom_probability_Dist-0.1.tar.gz/GausBinom_probability_Dist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GausBinom_probability_Dist',
+      version='0.1',
+      description='Gaussian and Bionomial distributions',
+      packages=['GausBinom_probability_Dist'],
+      author = "Rojan Saghian",
+      zip_safe=False)
