43876cc288efd6371739a616a373b2c3ddb446f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:05:16 +0000

    Add package savant-rs-etcd-dynamic-state/0.1.70/savant_rs_etcd_dynamic_state-0.1.70-pp38-pypy38_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/savant-rs-etcd-dynamic-state/savant_rs_etcd_dynamic_state-0.1.70-pp38-pypy38_pp73-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/savant_rs_etcd_dynamic_state/__init__.py
@@ -0,0 +1,5 @@
+from .savant_rs_etcd_dynamic_state import *
+
+__doc__ = savant_rs_etcd_dynamic_state.__doc__
+if hasattr(savant_rs_etcd_dynamic_state, "__all__"):
+    __all__ = savant_rs_etcd_dynamic_state.__all__
