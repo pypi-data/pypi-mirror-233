632c2cd405adac8abc05c1e082ed3839456bccd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:06:23 +0000

    Add package funread/0.2.0/funread-0.2.0-py3-none-any.whl

---
+++ packages/funread/funread-0.2.0-py3-none-any.whl/funread/manage/load/url.py
@@ -0,0 +1,12 @@
+from funread.manage.base.url import url_manage
+from tqdm import tqdm
+
+
+def load1():
+    for _id in tqdm(range(4100, 4167), "load"):
+        url = f"http://yckceo1.com/yuedu/shuyuan/json/id/{_id}.json"
+        url_manage.add_source(url=url, uid=_id, cate1="源仓库")
+
+
+def load_all():
+    load1()
