1b503cca30df159b221745bd785c54fc7edc9d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:08:22 +0000

    Add package week-time-ex-sar-forex-next3/1.1/week_time_ex_sar_forex_next3-1.1.tar.gz

---
+++ packages/week-time-ex-sar-forex-next3/week_time_ex_sar_forex_next3-1.1.tar.gz/week_time_ex_sar_forex_next3-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (name='week_time_ex_sar_forex_next3' ,
+       version = '1.1' ,
+       description = 'week_time_ex_sar_forex_next3' ,
+       author = 'omid' ,
+       packages =['week_time_ex_sar_forex_next3'] ,
+       zip_safe = False
+       )
---
+++ packages/week-time-ex-sar-forex-next3/week_time_ex_sar_forex_next3-1.1.tar.gz/week_time_ex_sar_forex_next3-1.1/week_time_ex_sar_forex_next3/__init__.py
@@ -0,0 +1,1 @@
+from .week_time_ex_sar_forex_next3 import Time_manage
