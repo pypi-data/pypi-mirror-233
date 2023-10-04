e1e93443ffeed0d6557dcfc31b248f3f55c5955c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:46 +0000

    Add package pydantic-prompter/0.1.19/pydantic_prompter-0.1.19-py3-none-any.whl

---
+++ packages/pydantic-prompter/pydantic_prompter-0.1.19-py3-none-any.whl/pydantic_prompter/settings.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+from pydantic import BaseSettings, BaseModel, Field
+
+_root_path = Path(__file__).resolve().parent
+
+
+class TemplatePaths(BaseModel):
+    anthropic: str = Field(
+        default=_root_path.joinpath("prompt_templates", "anthropic.jinja").as_posix()
+    )
+
+
+class Settings(BaseSettings):
+    openai_api_key: str = ""
+    template_paths: TemplatePaths = TemplatePaths()
+    aws_default_region: str = "us-east-1"
+    aws_profile: str = None
+    aws_access_key_id: str = ""
+    aws_secret_access_key: str = ""
+
+    class Config:
+        env_file = _root_path.joinpath(".env")
+        env_nested_delimiter = "__"
