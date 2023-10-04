e741e590fa790b43a2e89d3b992e22a7c5ed8fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:08:03 +0000

    Add package aioelectricitymaps/0.1.0/aioelectricitymaps-0.1.0.tar.gz

---
+++ packages/aioelectricitymaps/aioelectricitymaps-0.1.0.tar.gz/aioelectricitymaps-0.1.0/aioelectricitymaps/exceptions.py
@@ -0,0 +1,9 @@
+"""Exceptions for ElectricityMaps."""
+
+
+class ElectricityMapsError(Exception):
+    """Generic error occurred in ElectricityMaps package."""
+
+
+class ElectricityMapsDecodeError(ElectricityMapsError):
+    """Decoding error occurred in ElectricityMaps package."""
