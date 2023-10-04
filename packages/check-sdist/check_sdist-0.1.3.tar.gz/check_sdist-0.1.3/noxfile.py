eca07d602814f4559faedf1f9db6a065a054d697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:27 +0000

    Add package check-sdist/0.1.3/check_sdist-0.1.3.tar.gz

---
+++ packages/check-sdist/check_sdist-0.1.3.tar.gz/check_sdist-0.1.3/noxfile.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import nox
+
+nox.options.sessions = ["lint", "pylint", "tests"]
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    """
+    Run the linter.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+@nox.session
+def pylint(session: nox.Session) -> None:
+    """
+    Run PyLint.
+    """
+    # This needs to be installed into the package environment, and is slower
+    # than a pre-commit check
+    session.install(".", "pylint")
+    session.run("pylint", "src", *session.posargs)
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests.
+    """
+    session.install(".[test]")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session
+def coverage(session: nox.Session) -> None:
+    """
+    Run tests and compute coverage.
+    """
+
+    session.posargs.append("--cov=check-sdist")
+    tests(session)
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    """
+    Build an SDist and wheel.
+    """
+
+    session.install("build")
+    session.run("python", "-m", "build")
