1aa20710a691d93793e9c68d5e8ac794b58e3b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:33 +0000

    Add package eventix/0.18.6/eventix-0.18.6-py3-none-any.whl

---
+++ packages/eventix/eventix-0.18.6-py3-none-any.whl/eventix/functions/tools.py
@@ -0,0 +1,38 @@
+import importlib
+import logging
+import os
+from uuid import uuid4
+
+import pkg_resources
+import toml
+
+
+def uid() -> str:
+    return str(uuid4()).replace("-", "")
+
+
+_uid = uid
+
+
+def version():
+    # script_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")
+    # with open(os.path.join(script_dir, 'pyproject.toml'), "r") as fd:
+    #     t = toml.load(fd)
+    # return t['tool']['poetry']['version']
+    v = importlib.metadata.version('eventix')
+    a = importlib.metadata
+    return v
+
+
+def setup_logging(without_time: bool = False, level: str = "INFO", module: bool = False, prefix: str = ""):
+    time_str = '%(asctime)s ' if not without_time else ''
+    module = '[%(name)s]' if module else ''
+    f = f"{time_str}{module}[%(levelname)8s]{prefix} %(message)s"
+
+    logging.basicConfig(
+        level=level,
+        format=f,
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
