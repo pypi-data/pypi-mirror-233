1902cc760ebbc111b7d907cf2e54b366824529e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:07:08 +0000

    Add package aiovideo/0.1/aiovideo-0.1-py3-none-any.whl

---
+++ packages/aiovideo/aiovideo-0.1-py3-none-any.whl/aiovideo/__init__.py
@@ -0,0 +1,14 @@
+from pytube import YouTube
+
+def youtube(url, audio=False):
+    if url.startswith("https://www.youtube.com/") or url.startswith("https://www.youtube.com/"):
+        if audio == False:
+            print("Downloading started...")
+            YouTube(url).streams.get_highest_resolution().download()
+            print("Downloading completed!")
+        elif audio == True:
+            print("Audio downloading started...")
+            YouTube(url).streams.get_audio_only().download()
+            print("Audio downloading completed!")
+    else:
+        print("URL is invalid!")
