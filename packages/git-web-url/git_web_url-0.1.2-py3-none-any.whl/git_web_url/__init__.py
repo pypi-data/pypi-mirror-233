debc74276157308907b99c27dad50f03af3fded8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:06:53 +0000

    Add package git-web-url/0.1.2/git_web_url-0.1.2-py3-none-any.whl

---
+++ packages/git-web-url/git_web_url-0.1.2-py3-none-any.whl/git_web_url/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+A CLI tool and also a Python library can print the url of a local file in a git repo so you can one-click to open it in web browser.
+"""
+
+from ._version import __version__
+
+__short_description__ = "A CLI tool and also a Python library can print the url of a local file in a git repo so you can one-click to open it in web browser."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
