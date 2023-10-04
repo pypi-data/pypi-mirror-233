b58b57f428ec711b03131c93719abbf9950bd4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:07:36 +0000

    Add package AnkiChinese/1.3.8/AnkiChinese-1.3.8.tar.gz

---
+++ packages/AnkiChinese/AnkiChinese-1.3.8.tar.gz/AnkiChinese-1.3.8/README.md
@@ -0,0 +1,108 @@
+# AnkiChinese
+
+Asynchronously scrape the ArchChinese dictionary to generate [Anki](https://apps.ankiweb.net/) flashcards
+
+<div style="display:flex;">
+  <table class="table">
+    <tr>
+      <td>
+        <img src="media/文.gif" alt="Online Flashcard Demo" style="width:400px;">
+      </td>
+    </tr>
+    <tr>
+      <td class="caption">Online Flashcard</td>
+    </tr>
+  </table>
+
+  <table class="table">
+    <tr>
+      <td>
+        <img src="media/文.png" alt="Offline Flashcard Demo" style="width:400px;">
+      </td>
+    </tr>
+    <tr>
+      <td class="caption">Offline Flashcard</td>
+    </tr>
+  </table>
+</div>
+
+# Installation
+
+    pip install ankichinese
+    playwright install
+
+# Usage
+
+    ankichinese
+
+    -h, --help                          Show help message and exit
+
+    --export, -x {anki, csv, update}    Export mode (default: anki)
+                                        anki: Generate new AnkiChinese deck
+                                        csv: Generate CSV file
+                                        update: Update existing deck
+    --input, -i INPUT                   Input file with characters to scrape (default: input.txt)
+    --output, -o OUTPUT                 Name of output file (do not include extension)
+                                        (default: ankichinese_output)
+
+    --definitions, -def NUM             Number of definitions to scrape per character (default: 5)
+    --examples, -ex NUM                 Number of example words to scrape per character (default: 5)
+
+    --requests-at-once, -r NUM          Maximum number of requests at once (default: 10)
+    --requests-per-second, -rs NUM      Maximum number of requests per second (default: 5)
+
+## Generate New AnkiChinese Deck
+
+How to create an entirely new Anki deck with the name `ankichinese_output.apkg` in the current directory using custom AnkiChinese styling.
+
+1. Create `input.txt` with the characters you want to scrape.
+2. Run `ankichinese -x anki`.
+3. Open Anki and import `ankichinese_output.apkg`.
+
+**Updating is Easy!**  
+Just run `ankichinese -x anki` again with new characters in `input.txt` and import the new `ankichinese_output.apkg` file into Anki. Anki will automatically update the existing deck without losing progress.
+
+## Update Existing (Non-AnkiChinese) Deck Without Losing Progress
+
+1. Create `input.txt` with the characters you want to scrape (can be the same as the existing deck).
+2. Run `ankichinese -x update`.
+3. Choose deck and model of cards to update. AnkiChinese will search for and overwrite any fields with the same names as the following.
+
+| Field Name  | Description                              |
+| ----------- | ---------------------------------------- |
+| Hanzi       | Simplified character (**REQUIRED**)      |
+| Traditional | Traditional form                         |
+| Definition  | Meaning of character                     |
+| Pinyin      | Most common pinyin                       |
+| Pinyin 2    | Other possible pinyin                    |
+| Words       | Example words                            |
+| Formation   | Origin / mnemonic for character          |
+| HSK         | Hanyu Shuiping Kaoshi level              |
+| Audio       | Audio file name (**required for audio**) |
+
+4. Import the new `ankichinese_audio.apkg` file into Anki. This will import the audio files (and create an empty deck that can be deleted)
+
+# Tools Used
+
+- Asynchronous I/O: [Asyncio](https://docs.python.org/3/library/asyncio.html)
+- Limit concurrency: [Aiometer](https://github.com/florimondmanca/aiometer)
+- Web automation and HTML interaction: [Playwright](https://playwright.dev/python/)
+- Anki deck generation: [Genanki](https://github.com/kerrickstaley/genanki)
+- Anki database access: [AnkiPandas](https://github.com/klieret/AnkiPandas)
+- HTML parsing and scraping: [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/)
+- Data manipulation: [Pandas](https://pandas.pydata.org/)
+- GUI: [Gooey](https://github.com/chriskiehl/Gooey)
+
+# Credits
+
+Character information: [ArchChinese](https://www.archchinese.com/)
+
+Stroke order diagrams:
+
+- Online stroke order diagrams: [Hanzi Writer](https://hanziwriter.org/)
+- Offline stroke order font: [Reinaert Albrecht](https://rtega.be/chmn/index.php?subpage=68)
+
+Chinese audio:
+
+- Standard tones: [Yoyo Chinese](https://yoyochinese.com/chinese-learning-tools/Mandarin-Chinese-pronunciation-lesson/pinyin-chart-table)
+- Neutral tones: [Purple Culture](https://www.purpleculture.net/chinese_pinyin_chart/)
---
+++ packages/AnkiChinese/AnkiChinese-1.3.8.tar.gz/AnkiChinese-1.3.8/src/AnkiChinese/export.py
@@ -0,0 +1,157 @@
+import genanki
+import os
+import pandas as pd
+
+from ankipandas import Collection
+
+
+def gen_csv(results, output_name):
+    df = pd.DataFrame(results)
+    df.to_csv(output_name + ".csv", index=False, sep="\t")
+
+
+def get_full_path(relative_path):
+    return os.path.join(os.path.dirname(__file__), relative_path)
+
+
+def gen_model():
+    front_html = open(get_full_path("card_template/front.html"), "r")
+    front = front_html.read()
+    front_html.close()
+    back_html = open(get_full_path("card_template/back.html"), "r")
+    back = back_html.read()
+    back_html.close()
+    styles_css = open(get_full_path("card_template/styles.css"), "r")
+    styles = styles_css.read()
+    styles_css.close()
+
+    return genanki.Model(
+        1354988330,
+        "AnkiChinese",
+        fields=[
+            {"name": "Hanzi"},
+            {"name": "Traditional"},
+            {"name": "Definition"},
+            {"name": "Pinyin"},
+            {"name": "Pinyin 2"},
+            {"name": "Examples"},
+            {"name": "Formation"},
+            {"name": "HSK"},
+            {"name": "Audio"},
+        ],
+        templates=[
+            {
+                "name": "Card 1",
+                "qfmt": front,
+                "afmt": back,
+            }
+        ],
+        css=styles,
+        sort_field_index=0,
+    )
+
+
+def gen_note(model, data):
+    return genanki.Note(
+        model=model,
+        fields=[
+            data["Hanzi"],
+            data["Traditional"],
+            data["Definition"],
+            data["Pinyin"],
+            data["Pinyin 2"],
+            data["Examples"],
+            data["Formation"],
+            data["HSK"],
+            data["Audio"],
+        ],
+        guid=genanki.guid_for(data["Hanzi"]),
+    )
+
+
+def add_audio(package, audio_path):
+    audio_files = os.listdir(audio_path)
+    for file in audio_files:
+        package.media_files.append(audio_path + "/" + file)
+
+
+def gen_anki(results, output_name):
+    deck = genanki.Deck(2085137232, output_name)
+    model = gen_model()
+    for data in results:
+        deck.add_note(gen_note(model, data))
+
+    package = genanki.Package(deck)
+    add_audio(package, "ankichinese_audio")
+    package.media_files.append(get_full_path("card_template/_CNstrokeorder.ttf"))
+    package.write_to_file(output_name + ".apkg")
+
+
+def update_anki(results, col, deck_name, model_name, notes_in_deck):
+    selec = notes_in_deck.query(f"nmodel == '{model_name}'").copy()
+
+    # convert results to dataframe
+    res = pd.DataFrame(results)
+    res = res.add_prefix("nfld_")
+
+    # merge existing notes and results
+    old = selec.fields_as_columns().copy()
+    if "nfld_Hanzi" not in old.columns:
+        print("Error: model must have 'Hanzi' column!")
+        return
+    old.set_index("nfld_Hanzi", inplace=True)
+    res.set_index("nfld_Hanzi", inplace=True)
+    old.update(res)
+    res.reset_index(inplace=True)
+    old.reset_index(inplace=True)
+    old = old.filter(regex="^nfld_", axis="columns")
+    # update exisiting notes
+    selec["nflds"] = old.values.tolist()
+    col.notes.update(selec)
+
+    # find and add new notes
+    selec.fields_as_columns(inplace=True)
+    new_notes = res[~res["nfld_Hanzi"].isin(selec["nfld_Hanzi"])]
+    # include only columns that also exist in existing notes
+    diff_columns = old.columns.difference(new_notes.columns)
+    new_notes.loc[:, diff_columns] = ""
+    new_notes = new_notes[old.columns]
+    new_notes.columns = new_notes.columns.str.replace("nfld_", "")
+    col.notes.add_notes(
+        nmodel=model_name, nflds=new_notes.to_dict("records"), inplace=True
+    )
+
+    # print changes and ask for confirmation
+    # col.summarize_changes()
+    notes_added_nids = col.notes.loc[col.notes.was_added()].nid.tolist()
+    print("\nNotes updated:")
+    print(col.notes.loc[col.notes.was_modified(), "nflds"])
+    # print("\nNotes added:")
+    # print(col.notes.loc[notes_added_nids, "nflds"])
+
+    # confirm_changes = input("Confirm changes? [y/n] ").lower().strip()
+    # if confirm_changes == "y" or confirm_changes == "yes":
+    col.write(modify=True, add=True, delete=False)
+    if len(notes_added_nids) > 0:
+        col.cards.add_cards(notes_added_nids, deck_name, inplace=True)
+        col.write(add=True)
+
+    # generate empty deck with audio files
+    audio_data = []
+    for audio_file in res.nfld_Audio.to_list():
+        audio_data.append(
+            {
+                "Hanzi": "",
+                "Traditional": "",
+                "Definition": "",
+                "Pinyin": "",
+                "Pinyin 2": "",
+                "Examples": "",
+                "Formation": "",
+                "HSK": "",
+                "Audio": audio_file,
+            }
+        )
+    gen_anki(audio_data, "ankichinese_audio")
+
+    col.db.close()
