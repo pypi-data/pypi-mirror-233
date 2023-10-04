1fbbe1a109cc1d10165a42ccb005950d5e744d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:06:58 +0000

    Add package steamship/2.17.31b1/steamship-2.17.31b1.tar.gz

---
+++ packages/steamship/steamship-2.17.31b1.tar.gz/steamship-2.17.31b1/src/steamship/agents/schema/context.py
@@ -0,0 +1,110 @@
+import logging
+from typing import Any, Callable, Dict, List, Optional
+
+from steamship import Block, Steamship, Tag
+from steamship.agents.logging import StreamingOpts
+from steamship.agents.schema.action import Action
+from steamship.agents.schema.cache import ActionCache, LLMCache
+
+Metadata = Dict[str, Any]
+EmitFunc = Callable[[List[Block], Metadata], None]
+
+
+class AgentContext:
+    """AgentContext contains all relevant information about a particular execution of an Agent. It is used by the
+    AgentService to manage execution history as well as store/retrieve information and metadata that will be used
+    in the process of an agent execution.
+    """
+
+    # A steamship-assigned ID for this memory object
+    @property
+    def id(self) -> str:
+        return self.chat_history.file.id
+
+    metadata: Metadata
+    """Allows storage of arbitrary information that may be useful for agents and tools."""
+
+    client: Steamship
+    """Provides workspace-specific utilities for the agents and tools."""
+
+    chat_history: "ChatHistory"  # noqa: F821
+    """Record of user-package messages. It records user submitted queries/prompts and the final
+    agent-driven answer sent in response to those queries/prompts. It does NOT record any chat
+    history related to agent execution and action selection."""
+
+    completed_steps: List[Action]
+    """Record of agent-selected Actions and their outputs. This provides an ordered look at the
+    execution sequence for this context."""
+
+    # todo: in the future, this could be a set of callbacks like onError, onComplete, ...
+    emit_funcs: List[EmitFunc]
+    """Called when an agent execution has completed. These provide a way for the AgentService
+    to return the result of an agent execution to the package that requested the agent execution."""
+
+    action_cache: Optional[ActionCache]
+    """Caches all interations with Tools within a Context. This provides a way to avoid duplicated
+    calls to Tools when within the same context."""
+
+    llm_cache: Optional[LLMCache]
+    """Caches all interations with LLMs within a Context. This provides a way to avoid duplicated
+    calls to LLMs when within the same context."""
+
+    def __init__(self, streaming_opts: Optional[StreamingOpts] = None):
+        self.metadata = {}
+        self.completed_steps = []
+        self.emit_funcs = []
+        if streaming_opts is not None:
+            self._streaming_opts = streaming_opts
+        else:
+            self._streaming_opts = StreamingOpts()
+
+    @staticmethod
+    def get_or_create(
+        client: Steamship,
+        context_keys: Dict[str, str],
+        tags: List[Tag] = None,
+        searchable: bool = True,
+        use_llm_cache: Optional[bool] = False,
+        use_action_cache: Optional[bool] = False,
+        streaming_opts: Optional[StreamingOpts] = None,
+    ):
+        from steamship.agents.schema.chathistory import ChatHistory
+
+        if streaming_opts is None:
+            streaming_opts = StreamingOpts()
+
+        history = ChatHistory.get_or_create(client, context_keys, tags, searchable=searchable)
+        context = AgentContext(streaming_opts=streaming_opts)
+        context.chat_history = history
+        context.client = client
+
+        if use_action_cache:
+            context.action_cache = ActionCache.get_or_create(
+                client=client, context_keys=context_keys
+            )
+        else:
+            context.action_cache = None
+
+        if use_llm_cache:
+            context.llm_cache = LLMCache.get_or_create(client=client, context_keys=context_keys)
+        else:
+            context.llm_cache = None
+
+        return context
+
+    def __enter__(self):
+        from steamship.agents.schema.chathistory import ChatHistoryLoggingHandler
+
+        if self._streaming_opts.stream_intermediate_events:
+            self._chat_history_logger = ChatHistoryLoggingHandler(
+                chat_history=self.chat_history, streaming_opts=self._streaming_opts
+            )
+            logger = logging.getLogger()
+            logger.addHandler(self._chat_history_logger)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._chat_history_logger:
+            logger = logging.getLogger()
+            logger.removeHandler(self._chat_history_logger)
+            self._chat_history_logger = None
---
+++ packages/steamship/steamship-2.17.31b1.tar.gz/steamship-2.17.31b1/tests/steamship_tests/agents/test_agent_service.py
@@ -0,0 +1,286 @@
+from typing import Any, List, Union
+
+import pytest
+from pydantic.fields import PrivateAttr
+from steamship_tests import SRC_PATH
+from steamship_tests.utils.deployables import deploy_package
+
+from steamship import Block, Steamship, SteamshipError, Task
+from steamship.agents.functional import FunctionsBasedAgent
+from steamship.agents.llms.openai import ChatOpenAI
+from steamship.agents.schema import Action, AgentContext, Tool
+from steamship.agents.service.agent_service import AgentService
+from steamship.data.tags.tag_constants import ChatTag, RoleTag, TagKind, TagValueKey
+
+
+def _blocks_from_invoke(client: Steamship, potential_blocks) -> List[Block]:
+    try:
+        if isinstance(potential_blocks, list):
+            return [Block(client=client, **raw) for raw in potential_blocks]
+        else:
+            return [Block(client=client, **potential_blocks)]
+    except Exception as e:
+        raise SteamshipError(f"Could not convert to blocks: {e}")
+
+
+@pytest.mark.usefixtures("client")
+def test_example_with_caching_service(client: Steamship):
+    # TODO(dougreid): replace the example agent with fake/free/fast tools to minimize test time / costs?
+
+    example_caching_agent_path = (
+        SRC_PATH / "steamship" / "agents" / "examples" / "example_assistant_with_caching.py"
+    )
+    with deploy_package(client, example_caching_agent_path, wait_for_init=True) as (
+        _,
+        _,
+        caching_agent,
+    ):
+        context_id = "foo-test-caching"
+        context_keys = {"id": context_id}
+
+        # attempt without caching
+        blocks_json = caching_agent.invoke("prompt", prompt="draw a cat", context_id=context_id)
+        assert blocks_json
+        blocks = _blocks_from_invoke(client, blocks_json)
+        assert len(blocks) == 2
+        assert "image" in blocks[0].text
+        assert blocks[1].is_image()
+
+        # attempt with a max_actions_per_run budget of 0 (should fail!)
+        assert caching_agent.invoke("get_max_actions_per_run") == 5
+        caching_agent.invoke("set_max_actions_per_run", value=0)
+        assert caching_agent.invoke("get_max_actions_per_run") == 0
+
+        with pytest.raises(SteamshipError, match="budget"):
+            blocks_json = caching_agent.invoke("prompt", prompt="draw a cat", context_id=context_id)
+
+        caching_agent.invoke("set_max_actions_per_run", value=5)
+        assert caching_agent.invoke("get_max_actions_per_run") == 5
+
+        agent_context = AgentContext.get_or_create(
+            client=client, context_keys=context_keys, use_llm_cache=True, use_action_cache=True
+        )
+        # just clear the cache to start fresh
+        agent_context.llm_cache.clear()
+        agent_context.action_cache.clear()
+
+        fake_action = Action(tool="fake_tool", input=[Block(text="a cat")])
+        agent_context.llm_cache.update(key=[Block(text="draw a cat")], value=fake_action)
+
+        fake_output = [Block(text="psyche")]
+        agent_context.action_cache.update(key=fake_action, value=fake_output)
+
+        fake_finish_action = Action(
+            tool="Agent-FinishAction", input=[], output=[Block(text="insert cat image here")]
+        )
+        agent_context.llm_cache.update(key=fake_output, value=fake_finish_action)
+
+        # now attempt with the caching
+        blocks_json = caching_agent.invoke("prompt", prompt="draw a cat", context_id=context_id)
+        assert blocks_json
+        blocks = _blocks_from_invoke(client, blocks_json)
+        assert len(blocks) == 1
+        assert "insert cat image here" == blocks[0].text
+
+
+class FakeUncachableTool(Tool):
+    name = "FakeUncacheableTool"
+    human_description = "Fake tool"
+    agent_description = "Ignored"
+
+    _runs: int = PrivateAttr(default=0)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, cacheable=False)
+
+    @property
+    def runs(self):
+        return self._runs
+
+    def run(self, tool_input: List[Block], context: AgentContext) -> Union[List[Block], Task[Any]]:
+        self._runs += 1
+        return [Block(text="sunny and mild")]
+
+
+class FakeCachingService(AgentService):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs, use_llm_cache=True, use_action_cache=True)
+        self.set_default_agent(
+            FunctionsBasedAgent(
+                tools=[FakeUncachableTool()], llm=ChatOpenAI(self.client, temperature=0)
+            )
+        )
+
+    @property
+    def uncachable_tool(self):
+        return self.get_default_agent().tools[0]
+
+
+@pytest.mark.usefixtures("client")
+def test_non_cacheable_tool_actions(client: Steamship):
+    context_id = "foo-test-non-cacheable"
+    context_keys = {"id": context_id}
+
+    agent_context = AgentContext.get_or_create(
+        client=client, context_keys=context_keys, use_llm_cache=True, use_action_cache=True
+    )
+
+    service_under_test = FakeCachingService(client=client, config={})
+
+    # clear the cache to start fresh
+    agent_context.llm_cache.clear()
+    agent_context.action_cache.clear()
+
+    # build up cache to ensure Fake Weather Tool is called
+    fake_action = Action(
+        tool=service_under_test.uncachable_tool.name, input=[Block(text="weather in SF today")]
+    )
+    agent_context.llm_cache.update(
+        key=[Block(text="what is the weather in SF today?")], value=fake_action
+    )
+
+    fake_finish_action = Action(
+        tool="Agent-FinishAction", input=[], output=[Block(text="sunny and mild")]
+    )
+    agent_context.llm_cache.update(key=[Block(text="sunny and mild")], value=fake_finish_action)
+
+    # run once to "fill" caches
+    service_under_test.prompt(prompt="what is the weather in SF today?", context_id=context_id)
+    assert service_under_test.uncachable_tool.runs == 1
+
+    # now attempt again, and verify the Tool executed again
+    service_under_test.prompt(prompt="what is the weather in SF today?", context_id=context_id)
+    assert service_under_test.uncachable_tool.runs == 2, "The tool action was unexpectedly cached."
+
+
+def has_status_message(blocks: List[Block], role: RoleTag) -> bool:
+    for b in blocks:
+        for t in b.tags:
+            if (
+                t.kind == TagKind.CHAT
+                and t.name == ChatTag.ROLE
+                and t.value.get(TagValueKey.STRING_VALUE) == role
+            ):
+                return True
+
+
+@pytest.mark.usefixtures("client")
+def test_context_logging_to_chat_history_everything(client: Steamship):
+    example_agent_service_path = (
+        SRC_PATH / "steamship" / "agents" / "examples" / "example_assistant.py"
+    )
+    with deploy_package(client, example_agent_service_path, wait_for_init=True) as (
+        _,
+        _,
+        agent_service,
+    ):
+        # test for everything (should include actions, llm, and tool)
+        context_id = "foo-test-logging-everything"
+        context_keys = {"id": context_id}
+        agent_context = AgentContext.get_or_create(client=client, context_keys=context_keys)
+        chat_history = agent_context.chat_history
+
+        agent_service.invoke(
+            "prompt", prompt="who is the current president of Taiwan?", context_id=context_id
+        )
+        chat_history.refresh()
+        assert len(chat_history.messages) != 0
+        assert has_status_message(chat_history.messages, RoleTag.AGENT)
+        assert has_status_message(chat_history.messages, RoleTag.LLM)
+        assert has_status_message(chat_history.messages, RoleTag.TOOL)
+
+        # test for individual bits (only include actions, llm, or tool)
+        context_id = "foo-test-logging-agent-only"
+        context_keys = {"id": context_id}
+        agent_context = AgentContext.get_or_create(client=client, context_keys=context_keys)
+        chat_history = agent_context.chat_history
+
+        agent_service.invoke(
+            "prompt",
+            prompt="who is the current prime minister of Canada?",
+            context_id=context_id,
+            include_llm_messages=False,
+            include_tool_messages=False,
+        )
+        chat_history.refresh()
+
+        assert len(chat_history.messages) != 0
+        assert has_status_message(chat_history.messages, RoleTag.AGENT)
+        assert not has_status_message(chat_history.messages, RoleTag.LLM)
+        assert not has_status_message(chat_history.messages, RoleTag.TOOL)
+
+        context_id = "foo-test-logging-llm-only"
+        context_keys = {"id": context_id}
+        agent_context = AgentContext.get_or_create(client=client, context_keys=context_keys)
+        chat_history = agent_context.chat_history
+
+        agent_service.invoke(
+            "prompt",
+            prompt="who is the current prime minister of England?",
+            context_id=context_id,
+            include_agent_messages=False,
+            include_tool_messages=False,
+        )
+        chat_history.refresh()
+
+        assert len(chat_history.messages) != 0
+        assert not has_status_message(chat_history.messages, RoleTag.AGENT)
+        assert has_status_message(chat_history.messages, RoleTag.LLM)
+        assert not has_status_message(chat_history.messages, RoleTag.TOOL)
+
+        context_id = "foo-test-logging-tool-only"
+        context_keys = {"id": context_id}
+        agent_context = AgentContext.get_or_create(client=client, context_keys=context_keys)
+        chat_history = agent_context.chat_history
+
+        agent_service.invoke(
+            "prompt",
+            prompt="who is the current president of France?",
+            context_id=context_id,
+            include_agent_messages=False,
+            include_llm_messages=False,
+        )
+        chat_history.refresh()
+
+        assert len(chat_history.messages) != 0
+        assert not has_status_message(chat_history.messages, RoleTag.AGENT)
+        assert not has_status_message(chat_history.messages, RoleTag.LLM)
+        assert has_status_message(chat_history.messages, RoleTag.TOOL)
+
+
+@pytest.mark.usefixtures("client")
+def test_non_duplicate_messages(client: Steamship):
+    example_agent_service_path = (
+        SRC_PATH / "steamship" / "agents" / "examples" / "example_assistant.py"
+    )
+    version_config_template = {
+        "model_name": {"type": "string"},
+    }
+    instance_config = {"model_name": "gpt-3.5-turbo"}
+    with deploy_package(
+        client=client,
+        py_path=example_agent_service_path,
+        version_config_template=version_config_template,
+        instance_config=instance_config,
+        wait_for_init=True,
+    ) as (
+        _,
+        _,
+        agent_service,
+    ):
+        context_id = "test-for-message-duplication"
+        agent_service.blocks_from_invoke(
+            "prompt", prompt="who is the president of Taiwan?", context_id=context_id
+        )
+        final_blocks = agent_service.blocks_from_invoke(
+            "prompt", prompt="totally. thanks.", context_id=context_id
+        )
+
+        assert (
+            len(final_blocks) == 1
+        ), f"There should only be a single block. Got: {len(final_blocks)}"
+        text = final_blocks[0].text
+        assert "\n" not in text, f"Unexpected response. Should be single line. Got: {text}"
+        assert (
+            "function_call" not in text
+        ), f"Unexpected response. Should not include function call. Got: {text}"
