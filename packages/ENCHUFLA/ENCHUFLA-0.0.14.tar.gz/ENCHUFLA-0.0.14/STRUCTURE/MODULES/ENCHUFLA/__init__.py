918ad1ddac4179d024ee94aeb715f16998f263c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:06:12 +0000

    Add package ENCHUFLA/0.0.14/ENCHUFLA-0.0.14.tar.gz

---
+++ packages/ENCHUFLA/ENCHUFLA-0.0.14.tar.gz/ENCHUFLA-0.0.14/STRUCTURE/MODULES/ENCHUFLA/__init__.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+
+def START ():
+	def ADD_TO_SYSTEM_PATHS (PATHS):
+		import pathlib
+		from os.path import dirname, join, normpath
+		import sys
+		
+		THIS_FOLDER = pathlib.Path (__file__).parent.resolve ()	
+		for PATH in PATHS:
+			sys.path.insert (0, normpath (join (THIS_FOLDER, PATH)))
+
+		return;
+
+	ADD_TO_SYSTEM_PATHS ([ 
+		'MODULES'
+	])
+
+	import CLIQUE	
+	CLIQUE.START ()
