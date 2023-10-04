3ca6a137262e68df201720055172eaea0905b87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:07:13 +0000

    Add package BINARY-DISTRIBUTION-TEMPLATE/0.0.9/BINARY_DISTRIBUTION_TEMPLATE-0.0.9.tar.gz

---
+++ packages/BINARY-DISTRIBUTION-TEMPLATE/BINARY_DISTRIBUTION_TEMPLATE-0.0.9.tar.gz/BINARY_DISTRIBUTION_TEMPLATE-0.0.9/setup.py
@@ -0,0 +1,27 @@
+
+#
+#	https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+#
+#	https://github.com/pypa/sampleproject/blob/db5806e0a3204034c51b1c00dde7d5eb3fa2532e/setup.py
+#
+from setuptools import setup, find_packages
+
+setup (
+    version = "0.0.9",
+
+	name = "BINARY_DISTRIBUTION_TEMPLATE",
+    install_requires = [],	
+	
+	package_dir = { 
+		#'bin': 'src/START.py',
+		'BINARY_DISTRIBUTION_TEMPLATE': 'src',
+	},
+	package_data = {
+		'BINARY_DISTRIBUTION_TEMPLATE': [ 'bin/BINARY_DISTRIBUTION_TEMPLATE' ]
+    },
+	scripts = [ 'src/bin/BINARY_DISTRIBUTION_TEMPLATE' ],
+	
+	license = "",
+	long_description = "",
+	long_description_content_type = "text/markdown"
+)
