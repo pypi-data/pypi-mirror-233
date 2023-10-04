7002df9216a707aafd89d6751aeccd206ac99529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:06:51 +0000

    Add package yutto/2.0.0b30/yutto-2.0.0b30.tar.gz

---
+++ packages/yutto/yutto-2.0.0b30.tar.gz/yutto-2.0.0b30/yutto/utils/funcutils/as_sync.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Coroutine
+from functools import wraps
+from typing import Any, Callable, TypeVar
+
+T = TypeVar("T")
+
+
+def as_sync(async_func: Callable[..., Coroutine[Any, Any, T]]) -> Callable[..., T]:
+    """将异步函数变成同步函数，避免在调用时需要显式使用 asyncio.run
+
+    ### Examples
+
+    ``` python
+
+    # 不使用 sync
+    async def itoa(a: int) -> str:
+        return str(a)
+
+    s: str = asyncio.run(itoa(1))
+
+    # 使用 sync
+    @as_sync
+    async def itoa(a: int) -> str:
+        return str(a)
+    s: str = itoa(1)
+    ```
+    """
+
+    @wraps(async_func)
+    def sync_func(*args: Any, **kwargs: Any):
+        return asyncio.run(async_func(*args, **kwargs))
+
+    return sync_func
+
+
+if __name__ == "__main__":
+
+    @as_sync
+    async def run(a: int) -> int:
+        return a
+
+    print(run(1))
---
+++ packages/yutto/yutto-2.0.0b30.tar.gz/yutto-2.0.0b30/yutto/utils/funcutils/singleton.py
@@ -0,0 +1,29 @@
+# type: ignore
+from __future__ import annotations
+
+
+class Singleton(type):
+    """单例模式元类
+
+    ### Refs
+
+    - https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+
+    ### Examples
+
+    ``` python
+    class MyClass(BaseClass, metaclass=Singleton):
+        pass
+
+    obj1 = MyClass()
+    obj2 = MyClass()
+    assert obj1 is obj2
+    ```
+    """
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
