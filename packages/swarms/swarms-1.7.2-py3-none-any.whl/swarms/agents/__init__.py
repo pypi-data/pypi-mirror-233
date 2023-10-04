6ed111ac3aefbae96111ecf00ebba2a22caa80c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:06:44 +0000

    Add package swarms/1.7.2/swarms-1.7.2-py3-none-any.whl

---
+++ packages/swarms/swarms-1.7.2-py3-none-any.whl/swarms/agents/__init__.py
@@ -0,0 +1,20 @@
+"""Agent Infrastructure, models, memory, utils, tools"""
+
+
+###########
+# from swarms.tools.base import BaseTool, Tool, StructuredTool, ToolWrapper, BaseToolSet, ToolCreator, GlobalToolsCreator, SessionToolsCreator, ToolsFactory
+# from swarms.tools.autogpt import pushd, process_csv, async_load_playwright, run_async, browse_web_page, WebpageQATool, web_search, query_website_tool
+# from swarms.tools.exit_conversation import ExitConversation
+
+# from swarms.tools.models import MaskFormer, ImageEditing, InstructPix2Pix, Text2Image, VisualQuestionAnswering, ImageCaptioning
+# from swarms.tools.file_mangagement import read_tool, write_tool, list_tool
+# from swarms.tools.requests import RequestsGet
+
+# from swarms.tools.developer import Terminal, CodeEditor
+
+
+#agents
+# from swarms.agents.profitpilot import ProfitPilot
+from swarms.agents.aot import AoTAgent
+# from swarms.agents.multi_modal_agent import MultiModalVisualAgent
+# from swarms.agents.omni_modal_agent import OmniModalAgent
