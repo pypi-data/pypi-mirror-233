17fdf8a5a86570e1a53e15d14064437214d4aee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:06:25 +0000

    Add package mrsqm/0.0.5/mrsqm-0.0.5.tar.gz

---
+++ packages/mrsqm/mrsqm-0.0.5.tar.gz/mrsqm-0.0.5/setup.py
@@ -0,0 +1,33 @@
+__author__ = "Thach Le Nguyen"
+
+from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+
+
+cstuff = Extension('mrsqm.mrsqm_wrapper',
+                   language='c++',
+                   sources=["src/mrsqm/mrsqm_wrapper.pyx","src/mrsqm/sfa/MFT.cpp","src/mrsqm/sfa/DFT.cpp","src/mrsqm/sfa/SFA.cpp","src/mrsqm/sfa/TimeSeries.cpp"],
+                   extra_compile_args=["-Wall", "-Ofast", "-g", "-std=c++11", "-ffast-math"],
+                   extra_link_args=["-lfftw3", "-lm", "-L/opt/local/lib"],           
+                   include_dirs=['src/mrsqm'])
+
+setup(
+    name='mrsqm',
+    version="0.0.5",
+    author='Thach Le Nguyen',
+    author_email='thalng@protonmail.com',
+    python_requires='>=3.7',
+    install_requires=[
+        "numpy>=1.18",
+        "pandas>=1.0.3",
+        "scikit-learn >= 0.22",        
+    ],
+    packages=find_packages(where='src'),
+    package_dir={
+        '': 'src'
+    },
+    description='MrSQM: Fast Time Series Classification with Symbolic Representations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    ext_modules=cythonize([cstuff]),
+)
