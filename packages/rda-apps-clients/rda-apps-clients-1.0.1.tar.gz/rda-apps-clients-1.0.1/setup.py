39b71fd9749773d28f6d83d746f3b7e15b9f302c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:55 +0000

    Add package rda-apps-clients/1.0.1/rda-apps-clients-1.0.1.tar.gz

---
+++ packages/rda-apps-clients/rda-apps-clients-1.0.1.tar.gz/rda-apps-clients-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='rda-apps-clients',
+    version='1.0.1',
+    description='Python Client to interact with Research Data Archive\'s API',
+    author='Riley Conroy',
+    url='https://github.com/NCAR/rda-apps-clients',
+    author_email='rpconroy@ucar.edu',
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+    'console_scripts': [
+        'rdams_client = rda_apps_clients.rdams_client:main',
+    ],
+},
+)
