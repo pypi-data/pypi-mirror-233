225b648b0e41536b902c2fb0aa669e03ddac93c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:07:32 +0000

    Add package grapycal-builtin/0.4.0/grapycal_builtin-0.4.0-py3-none-any.whl

---
+++ packages/grapycal-builtin/grapycal_builtin-0.4.0-py3-none-any.whl/grapycal_builtin/__init__.py
@@ -0,0 +1,5 @@
+from .interaction import *
+from .data import *
+from .function import *
+from .procedural import *
+del FunctionNode, SourceNode, Node
---
+++ packages/grapycal-builtin/grapycal_builtin-0.4.0-py3-none-any.whl/grapycal_builtin/procedural/forNode.py
@@ -0,0 +1,28 @@
+from typing import Iterable
+from grapycal.sobjects.sourceNode import SourceNode
+
+class ForNode(SourceNode):
+    category = 'procedural'
+
+    def build_node(self):
+        super().build_node()
+        self.item_port = self.add_out_port('item')
+        self.label.set('For')
+        self.shape.set('normal')
+
+    def init_node(self):
+        super().init_node()
+        self.iterator:Iterable|None = None
+
+    def task(self):
+        self.iterator = iter(self.run_port.get_one_data()) #type: ignore
+        self.run(self.next,to_queue=False)
+
+    def next(self):
+        try:
+            item = next(self.iterator) #type: ignore
+        except StopIteration:
+            return
+        for edge in self.item_port.edges:
+            edge.push_data(item)
+        self.run(self.next,to_queue=False)
