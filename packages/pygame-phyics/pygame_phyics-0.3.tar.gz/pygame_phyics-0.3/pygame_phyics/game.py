f9121a1b5c617eb6c25c186110e7c6b8428016cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:06:43 +0000

    Add package pygame-phyics/0.3/pygame_phyics-0.3.tar.gz

---
+++ packages/pygame-phyics/pygame_phyics-0.3.tar.gz/pygame_phyics-0.3/pygame_phyics/game.py
@@ -0,0 +1,140 @@
+"""
+
+게임 모듈 
+
+Example:
+    게임의 초기 샛팅을 실행하고 
+    게임 맵을 정의하고
+    실행한다
+    literal blocks::
+
+        $ Game.init((1000, 800), "MyGame")
+        $
+        $ @pygame_phyics.world("example/example.json")
+        $ def main():
+        $   Game.loop([], [])
+        $   
+        $ main()
+
+"""
+
+import pygame
+from pygame import display
+
+from Box2D import b2World
+from pygame_phyics.manger import Manger
+from pygame_phyics.scene import Scene
+from pygame_phyics.mouse import mouse_event
+from pygame_phyics.instantiate import import_module, import_classes
+from pygame_phyics.instantiate import load as instantiate_load
+from pygame_phyics.input import Input
+from pygame_phyics.event import Event
+import pygame_phyics.mouse as mouse
+
+def world(world_path: str):
+    """
+    함수를 새상으로 등록하는 데코레이터 입니다
+    
+    Args:
+        world_path (str) : json 경로
+    """
+    def real_world(func):
+        def wrapper():
+            Manger.scene.darkening()
+            Manger.world = b2World()
+            Manger.scene = Scene()
+            instantiate_load(world_path, Manger.classes)
+            Manger.scene.brightening()
+            func()
+            del Manger.scene
+        return wrapper
+    return real_world
+
+event_event = Event()
+
+class Game:
+    """게임 엔진"""
+    is_running = True
+    clock = pygame.time.Clock()
+    time_step = 1 / 60
+    ve_iters = 10
+    pos_iters = 10
+    
+    @classmethod
+    def init(cls, size : tuple[int, int], title : str):
+        """개임 초기 샛팅입니다"""
+        pygame.init()
+        display.set_caption(title)
+        Manger.init(
+            display.set_mode(size),
+            b2World(),
+            Scene())
+        Manger.classes.update(import_classes('object', './pygame_phyics/'))
+    
+    @classmethod
+    def import_classes(cls, obj_dir : str):
+        """클래스들을 불러와 Manger 에 저장합니다"""
+        Manger.classes.update(import_module(obj_dir))
+        
+    @classmethod
+    def loop(cls, events=[], funcs=[]):
+        """이벤트랑 함수를 받아 반복문 안에서 실행합니다"""
+        
+        pygame.key.start_text_input()
+        
+        while cls.is_running:
+            
+            cls.clock.tick(60)
+            
+                    
+            for func in funcs:
+                func()
+            
+            #collider event
+            
+            mouse_pressed = mouse.get_pressed()
+            
+            for i in range(3):
+                mouse_click = Input.mouse_click[i]
+                if mouse_pressed[i]:
+                    if mouse_click <= 1:
+                        Input.mouse_click[i] = 2
+                    else:
+                        Input.mouse_click[i] = 3
+                else:
+                    if mouse_click >= 2:
+                        Input.mouse_click[i] = 1
+                    else:
+                        Input.mouse_click[i] = 0
+                        
+            mouse_event()
+            
+            Manger.scene.update()
+            
+            for key, value in Input.key_board.items():
+                if value == 2:
+                    Input.key_board[key] = 3
+                elif value == 1:
+                    Input.key_board[key] = 0
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
+                elif event.type == pygame.KEYDOWN:
+                    Input.key_board[event.key] = 2
+                elif event.type == pygame.KEYUP:
+                    Input.key_board[event.key] = 1
+                    
+                event_event.invoke(event)
+                    
+                for event_func in events:
+                    event_func(event)
+
+            Manger.world.Step(
+                cls.time_step,
+                cls.ve_iters,
+                cls.pos_iters
+                )
+            
+            Manger.scene.render(Manger.screen)
+            pygame.display.update()
---
+++ packages/pygame-phyics/pygame_phyics-0.3.tar.gz/pygame_phyics-0.3/pygame_phyics/instantiate.py
@@ -0,0 +1,75 @@
+"""object을 가져오고 object 를 새계에 생성하는 모듈"""
+
+import os
+import json
+import importlib.util
+import inspect
+from typing import Dict
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+def import_module(import_dir):
+    """
+    폴더를 선회하며 가져온 클래스를 반환함
+    
+    Args:
+        import_dir (str): 디랙토리 경로
+        
+    Returns:
+        list: 클래스들
+    
+    """
+    class_list = {}
+    for file in os.listdir(import_dir):
+        if file.endswith(".py") and file != '__init__.py':
+            classes = import_classes(file[:-3], import_dir)
+            class_list.update(classes)
+    return class_list
+
+def import_classes(file: str, dir: str):
+    """
+    모듈에서 가져온 클래스를 리스트로 반환함
+    
+    Args:
+        file (str): 파일 이름
+        dir (str): 폴더 경로
+    
+    Returns:
+        list: 클래스들    
+    """
+    class_list = {}
+    spec = importlib.util.spec_from_file_location(file, dir + file + ".py")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    try:
+        for name, obj in inspect.getmembers(module):
+            if inspect.isclass(obj) and hasattr(obj, 'instantiate'):
+                print(f"Imported class: {name} from {file}")
+                class_list[name] = obj
+    except ImportError as e:
+        print(f"모듈 {file}을(를) 임포트하는 중 오류 발생: {e}")
+    finally:
+        return class_list
+
+def load(path: str, class_list):
+    """
+    오브젝트를 생성하고 새계에 등록합니다
+    
+    Args:
+        path (str): 경로
+        class_list (list): 임포트한 클래스 리스트
+        
+    Raises:
+        ImportError: path 경로에 json 에서 class_list 에 존재하지 않는 클래스를 불러오려 할때
+    """
+    GameObject = class_list['GameObject']
+    with open(path, 'r') as f:
+        js : Dict = json.loads(f.read())
+        for name in js.keys():
+            for json_object in js[name]:
+                args = list(json_object.values())
+                prefab_class = class_list.get(name)
+                if prefab_class == None:
+                    raise ImportError(f"{name} 클레스가 존재하지 않거나 불러지지 않았습니다. \n 현재 불러온 클래스 {class_list}")
+                prefab = prefab_class(*args)
+                GameObject.instantiate(prefab)
