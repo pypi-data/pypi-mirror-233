61ca6b74c4ea3a2a117832da2239b47fb6bd612a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:06:03 +0000

    Add package cubicweb-prov/1.0.0/cubicweb-prov-1.0.0.tar.gz

---
+++ packages/cubicweb-prov/cubicweb-prov-1.0.0.tar.gz/cubicweb-prov-1.0.0/cubicweb_prov/__pkginfo__.py
@@ -0,0 +1,27 @@
+# pylint: disable=W0622
+"""cubicweb-prov application packaging information"""
+
+modname = "prov"
+distname = "cubicweb-prov"
+
+numversion = (1, 0, 0)
+version = ".".join(str(num) for num in numversion)
+
+license = "LGPL"
+author = "LOGILAB S.A. (Paris, FRANCE)"
+author_email = "contact@logilab.fr"
+description = "The PROV Ontology"
+web = f"http://www.cubicweb.org/project/{distname}"
+
+__depends__ = {
+    "cubicweb": ">= 4.0.0, < 5.0.0",
+    "cubicweb-web": ">= 1.0.0, < 2.0.0",
+}
+__recommends__ = {}
+
+classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: CubicWeb",
+    "Programming Language :: Python",
+    "Programming Language :: JavaScript",
+]
