ce2a8722c2fe89e77f593ddd23df5c5ffe98fa9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:08:31 +0000

    Add package onepi/1.0.5/onepi-1.0.5.tar.gz

---
+++ packages/onepi/onepi-1.0.5.tar.gz/onepi-1.0.5/onepi/examples/basic/motors.py
@@ -0,0 +1,70 @@
+"""
+ Latest update: 08-09-2023
+
+ This code example is in the public domain.
+ http://www.botnroll.com
+
+Description:
+The robot moves and messages are printed on the LCD identifying the movements.
+"""
+
+import time
+from onepi.one import BnrOneA
+
+one = BnrOneA(0, 0)  # object variable to control the Bot'n Roll ONE A
+
+
+def setup():
+    one.stop()  # stop motors
+
+
+def loop():
+    one.lcd2("    Forward ")  # print data on LCD line 2
+    one.move(50, 50)  # Forward
+    time.sleep(1)  # wait 1 second
+    one.lcd2("     Stop   ")
+    one.stop()  # Stop Motors
+    time.sleep(0.5)
+    one.lcd2("   Backwards ")
+    one.move(-50, -50)  # Backwards
+    time.sleep(1)
+    one.lcd2("     Stop   ")
+    one.move(0, 0)  # Stop Motors
+    time.sleep(0.5)
+    one.lcd2("  Rotate Right ")
+    one.move(50, -50)  # Rotate Right
+    time.sleep(0.5)
+    one.lcd2("     Stop   ")
+    one.stop()  # Stop
+    time.sleep(0.5)
+    one.lcd2("  Rotate Left ")
+    one.move(-50, 50)  # Rotate Left
+    time.sleep(0.5)
+    one.lcd2("     Stop   ")
+    one.stop()  # Stop Motors
+    time.sleep(0.5)
+    one.lcd2("    Forward ")
+    one.move(100, 100)  # Forward
+    time.sleep(1)
+    one.lcd2("     Brake    ")
+    one.brake(100, 100)  # Stop motors with torque
+    time.sleep(0.8)
+    one.lcd2("   Backwards ")
+    one.move(-100, -100)  # Backwards
+    time.sleep(1)
+    one.lcd2("     Brake    ")
+    one.brake(100, 100)  # Stop motors with torque
+    time.sleep(0.8)
+    one.lcd2("     Stop   ")
+    one.stop()  # Stop Motors
+    time.sleep(1.5)
+
+
+def main():
+    setup()
+    while True:
+        loop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/onepi/onepi-1.0.5.tar.gz/onepi-1.0.5/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+name = "onepi"
+version = "1.0.5"
+description = "Python library to interface with BotnRoll One A"
+url = "https://github.com/ninopereira/bnronepi/tree/main/onepi"
+author = "Nino Pereira"
+author_email = "ninopereira.pt@gmail.com"
+license = "MIT"
+packages = find_packages()
+py_modules = ["onepi.one"]
+data_files = [
+    ("config", ["onepi/utils/config.json"]),
+    ("requirements", ["onepi/requirements.txt"]),
+    ("test_cfg", ["onepi/tests/test_cfg.json"]),
+    ("config_line_follow", ["onepi/examples/line_sensor/config_line_follow.json"]),
+    (
+        "config_line_follow_pid",
+        ["onepi/examples/line_sensor/config_line_follow_pid.json"],
+    ),
+    (
+        "config_line_follow_cosine",
+        ["onepi/examples/line_sensor/config_line_follow_cosine.json"],
+    ),
+]
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
+
+# Package dependencies
+install_requires = ["spidev"]
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url=url,
+    author=author,
+    author_email=author_email,
+    license=license,
+    classifiers=classifiers,
+    packages=find_packages(),
+    install_requires=install_requires,
+)
