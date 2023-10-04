074f11790433481d63c45289920f98a2dc62a609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:06:17 +0000

    Add package fast-resume-parser/0.1.0/fast-resume-parser-0.1.0.tar.gz

---
+++ packages/fast-resume-parser/fast-resume-parser-0.1.0.tar.gz/fast-resume-parser-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fast-resume-parser",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+)
