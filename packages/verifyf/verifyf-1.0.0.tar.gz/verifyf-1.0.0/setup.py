0e43367f2fd9e6bf10ea4c2d057c50e2255a2e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:06:34 +0000

    Add package verifyf/1.0.0/verifyf-1.0.0.tar.gz

---
+++ packages/verifyf/verifyf-1.0.0.tar.gz/verifyf-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='verifyf',
+    packages=find_packages(),
+    install_requires=['numpy','scikit-learn'], 
+    version='1.0.0',
+    description='Python package to verify Embeddings',
+    author='',
+)
---
+++ packages/verifyf/verifyf-1.0.0.tar.gz/verifyf-1.0.0/verifyf/__init__.py
@@ -0,0 +1,1 @@
+from verifyf.verify import verify
