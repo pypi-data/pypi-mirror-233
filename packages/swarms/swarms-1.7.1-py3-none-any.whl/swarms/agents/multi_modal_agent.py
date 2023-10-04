1f7eb9c095da6b697faa539670ebe85c5e2bf6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 05:06:27 +0000

    Add package swarms/1.7.1/swarms-1.7.1-py3-none-any.whl

---
+++ packages/swarms/swarms-1.7.1-py3-none-any.whl/swarms/agents/multi_modal_agent.py
@@ -0,0 +1,33 @@
+from agent_protocol import Agent, Step, Task
+
+from swarms.workers.multi_modal_workers.multi_modal_agent import MultiModalVisualAgent
+
+class MultiModalVisualAgent:
+    def __init__(
+            self, 
+            agent: MultiModalVisualAgent
+        ):
+        self.agent = agent
+    
+    async def run(self, text: str) -> str:
+        #run the multi-modal visual agent with the give task
+        return self.agent.run_text(text)
+    
+    async def __call__(self, text: str) -> str:
+        return self.agent.run(text)
+    
+    async def plan(self, step: Step) -> Step:
+        task = Agent
+        pass
+
+    async def task_handler(self, task: Task):
+        await self.agent.run()
+    
+    async def step_handler(self, step: Step):
+        if step.name == "plan":
+            await self.plan(step)
+        else:
+            await self.agent.run(step)
+
+        return step
+    
