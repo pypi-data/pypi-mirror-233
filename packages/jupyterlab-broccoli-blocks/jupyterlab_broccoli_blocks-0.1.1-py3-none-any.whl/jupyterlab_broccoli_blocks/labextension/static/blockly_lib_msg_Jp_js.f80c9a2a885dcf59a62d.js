bac0b63879b7231c2db8c230932b7ae3e05b52dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:08:59 +0000

    Add package jupyterlab-broccoli-blocks/0.1.1/jupyterlab_broccoli_blocks-0.1.1-py3-none-any.whl

---
+++ packages/jupyterlab-broccoli-blocks/jupyterlab_broccoli_blocks-0.1.1-py3-none-any.whl/jupyterlab_broccoli_blocks/labextension/static/blockly_lib_msg_Jp_js.f80c9a2a885dcf59a62d.js
@@ -0,0 +1,28 @@
+"use strict";
+(self["webpackChunkjupyterlab_broccoli_blocks"] = self["webpackChunkjupyterlab_broccoli_blocks"] || []).push([["blockly_lib_msg_Jp_js"],{
+
+/***/ "../blockly/lib/msg/Jp.js":
+/*!********************************!*\
+  !*** ../blockly/lib/msg/Jp.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var blockly__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! blockly */ "webpack/sharing/consume/default/blockly/blockly");
+/* harmony import */ var blockly__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(blockly__WEBPACK_IMPORTED_MODULE_0__);
+
+// for Toolbox
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_LOGIC = '論理';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_LOOPS = '繰返し';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_MATH = '数学';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_TEXT = 'テキスト';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_LISTS = 'リスト';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_COLOR = '色';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_VARIABLES = '変数';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_FUNCTIONS = '関数';
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=blockly_lib_msg_Jp_js.f80c9a2a885dcf59a62d.js.map
