1fa6b485c2a24fdb94523574f8d21da38ad783f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:08:39 +0000

    Add package sisl/0.14.2/sisl-0.14.2-cp312-cp312-macosx_10_9_x86_64.whl

---
+++ packages/sisl/sisl-0.14.2-cp312-cp312-macosx_10_9_x86_64.whl/sisl/viz/plots/tests/test_bands.py
@@ -0,0 +1,24 @@
+import pytest
+
+from sisl import Spin
+from sisl.viz.data import BandsData
+from sisl.viz.plots import bands_plot
+
+@pytest.fixture(scope="module", params=["plotly", "matplotlib"])
+def backend(request):
+    return request.param
+
+@pytest.fixture(scope="module", params=["unpolarized", "polarized", "noncolinear", "spinorbit"])
+def spin(request):
+    return Spin(request.param)
+
+@pytest.fixture(scope="module")
+def gap():
+    return 2.5
+
+@pytest.fixture(scope="module")
+def bands_data(spin, gap):
+    return BandsData.toy_example(spin=spin, gap=gap)
+
+def test_bands_plot(bands_data, backend):
+    bands_plot(bands_data, backend=backend)
