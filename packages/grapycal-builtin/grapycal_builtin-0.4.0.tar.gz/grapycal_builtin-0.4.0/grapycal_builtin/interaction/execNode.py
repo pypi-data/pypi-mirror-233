d35eec9ab8f66fb73ab0b7cf0cfcaad099b10ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:07:32 +0000

    Add package grapycal-builtin/0.4.0/grapycal_builtin-0.4.0.tar.gz

---
+++ packages/grapycal-builtin/grapycal_builtin-0.4.0.tar.gz/grapycal_builtin-0.4.0/grapycal_builtin/interaction/execNode.py
@@ -0,0 +1,30 @@
+from grapycal.sobjects.sourceNode import SourceNode
+from grapycal.sobjects.controls import TextControl
+
+class ExecNode(SourceNode):
+    '''
+    Equivalent to Python's `exec` function. It executes the statements in the input text box.
+
+    To make it run, either send in a signal to the `run` input port, or double click on the node.
+
+    :inputs:
+        - run: send in a signal to run the statements
+        
+
+    :outputs:
+        - done: send out a signal when the statements are done
+    '''
+    category = 'interaction'
+
+    def build_node(self):
+        super().build_node()
+        self.out_port = self.add_out_port('done')
+        self.text_control = self.add_control(TextControl)
+        self.label.set('Execute')
+        self.shape.set('simple')
+        self.expose_attribute(self.text_control.text,'text',display_name='statements')
+
+    def task(self):
+        stmt = self.text_control.text.get()
+        exec(stmt,self.workspace.vars())
+        self.out_port.push_data(None)
---
+++ packages/grapycal-builtin/grapycal_builtin-0.4.0.tar.gz/grapycal_builtin-0.4.0/grapycal_builtin/procedural/__init__.py
@@ -0,0 +1,5 @@
+from .forNode import ForNode
+from .eventNode import EventNode
+from .procedureNode import ProcedureNode
+from .limiterNode import LimiterNode
+from .funcDef import *
