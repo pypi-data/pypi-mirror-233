c70a17062b00595c934a16e6ff3cebfa01916179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:38 +0000

    Add package info-sec-tool/1.0.2/info_sec_tool-1.0.2.tar.gz

---
+++ packages/info-sec-tool/info_sec_tool-1.0.2.tar.gz/info_sec_tool-1.0.2/mycli.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+def Command(text):
+    print("="*80)
+    command = input(" "*6+f"FBC: {text} :> ")
+    return command
+def run(args):
+    print(args)
+    subprocess.run(['python', args.file])
+
+def main():
+    parameters = {
+        "recertification":["recert","recet","rct"]
+    }
+    parser = argparse.ArgumentParser(description='Run a Python file')
+    parser.add_argument('-file', type=str, help='the Python file to be executed')
+    parser.add_argument('-file1', type=str, help='the Python file to be executed')
+    args = parser.parse_args()
+    print(args)
+    command = Command("Enter Command")
+    print(command)
+    breaker =True
+    while breaker:
+        if command.lower() in parameters['recertification']:
+            print("-"*50+"\n"+" Enter Recert Type eg Postilion, direct inject etc"+"\n"+"-"*50)
+            recert_type = Command("Enter Recertification type")
+            if recert_type in ["exit","out","ex","terminate","break"]:
+                breaker = False
+            elif recert_type.lower() in ["postilion","postillion","postl"]:
+                subprocess.run(["python","interface.py"])
+            
+ 
+
+if __name__ == '__main__':
+    main()
