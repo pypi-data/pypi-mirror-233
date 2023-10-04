d39bbff14a79ca63a8f135996e1cdb37e9ebdc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:07:17 +0000

    Add package experimaestro/0.0.0/experimaestro-0.0.0-py3-none-any.whl

---
+++ packages/experimaestro/experimaestro-0.0.0-py3-none-any.whl/experimaestro/settings.py
@@ -0,0 +1,41 @@
+import os
+from omegaconf import OmegaConf
+from dataclasses import dataclass, field
+from functools import lru_cache
+from pathlib import Path
+from typing import Optional
+
+
+@dataclass
+class ServerSettings:
+    port: Optional[int] = None
+    """Port for the server"""
+
+    host: Optional[str] = None
+    """Hostname for the server"""
+
+    autohost: Optional[str] = None
+    """Automatic hostname: values can be `fqdn` or `name`"""
+
+    token: Optional[str] = None
+    """Token for the server"""
+
+
+@dataclass
+class Settings:
+    server: ServerSettings = field(default_factory=ServerSettings)
+
+
+@lru_cache()
+def get_settings(path: Optional[Path] = None) -> Settings:
+    if "PYTEST_CURRENT_TEST" in os.environ:
+        return Settings()
+    else:
+        schema = OmegaConf.structured(Settings)
+
+        path = path or Path("~/.config/experimaestro/settings.yaml").expanduser()
+        if not path.is_file():
+            return schema
+
+        conf = OmegaConf.load(path)
+        return OmegaConf.merge(schema, conf)
