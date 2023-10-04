4661d51a8e672b6b622ccebcbf3fe84d05ea3679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:05:21 +0000

    Add package pyrfu/2.4.9/pyrfu-2.4.9-py3-none-any.whl

---
+++ packages/pyrfu/pyrfu-2.4.9-py3-none-any.whl/pyrfu/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pyrfu import dispersion, lp, mms, models, plot, pyrf
+
+__author__ = "Louis Richard"
+__email__ = "louisr@irfu.se"
+__copyright__ = "Copyright 2020-2023"
+__license__ = "MIT"
+__version__ = "2.4.9"
+__status__ = "Prototype"
+
+__all__ = ["dispersion", "lp", "mms", "models", "plot", "pyrf"]
