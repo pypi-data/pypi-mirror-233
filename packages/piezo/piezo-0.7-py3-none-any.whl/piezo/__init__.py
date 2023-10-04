6b7c465af7596257d5f71b9e143efb52653b5e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:06:15 +0000

    Add package piezo/0.7/piezo-0.7-py3-none-any.whl

---
+++ packages/piezo/piezo-0.7-py3-none-any.whl/piezo/__init__.py
@@ -0,0 +1,7 @@
+'''Piezo is an AMR prediction module. Logic for handling prediction values should reside within the catalogues.
+'''
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+from .catalogue import ResistanceCatalogue
