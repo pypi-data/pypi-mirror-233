c1ec8fee2b4e7fb0db1a6fdd119342a01cbc7a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:06:30 +0000

    Add package lexset/4.2.4/lexset-4.2.4.tar.gz

---
+++ packages/lexset/lexset-4.2.4.tar.gz/lexset-4.2.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(name='lexset',
+      version='4.2.4',
+      author='F. Bitonti',
+      author_email='Francis@lexset.ai',
+      license='Apache 2.0',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['lexset'],
+      install_requires=[
+          'pyjson',
+          'PyYAML',
+          'pyBase64',
+          'tqdm',
+          'requests',
+          'matplotlib',
+          'numpy',
+          'opencv-python'
+      ]
+)
