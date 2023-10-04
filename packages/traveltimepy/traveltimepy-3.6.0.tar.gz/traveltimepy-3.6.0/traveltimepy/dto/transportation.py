1d956531bd46d2ae3d253d52ae8c54d5e446dde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:06:04 +0000

    Add package traveltimepy/3.6.0/traveltimepy-3.6.0.tar.gz

---
+++ packages/traveltimepy/traveltimepy-3.6.0.tar.gz/traveltimepy-3.6.0/traveltimepy/dto/transportation.py
@@ -0,0 +1,44 @@
+from typing import Optional
+from typing_extensions import Literal
+
+
+from pydantic.main import BaseModel
+
+
+class MaxChanges(BaseModel):
+    enabled: bool
+    limit: int
+
+
+class Driving(BaseModel):
+    type: Literal["driving"] = "driving"
+    disable_border_crossing: Optional[bool] = None
+
+
+class Walking(BaseModel):
+    type: Literal["walking"] = "walking"
+
+
+class Cycling(BaseModel):
+    type: Literal["cycling"] = "cycling"
+
+
+class Ferry(BaseModel):
+    type: Literal["ferry", "cycling+ferry", "driving+ferry"] = "ferry"
+    boarding_time: Optional[int] = None
+
+
+class DrivingTrain(BaseModel):
+    type: Literal["driving+train"] = "driving+train"
+    pt_change_delay: Optional[int] = None
+    driving_time_to_station: Optional[int] = None
+    parking_time: Optional[int] = None
+    walking_time: Optional[int] = None
+    max_changes: Optional[MaxChanges] = None
+
+
+class PublicTransport(BaseModel):
+    type: Literal["public_transport", "train", "bus", "coach"] = "public_transport"
+    pt_change_delay: Optional[int] = None
+    walking_time: Optional[int] = None
+    max_changes: Optional[MaxChanges] = None
