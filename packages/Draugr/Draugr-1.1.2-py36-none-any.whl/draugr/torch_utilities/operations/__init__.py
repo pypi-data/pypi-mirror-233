e2912a3d706048bf212bdbe33b67161a207d3ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:06:12 +0000

    Add package Draugr/1.1.2/Draugr-1.1.2-py36-none-any.whl

---
+++ packages/Draugr/Draugr-1.1.2-py36-none-any.whl/draugr/torch_utilities/operations/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 23/07/2020
+           """
+
+from .activations import *
+from .boxes_2d import *
+from .enums import *
+from .sizes import *
+from .torch_transforms import *
