3543cb13a3cb756ce3da94ea740c8db122383fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:07:14 +0000

    Add package investigraph/0.3.3/investigraph-0.3.3.tar.gz

---
+++ packages/investigraph/investigraph-0.3.3.tar.gz/investigraph-0.3.3/investigraph/logic/transform.py
@@ -0,0 +1,27 @@
+"""
+Transform stage: map data records to ftm proxies
+"""
+
+from typing import TYPE_CHECKING
+
+from investigraph.model.mapping import QueryMapping
+
+if TYPE_CHECKING:
+    from investigraph.model import Context
+
+from ftmq.io import make_proxy
+
+from investigraph.types import CEGenerator, SDict
+
+
+def map_record(record: SDict, mapping: QueryMapping) -> CEGenerator:
+    mapping = mapping.get_mapping()
+    if mapping.source.check_filters(record):
+        entities = mapping.map(record)
+        for proxy in entities.values():
+            yield make_proxy(proxy.to_dict())
+
+
+def map_ftm(ctx: "Context", data: SDict, ix: int) -> CEGenerator:
+    for mapping in ctx.config.transform.queries:
+        yield from map_record(data, mapping)
