9f4a610de54015e0842ffc79d21d88e527ea505f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:07:40 +0000

    Add package pymeos/1.1.3a5/pymeos-1.1.3a5.tar.gz

---
+++ packages/pymeos/pymeos-1.1.3a5.tar.gz/pymeos-1.1.3a5/pymeos/meos_init.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from pymeos_cffi import meos_initialize, meos_finalize
+
+
+def pymeos_initialize(timezone: Optional[str] = None) -> None:
+    """
+    Initializes the underlying MEOS platform.
+    Must be called before any other PyMEOS function.
+
+    Args:
+        timezone: :class:`str` indicating the desired timezone to be used. Defaults to system timezone.
+
+    MEOS Functions:
+        meos_initialize
+    """
+    meos_initialize(timezone)
+
+
+def pymeos_finalize() -> None:
+    """
+    Cleans up the underlying MEOS platform.
+    Should be called at the end after PyMEOS is no longer used.
+
+    MEOS Functions:
+        meos_finalize
+    """
+    meos_finalize()
