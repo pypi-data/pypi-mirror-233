0846fe5e99e96b1c3bd30a0388a430ab38665889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:05:47 +0000

    Add package xecs/0.6.0/xecs-0.6.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/xecs/xecs-0.6.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/xecs/__init__.py
@@ -0,0 +1,67 @@
+"""
+A fast ECS library.
+"""
+
+from xecs._internal.commands import Commands
+from xecs._internal.component import Component, ComponentPool
+from xecs._internal.events import EventReader, EventWriter
+from xecs._internal.float import Float
+from xecs._internal.input import Mouse, MouseButton
+from xecs._internal.int import Int
+from xecs._internal.py_field import PyField, py_field
+from xecs._internal.query import Query
+from xecs._internal.real_time_app import (
+    RealTimeApp,
+    RealTimeAppPlugin,
+)
+from xecs._internal.resource import Resource
+from xecs._internal.simulation_app import SimulationApp
+from xecs._internal.struct import Struct
+from xecs._internal.systems import (
+    FixedTimeStepSystems,
+    FixedTimeStepSystemSpec,
+    PendingStartupSystems,
+    PendingSystems,
+    StartupSystems,
+    Systems,
+    SystemSpec,
+)
+from xecs._internal.transform2 import Transform2
+from xecs._internal.vec2 import Vec2
+from xecs._internal.world import World
+from xecs.xecs import ArrayViewIndices, Bool, Duration, Float32, Int32
+
+__all__ = [
+    "ArrayViewIndices",
+    "Bool",
+    "Commands",
+    "Component",
+    "ComponentPool",
+    "Duration",
+    "EventReader",
+    "EventWriter",
+    "FixedTimeStepSystems",
+    "FixedTimeStepSystemSpec",
+    "Float",
+    "Float32",
+    "Int",
+    "Int32",
+    "Mouse",
+    "MouseButton",
+    "PendingStartupSystems",
+    "PendingSystems",
+    "PyField",
+    "py_field",
+    "Query",
+    "RealTimeApp",
+    "RealTimeAppPlugin",
+    "Resource",
+    "SimulationApp",
+    "StartupSystems",
+    "Struct",
+    "Systems",
+    "SystemSpec",
+    "Transform2",
+    "Vec2",
+    "World",
+]
