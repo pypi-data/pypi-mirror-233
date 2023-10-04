6579200ac702d4ab1ba3a831ca32ba315c1a2f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:08:31 +0000

    Add package onepi/1.0.5/onepi-1.0.5-py3-none-any.whl

---
+++ packages/onepi/onepi-1.0.5-py3-none-any.whl/onepi/examples/advanced/encoders.py
@@ -0,0 +1,90 @@
+"""
+ Latest update: 10-09-2023
+
+ This code example is in the public domain.
+ http://www.botnroll.com
+
+ Description:
+ Read single channel encoders attached to Bot'n Roll ONE A wheels.
+ This example sets the robot moving at a constant speed.")
+ It reads the encoders and displays the readings in the lcd and terminal.
+ Use PB1 to increase the speed and PB2 to decrease the speed of the motors.
+ Motors will automatically stop after left encoder count gets over 495.
+ To reset press PB3 and change the motor speeed with PB1 and PB2.
+
+ Encoders
+"""
+
+import time
+from onepi.one import BnrOneA
+
+one = BnrOneA(0, 0)  # object variable to control the Bot'n Roll ONE A
+
+speed_1 = 35
+speed_2 = 35
+
+
+def setup():
+    one.stop()
+    # stop motors
+    one.lcd1("Bot'n Roll ONE A")
+    one.lcd2("www.botnroll.com")
+
+    print("This example sets the robot moving at a constant speed.")
+    print("It reads the encoders and displays the readings in the lcd and terminal.")
+    print("Use PB1 to increase the speed and PB2 to decrease the speed of the motors.")
+    print("Motors will automatically stop after left encoder count gets over 495.")
+    print(
+        "To reset press PB3 and change the motor speeed with PB1 and PB2.", end="\n\n"
+    )
+
+    time.sleep(3)
+    one.read_left_encoder()
+    one.read_right_encoder()
+
+
+def loop():
+    global speed_1, speed_2
+    encoder_left = one.read_left_encoder_increment()
+    encoder_right = one.read_right_encoder_increment()
+    button = one.read_button()
+    if button == 1:
+        speed_1 += 1
+        speed_2 += 1
+    elif button == 2:
+        speed_1 -= 1
+        speed_2 -= 1
+    elif button == 3:
+        speed_1 = 0
+        speed_2 = 0
+        encoder_left = one.read_left_encoder()
+        encoder_right = one.read_right_encoder()
+    one.lcd1("L:", encoder_left, "vel:", speed_1)
+    one.lcd2("R:", encoder_right, "vel:", speed_2)
+    print(
+        "Left:",
+        encoder_left,
+        " vel:",
+        speed_1,
+        " ||  Right:",
+        encoder_right,
+        " vel:",
+        speed_2,
+        " " * 10,
+        end="\r",
+    )
+    if encoder_left >= 495:
+        speed_1 = 0
+        speed_2 = 0
+    one.move(speed_1, speed_2)
+    time.sleep(0.05)
+
+
+def main():
+    setup()
+    while True:
+        loop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/onepi/onepi-1.0.5-py3-none-any.whl/onepi/examples/line_sensor/04_3_line_follow_linear.py
@@ -0,0 +1,195 @@
+"""
+ Latest update: 04-09-2023
+
+ This code example is in the public domain.
+ http://www.botnroll.com
+
+IMPORTANT!!!!
+Before you use this example you MUST calibrate the line sensor.
+Use example 04_1_line_calibration first!!!
+
+Line reading provides a linear value between -100 to 100
+Line follow:
+Motors speed varies according to a linear function.
+Press push button 3 (PB3) to enter control configuration menu.
+You can adjust the max speed, the speed boost and the gain.
+The speeds of each motor are calculated as follows:
+    vel_m1 = max_linear_speed + line * gain     # Linear function for Motor1
+    vel_m2 = max_linear_speed - line * gain     # Linear function for Motor2
+After this operation the values are capped using the speed_boost value
+    vel_m1 = cap_value(vel_m1, -1, max_linear_speed + speed_boost)
+    vel_m2 = cap_value(vel_m2, -1, max_linear_speed + speed_boost)
+"""
+
+import os
+import json
+import time
+
+from onepi.one import BnrOneA
+
+one = BnrOneA(0, 0)  # object variable to control the Bot'n Roll ONE A
+
+max_linear_speed = 60
+gain = 1.10  # Linear gain
+speed_boost = 8  # Curve outside wheel max speed limit
+file_name = "config_line_follow.json"
+filename = os.path.join(os.path.dirname(__file__), file_name)
+
+
+def set_max_speed(new_max_linear_speed):
+    button = 0
+    while button != 3:
+        one.lcd2("   VelMax:", new_max_linear_speed)
+        button = one.read_button()
+        if button == 1:
+            new_max_linear_speed += 1
+            time.sleep(0.150)
+        if button == 2:
+            new_max_linear_speed -= 1
+            time.sleep(0.150)
+    while button == 3:  # Wait PB3 to be released
+        button = one.read_button()
+    return new_max_linear_speed
+
+
+def set_speed_boost(new_speed_boost):
+    button = 0
+    while button != 3:
+        one.lcd2("  Curve Boost:", new_speed_boost)
+        button = one.read_button()
+        if button == 1:
+            new_speed_boost += 1
+            time.sleep(0.150)
+        if button == 2:
+            new_speed_boost -= 1
+            time.sleep(0.150)
+    while button == 3:  # Wait PB3 to be released
+        button = one.read_button()
+    return new_speed_boost
+
+
+def set_linear_gain(new_gain):
+    new_gain = int(new_gain * 1000.0)
+    button = 0
+    while button != 3:
+        one.lcd2(" Line Gain:", new_gain)
+        button = one.read_button()
+        if button == 1:
+            new_gain += 10
+            time.sleep(0.150)
+        if button == 2:
+            new_gain -= 10
+            time.sleep(0.150)
+    while button == 3:  # Wait PB3 to be released
+        button = one.read_button()
+    return new_gain / 1000.0
+
+
+def menu():
+    global max_linear_speed
+    global speed_boost
+    global gain
+
+    one.stop()
+    one.lcd1("  Menu Config:")
+    one.lcd2("PB1+ PB2- PB3ok")
+    time.sleep(0.250)
+    while one.read_button() == 3:  # Wait PB3 to be released
+        time.sleep(0.150)
+
+    max_linear_speed = set_max_speed(max_linear_speed)  # Maximum speed
+    speed_boost = set_speed_boost(speed_boost)  # Outside wheel speed boost
+    gain = set_linear_gain(gain)  # Linear gain KLine
+    save_config(
+        max_linear_speed, speed_boost, gain
+    )  # Save values to configuration file
+
+    one.lcd1("Line  Following!")
+    one.lcd2("www.botnroll.com")
+    time.sleep(0.250)
+
+
+def load_config():
+    """
+    Read config values from file.
+    max_linear_speed, speed_boost and gain
+    """
+    global max_linear_speed
+    global speed_boost
+    global gain
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+            # Access values from JSON file
+            max_linear_speed = data["max_linear_speed"]
+            speed_boost = data["speed_boost"]
+            gain = data["gain"]
+
+    except FileNotFoundError:
+        # Handle the case when the file doesn't exist
+        print(f"The file '{filename}' doesn't exist. Using default values.")
+
+
+def save_config(new_max_linear_speed, new_speed_boost, new_gain):
+    """
+    Save config values to file.
+    max_linear_speed, speed_boost and gain
+    """
+    data = {
+        "max_linear_speed": new_max_linear_speed,
+        "speed_boost": new_speed_boost,
+        "gain": new_gain,
+    }
+
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+
+
+def cap_value(value, lower_limit, upper_limit):
+    """
+    Caps the value to lower and upper limits
+    """
+    if value < lower_limit:
+        return lower_limit
+    elif value > upper_limit:
+        return upper_limit
+    else:
+        return value
+
+
+def setup():
+    one.min_battery(10.5)  # safety voltage for discharging the battery
+    one.stop()  # stop motors
+    load_config()
+    one.lcd1("Line Follow Lin.")
+    one.lcd2(" Press a button ")
+    while one.read_button() == 0:  # Wait a button to be pressed
+        pass
+    while one.read_button() != 0:  # Wait for button release
+        pass
+
+
+def loop():
+    line = one.read_line()
+    vel_m1 = max_linear_speed + line * gain  # Linear function for Motor1
+    vel_m2 = max_linear_speed - line * gain  # Linear function for Motor2
+
+    # Limit motors maximum and minimum speed
+    vel_m1 = cap_value(vel_m1, -1, max_linear_speed + speed_boost)
+    vel_m2 = cap_value(vel_m2, -1, max_linear_speed + speed_boost)
+
+    one.move(vel_m1, vel_m2)
+
+    # Configuration menu
+    if one.read_button() == 3:
+        menu()  # PB3 to enter menu
+
+
+def main():
+    setup()
+    while True:
+        loop()
+
+
+if __name__ == "__main__":
+    main()
