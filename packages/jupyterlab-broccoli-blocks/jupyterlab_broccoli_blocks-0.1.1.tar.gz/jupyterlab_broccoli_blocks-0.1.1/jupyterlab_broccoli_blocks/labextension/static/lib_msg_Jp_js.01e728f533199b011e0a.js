e80eb3a242f11f85bd7d71d6b533512db330ce8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 15:08:59 +0000

    Add package jupyterlab-broccoli-blocks/0.1.1/jupyterlab_broccoli_blocks-0.1.1.tar.gz

---
+++ packages/jupyterlab-broccoli-blocks/jupyterlab_broccoli_blocks-0.1.1.tar.gz/jupyterlab_broccoli_blocks-0.1.1/jupyterlab_broccoli_blocks/labextension/static/lib_msg_Jp_js.01e728f533199b011e0a.js
@@ -0,0 +1,27 @@
+"use strict";
+(self["webpackChunkjupyterlab_broccoli_blocks"] = self["webpackChunkjupyterlab_broccoli_blocks"] || []).push([["lib_msg_Jp_js"],{
+
+/***/ "./lib/msg/Jp.js":
+/*!***********************!*\
+  !*** ./lib/msg/Jp.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var blockly__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! blockly */ "webpack/sharing/consume/default/blockly/blockly");
+/* harmony import */ var blockly__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(blockly__WEBPACK_IMPORTED_MODULE_0__);
+
+// for Toolbox
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.TOOLBOX_SPECIAL = '特別';
+// for Blocks
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.BLOCK_TEXT_NOCRLF_PRINT = '改行無しで表示';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.BLOCK_COLOR_HSV2RGB = '色相';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.BLOCK_COLOR_H = 'H';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.BLOCK_COLOR_S = 'S';
+blockly__WEBPACK_IMPORTED_MODULE_0__.Msg.BLOCK_COLOR_V = 'V';
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_msg_Jp_js.01e728f533199b011e0a.js.map
