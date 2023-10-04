5aa717c8532a927ff96c680442615fd994a4cbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 14:08:22 +0000

    Add package aga/0.13.10/aga-0.13.10.tar.gz

---
+++ packages/aga/aga-0.13.10.tar.gz/aga-0.13.10/src/aga/core/context.py
@@ -0,0 +1,29 @@
+"""Environment value wrapper."""
+from __future__ import annotations
+
+from typing import Iterable, Any
+
+
+class SubmissionContext(dict[str, Any]):
+    """Environment value wrapper."""
+
+    def __init__(self, env_targets: Iterable[str]) -> None:
+        super().__init__()
+        for target in env_targets:
+            self[target] = None
+
+    def update_from_path(self, path: str) -> None:
+        """Update environment values from a given module."""
+        # pylint: disable=import-outside-toplevel, cyclic-import
+        # to avoid circular imports, I place the import here
+        from ..loader import load_symbol_from_path
+
+        for symbol in self.keys():
+            self[symbol] = load_symbol_from_path(path, symbol)
+
+    def __getattr__(self, item: str) -> Any:
+        """Get the value of an environment variable."""
+        try:
+            return self[item]
+        except KeyError as e:
+            raise AttributeError(e) from e
