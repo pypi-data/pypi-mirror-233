3ba86a5434384bbe446cfd2de902359399156bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:06:01 +0000

    Add package ptitprince/0.2.7/ptitprince-0.2.7.tar.gz

---
+++ packages/ptitprince/ptitprince-0.2.7.tar.gz/ptitprince-0.2.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+setup(name='ptitprince',
+      version='0.2.7',
+      description='A Python implementation of Rainclouds, originally on R, ggplot2. Written on top of seaborn.',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/pog87/PtitPrince',
+      author='Davide Poggiali',
+      author_email='davide.poggiali@unipd.it',
+      license='MIT',
+      packages=find_packages(),
+      platform='any',
+      keywords=[
+          'data visualization',
+          'raincloud plots',
+      ],
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved',
+          'Programming Language :: Python',
+          'Topic :: Software Development',
+          'Topic :: Scientific/Engineering',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Operating System :: MacOS',
+      ],
+      install_requires=[
+          'matplotlib',
+          'numpy>=1.20',
+          'scipy',
+          'seaborn==0.11',
+      ],
+      zip_safe=False)
