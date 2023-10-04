0d666816508e2e0efe61851a08c7dda319d58a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 04:07:05 +0000

    Add package oarepo-vocabularies/2.0.40/oarepo_vocabularies-2.0.40-py3-none-any.whl

---
+++ packages/oarepo-vocabularies/oarepo_vocabularies-2.0.40-py3-none-any.whl/oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/components/index.js
@@ -0,0 +1,6 @@
+export * from "./CurrentLocationInformation";
+export * from "./PropFieldsComponent";
+export * from "./PublishButton";
+export * from "./ResetButton";
+export * from "./VocabularySelectField";
+export * from "./VocabularyMultilingualInputField";
