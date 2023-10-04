93fb63c9f7ba3785a832ca3bd89c7b34eb910502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:08:00 +0000

    Add package llvm-diagnostics/3.0.1/llvm_diagnostics-3.0.1-py3-none-any.whl

---
+++ packages/llvm-diagnostics/llvm_diagnostics-3.0.1-py3-none-any.whl/llvm_diagnostics/messages.py
@@ -0,0 +1,78 @@
+# Copyright (c) 2021 - 2021 TomTom N.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Diagnostic Messages"""
+
+from dataclasses import dataclass
+from sys import stderr
+from typing import Optional
+from llvm_diagnostics.utils import Level
+
+from llvm_diagnostics import formatters
+
+
+@dataclass
+class Range:
+    """Diagnostics Range"""
+
+    start: int = 1
+    range: Optional[int] = None
+
+    def end(self):
+        """Returns the last index of the Range"""
+        return self.start + self.range
+
+    def __hash__(self):
+        return hash((self.start, self.range))
+
+
+@dataclass
+class __Message(Exception):  # pylint: disable=C0103
+    """Diagnostics Message"""
+
+    message: str
+    file_path: Optional[str] = None
+    column_number: Range = Range()
+    expectations: Optional[str] = None
+    line: Optional[str] = None
+    line_number: Range = Range()
+
+    def report(self):
+        """Formats the Diagnostics message and sends it to `stderr`"""
+        print(self, file=stderr)
+
+    def __str__(self):
+        """Formats the Diagnostics message"""
+        return formatters.get_config().format(message=self)
+
+
+@dataclass
+class Info(__Message):
+    """Diagnostics Information"""
+
+    level: Level = Level.NOTE
+
+
+@dataclass
+class Error(__Message):
+    """Diagnostics Error"""
+
+    level: Level = Level.ERROR
+
+
+@dataclass
+class Warning(__Message):  # pylint: disable=W0622
+    """Diagnostics Warning"""
+
+    level: Level = Level.WARNING
---
+++ packages/llvm-diagnostics/llvm_diagnostics-3.0.1-py3-none-any.whl/llvm_diagnostics/utils.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2021 - 2021 TomTom N.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utilities"""
+
+from enum import Enum, auto
+import re
+
+_ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+
+
+class TextFormat(Enum):
+    """ANSI Code text formatting"""
+
+    BOLD = 1
+
+    RED = 31
+    BLUE = 34
+    LIGHT_GREEN = 92
+    CYAN = 94
+
+
+def format_string(string: str, color: TextFormat):
+    """Applies ANSI code formatting to string"""
+    return f"\033[{color.value}m{string}\033[0m"
+
+
+def strip_ansi_escape_chars(string: str):
+    """Removes all ANSI code characters from string"""
+    return _ANSI_ESCAPE.sub("", string)
+
+
+class Level(Enum):
+    """Diagnostics Level"""
+
+    ERROR = auto()
+    WARNING = auto()
+    NOTE = auto()
