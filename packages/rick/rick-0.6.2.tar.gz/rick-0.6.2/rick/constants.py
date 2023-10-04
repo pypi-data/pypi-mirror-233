2d0bccba14a91cc4127352e4eb831142792a4cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-04 03:05:59 +0000

    Add package rick/0.6.2/rick-0.6.2.tar.gz

---
+++ packages/rick/rick-0.6.2.tar.gz/rick-0.6.2/rick/constants.py
@@ -0,0 +1,5 @@
+RICK_VERSION = ["0", "6", "2"]
+
+
+def get_version():
+    return ".".join(RICK_VERSION)
---
+++ packages/rick/rick-0.6.2.tar.gz/rick-0.6.2/rick/crypto/buffer.py
@@ -0,0 +1,27 @@
+import hashlib
+from io import BytesIO
+
+
+def hash_buffer(method, buf: BytesIO) -> str:
+    fn = getattr(hashlib, method)
+    if not callable(fn):
+        raise RuntimeError(
+            "hash_buffer(): invalid hashing method:  '{}'".format(method)
+        )
+    buf.seek(0)
+    return fn(buf.read()).hexdigest()
+
+
+def sha256_hash(buf: BytesIO) -> str:
+    buf.seek(0)
+    return hashlib.sha256(buf.read()).hexdigest()
+
+
+def sha1_hash(buf: BytesIO) -> str:
+    buf.seek(0)
+    return hashlib.sha1(buf.read()).hexdigest()
+
+
+def sha512_hash(buf: BytesIO) -> str:
+    buf.seek(0)
+    return hashlib.sha512(buf.read()).hexdigest()
