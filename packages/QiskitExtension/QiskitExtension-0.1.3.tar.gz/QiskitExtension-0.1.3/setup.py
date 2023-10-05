bf5d4a38514f905328941860ce5f0963caca272f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:33 +0000

    Add package QiskitExtension/0.1.3/QiskitExtension-0.1.3.tar.gz

---
+++ packages/QiskitExtension/QiskitExtension-0.1.3.tar.gz/QiskitExtension-0.1.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="QiskitExtension",
+    version="0.1.3",
+    author="HSIEH, LI-YU",
+    author_email="cjh9027@smail.nchu.edu.tw",
+    description="A Qiskit visualize extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/Slope86/QiskitExtension",
+    packages=setuptools.find_packages(exclude=["experiments", "tests"]),
+    package_data={"": ["*.ini"]},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["qiskit[visualization] >= 0.22.3"],
+    python_requires=">=3.10",
+)
