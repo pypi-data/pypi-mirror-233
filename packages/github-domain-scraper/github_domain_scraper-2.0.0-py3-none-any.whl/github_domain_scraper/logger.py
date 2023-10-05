0af32775b78df2e8d26475c13809d058943f471d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:15 +0000

    Add package github-domain-scraper/2.0.0/github_domain_scraper-2.0.0-py3-none-any.whl

---
+++ packages/github-domain-scraper/github_domain_scraper-2.0.0-py3-none-any.whl/github_domain_scraper/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    console_log_handler = logging.StreamHandler()
+    console_log_handler.setLevel(logging.INFO)
+    console_log_handler.setFormatter(formatter)
+
+    logger.addHandler(console_log_handler)
+
+    return logger
