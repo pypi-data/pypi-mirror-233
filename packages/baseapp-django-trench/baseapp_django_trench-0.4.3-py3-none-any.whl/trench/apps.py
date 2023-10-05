50507c4db3afa9ebec2204a614979e4b03e925d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:08:35 +0000

    Add package baseapp-django-trench/0.4.3/baseapp_django_trench-0.4.3-py3-none-any.whl

---
+++ packages/baseapp-django-trench/baseapp_django_trench-0.4.3-py3-none-any.whl/trench/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TrenchConfig(AppConfig):
+    name = "trench"
+    verbose_name = "django-trench"
+    default_auto_field = "django.db.models.BigAutoField"
