2a698909bc8a88d83aca10e578999027a05f7d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:42 +0000

    Add package auxiliary/0.0.34/auxiliary-0.0.34.tar.gz

---
+++ packages/auxiliary/auxiliary-0.0.34.tar.gz/auxiliary-0.0.34/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['auxiliary', 'auxiliary.nifti', 'auxiliary.normalization', 'auxiliary.tiff']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nibabel>3.0',
+ 'numpy>1.24',
+ 'path>=16.7.1,<17.0.0',
+ 'pathlib>=1.0,<2.0',
+ 'pillow>=10.0.0',
+ 'tifffile>2023.8.25']
+
+setup_kwargs = {
+    'name': 'auxiliary',
+    'version': '0.0.34',
+    'description': 'TODO.',
+    'long_description': None,
+    'author': 'Florian Kofler',
+    'author_email': 'florian.kofler@tum.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
