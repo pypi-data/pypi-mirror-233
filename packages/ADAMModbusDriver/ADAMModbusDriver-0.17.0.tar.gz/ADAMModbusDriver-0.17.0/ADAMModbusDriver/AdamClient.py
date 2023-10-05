5b5f08d7c041f7bee474b7f5f7ba97141b2010c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:08 +0000

    Add package ADAMModbusDriver/0.17.0/ADAMModbusDriver-0.17.0.tar.gz

---
+++ packages/ADAMModbusDriver/ADAMModbusDriver-0.17.0.tar.gz/ADAMModbusDriver-0.17.0/ADAMModbusDriver/AdamClient.py
@@ -0,0 +1,64 @@
+from pyModbusTCP.client import ModbusClient
+
+class AdamClient:
+        
+    thermocouple_ranges = {
+        "J": {"min": 0.0, "max": 760.0},
+        "K": {"min": 0.0, "max": 1370.0},
+        "T": {"min": -100.0, "max": 400.0},
+        "E": {"min": 0.0, "max": 1000.0},
+        "R": {"min": 500.0, "max": 1750.0},
+        "S": {"min": 500.0, "max": 1750.0},
+        "B": {"min": 500.0, "max": 1800.0},
+    }
+
+    def __init__(self, host, thermocouple_type, port=502, unit_id=1):
+        if thermocouple_type not in self.thermocouple_ranges:
+            raise ValueError(f"Unsupported thermocouple type: {thermocouple_type}")
+        
+        self.client = ModbusClient(host=host, port=port, unit_id=unit_id)
+        self.thermocouple_type = thermocouple_type
+        self.chans_in = 8
+        self.channel_names = [f"Thermo Channel {i+1}" for i in range(self.chans_in)]
+
+    def convert(self, reg_values):
+        thermocouple_range = self.thermocouple_ranges[self.thermocouple_type]
+        range_min = thermocouple_range["min"]
+        range_max = thermocouple_range["max"]
+        converted_values = [(raw_temperature / 65535.0) * (range_max - range_min) + range_min for raw_temperature in reg_values]
+        return [round(value, 2) for value in converted_values]
+
+    def read_samples(self, address=0, count=8):
+        if self.client.open():
+            regs = self.client.read_holding_registers(address, count)
+            self.client.close()
+            if regs:
+                return self.convert(regs)
+            else:
+                raise Exception("Read failed")
+        else:
+            raise Exception("Failed to Connect")
+        
+    def get_channel_names(self):
+        return self.channel_names
+    
+    def set_channel_name(self, position, name):
+        """
+        Set the name of a specific channel based on its position.
+
+        Args:
+            position (int): Position of the channel (0-based index).
+            name (str): New name for the channel.
+        """
+        if position < 0 or position >= self.chans_in:
+            raise ValueError(f"Invalid position value. Must be between 0 and {self.chans_in-1}.")
+        self.channel_names[position] = name
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def close(self):
+        pass
---
+++ packages/ADAMModbusDriver/ADAMModbusDriver-0.17.0.tar.gz/ADAMModbusDriver-0.17.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ADAMModbusDriver',  #
+    version='0.17.0',
+    description='A Python client for interfacing with Adam devices via ModbusTCP',  
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Brian Benchoff',
+    author_email='brian.benchoff@span.io',
+    url='https://github.com/spanio/ADAM-driver',
+    packages=find_packages(),
+    install_requires=[
+        'pyModbusTCP>=0.1.8',  
+        'pymodbus>=2.5.3',  
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', # Change if you have a different license
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
