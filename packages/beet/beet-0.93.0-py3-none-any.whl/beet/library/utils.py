3311516bffc16a31593c3986fc377406d2121531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:08:14 +0000

    Add package beet/0.93.0/beet-0.93.0-py3-none-any.whl

---
+++ packages/beet/beet-0.93.0-py3-none-any.whl/beet/library/utils.py
@@ -0,0 +1,27 @@
+__all__ = [
+    "list_files",
+    "list_extensions",
+]
+
+
+import os
+from itertools import accumulate
+from pathlib import Path, PurePath
+from typing import Iterator, List
+
+from beet.core.utils import FileSystemPath
+
+
+def list_files(directory: FileSystemPath) -> Iterator[Path]:
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            yield Path(root, filename).relative_to(directory)
+
+
+def list_extensions(path: PurePath) -> List[str]:
+    extensions: List[str] = list(
+        accumulate(reversed(path.suffixes), lambda a, b: b + a)  # type: ignore
+    )
+    extensions.reverse()
+    extensions.append("")
+    return extensions
