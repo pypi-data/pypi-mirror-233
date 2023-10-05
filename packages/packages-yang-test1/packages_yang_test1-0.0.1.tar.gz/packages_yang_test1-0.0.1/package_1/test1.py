5e0f2d96f364e03ec4b4a125021eeecef9e5b7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:07:02 +0000

    Add package packages-yang-test1/0.0.1/packages_yang_test1-0.0.1.tar.gz

---
+++ packages/packages-yang-test1/packages_yang_test1-0.0.1.tar.gz/packages_yang_test1-0.0.1/package_1/test1.py
@@ -0,0 +1,14 @@
+class TestPip(object):
+
+    def __init__(self):
+        super(TestPip, self).__init__()
+
+    def run(self):
+
+        print("zhangyang test1")
+
+
+if __name__=='__main__':
+
+    aa = TestPip()
+    aa.run()
---
+++ packages/packages-yang-test1/packages_yang_test1-0.0.1.tar.gz/packages_yang_test1-0.0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='packages_yang_test1',
+    version='0.0.1',
+    packages=['package_1'],
+)
