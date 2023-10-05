8931b9fc33bc0e34ba99af4af797e07a3476820b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:14 +0000

    Add package nodeps/0.0.2/nodeps-0.0.2-py3-none-any.whl

---
+++ packages/nodeps/nodeps-0.0.2-py3-none-any.whl/nodeps/__init__.py
@@ -0,0 +1,110 @@
+"""NoDeps Helpers and Utils Module."""
+__all__ = (
+    "EXECUTABLE",
+    "EXECUTABLE_SITE",
+    "LINUX",
+    "MACOS",
+    "PW_ROOT",
+    "PW_USER",
+    "USER",
+    "TempDir",
+    "ami",
+    "toiter",
+)
+
+import getpass
+import os
+import pwd
+import sys
+import tempfile
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Any, ParamSpec, TypeVar
+
+EXECUTABLE = Path(sys.executable)
+EXECUTABLE_SITE = Path(EXECUTABLE).resolve()
+LINUX = sys.platform == "linux"
+"""Is Linux? sys.platform == 'linux'"""
+MACOS = sys.platform == "darwin"
+"""Is macOS? sys.platform == 'darwin'"""
+PW_ROOT = pwd.getpwnam("root")
+PW_USER = pwd.getpwnam(os.environ["USER"])
+USER = os.getenv("USER")
+""""Environment Variable $USER"""
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+class TempDir(tempfile.TemporaryDirectory):
+    """Wrapper for :class:`tempfile.TemporaryDirectory` that provides Path-like.
+
+    Examples:
+        >>> from nodeps import TempDir
+        >>> from nodeps import MACOS
+        >>> with TempDir() as tmp:
+        ...     if MACOS:
+        ...         assert tmp.parts[1] == "var"
+        ...         assert tmp.resolve().parts[1] == "private"
+    """
+
+    def __enter__(self) -> Path:
+        """Return the path of the temporary directory.
+
+        Returns:
+            Path of the temporary directory
+        """
+        return Path(self.name)
+
+
+def ami(user: str = "root") -> bool:
+    """Check if Current User is User in Argument (default: root).
+
+    Examples:
+        >>> from nodeps import ami
+        >>> from nodeps import USER
+        >>>
+        >>> ami(USER)
+        True
+        >>> ami()
+        False
+
+    Arguments:
+        user: to check against current user (Default: False)
+
+    Returns:
+        CompletedProcess if the current user is not the same as user, None otherwise
+    """
+    return os.getuid() == pwd.getpwnam(user or getpass.getuser()).pw_uid
+
+
+def toiter(obj: Any, always: bool = False, split: str = " ") -> Any:
+    """To iter.
+
+    Examples:
+        >>> import pathlib
+        >>> from nodeps import toiter
+        >>>
+        >>> assert toiter('test1') == ['test1']
+        >>> assert toiter('test1 test2') == ['test1', 'test2']
+        >>> assert toiter({'a': 1}) == {'a': 1}
+        >>> assert toiter({'a': 1}, always=True) == [{'a': 1}]
+        >>> assert toiter('test1.test2') == ['test1.test2']
+        >>> assert toiter('test1.test2', split='.') == ['test1', 'test2']
+        >>> assert toiter(pathlib.Path("/tmp/foo")) == ('/', 'tmp', 'foo')
+
+    Args:
+        obj: obj.
+        always: return any iterable into a list.
+        split: split for str.
+
+    Returns:
+        Iterable.
+    """
+    if isinstance(obj, str):
+        obj = obj.split(split)
+    elif hasattr(obj, "parts"):
+        obj = obj.parts
+    elif not isinstance(obj, Iterable) or always:
+        obj = [obj]
+    return obj
