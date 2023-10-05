5a78c267a6304bc40f90021b4fb865f657f4c355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:06:50 +0000

    Add package laff/0.8.2/laff-0.8.2.tar.gz

---
+++ packages/laff/laff-0.8.2.tar.gz/laff-0.8.2/laff/__init__.py
@@ -0,0 +1,3 @@
+from .laff import findFlares, fitContinuum, fitFlares, fitGRB, plotGRB
+from .lightcurve_import import lcimport
+from .utility import STAT_NAMES_CONTINUUM, PAR_NAMES_FLARE, PAR_NAMES_CONTINUUM
