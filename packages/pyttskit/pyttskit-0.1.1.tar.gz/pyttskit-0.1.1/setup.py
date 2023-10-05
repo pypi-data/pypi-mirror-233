11c3ad9a47dd67956ba1d0563445ed876f631a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:05 +0000

    Add package pyttskit/0.1.1/pyttskit-0.1.1.tar.gz

---
+++ packages/pyttskit/pyttskit-0.1.1.tar.gz/pyttskit-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyttskit',
+    version='0.1.1',
+    packages=find_packages(),
+    author='mrfakename',
+    author_email='me@mrfake.name',
+    description='Easily interact with many cutting-edge neural/AI text-to-speech models using one API, including transformer-powered text to speech and other advanced models!',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ospyp/TTSKit',
+    license='Multiple (See File). Main: NOSCL-C-2.0. Includes MIT, etc',
+)
