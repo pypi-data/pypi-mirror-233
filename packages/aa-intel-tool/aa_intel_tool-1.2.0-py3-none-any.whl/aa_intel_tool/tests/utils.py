4200f9fb19bae008a040d01d858ceaef1fb24c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:07:45 +0000

    Add package aa-intel-tool/1.2.0/aa_intel_tool-1.2.0-py3-none-any.whl

---
+++ packages/aa-intel-tool/aa_intel_tool-1.2.0-py3-none-any.whl/aa_intel_tool/tests/utils.py
@@ -0,0 +1,67 @@
+"""
+Helper for our tests
+"""
+
+# Standard Library
+from pathlib import Path
+
+# Django
+from django.template import Context, Template
+
+
+def render_template(string, context=None):
+    """
+    Helper to render templates
+    :param string:
+    :param context:
+    :return:
+    """
+
+    context = context or {}
+    context = Context(dict_=context)
+
+    return Template(template_string=string).render(context=context)
+
+
+def load_chatscan_txt() -> str:
+    """
+    Loading chatscan.txt
+
+    :return:
+    :rtype:
+    """
+
+    return (Path(__file__).parent / "test-data/chatscan.txt").read_text()
+
+
+def load_chatscan_faulty_txt() -> str:
+    """
+    Loading chatscan-faulty.txt
+
+    :return:
+    :rtype:
+    """
+
+    return (Path(__file__).parent / "test-data/chatscan-faulty.txt").read_text()
+
+
+def load_dscan_txt() -> str:
+    """
+    Loading dscan.txt
+
+    :return:
+    :rtype:
+    """
+
+    return (Path(__file__).parent / "test-data/dscan.txt").read_text()
+
+
+def load_fleetcomp_txt() -> str:
+    """
+    Loading fleetcomp.txt
+
+    :return:
+    :rtype:
+    """
+
+    return (Path(__file__).parent / "test-data/fleetcomp.txt").read_text()
