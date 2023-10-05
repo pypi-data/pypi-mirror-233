22b5da5c27abff8be31615aee04c58251938cf9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:08:11 +0000

    Add package e3nn-jax/0.20.2/e3nn_jax-0.20.2-py3-none-any.whl

---
+++ packages/e3nn-jax/e3nn_jax-0.20.2-py3-none-any.whl/e3nn_jax/flax.py
@@ -0,0 +1,11 @@
+try:
+    import flax
+
+    del flax
+except ImportError:
+    pass
+else:
+    from e3nn_jax._src.linear_flax import Linear
+    from e3nn_jax._src.mlp_flax import MultiLayerPerceptron
+
+    __all__ = ["Linear", "MultiLayerPerceptron"]
