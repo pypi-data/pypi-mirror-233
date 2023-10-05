985465b63f5951168f39cdc40f42e8accf8304ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:51 +0000

    Add package generate-tools/0.0.5/generate_tools-0.0.5-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/generate-tools/generate_tools-0.0.5-cp39-cp39-manylinux2014_x86_64.whl/generate/method/delete_memory.py
@@ -0,0 +1,33 @@
+import generate
+from generate.exception import IsAuthorized
+
+class DeleteMemory:
+    def delete(
+        self: 'generate.Generate',
+        target: str,
+        data: tuple
+    ):
+        """
+        This function for delete memory
+
+        Arguments
+            objec (`str`):
+                The objec, get data for delete from memory.
+
+            data (`tuple`):
+                The data delete from memory values.
+
+        Example
+            
+            gen.delete(
+                objec='email username domain',
+                data=(username, domain, user_id)
+            )
+        """
+        if not self.is_authorize:
+            raise IsAuthorized()
+        
+        return self.memory.delete_memory(
+            objec=target,
+            data=data
+        )
