33be5edcce28d28a3a99e50cd110446e0d3dbb47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:59 +0000

    Add package scaleway-async/1.3.0/scaleway_async-1.3.0-py3-none-any.whl

---
+++ packages/scaleway-async/scaleway_async-1.3.0-py3-none-any.whl/scaleway_async/tem/v1alpha1/__init__.py
@@ -0,0 +1,49 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import DomainLastStatusRecordStatus
+from .types import DomainStatus
+from .types import EmailFlag
+from .types import EmailRcptType
+from .types import EmailStatus
+from .types import ListEmailsRequestOrderBy
+from .types import CreateEmailRequestAddress
+from .types import CreateEmailRequestAttachment
+from .types import CreateEmailResponse
+from .types import Domain
+from .types import DomainLastStatus
+from .types import DomainLastStatusDkimRecord
+from .types import DomainLastStatusSpfRecord
+from .types import DomainStatistics
+from .types import Email
+from .types import EmailTry
+from .types import ListDomainsResponse
+from .types import ListEmailsResponse
+from .types import Statistics
+from .content import DOMAIN_TRANSIENT_STATUSES
+from .content import EMAIL_TRANSIENT_STATUSES
+from .api import TemV1Alpha1API
+
+__all__ = [
+    "DomainLastStatusRecordStatus",
+    "DomainStatus",
+    "EmailFlag",
+    "EmailRcptType",
+    "EmailStatus",
+    "ListEmailsRequestOrderBy",
+    "CreateEmailRequestAddress",
+    "CreateEmailRequestAttachment",
+    "CreateEmailResponse",
+    "Domain",
+    "DomainLastStatus",
+    "DomainLastStatusDkimRecord",
+    "DomainLastStatusSpfRecord",
+    "DomainStatistics",
+    "Email",
+    "EmailTry",
+    "ListDomainsResponse",
+    "ListEmailsResponse",
+    "Statistics",
+    "DOMAIN_TRANSIENT_STATUSES",
+    "EMAIL_TRANSIENT_STATUSES",
+    "TemV1Alpha1API",
+]
