e227c832edfd691b6e49e28a8177a7b7613e4d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:06:28 +0000

    Add package dj-notebook/0.4.0/dj_notebook-0.4.0.tar.gz

---
+++ packages/dj-notebook/dj_notebook-0.4.0.tar.gz/dj_notebook-0.4.0/src/dj_notebook/__init__.py
@@ -0,0 +1,48 @@
+import django
+import os
+from django.core.management.color import no_style
+from django.conf import settings as django_settings
+from django_extensions.management import shells
+
+from IPython.utils.capture import capture_output
+
+from rich.console import Console
+from rich.status import Status
+
+
+from .shell_plus import Plus
+
+console = Console()
+
+
+def activate(settings: str, quiet_load: bool = True) -> Plus:
+    with Status("Loading dj-notebook...", spinner="bouncingBar"):
+        if settings == "test_harness":
+            # Used for testing
+            # NOTE: This is bad code smell, we'll improve on it
+            #      when we have a better idea of what we're doing
+            django_settings.configure(
+                INSTALLED_APPS=[
+                    "django.contrib.admin",
+                    "django.contrib.auth",
+                    "django.contrib.contenttypes",
+                    "django.contrib.sessions",
+                    "django.contrib.messages",
+                    "django.contrib.staticfiles",
+                ]
+            )
+        else:
+            # Used for standard operations
+            os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+            os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+            django.setup()
+
+        with capture_output() as c:
+            plus = Plus(shells.import_objects({"quiet_load": False}, no_style()))
+
+        plus._import_object_history = c.stdout
+
+        if quiet_load is False:
+            plus.print()
+
+    return plus
