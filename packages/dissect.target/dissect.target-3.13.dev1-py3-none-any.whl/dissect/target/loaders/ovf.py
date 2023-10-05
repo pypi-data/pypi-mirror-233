62f285a01d552fe73e31d8ff1deb2749f0904e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:06:36 +0000

    Add package dissect.target/3.13.dev1/dissect.target-3.13.dev1-py3-none-any.whl

---
+++ packages/dissect.target/dissect.target-3.13.dev1-py3-none-any.whl/dissect/target/loaders/ovf.py
@@ -0,0 +1,39 @@
+from dissect.hypervisor import ovf
+
+from dissect.target.containers.vmdk import VmdkContainer
+from dissect.target.loader import Loader
+
+
+class OvfLoader(Loader):
+    """Load Open Virtualization Format (OVF) files.
+
+    References:
+        - https://en.wikipedia.org/wiki/Open_Virtualization_Format
+    """
+
+    def __init__(self, path, **kwargs):
+        path = path.resolve()
+
+        super().__init__(path)
+        with open(path) as fh:
+            self.ovf = ovf.OVF(fh)
+        self.base_dir = path.parent
+
+    @staticmethod
+    def detect(path):
+        return path.suffix.lower() == ".ovf"
+
+    def map(self, target):
+        for disk in self.ovf.disks():
+            try:
+                path = disk.replace("\\", "/")
+                _, _, fname = path.rpartition("/")
+                fh = self.open(fname)
+            except IOError:
+                target.log.exception("Failed to find disk: %s", disk)
+                continue
+
+            target.disks.add(VmdkContainer(fh))
+
+    def open(self, path):
+        return self.base_dir.joinpath(path).open("rb")
