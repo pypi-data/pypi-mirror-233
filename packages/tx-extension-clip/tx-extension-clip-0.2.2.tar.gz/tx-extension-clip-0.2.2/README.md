503398f1ee539293557387971922ede18a68f060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:08 +0000

    Add package tx-extension-clip/0.2.2/tx-extension-clip-0.2.2.tar.gz

---
+++ packages/tx-extension-clip/tx-extension-clip-0.2.2.tar.gz/tx-extension-clip-0.2.2/README.md
@@ -0,0 +1,21 @@
+CLIP Extension for Threat Exchange
+
+# Installing
+
+https://test.pypi.org/project/tx-exchange-clip/
+
+# what is CLIP?
+CLIP (Contrastive Language–Image Pre-training)
+Model built by OpenAI: https://openai.com/research/clip
+
+https://github.com/openai/CLIP
+
+
+## Requirements
+
+```
+> python3 -m venv omm_clip
+> source omm_clip/bin/activate
+> (omm_clip) pip install -r requirements.txt
+```
+
---
+++ packages/tx-extension-clip/tx-extension-clip-0.2.2.tar.gz/tx-extension-clip-0.2.2/setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+here = Path(__file__).parent
+
+long_description = (here / "README.md").read_text(encoding="utf-8").strip()
+
+setup(
+    name="tx-extension-clip",
+    version="0.2.2",
+    author="Technology Coalition Org",
+    author_email="tech@technologycoalition.org",
+    description="Python Library for Threat Exchange CLIP Extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TechnologyCoalitionOrg/tx-extension-clip",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "faiss-cpu>=1.7.3",  # faiss
+        "numpy>=1.24.2",  # faiss
+        "open-clip-torch>=2.20.0",
+        "Pillow>=9.4.0",
+        "scipy>=1.11.3",
+        "threatexchange>=1.0.13",
+        "torch>=2.0.1",
+        "torchvision>=0.15.2",
+        "transformers>=4.34.0",
+    ],
+    packages=find_packages(exclude=["tests*"]),
+    python_requires=">=3.6",
+)
