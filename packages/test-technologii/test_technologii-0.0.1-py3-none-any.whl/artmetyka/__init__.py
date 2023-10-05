592b6f53b22154ba4a213faefd8fcd542ebfc91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:10:16 +0000

    Add package test-technologii/0.0.1/test_technologii-0.0.1-py3-none-any.whl

---
+++ packages/test-technologii/test_technologii-0.0.1-py3-none-any.whl/artmetyka/__init__.py
@@ -0,0 +1,1 @@
+from .arth_operations import *
---
+++ packages/test-technologii/test_technologii-0.0.1-py3-none-any.whl/artmetyka/arth_operations.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+# FUN_CONSTANT = "Fun constant"
+#
+# class Human:
+#     pass
+
+if __name__ == "__main__":
+    print(add(1, 2))
