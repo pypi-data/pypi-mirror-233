b65bcef972c80e5893c23e49b906af09c5e9fd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:43 +0000

    Add package hezar/0.29.0/hezar-0.29.0-py3-none-any.whl

---
+++ packages/hezar/hezar-0.29.0-py3-none-any.whl/hezar/models/image2text/crnn/crnn_image2text_config.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import Dict
+
+from ....configs import ModelConfig
+
+
+@dataclass
+class CRNNImage2TextConfig(ModelConfig):
+    name = "crnn_image2text"
+    id2label: Dict[int, str] = None
+    blank_id: int = 0
+    n_channels: int = 1
+    image_height: int = 32
+    image_width: int = 128
+    map2seq_dim: int = 64
+    rnn_dim: int = 256
+
