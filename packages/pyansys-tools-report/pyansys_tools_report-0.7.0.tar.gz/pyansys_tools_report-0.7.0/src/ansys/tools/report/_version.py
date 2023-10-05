a735e5caa3a54d9ec74d4e46b405a091e043f5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:48 +0000

    Add package pyansys-tools-report/0.7.0/pyansys_tools_report-0.7.0.tar.gz

---
+++ packages/pyansys-tools-report/pyansys_tools_report-0.7.0.tar.gz/pyansys_tools_report-0.7.0/src/ansys/tools/report/_version.py
@@ -0,0 +1,19 @@
+"""
+PyAnsys Tools Report version module.
+
+Module containing the version function for .
+"""
+import importlib.metadata as importlib_metadata
+
+__version__ = importlib_metadata.version("pyansys-tools-report")
+
+
+def version():
+    """Return the version of the PyAnsys Report Tool.
+
+    Returns
+    -------
+    str
+        The version of the tool being used.
+    """
+    return __version__
