8a5490a3b2886ff57dc3d238de0d4aee3c2d91a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 15:07:17 +0000

    Add package pytedee-async/0.2.0b7/pytedee_async-0.2.0b7.tar.gz

---
+++ packages/pytedee-async/pytedee_async-0.2.0b7.tar.gz/pytedee_async-0.2.0b7/pytedee_async/TedeeClientException.py
@@ -0,0 +1,19 @@
+class TedeeClientException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+        
+class TedeeAuthException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class TedeeRateLimitException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class TedeeWebhookException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class TedeeDataUpdateException(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
