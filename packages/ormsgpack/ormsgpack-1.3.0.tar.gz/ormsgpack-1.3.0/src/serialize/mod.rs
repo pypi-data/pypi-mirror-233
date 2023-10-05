4e862503164308c56fddc916b9465924042c6a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:10:16 +0000

    Add package ormsgpack/1.3.0/ormsgpack-1.3.0.tar.gz

---
+++ packages/ormsgpack/ormsgpack-1.3.0.tar.gz/ormsgpack-1.3.0/src/serialize/mod.rs
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+mod bytes;
+mod dataclass;
+mod datetime;
+mod default;
+mod dict;
+mod int;
+mod list;
+mod numpy;
+mod serializer;
+mod str;
+mod tuple;
+mod uuid;
+mod writer;
+
+pub use serializer::serialize;
