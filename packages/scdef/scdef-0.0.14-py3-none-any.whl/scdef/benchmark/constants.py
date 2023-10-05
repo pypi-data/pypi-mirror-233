b573bb77500c557ddd36d97d18bcfa24af58f038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:33 +0000

    Add package scdef/0.0.14/scdef-0.0.14-py3-none-any.whl

---
+++ packages/scdef/scdef-0.0.14-py3-none-any.whl/scdef/benchmark/constants.py
@@ -0,0 +1,24 @@
+OTHERS_LABELS = [
+    "Leiden",
+    "scVI",
+    "Harmony",
+    "Scanorama",
+    "LDVAE",
+    "NMF",
+    "scHPF",
+]
+
+OTHERS_RES_SWEEPS = dict(
+    zip(
+        OTHERS_LABELS,
+        [
+            [1.0, 0.3, 0.1, 0.03],
+            [1.0, 0.3, 0.1, 0.03],
+            [1.0, 0.3, 0.1, 0.03],
+            [1.0, 0.3, 0.1, 0.03],
+            [10, 3, 1, 0.3],
+            [10, 3, 1, 0.3],
+            [10, 3, 1, 0.3],
+        ],
+    )
+)
