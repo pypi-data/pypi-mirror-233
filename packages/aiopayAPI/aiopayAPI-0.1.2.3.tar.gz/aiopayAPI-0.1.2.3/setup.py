8bf62273ac57941a3547b6a1b2440bdcf011aeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:07:02 +0000

    Add package aiopayAPI/0.1.2.3/aiopayAPI-0.1.2.3.tar.gz

---
+++ packages/aiopayAPI/aiopayAPI-0.1.2.3.tar.gz/aiopayAPI-0.1.2.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', encoding="utf-8") as f:
+  long_description = f.read()
+
+
+setup(
+  name='aiopayAPI',
+  version='0.1.2.3',
+  author='xllebbSQ',
+  author_email='090504opo@gmail.com',
+  description='Асинхронный API для работы с платежной системой Payok.io.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/xllebbSQ/aiopay',
+  packages=find_packages(),
+  install_requires=['aiohttp>=3.8.5',
+                    "asyncio>=3.4.3"],
+  classifiers=[
+    'Programming Language :: Python :: 3.8',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='python PayOk payok api API ayncio payok api',
+  project_urls={
+    'GitHub': 'https://github.com/xllebbSQ'
+  },
+  python_requires='>=3.8'
+)
