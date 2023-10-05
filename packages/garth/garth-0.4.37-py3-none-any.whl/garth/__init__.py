f35676d228014747684c32716aeadf4ad2253f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:00 +0000

    Add package garth/0.4.37/garth-0.4.37-py3-none-any.whl

---
+++ packages/garth/garth-0.4.37-py3-none-any.whl/garth/__init__.py
@@ -0,0 +1,42 @@
+from .data import HRVData, SleepData
+from .http import Client, client
+from .stats import (
+    DailyHRV,
+    DailyIntensityMinutes,
+    DailySleep,
+    DailySteps,
+    DailyStress,
+    WeeklyIntensityMinutes,
+    WeeklySteps,
+    WeeklyStress,
+)
+from .version import __version__
+
+__all__ = [
+    "Client",
+    "DailyHRV",
+    "DailyIntensityMinutes",
+    "DailySleep",
+    "DailySteps",
+    "DailyStress",
+    "HRVData",
+    "SleepData",
+    "WeeklyIntensityMinutes",
+    "WeeklySteps",
+    "WeeklyStress",
+    "__version__",
+    "client",
+    "configure",
+    "connectapi",
+    "login",
+    "resume",
+    "save",
+]
+
+configure = client.configure
+connectapi = client.connectapi
+download = client.download
+login = client.login
+resume = client.load
+save = client.dump
+upload = client.upload
