51559031f0430f40b8ecbd60ae291dba1b0644fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 15:08:04 +0000

    Add package EasyPro/39.2023.10.5.15.29/EasyPro-39.2023.10.5.15.29.tar.gz

---
+++ packages/EasyPro/EasyPro-39.2023.10.5.15.29.tar.gz/EasyPro-39.2023.10.5.15.29/EasyPro/FileSys/edf.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/16 16:47
+# @Author  : Quanfa
+
---
+++ packages/EasyPro/EasyPro-39.2023.10.5.15.29.tar.gz/EasyPro-39.2023.10.5.15.29/EasyPro/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/8/31 11:07
+# @Author  : Quanfa
+from .FileSys.path_tool import MyPath
