cef357444a48d1739dfe93b2d0aa0c0c6a752d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:06:12 +0000

    Add package dialog-workflow-local/0.0.54/dialog-workflow-local-0.0.54.tar.gz

---
+++ packages/dialog-workflow-local/dialog-workflow-local-0.0.54.tar.gz/dialog-workflow-local-0.0.54/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='dialog-workflow-local',  
+     version='0.0.54',
+     author="Circles",
+     author_email="info@circle.life",
+     description="PyPI Package for dialog workflow",
+     long_description="This is a package for running the dialog workflow",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
