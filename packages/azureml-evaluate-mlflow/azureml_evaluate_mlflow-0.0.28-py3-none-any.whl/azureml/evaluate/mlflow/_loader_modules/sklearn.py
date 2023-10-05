026e21c1df906f720559e5feed9679353b5342d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 15:06:38 +0000

    Add package azureml-evaluate-mlflow/0.0.28/azureml_evaluate_mlflow-0.0.28-py3-none-any.whl

---
+++ packages/azureml-evaluate-mlflow/azureml_evaluate_mlflow-0.0.28-py3-none-any.whl/azureml/evaluate/mlflow/_loader_modules/sklearn.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+import mlflow
+
+
+def _load_azureml(path):
+    """
+        Load Azureml implementation. Called by ``azureml.load_model``.
+
+        :param path: Local filesystem path to the MLflow Model with the ``sklearn`` flavor.
+    """
+    return mlflow.sklearn.load_model(path)
