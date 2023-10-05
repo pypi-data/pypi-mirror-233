36739c89fbfd1d48d95cf47592cd60eb96dac94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:08:04 +0000

    Add package OIDC-example-packagingCon23/0.0.1/OIDC-example-packagingCon23-0.0.1.tar.gz

---
+++ packages/OIDC-example-packagingCon23/OIDC-example-packagingCon23-0.0.1.tar.gz/OIDC-example-packagingCon23-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='OIDC-example-packagingCon23',
+    version="1.0.0",
+    description='OIDC-example-packagingCon23',
+    long_description="OIDC-example-packagingCon23",
+    long_description_content_type='text/markdown',
+)
