425d394c779051e9ff1f503e69853892618f645d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:08:08 +0000

    Add package devpi-plumber/0.7.0/devpi-plumber-0.7.0.tar.gz

---
+++ packages/devpi-plumber/devpi-plumber-0.7.0.tar.gz/devpi-plumber-0.7.0/CHANGELOG.rst
@@ -0,0 +1,45 @@
+0.7.0
+-----
+
+- Add official support for Python 3.10 and 3.11.
+- Improve compatibility with new Devpi Client and Setuptools releases.
+- Drop support for Python 3.6 and 3.7.
+
+0.6.0
+-----
+
+- Establish compatibility with Devpi Server 6. Compatibility with Devpi Server < 5.2.0 is dropped for this.
+- Add official support for Python 3.7 to 3.9.
+- Drop support for Python 2.7 and 3.5.
+
+0.5.1
+-----
+
+- Improve compatibility with Devpi 5.x by not processing events during state import.
+
+0.5.0
+-----
+
+- Support for Python 3.2, 3.3, and 3.4 is no longer tested.
+- Add new list_users, delete_user and delete_index functions to DevpiCommandWrapper
+- Add DevpiCommandWrapper.user_session, a contextmanager which handles logon/logoff with supplied credentials
+
+0.4.3
+-----
+
+- Fix compatibility with devpi-server 4.3.0 and up.
+- The new environment variables ``DEVPI_PLUMBER_SERVER_HOST`` and ``DEVPI_PLUMBER_SERVER_PORT`` allow you to tune where
+  the test server binds to from the outside.
+- Add official support for Python 3.6.
+
+0.4.2
+-----
+- Don't cache servers started with `--no-root-pypi`.
+
+0.4.1
+-----
+- Fix bug in `import_state`.
+
+0.4.0
+-----
+- Adapt for compatibility with devpi>=4.2.1 . This breaks compatibility with devpi<4.2.1 .
---
+++ packages/devpi-plumber/devpi-plumber-0.7.0.tar.gz/devpi-plumber-0.7.0/setup.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+
+import multiprocessing  # avoid crash on teardown
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+with open('core-requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines() if line[:1] != '#']
+with open('extra-test-requirements.txt') as f:
+    extra_test_requirements = [line.strip() for line in f.readlines() if line[:1] != '#']
+
+
+setup(
+    name='devpi-plumber',
+    use_scm_version=True,
+    packages=find_packages(exclude=['tests']),
+    author='Stephan Erb',
+    author_email='stephan.erb@blue-yonder.com',
+    url='https://github.com/blue-yonder/devpi-plumber',
+    description='Mario, the devpi-plumber, helps to automate and test large devpi installations.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    license='new BSD',
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    install_requires=requirements,
+    extras_require={
+        'test': extra_test_requirements,
+    },
+    tests_require=[
+        'mock',
+        'pytest',
+        'pytest-cov',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: System :: Archiving :: Packaging',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
