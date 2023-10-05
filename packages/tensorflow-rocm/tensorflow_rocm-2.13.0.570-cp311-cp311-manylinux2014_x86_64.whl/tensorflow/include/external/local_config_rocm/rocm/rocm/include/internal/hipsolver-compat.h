69217e657bb168f9ee5dfd7bc00e691354200e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:09:26 +0000

    Add package tensorflow-rocm/2.13.0.570/tensorflow_rocm-2.13.0.570-cp311-cp311-manylinux2014_x86_64.whl

---
+++ packages/tensorflow-rocm/tensorflow_rocm-2.13.0.570-cp311-cp311-manylinux2014_x86_64.whl/tensorflow/include/external/local_config_rocm/rocm/rocm/include/internal/hipsolver-compat.h
@@ -0,0 +1,33 @@
+/*
+    Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+*/
+
+#ifndef ROCM_SYMLINK_INTERNAL_HIPSOLVER_COMPAT_H
+#define ROCM_SYMLINK_INTERNAL_HIPSOLVER_COMPAT_H
+
+#if defined(ROCM_NO_WRAPPER_HEADER_WARNING) || defined(ROCM_SYMLINK_GAVE_WARNING)
+/* include file */
+#include "../hipsolver/internal/hipsolver-compat.h"
+#else
+#ifndef ROCM_HEADER_WRAPPER_WERROR
+#define ROCM_HEADER_WRAPPER_WERROR 0
+#endif
+#if ROCM_HEADER_WRAPPER_WERROR  /* ROCM_HEADER_WRAPPER_WERROR 1 */
+#error "This file is deprecated. Use the header file from /opt/rocm-5.7.0/include/hipsolver/internal/hipsolver-compat.h by using #include <hipsolver/internal/hipsolver-compat.h>"
+#else  /* ROCM_HEADER_WRAPPER_WERROR 0 */
+/* give warning */
+#if defined(_MSC_VER)
+#pragma message(": warning:This file is deprecated. Use the header file from /opt/rocm-5.7.0/include/hipsolver/internal/hipsolver-compat.h by using #include <hipsolver/internal/hipsolver-compat.h>")
+#elif defined(__GNUC__)
+#warning "This file is deprecated. Use the header file from /opt/rocm-5.7.0/include/hipsolver/internal/hipsolver-compat.h by using #include <hipsolver/internal/hipsolver-compat.h>"
+#endif
+#endif  /* ROCM_HEADER_WRAPPER_WERROR */
+/* include file */
+#define ROCM_SYMLINK_GAVE_WARNING
+#include "../hipsolver/internal/hipsolver-compat.h"
+#undef ROCM_SYMLINK_GAVE_WARNING
+#endif /* defined(ROCM_NO_WRAPPER_HEADER_WARNING) || defined(ROCM_SYMLINK_GAVE_WARNING) */
+
+#endif /* ROCM_SYMLINK_INTERNAL_HIPSOLVER_COMPAT_H */
+
+
