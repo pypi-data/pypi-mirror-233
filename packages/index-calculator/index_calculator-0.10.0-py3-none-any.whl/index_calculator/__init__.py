3d57de64a1be94d40f9f99bcb9fc39dc19ad86a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:06:21 +0000

    Add package index-calculator/0.10.0/index_calculator-0.10.0-py3-none-any.whl

---
+++ packages/index-calculator/index_calculator-0.10.0-py3-none-any.whl/index_calculator/__init__.py
@@ -0,0 +1,28 @@
+"""Top-level package for index_calculator."""
+
+__author__ = """Ludwig Lierhammer"""
+__email__ = "ludwig.lierhammer@hereon.de"
+__version__ = "0.10.0"
+
+from ._outputwriter import OutputWriter as outputwriter
+from ._postprocessing import PostProcessing as postprocessing
+from ._preprocessing import PreProcessing as preprocessing
+from ._processing import Processing as processing
+from ._tables import cfjson  # noqa
+from ._tables import fjson  # noqa
+from ._tables import mjson  # noqa
+from ._tables import pjson  # noqa
+from ._tables import vjson  # noqa
+from ._tables import xjson  # noqa
+from .index_calculator import IndexCalculator as index_calculator
+
+preprocessing.__module__ = __name__
+preprocessing.__name__ = "preprocessing"
+processing.__module__ = __name__
+processing.__name__ = "processing"
+postprocessing.__module__ = __name__
+postprocessing.__name__ = "postprocessing"
+outputwriter.__module__ = __name__
+outputwriter.__name__ = "outputwriter"
+index_calculator.__module__ = __name__
+index_calculator.__name__ = "index_calculator"
