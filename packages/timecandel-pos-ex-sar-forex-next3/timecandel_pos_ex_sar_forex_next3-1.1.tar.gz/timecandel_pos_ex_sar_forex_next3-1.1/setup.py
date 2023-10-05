e8e54c881e49b0a285698a46aa07a70c2e21cf23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:03 +0000

    Add package timecandel-pos-ex-sar-forex-next3/1.1/timecandel_pos_ex_sar_forex_next3-1.1.tar.gz

---
+++ packages/timecandel-pos-ex-sar-forex-next3/timecandel_pos_ex_sar_forex_next3-1.1.tar.gz/timecandel_pos_ex_sar_forex_next3-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name='timecandel_pos_ex_sar_forex_next3' ,
+       version = '1.1' ,
+       description = 'timecandel_pos_ex_sar_forex_next3' ,
+       author = 'omid' ,
+       packages =['timecandel_pos_ex_sar_forex_next3'] ,
+       zip_safe = False
+       
+       )
