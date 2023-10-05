39cf812b567368b326b5d5504250b0c24d94e924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:41 +0000

    Add package larvaworld/0.0.63/larvaworld-0.0.63-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.63-py3-none-any.whl/larvaworld/examples/data_import/import_Schleyer.py
@@ -0,0 +1,38 @@
+from ...lib import reg
+
+
+from ...lib.process.importing import import_dataset
+
+kws0 = {
+        'labID': 'Schleyer',
+        'group_id': 'exploration',
+    }
+
+    # Merged case
+N=40
+kws1 = {
+    'parent_dir': 'exploration',
+    'merged': True,
+    'N': N,
+    'min_duration_in_sec': 120,
+    'refID': f'exploration.{N}controls',
+    **kws0
+}
+
+# Single dish case
+folder='dish01'
+kws2 = {
+    'parent_dir': f'exploration/{folder}',
+    'merged': False,
+    'N': None,
+    'min_duration_in_sec': 90,
+    'id': folder,
+    'refID': f'exploration.{folder}',
+    **kws0
+}
+
+
+
+d1 = import_dataset(**kws1)
+d2 = import_dataset(**kws2)
+
