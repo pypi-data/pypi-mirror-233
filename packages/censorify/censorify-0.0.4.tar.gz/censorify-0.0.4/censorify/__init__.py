993d7f3ff0e076ccd1294d56283fdae372909d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:10:35 +0000

    Add package censorify/0.0.4/censorify-0.0.4.tar.gz

---
+++ packages/censorify/censorify-0.0.4.tar.gz/censorify-0.0.4/censorify/__init__.py
@@ -0,0 +1,23 @@
+import re
+
+def censor(text, censor_words):
+    censor_words = set([word.lower() for word in censor_words])
+    text_words = re.findall(r'\w+', text.lower())
+    censored_text = text
+
+    for word in text_words:
+        if word in censor_words:
+            censored_word = '*' * len(word)
+            censored_text = re.sub(r'\b' + re.escape(word) + r'\b', censored_word, censored_text, flags=re.IGNORECASE)
+
+    return censored_text
+
+def check(text, censor_words):
+    censor_words = set([word.lower() for word in censor_words])
+    text_words = re.findall(r'\w+', text.lower())
+
+    for word in text_words:
+        if word in censor_words:
+            return True
+
+    return False
