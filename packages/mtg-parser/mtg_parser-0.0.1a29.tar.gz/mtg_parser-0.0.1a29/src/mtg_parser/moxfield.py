a38b87204129e31dabcf1b3d7f131275f1aee3b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:17 +0000

    Add package mtg-parser/0.0.1a29/mtg_parser-0.0.1a29.tar.gz

---
+++ packages/mtg-parser/mtg_parser-0.0.1a29.tar.gz/mtg_parser-0.0.1a29/src/mtg_parser/moxfield.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import requests
+from mtg_parser.card import Card
+
+
+__all__ = []
+
+
+_DOMAIN_PATTERN = r'(?:https?://)?.*?moxfield\.com'
+_PATH_PATTERN = r'/.*/'
+_ID_PATTERN = r'([a-zA-Z0-9-_]+)'
+
+
+def can_handle(src):
+    return (
+        isinstance(src, str)
+        and
+        re.match(_DOMAIN_PATTERN, src)
+    )
+
+
+def parse_deck(src, session=requests):
+    deck = None
+    if can_handle(src):
+        deck = _parse_deck(_download_deck(src, session))
+    return deck
+
+
+def _download_deck(src, session):
+    pattern = _DOMAIN_PATTERN + _PATH_PATTERN + _ID_PATTERN
+    deck_id = re.search(pattern, src).group(1)
+    url = f"https://api.moxfield.com/v2/decks/all/{deck_id}"
+    return session.get(url).json()
+
+
+def _parse_deck(deck):
+    for key, value in deck['commanders'].items():
+        yield Card(key, value['quantity'], tags=['commander'])
+
+    for key, value in deck['companions'].items():
+        yield Card(key, value['quantity'], tags=['companion'])
+
+    for key, value in deck['mainboard'].items():
+        yield Card(key, value['quantity'])
---
+++ packages/mtg-parser/mtg_parser-0.0.1a29.tar.gz/mtg_parser-0.0.1a29/src/mtg_parser/utils.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from urllib.parse import quote_plus
+
+
+__all__ = []
+
+
+def _format_name(card_name):
+    card_name = card_name.split()
+    card_name = map(str.strip, card_name)
+    card_name = filter(len, card_name)
+    card_name = ' '.join(card_name)
+    card_name = quote_plus(card_name)
+    return card_name
+
+
+def _format_extension(extension):
+    return str(extension).lower()
+
+
+def _format_collector_number(collector_number):
+    return str(collector_number).lower()
+
+
+def get_scryfall_url(name=None, extension=None, collector_number=None):
+    scryfall_url = 'https://api.scryfall.com/cards'
+
+    if extension and collector_number:
+        scryfall_url += f"/{_format_extension(extension)}"
+        scryfall_url += f"/{_format_collector_number(collector_number)}"
+    elif name and extension:
+        scryfall_url += '/named'
+        scryfall_url += f"?set={_format_extension(extension)}"
+        scryfall_url += f"&exact={_format_name(name)}"
+    elif name:
+        scryfall_url += '/named'
+        scryfall_url += f"?exact={_format_name(name)}"
+    return scryfall_url
