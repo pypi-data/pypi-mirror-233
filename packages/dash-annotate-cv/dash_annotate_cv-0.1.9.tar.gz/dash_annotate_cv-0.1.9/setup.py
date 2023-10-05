023648beab7343108cccf258e2eeb7f39d7a9cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:08:26 +0000

    Add package dash-annotate-cv/0.1.9/dash_annotate_cv-0.1.9.tar.gz

---
+++ packages/dash-annotate-cv/dash_annotate_cv-0.1.9.tar.gz/dash_annotate_cv-0.1.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dash_annotate_cv",
+    version="0.1.9",
+    author="Oliver K. Ernst",
+    description="A Python library for computer vision annotation tasks using Dash",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smrfeld/dash-annotate-cv",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "dash",
+        "scikit_image",
+        "plotly",
+        "dash_bootstrap_components",
+        "dataclasses",
+        "mashumaro",
+        "pandas",
+        "Pillow",
+        "pyyaml",
+        "scikit-image"
+    ],
+    python_requires=">=3.6",
+    entry_points = {
+        'console_scripts': ['dacv=dash_annotate_cv.command_line:cli'],
+    }
+)
