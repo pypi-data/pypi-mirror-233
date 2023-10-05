8050bcce9180c4f42f92bf5146cf14144783b315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:50 +0000

    Add package generate-tools/0.0.5/generate_tools-0.0.5-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/generate-tools/generate_tools-0.0.5-cp37-cp37m-manylinux2014_x86_64.whl/generate/method/update_memory.py
@@ -0,0 +1,33 @@
+import generate
+from generate.exception import IsAuthorized
+
+class UpdateMemory:
+    def update(
+        self: 'generate.Generate',
+        target: str,
+        data: tuple
+    ):
+        """
+        This function for update memory
+
+        Arguments
+            objec (`str`):
+                The objec, get data for update to memory.
+
+            data (`tuple`):
+                The data update to memory values.
+
+        Example
+            
+            gen.updates(
+                objec='email username domain',
+                data=(username, domain, user_id)
+            )
+        """
+        if not self.is_authorize:
+            raise IsAuthorized()
+        
+        return self.memory.update_memory(
+            objec=target,
+            data=data
+        )
