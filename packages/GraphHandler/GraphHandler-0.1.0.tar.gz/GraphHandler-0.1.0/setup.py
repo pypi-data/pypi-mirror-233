79bf2afd713f07009c2ec187bd9ca33a7d89ed7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:07:10 +0000

    Add package GraphHandler/0.1.0/GraphHandler-0.1.0.tar.gz

---
+++ packages/GraphHandler/GraphHandler-0.1.0.tar.gz/GraphHandler-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import codecs
+import os
+
+
+VERSION = '0.1.0'
+DESCRIPTION = 'Unofficial Facebook Graph API Handler'
+LONG_DESCRIPTION = 'A Facebook Graph API Handler to ease the use of Meta Graph APIs.'
+
+setup(
+    name="GraphHandler",
+    version=VERSION,
+    author="Ambar Rizvi",
+    author_email="<brannstrom9911@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    py_modules=["GraphHandler"],
+    package_dir={'' : 'src'},
+    install_requires=['requests==2.31.0', 'typing==3.7.4.3'],
+    keywords=['python', 'facebook', 'graph api', 'social media', 'instagram', 'meta', 'meta api'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
