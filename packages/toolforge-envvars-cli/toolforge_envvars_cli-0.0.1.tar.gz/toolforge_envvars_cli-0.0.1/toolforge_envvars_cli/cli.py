9ac940d15a1fb024cdd060a21f4461840fc2d18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:39 +0000

    Add package toolforge-envvars-cli/0.0.1/toolforge_envvars_cli-0.0.1.tar.gz

---
+++ packages/toolforge-envvars-cli/toolforge_envvars_cli-0.0.1.tar.gz/toolforge_envvars_cli-0.0.1/toolforge_envvars_cli/cli.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import json as json_mod
+import logging
+import os
+import subprocess
+import sys
+from functools import lru_cache, wraps
+from pathlib import Path
+from typing import Callable
+
+import click
+from tabulate import tabulate
+from toolforge_envvars_cli.envvars import EnvvarsClient, EnvvarsClientError
+from toolforge_envvars_cli.config import get_loaded_config
+
+from toolforge_weld.config import Config
+
+LOGGER = logging.getLogger("toolforge" if __name__ == "__main__" else __name__)
+
+
+def _with_nice_error(func):
+    @wraps(func)
+    def _inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except EnvvarsClientError as error:
+            click.echo(err=True, message=click.style(error.to_str(), fg="red"))
+            sys.exit(1)
+
+    return _inner
+
+
+def _format_headers(headers: list[str]) -> list[str]:
+    return [click.style(item, bold=True) for item in headers]
+
+
+@lru_cache(maxsize=None)
+def _load_config_from_ctx() -> Config:
+    ctx = click.get_current_context()
+    return ctx.obj["config"]
+
+
+def shared_options(func: Callable) -> Callable:
+    @click.option(
+        "--kubeconfig",
+        hidden=True,
+        default=Path(os.environ.get("KUBECONFIG", "~/.kube/config")),
+        type=Path,
+    )
+    @_with_nice_error
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@click.version_option(prog_name="Toolforge envvars CLI")
+@click.group(name="toolforge", help="Toolforge command line")
+@click.option(
+    "-v",
+    "--verbose",
+    help="Show extra verbose output. NOTE: Do no rely on the format of the verbose output",
+    is_flag=True,
+)
+@click.option(
+    "-d",
+    "--debug",
+    help=(
+        "show logs to debug the toolforge-envvars-* packages. For extra verbose output for say build or "
+        "job, see --verbose"
+    ),
+    is_flag=True,
+)
+@click.pass_context
+def toolforge_envvars(ctx: click.Context, verbose: bool, debug: bool) -> None:
+    ctx.ensure_object(dict)
+    ctx.obj["verbose"] = verbose
+    ctx.obj["debug"] = debug
+    ctx.obj["config"] = get_loaded_config()
+    pass
+
+
+@toolforge_envvars.command(name="list", help="List all your envvars.")
+@click.option(
+    "--json",
+    help="If set, will output in json format",
+    is_flag=True,
+)
+@shared_options
+def envvar_list(
+    kubeconfig: Path,
+    json: bool = False,
+) -> None:
+    config = _load_config_from_ctx()
+    envvars_client = EnvvarsClient.from_config(config=config, kubeconfig=kubeconfig)
+    envvars = envvars_client.get("/envvar")
+
+    if json:
+        click.echo(json_mod.dumps(envvars, indent=4))
+    else:
+        click.echo(
+            tabulate(
+                [[envvar["name"], envvar["value"]] for envvar in envvars],
+                headers=_format_headers(["name", "value"]),
+                tablefmt="plain",
+            )
+        )
+
+
+@toolforge_envvars.command(name="show", help="Show a specific envvar.")
+@click.argument("ENVVAR_NAME", required=True)
+@click.option(
+    "--json",
+    help="If set, will output in json format",
+    is_flag=True,
+)
+@shared_options
+def envvar_show(
+    kubeconfig: Path,
+    envvar_name: str,
+    json: bool = False,
+) -> None:
+    config = _load_config_from_ctx()
+    envvars_client = EnvvarsClient.from_config(config=config, kubeconfig=kubeconfig)
+    envvar = envvars_client.get(f"/envvar/{envvar_name}")
+
+    if json:
+        click.echo(json_mod.dumps(envvar, indent=4))
+    else:
+        click.echo(
+            tabulate(
+                [[envvar["name"], envvar["value"]]],
+                headers=_format_headers(["name", "value"]),
+                tablefmt="plain",
+            )
+        )
+
+
+@toolforge_envvars.command(name="create", help="Create/update an envvar.")
+@click.argument("ENVVAR_NAME", required=True)
+@click.argument("ENVVAR_VALUE", required=True)
+@click.option(
+    "--json",
+    help="If set, will output in json format",
+    is_flag=True,
+)
+@shared_options
+def envvar_create(
+    kubeconfig: Path,
+    envvar_name: str,
+    envvar_value: str,
+    json: bool = False,
+) -> None:
+    config = _load_config_from_ctx()
+    envvars_client = EnvvarsClient.from_config(config=config, kubeconfig=kubeconfig)
+    envvar = envvars_client.post(f"/envvar/{envvar_name}", json={"value": envvar_value})
+
+    if json:
+        click.echo(json_mod.dumps(envvar, indent=4))
+    else:
+        click.echo(
+            tabulate(
+                [[envvar["name"], envvar["value"]]],
+                headers=_format_headers(["name", "value"]),
+                tablefmt="plain",
+            )
+        )
+
+
+@toolforge_envvars.command(name="delete", help="Delete an envvar.")
+@click.argument("ENVVAR_NAME", required=True)
+@click.option(
+    "--json",
+    help="If set, will output in json format",
+    is_flag=True,
+)
+@click.option(
+    "--yes-im-sure",
+    help="If set, will not ask for confirmation",
+    is_flag=True,
+)
+@shared_options
+def envvar_delete(
+    kubeconfig: Path,
+    envvar_name: str,
+    json: bool = False,
+    yes_im_sure: bool = False,
+) -> None:
+    config = _load_config_from_ctx()
+
+    if not yes_im_sure:
+        if not click.prompt(
+            text=f"Are you sure you want to delete {envvar_name}? (this can't be undone) [yN]",
+            default="no",
+            show_default=False,
+            type=lambda val: val.lower() in ["y", "Y", "1", "yes", "true"],
+        ):
+            click.echo("Aborting at user's request")
+            sys.exit(1)
+
+    envvars_client = EnvvarsClient.from_config(config=config, kubeconfig=kubeconfig)
+    envvar = envvars_client.delete(f"/envvar/{envvar_name}")
+
+    if json:
+        click.echo(json_mod.dumps(envvar, indent=4))
+    else:
+        click.echo(f"Deleted {envvar_name}, here it's it's last value:")
+        click.echo(
+            tabulate(
+                [[envvar["name"], envvar["value"]]],
+                headers=_format_headers(["name", "value"]),
+                tablefmt="plain",
+            )
+        )
+
+
+def main() -> int:
+    # this is needed to setup the logging before the subcommand discovery
+    res = toolforge_envvars.parse_args(ctx=click.Context(command=toolforge_envvars), args=sys.argv)
+    if "-d" in res or "--debug" in res:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    try:
+        toolforge_envvars()
+    except subprocess.CalledProcessError as err:
+        return err.returncode
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/toolforge-envvars-cli/toolforge_envvars_cli-0.0.1.tar.gz/toolforge_envvars_cli-0.0.1/toolforge_envvars_cli/config.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from toolforge_weld.config import Section, load_config, Config
+from functools import lru_cache
+from typing import Any
+
+
+@dataclass
+class EnvvarsConfig(Section):
+    _NAME_: str = field(default="envvars", init=False)
+    endpoint: str = "/envvars/v1"
+
+    @classmethod
+    def from_dict(cls, my_dict: dict[str, Any]):
+        params = {}
+        if "endpoint" in my_dict:
+            params["endpoint"] = my_dict["endpoint"]
+        return cls(**params)
+
+
+@lru_cache(maxsize=None)
+def get_loaded_config() -> Config:
+    return load_config(client_name="envvars", extra_sections=[EnvvarsConfig])
