5ac33d25429dc5ea7c91c88b2e4e55033f565405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:34 +0000

    Add package komodo-cli/0.0.4/komodo_cli-0.0.4.tar.gz

---
+++ packages/komodo-cli/komodo_cli-0.0.4.tar.gz/komodo_cli-0.0.4/komodo_cli/commands/cmd_register.py
@@ -0,0 +1,32 @@
+import getpass
+
+import click
+
+import komodo_cli.printing as printing
+from komodo_cli.utils import APIClient
+
+MIN_PASSWORD_LENGTH = 8
+
+
+@click.command("register")
+def cmd_register():
+    email = input("Email: ")
+
+    while True:
+        pw1 = getpass.getpass("Password: ")
+        if len(pw1) < MIN_PASSWORD_LENGTH:
+            printing.error(
+                "Password must be at least 8 characters, please enter a new password."
+            )
+            continue
+        pw2 = getpass.getpass("Re-enter password: ")
+
+        if pw1 != pw2:
+            printing.error("Passwords do not match, please try again.")
+            continue
+
+        break
+
+    APIClient.register(email, pw1)
+
+    printing.success(f"Successfully registered user {email}")
