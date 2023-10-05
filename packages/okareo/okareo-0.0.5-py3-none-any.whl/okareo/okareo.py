6d3a5a9a8efd0dd1d9ddad528d642c21d3a8b445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:10:37 +0000

    Add package okareo/0.0.5/okareo-0.0.5-py3-none-any.whl

---
+++ packages/okareo/okareo-0.0.5-py3-none-any.whl/okareo/okareo.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from typing import List, TypedDict, Union
+
+from okareo_api_client import Client
+from okareo_api_client.api.default import get_generations_v0_generations_get
+from okareo_api_client.models.generation_list import GenerationList
+from okareo_api_client.models.http_validation_error import HTTPValidationError
+
+from .common import BASE_URL
+
+
+class Generation(TypedDict):
+    id: str
+    data: datetime
+
+
+class Okareo:
+    """A class for interacting with Okareo API"""
+
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        self.client = Client(base_url=BASE_URL)
+
+    def get_generations(self) -> Union[List[GenerationList], None]:
+        """Get a list of generations"""
+        data = get_generations_v0_generations_get.sync(
+            client=self.client, api_key=self.api_key
+        )
+        if isinstance(data, HTTPValidationError):
+            print(f"Unexpected {data=}, {type(data)=}")
+            raise
+        return data
