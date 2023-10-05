0504e26d3b9ac9995148ed4fb928517cb08d9f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:06:07 +0000

    Add package sint/0.0.6/sint-0.0.6.tar.gz

---
+++ packages/sint/sint-0.0.6.tar.gz/sint-0.0.6/sint/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.6"
+from .types import NumStr, SignInt, SignChr, SignSym
+
+from .sign import sign, P, N, Z
+from .sint import sint, SignIntError
+from .utils import (matchsign, topow, tosign, totuple)
+
+
+__all__ = ['NumStr', 'SignInt', 'SignChr', 'SignSym', 'sign', 'P', 'N', 'Z', 'sint', 'SignIntError', 'matchsign', 'topow', 'tosign', 'totuple']
