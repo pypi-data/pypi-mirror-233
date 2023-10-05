dae44a46c2eca56bf2fae1cc8111568293583396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:40 +0000

    Add package edgepi-rpc-client/0.0.12/edgepi_rpc_client-0.0.12-py3-none-any.whl

---
+++ packages/edgepi-rpc-client/edgepi_rpc_client-0.0.12-py3-none-any.whl/tests/integration/conftest.py
@@ -0,0 +1,11 @@
+# """Integration test setup for RPC Server"""
+# import subprocess
+# import pytest
+
+# @pytest.fixture(scope="session", autouse=True)
+# def setup_server():
+#     """Server Setup"""
+#     subprocess.run(["bash", "./start_server.sh"], check=True)
+
+
+# TODO: Enable this once the server issue is fixed for integration tests
