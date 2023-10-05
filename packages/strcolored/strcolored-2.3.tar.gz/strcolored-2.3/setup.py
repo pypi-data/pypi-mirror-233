d1ef1a7a622dba311e025b2313342199fdcb6fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 15:07:16 +0000

    Add package strcolored/2.3/strcolored-2.3.tar.gz

---
+++ packages/strcolored/strcolored-2.3.tar.gz/strcolored-2.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="strcolored",
+    version='2.3',
+    license='Eclipse Public License 2.0',
+    authors=["johnydev"],
+    author_email="media.hr@execs.com",
+    description="python library for colored strings",
+    long_description='Documentation: https://github.com/johnysec/strcolored',
+    packages=['strcolored']
+)
+
---
+++ packages/strcolored/strcolored-2.3.tar.gz/strcolored-2.3/strcolored/test.py
@@ -0,0 +1,5 @@
+#from strcolored import *
+
+#u can use it like this
+#colors are red,white,green,yellow etc
+
