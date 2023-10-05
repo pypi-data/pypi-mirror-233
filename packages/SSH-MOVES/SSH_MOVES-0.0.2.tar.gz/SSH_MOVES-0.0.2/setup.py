6d6700deefaa1ced50a6bc5f16c7433c8bb730e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:35 +0000

    Add package SSH-MOVES/0.0.2/SSH_MOVES-0.0.2.tar.gz

---
+++ packages/SSH-MOVES/SSH_MOVES-0.0.2.tar.gz/SSH_MOVES-0.0.2/setup.py
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+#
+#	https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+#
+#	https://github.com/pypa/sampleproject/blob/db5806e0a3204034c51b1c00dde7d5eb3fa2532e/setup.py
+#
+from setuptools import setup, find_packages
+
+
+def SCAN_VARIABLES ():
+	import json
+
+	VARIABLES = {}
+	try:
+		with open ('VARIABLES.JSON') as f:
+			STRING = f.read ().strip()
+			print (STRING)
+		
+			VARIABLES = json.loads (STRING)
+	except Exception as E:
+		print (E)
+	
+		pass;
+		
+	return VARIABLES;
+
+
+def SCAN_DESCRIPTION ():
+	DESCRIPTION = ''
+	try:
+		with open ('STRUCTURE/SSH.HTML') as f:
+			DESCRIPTION = f.read ()
+		print (DESCRIPTION)
+	except Exception as E:
+		pass;
+		
+	return DESCRIPTION;
+
+VARIABLES = SCAN_VARIABLES ()
+
+
+setup (
+    name = VARIABLES ['NAME'],
+    version = VARIABLES ['VERSION'],
+    install_requires = VARIABLES ['INSTALL_REQUIRES'],	
+	
+	package_dir = { 
+		VARIABLES ['NAME']: "STRUCTURE"
+	},
+	package_data = {
+		VARIABLES ['NAME']: [ 'STRUCTURE/SSH.HTML' ]
+    },
+
+	license = "",
+	long_description = SCAN_DESCRIPTION (),
+	long_description_content_type = "text/plain"
+)
+
