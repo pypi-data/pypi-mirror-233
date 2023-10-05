205602e1e039bd4cf97c93e1fcd7d1954b330982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:06:25 +0000

    Add package laff/0.8.1/laff-0.8.1.tar.gz

---
+++ packages/laff/laff-0.8.1.tar.gz/laff-0.8.1/laff/modelling/__init__.py
@@ -0,0 +1,13 @@
+from .fit_continuum import (
+ broken_powerlaw,
+ find_intial_fit,
+ fit_continuum_mcmc,
+ PAR_NAMES_CONTINUUM,
+ STAT_NAMES_CONTINUUM
+)
+
+from .fit_flare import (
+    fred_flare,
+    flare_fitter,
+    PAR_NAMES_FLARE
+)
---
+++ packages/laff/laff-0.8.1.tar.gz/laff-0.8.1/laff/utility/__init__.py
@@ -0,0 +1,2 @@
+from .utility import calculate_fit_statistics, check_data_input
+from .utility import PAR_NAMES_CONTINUUM, PAR_NAMES_FLARE, STAT_NAMES_CONTINUUM
