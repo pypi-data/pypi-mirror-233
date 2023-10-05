d994efaf5370a373244ca384a639eb15f0e4f290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:10:47 +0000

    Add package snowmate-collector/0.0.61/snowmate_collector-0.0.61-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/snowmate-collector/snowmate_collector-0.0.61-cp310-cp310-macosx_10_9_universal2.whl/snowmate_collector/data_handling/msg_parsers/utils.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+# from rusty_wizard import replace_secrets_in_json
+
+
+def serialize_data(data: Any, _should_obfuscate_data=True) -> str:
+    # TODO: implement obfuscation
+    return data
