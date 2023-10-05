bef976e5d320d77edbefae971bdb7449f73808c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:01 +0000

    Add package sounderpy/2.0.6/sounderpy-2.0.6.tar.gz

---
+++ packages/sounderpy/sounderpy-2.0.6.tar.gz/sounderpy-2.0.6/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="sounderpy",
+	version="2.0.6",
+	author="Kyle J Gillett",
+	description="Vertical Profile Data Retrieval and Analysis Tool For Python",
+	include_package_data=True,
+)
