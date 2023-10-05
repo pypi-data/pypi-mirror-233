33742928cf9f6ad47016ce8a0040c2ac1d5bd655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:20 +0000

    Add package gbpcli/1.6.0/gbpcli-1.6.0-py3-none-any.whl

---
+++ packages/gbpcli/gbpcli-1.6.0-py3-none-any.whl/gbpcli/subcommands/pull.py
@@ -0,0 +1,19 @@
+"""Pull a build"""
+import argparse
+
+from gbpcli import GBP, Build, Console
+
+
+def handler(args: argparse.Namespace, gbp: GBP, _console: Console) -> int:
+    """Pull a build"""
+    build = Build(machine=args.machine, number=args.number)
+
+    gbp.pull(build)
+
+    return 0
+
+
+def parse_args(parser: argparse.ArgumentParser) -> None:
+    """Set subcommand arguments"""
+    parser.add_argument("machine", metavar="MACHINE", help="name of the machine")
+    parser.add_argument("number", type=int, metavar="NUMBER", help="build number")
