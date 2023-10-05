fe956faff9f6eef6945c99420d79575d56f0a019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:08:10 +0000

    Add package ape-polygon/0.6.6/ape-polygon-0.6.6.tar.gz

---
+++ packages/ape-polygon/ape-polygon-0.6.6.tar.gz/ape-polygon-0.6.6/ape_polygon/ecosystem.py
@@ -0,0 +1,48 @@
+from typing import cast
+
+from ape.api.config import PluginConfig
+from ape.api.networks import LOCAL_NETWORK_NAME
+from ape.utils import DEFAULT_LOCAL_TRANSACTION_ACCEPTANCE_TIMEOUT
+from ape_ethereum.ecosystem import Ethereum, NetworkConfig
+
+NETWORKS = {
+    # chain_id, network_id
+    "mainnet": (137, 137),
+    "mumbai": (80001, 80001),
+}
+
+
+def _create_network_config(
+    required_confirmations: int = 1, block_time: int = 2, **kwargs
+) -> NetworkConfig:
+    return NetworkConfig(
+        required_confirmations=required_confirmations, block_time=block_time, **kwargs
+    )
+
+
+def _create_local_config(**kwargs) -> NetworkConfig:
+    return _create_network_config(
+        block_time=0,
+        default_provider=kwargs.pop("default_provider", None),
+        gas_limit="max",
+        required_confirmations=0,
+        transaction_acceptance_timeout=DEFAULT_LOCAL_TRANSACTION_ACCEPTANCE_TIMEOUT,
+        **kwargs,
+    )
+
+
+class PolygonConfig(PluginConfig):
+    mainnet: NetworkConfig = _create_network_config()
+    mainnet_fork: NetworkConfig = _create_local_config()
+    mumbai: NetworkConfig = _create_network_config()
+    mumbai_fork: NetworkConfig = _create_local_config()
+    local: NetworkConfig = _create_local_config(default_provider="test")
+    default_network: str = LOCAL_NETWORK_NAME
+
+
+class Polygon(Ethereum):
+    fee_token_symbol: str = "MATIC"
+
+    @property
+    def config(self) -> PolygonConfig:  # type: ignore[override]
+        return cast(PolygonConfig, self.config_manager.get_config("polygon"))
