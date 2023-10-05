57278cdbcdbba5bd5dd6ca821a683f5d14b0faa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:01 +0000

    Add package jbturtle/0.2.6/jbturtle-0.2.6.tar.gz

---
+++ packages/jbturtle/jbturtle-0.2.6.tar.gz/jbturtle-0.2.6/jbturtle/__init__.py
@@ -0,0 +1,2 @@
+
+from jbturtle.jbturtle import *
---
+++ packages/jbturtle/jbturtle-0.2.6.tar.gz/jbturtle-0.2.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+long_license = (this_directory / 'LICENSE').read_text()
+
+setup(
+    name = 'jbturtle',
+    version = '0.2.6',
+    install_requires = [ 'Pillow' ],
+    description = 'Simple turtle system for JupyterLab',
+    author='Fumi.Iseki',
+    author_email='fumi.hax@gmail.com',
+    packages = find_packages(),
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    license = long_license,
+    url = 'https://github.com/fiseki/jupyterlab-jbturtle',
+    download_url = 'https://github.com/fiseki/jupyterlab-jbturtle',
+)
+
