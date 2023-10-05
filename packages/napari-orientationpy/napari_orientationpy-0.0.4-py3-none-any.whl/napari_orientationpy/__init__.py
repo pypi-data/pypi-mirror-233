5bdc24337cd0b28fb4b48bd057c9ed03530c9de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 05:05:58 +0000

    Add package napari-orientationpy/0.0.4/napari_orientationpy-0.0.4-py3-none-any.whl

---
+++ packages/napari-orientationpy/napari_orientationpy-0.0.4-py3-none-any.whl/napari_orientationpy/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.4"
+from ._widget import OrientationWidget
+from ._widget_plotting import OrientationPlottingWidget
+# from ._widget_boxes import OrientationBoxesWidget
+
+__all__ = (
+    "OrientationWidget", 
+    "OrientationPlottingWidget",
+    # "OrientationBoxesWidget"
+)
