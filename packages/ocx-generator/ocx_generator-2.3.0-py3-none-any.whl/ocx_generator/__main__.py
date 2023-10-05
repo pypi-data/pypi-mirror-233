d53b996697a7949dc16e247b9c9150f66808dcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:48 +0000

    Add package ocx-generator/2.3.0/ocx_generator-2.3.0-py3-none-any.whl

---
+++ packages/ocx-generator/ocx_generator-2.3.0-py3-none-any.whl/ocx_generator/__main__.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2023. OCX Consortium https://3docx.org. See the LICENSE
+"""Module script entry point."""
+# System imports
+
+
+# Third party imports
+
+
+# Module imports
+from ocx_generator.cli import databinding
+
+
+def main():
+    databinding()
+
+
+if __name__ == "__main__":
+    main()
