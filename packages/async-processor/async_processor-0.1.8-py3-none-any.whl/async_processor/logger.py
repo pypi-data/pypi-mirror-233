2a4a3f3728002d62e1ffddb77387cd56ea5a6899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:30 +0000

    Add package async-processor/0.1.8/async_processor-0.1.8-py3-none-any.whl

---
+++ packages/async-processor/async_processor-0.1.8-py3-none-any.whl/async_processor/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger("async_processor")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
