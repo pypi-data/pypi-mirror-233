7d6fff5449c107b80d6fa0f7ca2fc181b4bcb988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:33 +0000

    Add package orger/0.3.20231005/orger-0.3.20231005.tar.gz

---
+++ packages/orger/orger-0.3.20231005.tar.gz/orger-0.3.20231005/src/orger/common.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from typing import Optional
+from pathlib import Path
+
+from .inorganic import OrgNode, timestamp, timestamp_with_style, TimestampStyle
+
+
+# todo add error policy here?
+class settings:
+    DEFAULT_TIMESTAMP_STYLE = TimestampStyle.INACTIVE
+    USE_PANDOC: bool = True
+
+
+_timezones = set()  # type: ignore
+def dt_heading(dt: Optional[datetime], heading: str) -> str:
+    """
+    Helper to inline datetime in heading
+    """
+    # TODO move to inorganic? not sure
+    if dt is None:
+        return heading
+
+    tz = dt.tzinfo
+    # todo come up with a better way of reporting this..
+    if tz not in _timezones and len(_timezones) > 0:
+        import warnings
+        warnings.warn(f"Seems that a mixture of timezones is used. Org-mode doesn't support timezones, so this might end up confusing: {_timezones} {tz} {heading}")
+    _timezones.add(tz)
+
+    return timestamp_with_style(dt=dt, style=settings.DEFAULT_TIMESTAMP_STYLE) + ' ' + heading
+
+
+def error(e: Exception) -> OrgNode:
+    import traceback
+    return OrgNode(
+        heading="ERROR!",
+        body='\n'.join(traceback.format_exception(Exception, e, None)),
+    )
+
+
+# TODO not sure if belongs here
+def todo(dt: datetime, **kwargs):
+    """
+    Helper to simplify creating todos
+    """
+    props = kwargs.get('properties', {})
+    props.update({'CREATED': timestamp(dt, inactive=True)})
+    kwargs['properties'] = props
+    return OrgNode(
+        todo='TODO',
+        scheduled=dt.date(),
+        **kwargs,
+    )
+
+
+# todo use klogging2?
+from .klogging import LazyLogger, setup_logger
+
+
+def orger_user_dir() -> Path:
+    import appdirs
+    return Path(appdirs.user_config_dir('orger'))
