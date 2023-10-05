a152e3fe2ead801ee5403abf5963d76b4195e509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:13 +0000

    Add package merchant001_sdk/0.0.19/merchant001-sdk-0.0.19.tar.gz

---
+++ packages/merchant001_sdk/merchant001-sdk-0.0.19.tar.gz/merchant001-sdk-0.0.19/merchant001_sdk/core/data/schemas/responses/raw.py
@@ -0,0 +1,36 @@
+import http
+import json
+import typing as t
+from dataclasses import dataclass, field
+
+from merchant001_sdk.core.data.schemas.base import BaseSchema
+
+
+@dataclass(frozen=True, kw_only=True)
+class RawResult(BaseSchema):
+    """Raw json result."""
+
+    status_code: http.HTTPStatus = field()
+    body: t.Any | None = field(default=None)
+    content_type: str | None = field(default=None)
+
+    def get_json(self) -> dict[str, t.Any]:
+        """get_json.
+
+        Args:
+
+        Returns:
+            dict[str, t.Any]:
+        """
+        return json.loads(self.body)
+
+    @property
+    def data(self) -> dict[str, t.Any]:
+        """data.
+
+        Args:
+
+        Returns:
+            dict[str, t.Any]:
+        """
+        return {"status_code": self.status_code, "body": self.body, "content_type": self.content_type}
