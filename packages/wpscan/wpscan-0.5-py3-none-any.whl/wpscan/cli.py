bf156d9f3dd03bf83a80207b2514b9f6bc04ed44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:16 +0000

    Add package wpscan/0.5/wpscan-0.5-py3-none-any.whl

---
+++ packages/wpscan/wpscan-0.5-py3-none-any.whl/wpscan/cli.py
@@ -0,0 +1,4 @@
+def main():
+    print("VimLxrd and Mr. Robot were here. WPscan can be installed with 'gem install wpscan'."
+          " This is a warning about installing pypi packages."
+          " Join our discord: https://discord.gg/owlsec")
