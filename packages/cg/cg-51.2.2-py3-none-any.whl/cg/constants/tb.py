e60796a93e4ad201cefbddb0ac572152f35ed61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:08:13 +0000

    Add package cg/51.2.2/cg-51.2.2-py3-none-any.whl

---
+++ packages/cg/cg-51.2.2-py3-none-any.whl/cg/constants/tb.py
@@ -0,0 +1,9 @@
+class AnalysisStatus:
+    CANCELLED: str = "cancelled"
+    COMPLETED: str = "completed"
+    ERROR: str = "error"
+    FAILED: str = "failed"
+    PENDING: str = "pending"
+    RUNNING: str = "running"
+    TIMEOUT: str = "timeout"
+    QC: str = "qc"
