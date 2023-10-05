885be2de55ad0c55310155b881c7ab74969666ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 15:07:44 +0000

    Add package pywsdp/2.2.0/pywsdp-2.2.0-py3-none-any.whl

---
+++ packages/pywsdp/pywsdp-2.2.0-py3-none-any.whl/pywsdp/__init__.py
@@ -0,0 +1,7 @@
+from pywsdp.modules import GenerujCenoveUdajeDleKu
+from pywsdp.modules.SpravujSestavy import SeznamSestav, VratSestavu, SmazSestavu
+from pywsdp.modules.CtiOS import OutputFormat
+from pywsdp.base import __version__
+
+if __name__ == "__main__":
+    pass
