25ece97736aa178ea8e7e5593e47306434f9644b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:06:14 +0000

    Add package wpscan/0.4/wpscan-0.4.tar.gz

---
+++ packages/wpscan/wpscan-0.4.tar.gz/wpscan-0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Read the contents of README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='wpscan',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify the content type as markdown
+    entry_points={
+        'console_scripts': [
+            'wpscan = wpscan.cli:main',
+        ],
+    },
+)
---
+++ packages/wpscan/wpscan-0.4.tar.gz/wpscan-0.4/wpscan/cli.py
@@ -0,0 +1,4 @@
+def main():
+    print("Mr. Robot was here. WPscan can be installed with 'gem install wpscan'."
+          " This is a warning about installing pypi packages."
+          " join our discord: https://discord.gg/owlsec")
