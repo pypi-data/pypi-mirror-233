1f2b103f1e9f4112bb14e0ab2e277175f233801d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 03:07:17 +0000

    Add package handpick/0.16.0/handpick-0.16.0.tar.gz

---
+++ packages/handpick/handpick-0.16.0.tar.gz/handpick-0.16.0/handpick/__init__.py
@@ -0,0 +1,25 @@
+"""
+Handpick is a tool to traverse nested data structures and pick all
+objects that meet certain criteria.
+"""
+
+from .core import (
+    pick,
+    Predicate,
+    is_type,
+    no_error,
+    values_for_key,
+    max_depth,
+)
+
+__version__ = "0.16.0"
+
+__all__ = (
+    "__version__",
+    "pick",
+    "Predicate",
+    "is_type",
+    "no_error",
+    "values_for_key",
+    "max_depth",
+)
