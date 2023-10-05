9fb7e068b2b533d509a2a841ca60fc63dc4db247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 15:07:14 +0000

    Add package labrinth/0.1.0/labrinth-0.1.0.tar.gz

---
+++ packages/labrinth/labrinth-0.1.0.tar.gz/labrinth-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='labrinth',
+    version='0.1.0',
+    author='YourLocalMoon',
+    author_email='kovaslavs@gmail.com',
+    description='A Python API wrapper for Modrinth (labrinth because why not)',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+    ],
+)
