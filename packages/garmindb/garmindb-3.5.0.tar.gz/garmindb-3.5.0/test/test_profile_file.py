9d199c297523700574f29b9b159a5d3b1cf58933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 14:07:31 +0000

    Add package garmindb/3.5.0/garmindb-3.5.0.tar.gz

---
+++ packages/garmindb/garmindb-3.5.0.tar.gz/garmindb-3.5.0/test/test_profile_file.py
@@ -0,0 +1,43 @@
+"""Test profile file parsing."""
+
+__author__ = "Tom Goetz"
+__copyright__ = "Copyright Tom Goetz"
+__license__ = "GPL"
+
+import unittest
+import logging
+
+import fitfile
+
+from garmindb import ConfigManager, GarminProfile
+from garmindb.garmindb import GarminDb, Attributes
+
+
+root_logger = logging.getLogger()
+handler = logging.FileHandler('profile_file.log', 'w')
+root_logger.addHandler(handler)
+root_logger.setLevel(logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+class TestProfileFile(unittest.TestCase):
+    """Class for testing profile JSON file parsing."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.file_path = 'test_files'
+
+    def test_parse_uprofile(self):
+        db_params = ConfigManager.get_db_params(test_db=True)
+        gp = GarminProfile(db_params, self.file_path, debug=2)
+        if gp.file_count() > 0:
+            gp.process()
+        gdb = GarminDb(db_params)
+        measurement_system = Attributes.measurements_type(gdb)
+        self.assertEqual(measurement_system, fitfile.field_enums.DisplayMeasure.statute,
+                         'DisplayMeasure expected %r found %r from %r' % (fitfile.field_enums.DisplayMeasure.statute, measurement_system, gp.file_names))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
