e1029c7a00524bf9d5bfab4929ae663838df1410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-05 04:06:33 +0000

    Add package gcp-scraper/3.0.0/gcp_scraper-3.0.0-py3-none-any.whl

---
+++ packages/gcp-scraper/gcp_scraper-3.0.0-py3-none-any.whl/gscraper/__init__.py
@@ -0,0 +1,14 @@
+from .base.context import *
+from .base.parser import *
+from .base.session import *
+from .base.spider import *
+from .base.types import *
+
+from .utils.cast import *
+from .utils.date import *
+from .utils.gcloud import *
+from .utils.logs import *
+from .utils.map import *
+from .utils.parse import *
+
+__version__ = "3.0.0"
