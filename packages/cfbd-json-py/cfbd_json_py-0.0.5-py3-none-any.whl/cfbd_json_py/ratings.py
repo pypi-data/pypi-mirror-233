3727adc84985d70e72ede0df11b9517071527dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:28:23 +0000

    Add package cfbd-json-py/0.0.5/cfbd_json_py-0.0.5-py3-none-any.whl

---
+++ packages/cfbd-json-py/cfbd_json_py-0.0.5-py3-none-any.whl/cfbd_json_py/ratings.py
@@ -0,0 +1,65 @@
+
+def get_cfbd_sp_plus_ratings(
+        api_key: str = None,
+        api_key_dir: str = None,
+        year: int = None,
+        team: int = None,
+        # Either `year` or `team` have to be not null for this function to work.
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_srs_ratings(
+        api_key: str = None,
+        api_key_dir: str = None,
+        year: int = None,
+        team: int = None,
+        # Either `year` or `team` have to be not null for this function to work.
+        conferenece: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_sp_plus_conference_ratings(
+        year: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+        conference: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
+
+
+def get_cfbd_elo_ratings(
+        year: int,
+        api_key: str = None,
+        api_key_dir: str = None,
+        week: int = None,
+        season_type: str = 'regular',  # "regular" or "postseason"
+        team: str = None,
+        conference: str = None,
+
+        return_as_dict: bool = False):
+    """
+
+    """
+    raise NotImplementedError(
+        'This function has yet to be implemented by this version.'
+    )
