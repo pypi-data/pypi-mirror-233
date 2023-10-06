7d0d46288d82cd8cb1a68701b37f554894bfaaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:08:15 +0000

    Add package edc-visit-schedule/0.3.73/edc-visit-schedule-0.3.73.tar.gz

---
+++ packages/edc-visit-schedule/edc-visit-schedule-0.3.73.tar.gz/edc-visit-schedule-0.3.73/edc_visit_schedule/exceptions.py
@@ -0,0 +1,38 @@
+class SubjectScheduleError(Exception):
+    pass
+
+
+class NotOnScheduleError(Exception):
+    pass
+
+
+class OnScheduleError(Exception):
+    pass
+
+
+class OffScheduleError(Exception):
+    pass
+
+
+class NotOffScheduleError(Exception):
+    pass
+
+
+class NotOnScheduleForDateError(Exception):
+    pass
+
+
+class OnScheduleForDateError(Exception):
+    pass
+
+
+class OnScheduleFirstAppointmentDateError(Exception):
+    pass
+
+
+class UnknownSubjectError(Exception):
+    pass
+
+
+class InvalidOffscheduleDate(Exception):
+    pass
