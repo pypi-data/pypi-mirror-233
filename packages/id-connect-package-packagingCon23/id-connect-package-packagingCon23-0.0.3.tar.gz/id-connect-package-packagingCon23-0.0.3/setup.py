c08bbed883b4a8acb8afe0f8bd6ae12353c42c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:16:57 +0000

    Add package id-connect-package-packagingCon23/0.0.3/id-connect-package-packagingCon23-0.0.3.tar.gz

---
+++ packages/id-connect-package-packagingCon23/id-connect-package-packagingCon23-0.0.3.tar.gz/id-connect-package-packagingCon23-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='id-connect-package-packagingCon23',
+    version="1.0.0",
+    description='id-connect-package-packagingCon23',
+    long_description="id-connect-package-packagingCon23",
+    long_description_content_type='text/markdown',
+)
