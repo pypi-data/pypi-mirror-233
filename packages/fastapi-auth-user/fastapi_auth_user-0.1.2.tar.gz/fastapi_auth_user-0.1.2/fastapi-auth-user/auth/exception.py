9ebe827802a49e8cbfaddc20f683a174169e3d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:22:10 +0000

    Add package fastapi-auth-user/0.1.2/fastapi_auth_user-0.1.2.tar.gz

---
+++ packages/fastapi-auth-user/fastapi_auth_user-0.1.2.tar.gz/fastapi_auth_user-0.1.2/fastapi-auth-user/auth/exception.py
@@ -0,0 +1,8 @@
+from models import RoleNameEnum
+
+
+class PermissionException(Exception):
+	def __init__(self, message: str, role: RoleNameEnum):
+		self.message = message
+		self.role = role
+		super().__init__(self.message)
