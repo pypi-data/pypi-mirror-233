ac3062ecfc94b171cd2c50f922c86a0ce980091f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:16:33 +0000

    Add package bytewax/0.17.2/bytewax-0.17.2-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/bytewax/bytewax-0.17.2-cp39-cp39-macosx_10_9_x86_64.whl/bytewax/connectors/stdio.py
@@ -0,0 +1,30 @@
+"""Connectors to console IO."""
+from bytewax.outputs import DynamicOutput, StatelessSink
+
+__all__ = [
+    "StdOutput",
+]
+
+
+class _PrintSink(StatelessSink):
+    def write_batch(self, items):
+        for item in items:
+            print(item)
+
+
+class StdOutput(DynamicOutput):
+    """Write each output item to stdout on that worker.
+
+    Items consumed from the dataflow must look like a string. Use a
+    proceeding map step to do custom formatting.
+
+    Workers are the unit of parallelism.
+
+    Can support at-least-once processing. Messages from the resume
+    epoch will be duplicated right after resume.
+
+    """
+
+    def build(self, worker_index, worker_count):
+        """See ABC docstring."""
+        return _PrintSink()
