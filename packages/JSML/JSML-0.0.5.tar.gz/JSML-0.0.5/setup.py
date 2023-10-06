9fd390afd3821ccb0fe31e9997580a0566f09cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:21:08 +0000

    Add package JSML/0.0.5/JSML-0.0.5.tar.gz

---
+++ packages/JSML/JSML-0.0.5.tar.gz/JSML-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Barebones file structure for a Python package'
+
+# Setting up
+setup(
+    name="JSML",
+    version=VERSION,
+    author="JakeSilberstein",
+    author_email="<jake.silberstein8@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'Neural Networks', 'AI', 'CNN',
+              'RNN', 'DQN', 'LSTM', 'GRU', 'Transformers' 'Beyesian Optimization'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
