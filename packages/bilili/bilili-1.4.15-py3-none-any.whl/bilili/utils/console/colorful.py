ffd82317222cd9eafa1c246112659f402f25e54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:08:12 +0000

    Add package bilili/1.4.15/bilili-1.4.15-py3-none-any.whl

---
+++ packages/bilili/bilili-1.4.15-py3-none-any.whl/bilili/utils/console/colorful.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from typing import Dict, Optional
+
+# 使部分终端正确显示颜色
+if sys.platform == "win32":
+    os.system("")
+
+Fore = str
+Back = str
+Style = str
+CodeMap = Dict[str, Dict[str, int]]
+
+_no_color = False
+
+
+code_map: CodeMap = {
+    "fore": {
+        "black": 30,
+        "red": 31,
+        "green": 32,
+        "yellow": 33,
+        "blue": 34,
+        "magenta": 35,
+        "cyan": 36,
+        "white": 37,
+    },
+    "back": {
+        "black": 40,
+        "red": 41,
+        "green": 42,
+        "yellow": 43,
+        "blue": 44,
+        "magenta": 45,
+        "cyan": 46,
+        "white": 47,
+    },
+    "style": {
+        "reset": 0,
+        "bold": 1,
+        "italic": 3,
+        "underline": 4,
+        "defaultfg": 39,
+        "defaultbg": 49,
+    },
+}
+
+
+def colored_string(
+    string: str, fore: Optional[Fore] = None, back: Optional[Back] = None, style: Optional[Style] = None
+) -> str:
+    if _no_color:
+        return string
+    template = "\033[{code}m"
+    result = ""
+    if fore is not None:
+        result += template.format(code=code_map["fore"][fore])
+    if back is not None:
+        result += template.format(code=code_map["back"][back])
+    if style is not None:
+        result += template.format(code=code_map["style"][style])
+    result += string
+    result += template.format(code=code_map["style"]["reset"])
+    return result
+
+
+def set_no_color():
+    global _no_color
+    _no_color = True
