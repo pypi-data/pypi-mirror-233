02c403ccb41e012968faf0a2fbbb3e9188e3f9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:12:16 +0000

    Add package NeuroGraph/2.0.2/NeuroGraph-2.0.2.tar.gz

---
+++ packages/NeuroGraph/NeuroGraph-2.0.2.tar.gz/NeuroGraph-2.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='NeuroGraph',
+    version='2.0.2',
+    description='A Python package for graph-based neuroimaging benchmarks and tools',
+    author='Anwar Said',
+    author_email='<anwar.said@vanderbilt.edu>',
+    packages=['NeuroGraph'],
+    install_requires=[
+        # List any dependencies your package requires
+        'boto3==1.26.158',
+        'nilearn==0.10.1',
+        'networkx==2.6',
+        'pandas'
+        
+    ],
+    keywords = ['python','neuroimaging','graph machine learning'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+    ],
+)
