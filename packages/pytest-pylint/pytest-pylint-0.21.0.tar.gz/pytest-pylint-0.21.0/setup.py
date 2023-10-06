a489d29402b5e099094d2855ff0675a6994b8e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 15:13:08 +0000

    Add package pytest-pylint/0.21.0/pytest-pylint-0.21.0.tar.gz

---
+++ packages/pytest-pylint/pytest-pylint-0.21.0.tar.gz/pytest-pylint-0.21.0/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+pytest-pylint
+=============
+
+Plugin for py.test for doing pylint tests
+"""
+# pylint: disable=import-error
+from setuptools import setup
+
+with open("README.rst", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="pytest-pylint",
+    description="pytest plugin to check source code with pylint",
+    long_description=LONG_DESCRIPTION,
+    license="MIT",
+    version="0.21.0",
+    author="Carson Gee",
+    author_email="x@carsongee.com",
+    url="https://github.com/carsongee/pytest-pylint",
+    packages=["pytest_pylint"],
+    entry_points={"pytest11": ["pylint = pytest_pylint.plugin"]},
+    python_requires=">=3.7",
+    install_requires=[
+        "pytest>=7.0",
+        "pylint>=2.15.0",
+        "tomli>=1.1.0; python_version < '3.11'",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["coverage", "flake8", "black", "isort"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
