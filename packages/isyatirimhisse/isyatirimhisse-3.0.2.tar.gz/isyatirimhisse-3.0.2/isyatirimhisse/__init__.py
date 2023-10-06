2c44c05d703327ac4184e8b3b51fdf6b4281b53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:20:52 +0000

    Add package isyatirimhisse/3.0.2/isyatirimhisse-3.0.2.tar.gz

---
+++ packages/isyatirimhisse/isyatirimhisse-3.0.2.tar.gz/isyatirimhisse-3.0.2/isyatirimhisse/__init__.py
@@ -0,0 +1,2 @@
+from .FetchData import fetch_data, fetch_financials
+from .DataViz import visualize_data
---
+++ packages/isyatirimhisse/isyatirimhisse-3.0.2.tar.gz/isyatirimhisse-3.0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="isyatirimhisse",
+    version="3.0.2",
+    packages=find_packages(),
+    author="Uraz Akgül",
+    author_email="urazdev@gmail.com",
+    description="İş Yatırım'ın web sitesinden veri çekme işlemlerini kolaylaştıran ve isteğe göre özelleştirilebilen bir kütüphane.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/urazakgul/isyatirimhisse",
+    license="MIT",
+    install_requires=[
+        "requests",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "openpyxl",
+        "asyncio",
+        "aiohttp"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires=">=3.8",
+)
