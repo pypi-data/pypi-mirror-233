de164f600175adc1472758860e972db522be5b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:15:51 +0000

    Add package neurum-db/0.0.8/neurum-db-0.0.8.tar.gz

---
+++ packages/neurum-db/neurum-db-0.0.8.tar.gz/neurum-db-0.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = ['Development Status :: 2 - Pre-Alpha', 
+               'Intended Audience :: Developers', 
+               'Operating System :: MacOS :: MacOS X',
+               'Operating System :: Microsoft :: Windows', 
+               'Operating System :: Unix',  
+               'License :: OSI Approved :: MIT License', 
+               'Programming Language :: Python',]
+
+setup(
+    name= 'neurum-db',
+    version='0.0.8',
+    description='a simple powerful python database built on notion powered by Neurum.',
+    url='',
+    author='Vansh Shah',
+    author_email='vanshshah836@gmail.com',
+    License='MIT',
+    classifiers=classifiers, 
+    keywords='python notion database db free spreadsheet backend',
+    packages=find_packages(),
+    install_requires=[],
+)
