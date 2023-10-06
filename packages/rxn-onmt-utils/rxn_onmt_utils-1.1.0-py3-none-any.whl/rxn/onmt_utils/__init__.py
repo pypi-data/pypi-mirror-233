b3f2b2590205eac01d3f5336c67e8ca001dfab10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:20:54 +0000

    Add package rxn-onmt-utils/1.1.0/rxn_onmt_utils-1.1.0-py3-none-any.whl

---
+++ packages/rxn-onmt-utils/rxn_onmt_utils-1.1.0-py3-none-any.whl/rxn/onmt_utils/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "1.1.0"  # managed by bump2version
+
+from rxn.onmt_utils.translate import translate
+from rxn.onmt_utils.translator import Translator
+
+__all__ = [
+    "translate",
+    "Translator",
+]
