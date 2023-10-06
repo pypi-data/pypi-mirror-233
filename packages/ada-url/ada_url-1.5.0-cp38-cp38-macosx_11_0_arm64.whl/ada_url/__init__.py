a1990fcffee9cb00d566ee60fe77c50e819ee861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:10:24 +0000

    Add package ada-url/1.5.0/ada_url-1.5.0-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/ada-url/ada_url-1.5.0-cp38-cp38-macosx_11_0_arm64.whl/ada_url/__init__.py
@@ -0,0 +1,27 @@
+from ada_url.ada_adapter import (
+    HostType,
+    SchemeType,
+    URL,
+    check_url,
+    idna,
+    idna_to_ascii,
+    idna_to_unicode,
+    join_url,
+    normalize_url,
+    parse_url,
+    replace_url,
+)
+
+__all__ = [
+    'HostType',
+    'SchemeType',
+    'URL',
+    'check_url',
+    'idna',
+    'idna_to_ascii',
+    'idna_to_unicode',
+    'join_url',
+    'normalize_url',
+    'parse_url',
+    'replace_url',
+]
