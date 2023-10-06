a6cb07d0ae5f396d017641c3b16ab9e097aaf1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:22:09 +0000

    Add package defog-data/0.1.1/defog-data-0.1.1.tar.gz

---
+++ packages/defog-data/defog-data-0.1.1.tar.gz/defog-data-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="defog-data",
+    version="0.1.1",
+    description="Static SQL and JSON files containing the data we use for evaluations",
+    author="Defog",
+    author_email="support@defog.ai",
+    packages=find_packages(),
+    package_data={'': ['academic/*', 'advising/*', 'atis/*', 'geography/*', 'restaurants/*', 'scholar/*', 'yelp/*',]},
+    include_package_data=True,
+)
