035b6faea4640a2d94f6592caec7120681a5e356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:08:38 +0000

    Add package handyneo/0.0.2/handyneo-0.0.2.tar.gz

---
+++ packages/handyneo/handyneo-0.0.2.tar.gz/handyneo-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+main_dir = pathlib.Path(__file__).parent.resolve()
+readme_path = main_dir / "README.md"
+LONG_DESCRIPTION = readme_path.read_text() if readme_path.exists() else 'LONG DESCRIPTION'
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Facade for neo4j to create nodes and relationships in a more robust way'
+
+# Setting up
+# print(find_packages())
+# exit()
+setup(
+    name="handyneo",
+    version=VERSION,
+    author="ProxPxD (Piotr Maliszewski)",
+    # author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'handy', 'handyneo', 'neo', 'neo4j'],
+    classifiers=[],
+    include_package_data=True,
+)
---
+++ packages/handyneo/handyneo-0.0.2.tar.gz/handyneo-0.0.2/src/inrun_test.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from src.handyneo import N, Relationer, R
+
+PERSON = 'Person'
+
+
+rel = Relationer(PERSON)
+
+piotr = N('Piotr')
+kita = N('Kita')
+ania = N('Ania')
+karol = N('Karol')
+teresa = N('Teresa')
+robert = N('Robert')
