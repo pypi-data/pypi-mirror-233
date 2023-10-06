6c82c70b7bc916653077577591c2c38a3120e22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:17:04 +0000

    Add package whitecrop3d/0.3.0/whitecrop3d-0.3.0.tar.gz

---
+++ packages/whitecrop3d/whitecrop3d-0.3.0.tar.gz/whitecrop3d-0.3.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='whitecrop3d',
+    version='0.3.0',
+    url='https://github.com/cisar2218/whitecrop3d',
+    author='Dušan Jánsky',
+    author_email='cisar2218@gmail.com',
+    description='Crops unnecessary white background from images. Meant to be for 3D models previews.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'whitecrop3d=src.croping:main',
+        ],
+    },
+)
