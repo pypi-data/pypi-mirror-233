277239afb37a171ecf13b51fa29e34ecf52f2deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:10:56 +0000

    Add package django-joinfield/0.2.1/django_joinfield-0.2.1-py3-none-any.whl

---
+++ packages/django-joinfield/django_joinfield-0.2.1-py3-none-any.whl/runtests.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from optparse import OptionParser
+
+
+def parse_args():
+    parser = OptionParser()
+    parser.add_option('-s', '--settings', default='joinfield.tests.test_sqlite_settings', help='Define settings.')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    options, tests = parse_args()
+    os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
+
+    # Local imports because DJANGO_SETTINGS_MODULE needs to be set first
+    import django
+    from django.test.utils import get_runner
+    from django.conf import settings
+
+    if hasattr(django, 'setup'):
+        django.setup()
+
+    TestRunner = get_runner(settings)
+    runner = TestRunner(verbosity=2, interactive=True, failfast=False)
+    sys.exit(runner.run_tests(tests))
