73d281de90ea79035d9c2b2c8f77c5603626b3a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:12:16 +0000

    Add package TheSilent/0.0.740/TheSilent-0.0.740-py3-none-any.whl

---
+++ packages/TheSilent/TheSilent-0.0.740-py3-none-any.whl/TheSilent/kiwi.py
@@ -0,0 +1,39 @@
+import random
+import socket
+import time
+import urllib.parse
+import TheSilent.puppy_requests as puppy_requests
+from TheSilent.clear import clear
+
+CYAN = "\033[1;36m"
+
+def kiwi(host,delay=0):
+    clear()
+    hits = []
+    init_hosts = []
+    hosts = []
+
+    subdomains = ["adfs","aes","airwatch","alumni","ams","aplus","asg","atriuum","bbb","bcsdivanticsa","bes","bids","blocker","casarry","ccs","citrix","clinksso","clinkssoor","cobalt","collab-edge","compasslearning","d2l","designthefuture","destiny","dialin","discovervideo","docefill","documentservices","e2010","eaa","ecsinow","ecspowerschool","ees","eforms","email","ess","etcentral","etsts","exchange","expressway","falcon1","filewave","filter","fortis","ftp","helpdesk","hes","hhs","hms","hr","iboss","ibossreporter","inow","inowhome","intranet","jobapplications","kc","les","library","mail","mail2","mdm","mealapps","media","meet","moodle","mytime","nextgen","ns","ns1","ns2","nsa","nutrition","oldmail","parentportal","passwordreset","passwordresetregistration","payroll","pdexpress","portal","readydesk","res","rocket","security","sets","soara","spc","sso","sti","studentportal","support","technology","tes","transportation","vpn","webmail","websets","wiki","workorders","www"]
+
+    subdomains = random.sample(subdomains,len(subdomains))
+    for _ in subdomains:
+        # check reverse dns
+        print(CYAN + f"checking for reverse dns on {_}.{urllib.parse.urlparse(host).netloc}")
+        dns_host = f"{_}.{urllib.parse.urlparse(host).netloc}"
+        time.sleep(delay)
+        try:
+            hits.append(f"reverse dns {_}.{urllib.parse.urlparse(host).netloc}: {socket.gethostbyaddr(dns_host)}")
+        except:
+            pass
+        try:
+            data = puppy_requests.text(urllib.parse.urlparse(host).scheme + "://" + _ + "." + urllib.parse.urlparse(host).netloc)
+            init_hosts.append(urllib.parse.urlparse(host).scheme + "://" + _ + "." + urllib.parse.urlparse(host).netloc)
+        except:
+            pass
+
+    clear()
+    hits.sort()
+    for hit in hits:
+        print(CYAN + hit)
+
+    print(f"{len(hits)} results")
