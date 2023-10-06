990f5fc7e52dbb45b384c0dba375df0513c7f5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:20:51 +0000

    Add package django-silica/0.1.3/django_silica-0.1.3-py3-none-any.whl

---
+++ packages/django-silica/django_silica-0.1.3-py3-none-any.whl/django_silica/tests/test_query_params.py
@@ -0,0 +1,32 @@
+from django.test import RequestFactory
+from django.contrib.auth.models import AnonymousUser
+
+from django_silica.tests.SilicaTestCase import SilicaTestCase, SilicaTest
+from django_silica.tests.components.QueryParams import QueryParams
+
+
+class QueryParamTests(SilicaTestCase):
+    def test_query_params_can_be_set(self):
+        (
+            SilicaTest(component=QueryParams)
+            .assertSet("property_1", "foo")
+            .assertSee("foo")
+        )
+
+        request = RequestFactory().get("/?property_1=bar")
+        request.user = AnonymousUser()
+
+        (
+            SilicaTest(component=QueryParams, request=request)
+            .assertSet("property_1", "bar")
+            .assertSee("bar")
+        )
+
+        # response = self.client.get("/test?first_name=James")
+        #
+        # self.assertContains(response, "James")
+
+    def test_properties_set_query_params(self):
+        response = self.client.get("/test")
+
+        self.assertContains(response, "Smith")
