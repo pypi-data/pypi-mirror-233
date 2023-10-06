84aaf53dd420e074e65fde814b5a73521bfddfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:23:25 +0000

    Add package tum_esm_utils/1.6.1/tum_esm_utils-1.6.1.tar.gz

---
+++ packages/tum_esm_utils/tum_esm_utils-1.6.1.tar.gz/tum_esm_utils-1.6.1/tum_esm_utils/system.py
@@ -0,0 +1,54 @@
+"""Common system status related functions.
+
+Implements: `get_cpu_usage`, `get_memory_usage`, `get_disk_space`,
+`get_system_battery`, `get_last_boot_time`, `get_utc_offset`"""
+
+from typing import Any
+import psutil
+import datetime
+
+
+def get_cpu_usage() -> list[float]:
+    """returns cpu_percent for all cores -> list [cpu1%, cpu2%,...]"""
+    return psutil.cpu_percent(interval=1, percpu=True)  # type: ignore
+
+
+def get_memory_usage() -> float:
+    """returns -> tuple (total, available, percent, used, free, active, inactive,
+    buffers, cached, shared, slab)
+    """
+    v_memory = psutil.virtual_memory()
+    return v_memory.percent
+
+
+def get_disk_space() -> float:
+    """Returns disk space used in % as float.
+    -> tuple (total, used, free, percent)"""
+    disk = psutil.disk_usage("/")
+    return disk.percent
+
+
+def get_system_battery() -> int:
+    """Returns system battery in percent as an integer (1-100).
+    Returns 100 if device has no battery. """
+    battery_state: Any = psutil.sensors_battery()  # type:ignore
+    try:
+        assert battery_state is not None
+        percent = battery_state.percent
+        assert isinstance(percent, int)
+        assert 1 <= percent <= 100
+        return percent
+    except AssertionError:
+        return 100
+
+
+def get_last_boot_time() -> str:
+    """Returns last OS boot time."""
+    return datetime.datetime.fromtimestamp(psutil.boot_time()
+                                          ).strftime("%Y-%m-%d %H:%M:%S")
+
+
+def get_utc_offset() -> float:
+    """Returns the UTC offset of the system"""
+    return round((datetime.datetime.now() -
+                  datetime.datetime.utcnow()).total_seconds() / 3600, 1)
