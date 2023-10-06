eb7b5b71bf2e57493602ee558a9c5bd42e943bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:22:10 +0000

    Add package scrapify-ig/1.0.0/scrapify_ig-1.0.0.tar.gz

---
+++ packages/scrapify-ig/scrapify_ig-1.0.0.tar.gz/scrapify_ig-1.0.0/scrapify_ig/client.py
@@ -0,0 +1,22 @@
+from .request import APIRequest
+from .mixins import user, media_post, search
+
+
+class Client(
+    APIRequest,
+    user.UserMixin,
+    media_post.MediaPostMixin,
+    search.SearchMixin,
+):
+    """ Base class for receiving API data """
+    def __init__(
+            self,
+            token: str
+    ) -> None:
+        """
+        :param token: RAPID api token
+        """
+        APIRequest.__init__(
+            self,
+            token=token
+        )
