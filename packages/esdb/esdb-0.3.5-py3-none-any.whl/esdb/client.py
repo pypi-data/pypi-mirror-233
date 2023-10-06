b5a89dee862baa69350110122d0fce75972214de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:29:51 +0000

    Add package esdb/0.3.5/esdb-0.3.5-py3-none-any.whl

---
+++ packages/esdb/esdb-0.3.5-py3-none-any.whl/esdb/client.py
@@ -0,0 +1,275 @@
+from __future__ import annotations
+
+import asyncio
+import base64
+import contextlib
+import enum
+import itertools
+import logging
+import random
+import urllib.parse
+from collections.abc import AsyncIterator
+from dataclasses import dataclass
+from functools import cmp_to_key
+from typing import Optional
+
+import grpc
+
+from esdb.exceptions import DiscoveryError
+from esdb.generated.gossip_pb2_grpc import GossipStub
+from esdb.generated.persistent_pb2_grpc import PersistentSubscriptionsStub
+from esdb.generated.streams_pb2_grpc import StreamsStub
+from esdb.gossip import Gossip, Member, State
+from esdb.streams import Streams
+from esdb.subscriptions import PersistentSubscriptions
+
+logger = logging.getLogger(__name__)
+
+
+class Preference(enum.Enum):
+    LEADER = enum.auto()
+    FOLLOWER = enum.auto()
+    READ_ONLY_REPLICA = enum.auto()
+
+    @classmethod
+    def from_string(cls, s: str) -> Preference:
+        return {
+            "follower": cls.FOLLOWER,
+            "leader": cls.LEADER,
+            "readonlyreplica": cls.READ_ONLY_REPLICA,
+        }[s.lower()]
+
+
+@dataclass
+class Configuration:
+    address: Optional[Member.Endpoint] = None
+    gossip_seed: Optional[list[Member.Endpoint]] = None
+    username: Optional[str] = None
+    password: Optional[str] = None
+    disable_tls: bool = False
+    node_preference: Preference = Preference.LEADER
+    verify_cert: bool = True
+    root_cert: Optional[bytes] = None
+    max_discovery_attempts: int = 10
+    discovery_interval: int = 100
+    gossip_timeout: int = 5
+    dns_discover: bool = False
+    keep_alive_interval: int = 10
+    keep_alive_timeout: int = 10
+    default_deadline: int = 10
+
+
+class BasicAuthPlugin(grpc.AuthMetadataPlugin):
+    def __init__(self, user: str, password: str) -> None:
+        self.__auth = base64.b64encode(f"{user}:{password}".encode())
+
+    def __call__(self, _: grpc.AuthMetadataContext, callback: grpc.AuthMetadataPluginCallback) -> None:
+        callback((("authorization", b"Basic " + self.__auth),), None)
+
+
+@dataclass
+class Connection:
+    channel: grpc.aio._base_channel.Channel  # type: ignore
+    streams: Streams
+    subscriptions: PersistentSubscriptions
+    gossip: Gossip
+    config: Configuration
+
+
+def pick_node(preference: Preference, members: list[Member]) -> Optional[Member]:
+    preference_map = {
+        Preference.LEADER: [State.Leader],
+        Preference.FOLLOWER: [State.Follower],
+        Preference.READ_ONLY_REPLICA: [State.ReadOnlyLeaderless, State.PreReadOnlyReplica, State.ReadOnlyReplica],
+    }
+    preferred_states = preference_map[preference]
+
+    def _compare(a: Member, b: Member) -> int:
+        return (preferred_states.index(b.state) if b.state in preferred_states else -1) - (
+            preferred_states.index(a.state) if a.state in preferred_states else -1
+        )
+
+    members_ = members.copy()
+    random.shuffle(members_)
+
+    member: Optional[Member] = next(
+        (
+            m
+            for m in sorted(members_, key=cmp_to_key(_compare))
+            if m.is_alive and m.state in list(itertools.chain(*preference_map.values())) and m.endpoint
+        ),
+        None,
+    )
+    if not member or not member.endpoint:
+        return None
+    return member
+
+
+def parse_endpoint(s: str) -> Member.Endpoint:
+    if ":" in s:
+        items = s.split(":")
+        if len(items) != 2:
+            raise ValueError("Too many colons in a host")
+        host, port_str = items
+        try:
+            port = int(port_str)
+        except ValueError:
+            raise ValueError(f"{port_str} port is not a number")
+        return Member.Endpoint(host, port)
+
+    return Member.Endpoint(s, 2113)
+
+
+def parse_settings(query: str, c: Configuration) -> None:
+    def _str_to_bool(s: str) -> bool:
+        return {"true": True, "false": False}[s.lower()]
+
+    for k, v in urllib.parse.parse_qs(query, strict_parsing=True).items():
+        if len(v) != 1:
+            raise ValueError(f"Too many values for {k}")
+        key = k.lower()
+        [val] = v
+        if key == "discoveryinterval":
+            c.discovery_interval = int(val)
+        elif key == "gossiptimeout":
+            c.gossip_timeout = int(val)
+        elif key == "maxdiscoverattempts":
+            c.max_discovery_attempts = int(val)
+        elif key == "nodepreference":
+            c.node_preference = Preference.from_string(val)
+        elif key == "keepaliveinterval":
+            c.keep_alive_interval = int(val)
+        elif key == "keepalivetimeout":
+            c.keep_alive_timeout = int(val)
+        elif key == "tls":
+            c.disable_tls = not _str_to_bool(val)
+        elif key == "tlscafile":
+            with open(val, "rb") as fh:
+                c.root_cert = fh.read()
+        elif key == "tlsverifycert":
+            c.verify_cert = _str_to_bool(val)
+        elif key == "defaultdeadline":
+            c.default_deadline = int(val)
+        else:
+            raise ValueError(f"Invalid option {k}")
+
+
+def parse_connection_string(connection_string: str) -> Configuration:
+    config = Configuration()
+    scheme, rest = connection_string.split("://")
+    if scheme not in ("esdb", "esdb+discover"):
+        raise ValueError("esdb:// or esdb+discover:// scheme is required")
+
+    config.dns_discover = scheme == "esdb+discover"
+
+    if "@" in rest:
+        user_info, rest = rest.split("@")
+        user_info_items = user_info.split(":")
+        if len(user_info_items) != 2:
+            raise ValueError("Invalid user credentials")
+        user, password = user_info_items
+        if not user:
+            raise ValueError("Username is required")
+        if not password:
+            raise ValueError("Password is required")
+        config.username, config.password = user, password
+
+    hosts, *queries = rest.split("?")
+    endpoints = []
+    for host in hosts.split(","):
+        endpoints.append(parse_endpoint(host))
+    if len(endpoints) == 1:
+        config.address = endpoints[0]
+    else:
+        config.gossip_seed = endpoints
+
+    if queries:
+        [settings_query] = queries
+        parse_settings(settings_query, config)
+
+    return config
+
+
+class ESClient:
+    def __init__(
+        self,
+        connection_string: str,
+        root_certificates: Optional[bytes] = None,
+        private_key: Optional[bytes] = None,
+        certificate_chain: Optional[bytes] = None,
+    ) -> None:
+        self.config = parse_connection_string(connection_string)
+        self.options = [
+            ("grpc.keepalive_time_ms", self.config.keep_alive_interval * 1000),
+            ("grpc.keepalive_timeout_ms", self.config.keep_alive_timeout * 1000),
+        ]
+
+        if not self.config.disable_tls:
+            self.channel_credentials = grpc.ssl_channel_credentials(
+                root_certificates=root_certificates or self.config.root_cert,
+                private_key=private_key,
+                certificate_chain=certificate_chain,
+            )
+
+        if self.config.username and self.config.password:
+            self.call_credentials = grpc.metadata_call_credentials(
+                BasicAuthPlugin(self.config.username, self.config.password), name="auth"
+            )
+
+    def _create_channel(self, endpoint: Member.Endpoint) -> grpc.aio.Channel:  # type: ignore
+        if self.config.disable_tls:
+            return grpc.aio.insecure_channel(  # type: ignore
+                f"{endpoint.address}:{endpoint.port}", options=self.options
+            )
+        assert self.channel_credentials
+        credentials = (
+            grpc.composite_channel_credentials(self.channel_credentials, self.call_credentials)
+            if self.call_credentials
+            else self.channel_credentials
+        )
+        return grpc.aio.secure_channel(  # type: ignore
+            f"{endpoint.address}:{endpoint.port}", credentials, self.options
+        )
+
+    @contextlib.asynccontextmanager
+    async def connect(self) -> AsyncIterator[Connection]:
+        if self.config.dns_discover:
+            endpoint = await self.discover_endpoint()
+        else:
+            assert self.config.address
+            endpoint = self.config.address
+
+        async with self._create_channel(endpoint) as channel:
+            yield Connection(
+                channel=channel,
+                streams=Streams(StreamsStub(channel)),
+                subscriptions=PersistentSubscriptions(PersistentSubscriptionsStub(channel)),
+                gossip=Gossip(GossipStub(channel)),
+                config=self.config,
+            )
+
+    async def discover_endpoint(self) -> Member.Endpoint:
+        for attempt in range(1, self.config.max_discovery_attempts + 1):
+            candidates = (
+                self.config.gossip_seed.copy() if self.config.gossip_seed else [self.config.address]  # type: ignore
+            )
+            random.shuffle(candidates)
+            logger.info(
+                "Starting discovery attempt %s on %s", attempt, ",".join(f"{c.address}:{c.port}" for c in candidates)
+            )
+            for candidate in candidates:
+                async with self._create_channel(candidate) as chan:
+                    gossip = Gossip(GossipStub(chan))
+                    members = await gossip.get_members(self.config.gossip_timeout)
+                    if pick := pick_node(self.config.node_preference, members):
+                        assert pick.endpoint
+                        logger.info(
+                            "Discovered %s node at %s (Preference: %s)",
+                            pick.state.name,
+                            f"{pick.endpoint.address}:{pick.endpoint.port}",
+                            self.config.node_preference.name,
+                        )
+                        return pick.endpoint
+
+            await asyncio.sleep(self.config.discovery_interval)
+        raise DiscoveryError(f"Discovery failed after {self.config.max_discovery_attempts} attempt(s)")
---
+++ packages/esdb/esdb-0.3.5-py3-none-any.whl/esdb/subscriptions/subscriptions.py
@@ -0,0 +1,214 @@
+from __future__ import annotations
+
+import asyncio
+from typing import AsyncIterable, AsyncIterator, Optional
+
+from esdb.generated.persistent_pb2 import (
+    CreateReq,
+    CreateResp,
+    DeleteReq,
+    GetInfoReq,
+    GetInfoResp,
+    ListReq,
+    ListResp,
+    ReadReq,
+    ReadResp,
+    UpdateReq,
+    UpdateResp,
+)
+from esdb.generated.persistent_pb2_grpc import PersistentSubscriptionsStub
+from esdb.generated.shared_pb2 import UUID, Empty, StreamIdentifier
+from esdb.shared import Filter
+from esdb.subscriptions.types import (
+    Event,
+    NackAction,
+    SubscriptionInfo,
+    SubscriptionSettings,
+)
+
+
+class Subscription:
+    def __init__(
+        self, group_name: str, buffer_size: int, stream: Optional[str], stub: PersistentSubscriptionsStub
+    ) -> None:
+        self.stream = stream
+        self.group_name = group_name
+        self.buffer_size = buffer_size
+        self.send_queue: asyncio.Queue = asyncio.Queue()
+        self.stub = stub
+        self.subscription_id: Optional[str] = None
+
+    async def __aiter__(self) -> AsyncIterable[Event]:
+        read_request = ReadReq(
+            options=ReadReq.Options(
+                stream_identifier=StreamIdentifier(stream_name=self.stream.encode()) if self.stream else None,
+                all=Empty() if self.stream is None else None,
+                group_name=self.group_name,
+                buffer_size=self.buffer_size,
+                uuid_option=ReadReq.Options.UUIDOption(structured=Empty(), string=Empty()),
+            ),
+            ack=None,
+            nack=None,
+        )
+        await self.send_queue.put(read_request)
+
+        async def queue_iter():
+            while True:
+                yield await self.send_queue.get()
+                self.send_queue.task_done()
+
+        reader: AsyncIterator[ReadResp] = self.stub.Read(queue_iter())
+        async for response in reader:
+            if not self.subscription_id and response.WhichOneof("content") == "subscription_confirmation":
+                self.subscription_id = response.subscription_confirmation.subscription_id
+                continue
+            yield Event.from_read_response_event(response.event)
+
+    async def ack(self, events: list[Event]) -> None:
+        assert self.subscription_id, "Nothing to ack, not reading from a subscription yet"
+        await self.send_queue.put(
+            ReadReq(
+                ack=ReadReq.Ack(
+                    id=self.subscription_id.encode(),
+                    ids=(UUID(string=evt.id) for evt in events),
+                )
+            )
+        )
+        await self.send_queue.join()
+
+    async def nack(self, events: list[Event], action: NackAction, reason: Optional[str] = None) -> None:
+        assert self.subscription_id, "Nothing to nack, not reading from a subscription yet"
+        await self.send_queue.put(
+            ReadReq(
+                nack=ReadReq.Nack(
+                    id=self.subscription_id.encode(),
+                    ids=(UUID(string=evt.id) for evt in events),
+                    action=action.value,
+                    reason=reason or "",
+                )
+            )
+        )
+
+
+class PersistentSubscriptions:
+    def __init__(self, stub: PersistentSubscriptionsStub) -> None:
+        self._stub = stub
+
+    async def create_stream_subscription(
+        self, stream: str, group_name: str, settings: SubscriptionSettings, backwards: bool = False
+    ) -> None:
+        stream_identifier = StreamIdentifier(stream_name=stream.encode())
+        create_request = CreateReq(
+            options=CreateReq.Options(
+                stream=CreateReq.StreamOptions(
+                    stream_identifier=stream_identifier,
+                    start=None if backwards else Empty(),
+                    end=Empty() if backwards else None,
+                ),
+                stream_identifier=stream_identifier,
+                group_name=group_name,
+                settings=settings.to_protobuf(CreateReq.Settings),
+            )
+        )
+        response: CreateResp = await self._stub.Create(create_request)
+        assert isinstance(response, CreateResp), f"Expected {CreateResp} got {response.__class__}"
+
+    async def create_all_subscription(
+        self,
+        group_name: str,
+        settings: SubscriptionSettings,
+        backwards: bool = False,
+        filter_by: Optional[Filter] = None,
+    ) -> None:
+        create_request = CreateReq(
+            options=CreateReq.Options(
+                group_name=group_name,
+                settings=settings.to_protobuf(CreateReq.Settings),
+                all=CreateReq.AllOptions(
+                    # position=CreateReq.Position(), TODO: deal with position
+                    start=None if backwards else Empty(),
+                    end=Empty() if backwards else None,
+                    no_filter=Empty() if filter_by is None else None,
+                    filter=filter_by.to_protobuf(CreateReq.AllOptions.FilterOptions) if filter_by else None,
+                ),
+            )
+        )
+        response: CreateResp = await self._stub.Create(create_request)
+        assert isinstance(response, CreateResp), f"Expected {CreateResp} got {response.__class__}"
+
+    def subscribe(
+        self,
+        group_name: str,
+        buffer_size: int,
+        stream: Optional[str] = None,
+    ) -> Subscription:
+        return Subscription(stream=stream, group_name=group_name, buffer_size=buffer_size, stub=self._stub)
+
+    async def update_stream_subscription(
+        self, stream: str, group_name: str, settings: SubscriptionSettings, backwards: bool = False
+    ) -> None:
+        stream_identifier = StreamIdentifier(stream_name=stream.encode())
+        update_request = UpdateReq(
+            options=UpdateReq.Options(
+                stream=UpdateReq.StreamOptions(
+                    stream_identifier=stream_identifier,
+                    start=None if backwards else Empty(),
+                    end=Empty() if backwards else None,
+                ),
+                stream_identifier=stream_identifier,
+                group_name=group_name,
+                settings=settings.to_protobuf(UpdateReq.Settings),
+            )
+        )
+        response: UpdateResp = await self._stub.Update(update_request)
+        assert isinstance(response, UpdateResp), f"Expected {UpdateResp} got {response.__class__}"
+
+    async def update_all_subscription(
+        self, group_name: str, settings: SubscriptionSettings, backwards: bool = False
+    ) -> None:
+        update_request = UpdateReq(
+            options=UpdateReq.Options(
+                group_name=group_name,
+                settings=settings.to_protobuf(UpdateReq.Settings),
+                all=UpdateReq.AllOptions(
+                    # position=CreateReq.Position(), TODO: deal with position
+                    start=None if backwards else Empty(),
+                    end=Empty() if backwards else None,
+                ),
+            )
+        )
+        response: UpdateResp = await self._stub.Update(update_request)
+        assert isinstance(response, UpdateResp), f"Expected {UpdateResp} got {response.__class__}"
+
+    async def get_info(self, group_name: str, stream: Optional[str] = None) -> SubscriptionInfo:
+        info_request = GetInfoReq(
+            options=GetInfoReq.Options(
+                group_name=group_name,
+                stream_identifier=StreamIdentifier(stream_name=stream.encode()) if stream else None,
+                all=Empty() if stream is None else None,
+            )
+        )
+        response: GetInfoResp = await self._stub.GetInfo(info_request)
+        return SubscriptionInfo.from_protobuf(response.subscription_info)
+
+    async def list(self, stream: Optional[str] = None) -> list[SubscriptionInfo]:
+        request = ListReq(
+            options=ListReq.Options(
+                list_all_subscriptions=Empty() if stream is None else None,
+                list_for_stream=None
+                if stream is None
+                else ListReq.StreamOption(stream=StreamIdentifier(stream_name=stream.encode())),
+            )
+        )
+        response: ListResp = await self._stub.List(request)
+        return [SubscriptionInfo.from_protobuf(sub) for sub in response.subscriptions]
+
+    async def delete(self, group_name: str, stream: Optional[str] = None) -> None:
+        request = DeleteReq(
+            options=DeleteReq.Options(
+                group_name=group_name,
+                all=Empty() if stream is None else None,
+                stream_identifier=StreamIdentifier(stream_name=stream.encode()) if stream is not None else None,
+            )
+        )
+        await self._stub.Delete(request)
