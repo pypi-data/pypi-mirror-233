24cd000645b90b261d17dbf184bb10b9d6b7c92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:11:41 +0000

    Add package singulr/0.0.3/singulr-0.0.3.tar.gz

---
+++ packages/singulr/singulr-0.0.3.tar.gz/singulr-0.0.3/setup.py
@@ -0,0 +1,21 @@
+# Created by msinghal at 04/10/23
+from setuptools import setup, find_packages
+
+setup(
+    name='singulr',
+    version='0.0.3',
+    description='Package to trace generative apps and guard them',
+    url='',
+    author='Madan Singhal',
+    author_email='00848madan@gmail.com',
+    license='MIT',  # Choose an appropriate license
+
+    # Specify the packages to include
+    packages=find_packages(),
+
+    # Define package dependencies
+    install_requires=[
+        'langchain'
+        # List your dependencies here
+    ],
+)
