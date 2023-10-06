b717a3e0467575328f1011485e5a35000f53bada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 15:13:53 +0000

    Add package mage-ai/0.9.32/mage_ai-0.9.32-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.9.32-py3-none-any.whl/mage_ai/presenters/interactions/constants.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+INTERACTIONS_DIRECTORY_NAME = 'interactions'
+
+
+class InteractionInputType(str, Enum):
+    CHECKBOX = 'checkbox'
+    DROPDOWN_MENU = 'dropdown_menu'
+    SWITCH = 'switch'
+    TEXT_FIELD = 'text_field'
+
+
+class InteractionInputStyleInputType(str, Enum):
+    NUMBER = 'number'
+
+
+class InteractionVariableType(str, Enum):
+    DATE = 'date'
+    DATETIME = 'datetime'
+    DICTIONARY = 'dictionary'
+    FLOAT = 'float'
+    INTEGER = 'integer'
+    LIST = 'list'
+    STRING = 'string'
