27523ee7f67171c16cda5b3c54cce7d6ac23ccc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:22:07 +0000

    Add package primerchecker/0.0.1/primerchecker-0.0.1.tar.gz

---
+++ packages/primerchecker/primerchecker-0.0.1.tar.gz/primerchecker-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+        name='primerchecker',
+        version='0.0.1',
+        summary='a geekflare.com tuto',
+        author='gmorange',
+        author_email='gml.morange@gmail.com',
+        license='42',
+        location='/mnt/nfs/homes/gmorange/Documents/testPython/package/primechecker',
+        installer='pip',
+        classifiers='',
+        packages=find_packages(),
+        )
