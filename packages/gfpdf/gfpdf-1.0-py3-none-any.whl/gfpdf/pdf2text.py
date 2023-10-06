96f6b58a19c0b5ab0a95cf5b419f8958c8665723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:15:19 +0000

    Add package gfpdf/1.0/gfpdf-1.0-py3-none-any.whl

---
+++ packages/gfpdf/gfpdf-1.0-py3-none-any.whl/gfpdf/pdf2text.py
@@ -0,0 +1,14 @@
+"""This module converts PDF files into text"""
+
+
+def convert(path):
+    """
+    Converts PDF files into text. 
+
+    Parameters:
+    path(str): The path to the PDF file. 
+
+    Returns:
+    str: Content of the PDF file into text.
+    """
+    print("pdf2text")
