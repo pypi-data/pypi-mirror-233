eaa0e8db55d267e11a1b5f4a8daecf68e879895d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:23:40 +0000

    Add package nadb/0.0.4/nadb-0.0.4.tar.gz

---
+++ packages/nadb/nadb-0.0.4.tar.gz/nadb-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nadb',
+    version='0.0.4',
+    packages=find_packages(),
+    install_requires=[],
+    author='Leandro Ferreira',
+    author_email='leandrodsferreira@gmail.com',
+    description='A simple lock based key-value store',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/lsferreira42/nadb',
+)
+
