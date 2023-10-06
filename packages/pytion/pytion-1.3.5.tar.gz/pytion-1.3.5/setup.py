d4d84404d0528748118118955147e256c9eddcaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:29:51 +0000

    Add package pytion/1.3.5/pytion-1.3.5.tar.gz

---
+++ packages/pytion/pytion-1.3.5.tar.gz/pytion-1.3.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytion",
+    version="1.3.5",
+    author="Yegor Gomzin",
+    author_email="slezycmex@mail.ru",
+    description="Unofficial Python client for official Notion API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lastorel/pytion",
+    project_urls={
+        "Bug Tracker": "https://github.com/lastorel/pytion/issues",
+        "Changelog": "https://github.com/lastorel/pytion/blob/main/CHANGELOG.md",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "requests>=2.26.0"
+    ]
+)
