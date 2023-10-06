57eb5f39a99496ab6db3eb6ae34b4343738182f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:12:22 +0000

    Add package eppo-server-sdk/1.3.0/eppo_server_sdk-1.3.0-py3-none-any.whl

---
+++ packages/eppo-server-sdk/eppo_server_sdk-1.3.0-py3-none-any.whl/eppo_client/config.py
@@ -0,0 +1,13 @@
+from eppo_client.assignment_logger import AssignmentLogger
+from eppo_client.base_model import SdkBaseModel
+
+from eppo_client.validation import validate_not_blank
+
+
+class Config(SdkBaseModel):
+    api_key: str
+    base_url: str = "https://fscdn.eppo.cloud/api"
+    assignment_logger: AssignmentLogger
+
+    def _validate(self):
+        validate_not_blank("api_key", self.api_key)
