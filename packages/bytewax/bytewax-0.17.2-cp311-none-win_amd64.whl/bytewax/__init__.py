cae7364ba2ba6afc226eab350485bc28b7cb5d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:16:25 +0000

    Add package bytewax/0.17.2/bytewax-0.17.2-cp311-none-win_amd64.whl

---
+++ packages/bytewax/bytewax-0.17.2-cp311-none-win_amd64.whl/bytewax/__init__.py
@@ -0,0 +1,15 @@
+"""Bytewax is an open source Python framework for building highly
+scalable dataflows in a streaming or batch context.
+
+[See our readme for more
+documentation.](https://github.com/bytewax/bytewax)
+
+"""  # noqa: D205
+__all__ = []
+
+__pdoc__ = {
+    # This is the PyO3 module that has to be named "bytewax". Hide it
+    # since we re-import its members into the Python source files in
+    # their final locations.
+    "bytewax": False,
+}
