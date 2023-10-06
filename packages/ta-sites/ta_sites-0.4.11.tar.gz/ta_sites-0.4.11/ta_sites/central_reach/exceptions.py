3594e87d6f6c1124d541aef0beb0cda366d437a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:26:14 +0000

    Add package ta-sites/0.4.11/ta_sites-0.4.11.tar.gz

---
+++ packages/ta-sites/ta_sites-0.4.11.tar.gz/ta_sites-0.4.11/ta_sites/central_reach/exceptions.py
@@ -0,0 +1,46 @@
+class CentralReachException(Exception):
+    """
+    The class is deprecated, don't use it!
+    """
+
+    pass
+
+
+class ScheduledMaintenance(CentralReachException):
+    """
+    The class is deprecated, don't use it!
+    """
+
+    pass
+
+
+class EmptyPage(CentralReachException):
+    """
+    The class is deprecated, don't use it!
+    """
+
+    pass
+
+
+class BadRequest(CentralReachException):
+    """
+    The class is deprecated, don't use it!
+    """
+
+    pass
+
+
+class CentralReachError(CentralReachException):
+    pass
+
+
+class ScheduledMaintenanceError(ScheduledMaintenance, CentralReachError):
+    pass
+
+
+class EmptyPageError(EmptyPage, CentralReachError):
+    pass
+
+
+class BadRequestError(BadRequest, CentralReachError):
+    pass
