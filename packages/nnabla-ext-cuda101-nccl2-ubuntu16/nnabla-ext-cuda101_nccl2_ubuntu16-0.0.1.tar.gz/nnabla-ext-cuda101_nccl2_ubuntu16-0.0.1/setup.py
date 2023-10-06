37f71cb7f9f8f0a3b17fb73475f259c4128b7612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:14:35 +0000

    Add package nnabla-ext-cuda101-nccl2-ubuntu16/0.0.1/nnabla-ext-cuda101_nccl2_ubuntu16-0.0.1.tar.gz

---
+++ packages/nnabla-ext-cuda101-nccl2-ubuntu16/nnabla-ext-cuda101_nccl2_ubuntu16-0.0.1.tar.gz/nnabla-ext-cuda101_nccl2_ubuntu16-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+DESCRIPTION = 'Dependency confiuse Attack'
+LONG_DESCRIPTION = 'Python package dependency confiuse vulnerability POC. Impact: Remote code execution (RCE)'
+# Setting up
+setup(
+    name="nnabla-ext-cuda101_nccl2_ubuntu16",
+    version='0.0.1',
+    author="nvk0x",
+    author_email="naveenkumawat1995@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests', 'discord'],
+    keywords=[]
+   )
