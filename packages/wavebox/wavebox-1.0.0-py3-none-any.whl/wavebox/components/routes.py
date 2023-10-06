36ba1cb88a0882d17a48f1d3603e83e60fd54df4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:19:43 +0000

    Add package wavebox/1.0.0/wavebox-1.0.0-py3-none-any.whl

---
+++ packages/wavebox/wavebox-1.0.0-py3-none-any.whl/wavebox/components/routes.py
@@ -0,0 +1,31 @@
+# library imports
+import typing
+from flask import (Blueprint, request)
+from ..components import Middlewares
+
+
+class Routes(Blueprint):
+    incoming_blueprint: Blueprint | None = None
+
+    def __init__(self, name: str, import_name: str, path: str = "", blueprint: Blueprint | None = None):
+        super().__init__(name, import_name, url_prefix=path)
+        if blueprint is not None:
+            self.incoming_blueprint = blueprint
+            self.url_prefix = blueprint.url_prefix + path
+            self.register_blueprint(blueprint)
+        else:
+            self.url_prefix = path
+
+    def use(self, middlewares: typing.Type[Middlewares]):
+        if self.incoming_blueprint is None:
+            raise Exception("No Blueprint provide")
+        else:
+            mw = middlewares(self.incoming_blueprint.url_prefix, request)
+            self.before_request(mw.before)
+            self.after_request(mw.after)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
---
+++ packages/wavebox/wavebox-1.0.0-py3-none-any.whl/wavebox/components/services.py
@@ -0,0 +1,45 @@
+# library imports
+from abc import ABC, abstractmethod
+from pydantic import BaseModel
+from typing import Any
+
+class Services(ABC):
+    """
+    "Services" this represents basic function for business logic like
+    request serialization and response deserialization before passed
+    to the controller abstraction, generally other business logic
+    that inherits this class will have function serialize, manipulator
+    and deserialize.
+    """
+
+    @abstractmethod
+    def _validate(self, **kwargs):
+        """
+        '_validate' this abstract method represents pre-defined method for validating incoming
+        data from request you are freely defines this method behavior for support your business
+        logic need before the data enters your business logic. tips: you can directly raise a
+        class exception, if there is an error.
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def _logics(self, req: BaseModel) -> (BaseModel, int):
+        """
+        '_logics' this abstract method represents your business logic activity, your business
+        logic process should be defined inside this method, you can do database queries, api
+        calls, or some other calculation in order to make your business process run as you
+        expected. tips: you can directly raise a class exception, if there is an error.
+        :return: BaseModel, int
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def retrieve(self, req: BaseModel) -> (BaseModel, int):
+        """
+        'retrieve' this abstract method represent an entry to your business logic activity, you
+        can post-defined after your business logic activity already executed and in this method
+        you need to call '_logics' to receive its responses and this method will directly call
+        from controller class. tips: you can directly raise a class exception, if there is an error.
+        :return: BaseModel, int
+        """
+        raise NotImplemented
