76f5ea6773228d8d1ebafa41e4d8aa5f0c67d8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:18:03 +0000

    Add package kluff-python-sdk/0.0.1/kluff_python_sdk-0.0.1.tar.gz

---
+++ packages/kluff-python-sdk/kluff_python_sdk-0.0.1.tar.gz/kluff_python_sdk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="kluff_python_sdk",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.10',
+    description="a library representing multiple apps of frappe",
+    author="kluff",
+)
