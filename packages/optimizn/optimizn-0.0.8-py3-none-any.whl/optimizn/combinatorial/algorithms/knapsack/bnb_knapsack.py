6960a39ce4b58a4b5e457f97dbb8947553cacf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:08:42 +0000

    Add package optimizn/0.0.8/optimizn-0.0.8-py3-none-any.whl

---
+++ packages/optimizn/optimizn-0.0.8-py3-none-any.whl/optimizn/combinatorial/algorithms/knapsack/bnb_knapsack.py
@@ -0,0 +1,149 @@
+import numpy as np
+from optimizn.combinatorial.branch_and_bound import BnBProblem
+
+
+class KnapsackParams:
+    def __init__(self, values, weights, capacity, init_sol):
+        self.values = values
+        self.weights = weights
+        self.capacity = capacity
+        self.init_sol = init_sol
+
+    def __eq__(self, other):
+        return (
+            other is not None
+            and (len(self.values) == len(other.values)
+                 and (self.values == other.values).all())
+            and (len(self.weights) == len(other.weights)
+                 and (self.weights == other.weights).all())
+            and self.capacity == other.capacity
+            and (len(self.init_sol[0]) == len(other.init_sol[0])
+                 and (self.init_sol[0] == other.init_sol[0]).all())
+            and self.init_sol[1] == self.init_sol[1]
+        )
+
+
+# References:
+# https://www.youtube.com/watch?v=yV1d-b_NeK8
+class ZeroOneKnapsackProblem(BnBProblem):
+    '''
+    Class for the simplified knapsack problem, where each item is either
+    taken or omitted in its entirety
+    '''
+    def __init__(self, params):
+        self.values = np.array(params.values)
+        self.weights = np.array(params.weights)
+        self.capacity = params.capacity
+
+        # value/weight ratios, in decreasing order
+        vw_ratios = self.values / self.weights
+        vw_ratios_ixs = []
+        for i in range(len(vw_ratios)):
+            vw_ratios_ixs.append((vw_ratios[i], i))
+        self.sorted_vw_ratios = sorted(vw_ratios_ixs)
+        self.sorted_vw_ratios.reverse()
+        self.init_sol = params.init_sol
+        super().__init__(params)
+
+    def get_candidate(self):
+        return self.init_sol
+
+    def lbound(self, sol):
+        value = 0
+        weight = 0
+
+        # consider items already taken
+        for i in range(0, sol[1] + 1):
+            if sol[0][i] == 1:
+                value += self.values[i]
+                weight += self.weights[i]
+
+        # greedily take other items
+        for vw_ratio, ix in self.sorted_vw_ratios:
+            if ix < sol[1] + 1:
+                continue
+            rem_cap = self.capacity - weight
+            if rem_cap <= 0:
+                break
+            item_weight = min(rem_cap, self.weights[ix])
+            value += item_weight * vw_ratio
+            weight += item_weight
+
+        return -1 * value
+
+    def cost(self, sol):
+        return -1 * np.sum(sol[0] * self.values)
+
+    def branch(self, sol):
+        exp_idx = sol[1] + 1
+        if exp_idx >= len(self.weights):
+            return []
+
+        new_sols = []
+        for val in [0, 1]:
+            new_sol = np.zeros(len(sol[0]))
+            new_sol[0:exp_idx] = sol[0][0:exp_idx]
+            new_sol[exp_idx] = val
+            weight = np.sum(new_sol * self.weights)
+
+            # greedily take other items
+            for _, ix in self.sorted_vw_ratios:
+                if ix < exp_idx + 1:
+                    continue
+                rem_cap = self.capacity - weight
+                if rem_cap <= 0:
+                    break
+                if self.weights[ix] <= rem_cap:
+                    new_sol[ix] = 1
+                    weight += self.weights[ix]
+
+            new_sols.append((new_sol, exp_idx))
+        return new_sols
+
+    def is_feasible(self, sol):
+        # check that array is not longer than the number of weights/values
+        check_length1 = len(sol[0]) <= len(self.weights)
+        check_length2 = len(sol[0]) <= len(self.values)
+        check_length = check_length1 and check_length2
+
+        # check that the only values in the array are 0 and 1
+        check_values = len(set(sol[0].tolist()).difference({0, 1})) == 0
+
+        # check that the weight of the values in the array is not greater
+        # than the capacity
+        check_weight = np.sum(sol[0] * self.weights) <= self.capacity
+
+        return check_length and check_values and check_weight
+
+    def is_complete(self, sol):
+        # check that array length is the same as the number of weights/values
+        check_length1 = len(sol[0]) == len(self.weights)
+        check_length2 = len(sol[0]) == len(self.values)
+        check_length = check_length1 and check_length2
+
+        return check_length
+
+    def complete_solution(self, sol):
+        # greedily add other items to array
+        knapsack = list(sol[0])
+        value = 0
+        weight = 0
+        for i in range(len(knapsack)):
+            if knapsack[i] == 1:
+                value += self.values[i]
+                weight += self.weights[i]
+
+        # greedily take other items
+        for _, ix in self.sorted_vw_ratios:
+            if ix < sol[1] + 1:
+                knapsack.append(0)
+                continue
+            rem_cap = self.capacity - weight
+            if rem_cap < self.weights[ix]:
+                knapsack.append(0)
+                continue
+            value += self.values[ix]
+            weight += self.weights[ix]
+            knapsack.append(1)
+        
+        return (np.array(knapsack), sol[1])
---
+++ packages/optimizn/optimizn-0.0.8-py3-none-any.whl/optimizn/combinatorial/algorithms/travelling_salesman/sim_anneal_tsp.py
@@ -0,0 +1,88 @@
+import numpy as np
+from copy import deepcopy
+from optimizn.combinatorial.simulated_annealing import SimAnnealProblem
+# from ortools.constraint_solver import routing_enums_pb2
+# from ortools.constraint_solver import pywrapcp
+
+
+class TravSalsmn(SimAnnealProblem):
+    def __init__(self, params):
+        self.params = params
+        super().__init__()
+    
+    def _get_closest_city(self, city, visited):
+        # get the unvisited city closest to the one provided
+        min_city = None
+        min_dist = float('inf')
+        dists = self.params.dists[city]
+        for i in range(len(dists)):
+            if i != city and i not in visited and dists[i] < min_dist:
+                min_city = i
+                min_dist = dists[i]
+        return min_city
+
+    def _complete_path(self, path):
+        # complete the path greedily, iteratively adding the unvisited city
+        # closest to the last city in the accmulated path
+        visited = set(path)
+        complete_path = deepcopy(path)
+        while len(complete_path) != self.params.dists.shape[0]:
+            if len(complete_path) == 0:
+                next_city = 0
+            else:
+                last_city_idx = 0 if len(complete_path) == 0 else\
+                    complete_path[-1]
+                next_city = self._get_closest_city(last_city_idx, visited)
+            visited.add(next_city)
+            complete_path.append(next_city)
+        return complete_path
+
+    def get_candidate(self):
+        """
+        A candidate is going to be an array
+        representing the order of cities
+        visited.
+        """
+        # greedily assembled path of cities
+        return np.array(self._complete_path([]))
+
+    def reset_candidate(self):
+        # random path of cities
+        return np.random.permutation(np.arange(self.params.num_cities))
+
+    def cost(self, candidate):
+        tour_d = 0
+        for i in range(1, len(candidate)):
+            tour_d += self.params.dists[candidate[i], candidate[i-1]]
+        tour_d += self.params.dists[
+            candidate[0], candidate[len(candidate) - 1]]
+        return tour_d
+
+    def next_candidate(self, candidate):
+        nu_candidate = deepcopy(candidate)
+        swaps = np.random.choice(
+            np.arange(len(candidate)), size=2, replace=False)
+        to_swap = nu_candidate[swaps]
+        nu_candidate[swaps[0]] = to_swap[1]
+        nu_candidate[swaps[1]] = to_swap[0]
+        return nu_candidate
+
+
+def dist_from_lat_long(lat1, long1, lat2, long2):
+    """
+    Taken from: https://www.omnicalculator.com/other/latitude-longitude-distance
+    Doesn't currently work. Need to debug (230108)
+    """
+    theta1 = lat1
+    theta2 = lat2
+    phi1 = long1
+    phi2 = long2
+    r = 6400
+    dtheta1 = (theta2-theta1)/2
+    dtheta1 = np.sin(dtheta1)**2
+    dtheta2 = np.cos(theta1)*np.cos(theta1)
+    dtheta2 *= np.sin((phi2-phi1)/2)**2
+    d = np.sqrt(dtheta1+dtheta2)
+    d = np.arcsin(d)
+    dist = 2*r*d
+    return d
