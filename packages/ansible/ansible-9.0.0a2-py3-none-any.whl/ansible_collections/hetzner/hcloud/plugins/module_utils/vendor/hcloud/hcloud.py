bb3ea1b4660a416e8443b7929932f3bd196f970d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:25:28 +0000

    Add package ansible/9.0.0a2/ansible-9.0.0a2-py3-none-any.whl

---
+++ packages/ansible/ansible-9.0.0a2-py3-none-any.whl/ansible_collections/hetzner/hcloud/plugins/module_utils/vendor/hcloud/hcloud.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import warnings
+
+warnings.warn(
+    "The 'hcloud.hcloud' module is deprecated, please import from the 'hcloud' module instead (e.g. 'from hcloud import Client').",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+from ._client import *  # noqa
