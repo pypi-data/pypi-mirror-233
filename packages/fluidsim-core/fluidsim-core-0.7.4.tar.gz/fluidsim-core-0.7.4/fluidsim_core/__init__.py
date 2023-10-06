3d106529fdac74f0fd5555823ed75d17faaf829a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:28:22 +0000

    Add package fluidsim-core/0.7.4/fluidsim-core-0.7.4.tar.gz

---
+++ packages/fluidsim-core/fluidsim-core-0.7.4.tar.gz/fluidsim-core-0.7.4/fluidsim_core/__init__.py
@@ -0,0 +1,20 @@
+"""
+Pure-Python core library for FluidSim framework. This package provides
+generic base classes and utilities to build new solvers.
+
+.. autosummary::
+   :toctree:
+
+   params
+   info
+   solver
+   output
+   loader
+   magic
+   extend_simul
+   hexa_files
+   scripts
+   paths
+
+"""
+from ._version import __version__
