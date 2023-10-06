7424c3dae7454723dd0e992740f5d0ed1886f0e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:15:24 +0000

    Add package magic-motion-metrics/0.1/magic_motion_metrics-0.1.tar.gz

---
+++ packages/magic-motion-metrics/magic_motion_metrics-0.1.tar.gz/magic_motion_metrics-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='magic_motion_metrics',
+    version='0.1',
+    description='This package calculate metrics data using bvh motion data of a human',
+    author='Muhammad Tariq ',
+    author_email='tariq@movrs.com',
+    packages=find_packages(),
+    install_requires=[
+        # this package does not require any other installations
+    ],
+)
