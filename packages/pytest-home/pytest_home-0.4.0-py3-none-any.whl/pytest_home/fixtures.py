10e6bbefa5bd5af8520c38eb9348da1f8a1784bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:15:50 +0000

    Add package pytest-home/0.4.0/pytest_home-0.4.0-py3-none-any.whl

---
+++ packages/pytest-home/pytest_home-0.4.0-py3-none-any.whl/pytest_home/fixtures.py
@@ -0,0 +1,28 @@
+import pathlib
+import platform
+
+import pytest
+
+
+@pytest.fixture
+def alt_home(monkeypatch, tmp_path_factory):
+    """
+    >>> home = getfixture('alt_home')
+    >>> list(home.iterdir())
+    []
+    >>> import pathlib
+    >>> pathlib.Path('~').expanduser() == home
+    True
+    """
+    return _set(monkeypatch, tmp_path_factory.mktemp('home'))
+
+
+def _set(monkeypatch, path: pathlib.Path):
+    """
+    Set the home dir using a pytest monkeypatch context.
+    """
+    win = platform.system() == 'Windows'
+    vars = ['HOME'] + win * ['USERPROFILE']
+    for var in vars:
+        monkeypatch.setenv(var, str(path))
+    return path
