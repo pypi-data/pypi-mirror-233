1523e5dca877b46c8468be151c33fe10758cc196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:13:27 +0000

    Add package GlobalKit/1.1/GlobalKit-1.1.tar.gz

---
+++ packages/GlobalKit/GlobalKit-1.1.tar.gz/GlobalKit-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md', 'r') as readme:
+    description = readme.read()
+
+setup(
+    name='GlobalKit',
+    packages=['GlobalKit'],
+    version='1.1',
+    author='CrazyFlyKite',
+    author_email='karpenkoartem2846@gmail.com',
+    description='',
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
