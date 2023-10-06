b8b5ca8b0c4e5572e90089fe03784e5ea07e62a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:21:28 +0000

    Add package remote-log/1.0.11/remote-log-1.0.11.tar.gz

---
+++ packages/remote-log/remote-log-1.0.11.tar.gz/remote-log-1.0.11/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='remote-log',
+    version='1.0.11',
+    packages=find_packages(),
+    description = 'A simple function to send logging data to a remote server.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = 'Shubham Gupta',
+    author_email = 'shubhastro2@gmail.com',
+    install_requires=[
+            'psutil',
+            'py-cpuinfo',
+            'requests',
+        ],
+)
