24591d59d52b3c496caf86316a35260fac426bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:13:37 +0000

    Add package tom-antares/1.2.6/tom-antares-1.2.6.tar.gz

---
+++ packages/tom-antares/tom-antares-1.2.6.tar.gz/tom-antares-1.2.6/README.md
@@ -0,0 +1,29 @@
+[![pypi](https://img.shields.io/pypi/v/tom-antares.svg)](https://pypi.python.org/pypi/tom-antares)
+[![run-tests](https://github.com/TOMToolkit/tom_antares/actions/workflows/run-tests.yml/badge.svg)](https://github.com/TOMToolkit/tom_antares/actions/workflows/run-tests.yml)
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/6812def88cd5479ab4b833eedd52217f)](https://www.codacy.com/gh/TOMToolkit/tom_antares/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=TOMToolkit/tom_antares&amp;utm_campaign=Badge_Grade)
+[![Coverage Status](https://coveralls.io/repos/github/TOMToolkit/tom_antares/badge.svg?branch=main)](https://coveralls.io/github/TOMToolkit/tom_antares?branch=main)
+
+# Antares TOM Broker Module
+
+This module adds [Antares](https://antares.noao.edu/) support to the TOM
+Toolkit. Using this module TOMs can query and listen to Antares streams.
+
+## Installation
+
+Install the module into your TOM environment:
+
+    pip install tom-antares
+
+Add `tom_antares.antares.ANTARESBroker` to the `TOM_ALERT_CLASSES` in your TOM's
+`settings.py`:
+
+    TOM_ALERT_CLASSES = [
+        ...
+        'tom_antares.antares.ANTARESBroker'
+    ]
+
+## Running the tests
+
+In order to run the tests, run the following in your virtualenv:
+
+`python tom_antares/tests/run_tests.py`
---
+++ packages/tom-antares/tom-antares-1.2.6.tar.gz/tom-antares-1.2.6/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tom-antares',
+    description='Antares broker module for the TOM Toolkit',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://tomtoolkit.github.io',
+    author='TOM Toolkit Project',
+    author_email='llindstrom@lco.global, dmcollom@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    keywords=['tomtoolkit', 'astronomy', 'astrophysics', 'cosmology', 'science', 'fits', 'observatory', 'antares'],
+    packages=find_packages(),
+    use_scm_version=True,  # use_scm_version and setup_requires setuptools_scm are required for automated releases
+    setup_requires=['setuptools_scm', 'wheel'],
+    install_requires=[
+        'tomtoolkit>=2.12,<3.0',
+        'antares-client>=1.4,<2.0',
+        'elasticsearch-dsl>=7.3,<7.5'
+    ],
+    extras_require={
+        'test': ['factory_boy>=3.1,<3.4']
+    },
+    include_package_data=True,
+)
