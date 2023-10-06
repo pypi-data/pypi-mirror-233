d3967092712b22d6baf5ecd6ce90c0bdfe80fe25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:14:48 +0000

    Add package sfxcentralconfig/0.1/sfxcentralconfig-0.1.tar.gz

---
+++ packages/sfxcentralconfig/sfxcentralconfig-0.1.tar.gz/sfxcentralconfig-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup 
+setup(name='sfxcentralconfig',
+      version='0.1',
+      description='operation and library',
+      packages=['sfxcentralconfig'],
+      author_email='vardhinenimanoj4@gmail.com',
+      zip_safe=False)
+
