65e64a7a18b1685541d457e14573621c410a3c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:09:19 +0000

    Add package lottostar/0.1.0/lottostar-0.1.0.tar.gz

---
+++ packages/lottostar/lottostar-0.1.0.tar.gz/lottostar-0.1.0/setup.py
@@ -0,0 +1,19 @@
+"""
+Setup file for the package.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='lottostar',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lottostar=lottostar.main',
+        ],
+    },
+)
