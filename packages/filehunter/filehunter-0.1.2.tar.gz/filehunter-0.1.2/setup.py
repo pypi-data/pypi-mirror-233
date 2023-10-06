24694d08bc1bf974fed20de9c1333b13af171f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:17:47 +0000

    Add package filehunter/0.1.2/filehunter-0.1.2.tar.gz

---
+++ packages/filehunter/filehunter-0.1.2.tar.gz/filehunter-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Raúl Guerrero",
+    description="small functions useful to not increase the length of your code.",
+    name="filehunter",
+    version='0.1.2',
+    packages=find_packages(include=[]),
+    install_requires=[],
+
+    python_requires='>=3.8',
+)
