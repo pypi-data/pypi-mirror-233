9a8e614ba10ab9077d432c7ed2d75229927958e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:28:23 +0000

    Add package alanapy/0.34/alanapy-0.34.tar.gz

---
+++ packages/alanapy/alanapy-0.34.tar.gz/alanapy-0.34/setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from setuptools import setup
+
+setup(
+    name="alanapy",
+    version="0.34",
+    license="MIT",
+    packages = ['alanapy'],
+    author = "Orkahub Energy",
+	author_email = "orkahub@gmail.com",
+	url = "https://github.com/orkahub/alanapy",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type = "text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    keywords= ["RESERVOIR", "ENERGY", "OIL", "GAS", "PRODUCTION", "PYTHON","ALANA"],
+    install_requires=[
+        "requests",
+        "matplotlib",
+        "numpy",
+        "pandas"
+    ],
+)
