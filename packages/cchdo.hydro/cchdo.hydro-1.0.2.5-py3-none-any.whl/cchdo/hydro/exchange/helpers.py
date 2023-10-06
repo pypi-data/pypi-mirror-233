f62c30340322d093489e2c5902b63f70793cf818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 15:22:11 +0000

    Add package cchdo.hydro/1.0.2.5/cchdo.hydro-1.0.2.5-py3-none-any.whl

---
+++ packages/cchdo.hydro/cchdo.hydro-1.0.2.5-py3-none-any.whl/cchdo/hydro/exchange/helpers.py
@@ -0,0 +1,111 @@
+import io
+
+from cchdo.params import WHPNames
+
+
+def simple_bottle_exchange(
+    params=None, units=None, data=None, comments: str | None = None
+):
+    stamp = "BOTTLE,test"
+    min_params = [
+        "EXPOCODE",
+        "STNNBR",
+        "CASTNO",
+        "SAMPNO",
+        "LATITUDE",
+        "LONGITUDE",
+        "DATE",
+        "TIME",
+        "CTDPRS",
+    ]
+    min_units = ["", "", "", "", "", "", "", "", "DBAR"]
+    min_line = ["TEST", "1", "1", "1", "0", "0", "20200101", "0000", "0"]
+    end = "END_DATA"
+
+    if params is not None:
+        min_params.extend(params)
+    if units is not None:
+        min_units.extend(units)
+    if data is not None:
+        min_line.extend(data)
+
+    if comments is not None:
+        comments = "\n".join([f"#{line}" for line in comments.splitlines()])
+        simple = "\n".join(
+            [
+                stamp,
+                comments,
+                ",".join(min_params),
+                ",".join(min_units),
+                ",".join(min_line),
+                end,
+            ]
+        )
+    else:
+        simple = "\n".join(
+            [stamp, ",".join(min_params), ",".join(min_units), ",".join(min_line), end]
+        )
+    return simple.encode("utf8")
+
+
+def gen_complete_bottle(
+    ctd_params_only=False,
+    param_counts: dict[str, int] | None = None,
+    min_count=5,
+    filter_erddap=False,
+):
+    from . import read_exchange
+
+    exclude = set(
+        [
+            "EXPOCODE",
+            "STNNBR",
+            "CASTNO",
+            "SAMPNO",
+            "LATITUDE",
+            "LONGITUDE",
+            "DATE",
+            "TIME",
+            "CTDPRS",
+            "BTL_TIME",
+            "BTL_DATE",
+        ]
+    )
+
+    params = []
+    units = []
+    data = []
+    for name in set(WHPNames.values()):
+        if filter_erddap and not name.in_erddap:
+            continue
+        if name.whp_name in exclude:
+            continue
+        if param_counts is not None:
+            if param_counts.get(name.nc_name, 0) < min_count:
+                continue
+        if ctd_params_only is True and name.flag_w in {
+            "woce_discrete",
+            "woce_bottle",
+            "no_flags",
+        }:
+            continue
+        params.append(name.whp_name)
+        units.append(name.whp_unit if name.whp_unit is not None else "")
+        data.append("-999")
+        if name.flag_w is not None and name.flag_w != "no_flags":
+            params.append(f"{name.whp_name}_FLAG_W")
+            units.append("")
+            data.append("9")
+
+        if name.error_name is not None:
+            params.append(name.error_name)
+            units.append(name.whp_unit if name.whp_unit is not None else "")
+            data.append("-999")
+
+    # special case
+    params.extend(["BTL_DATE", "BTL_TIME"])
+    units.extend(["", ""])
+    data.extend(["20220421", "0944"])
+
+    ex = simple_bottle_exchange(params=params, units=units, data=data)
+    return read_exchange(io.BytesIO(ex))
---
+++ packages/cchdo.hydro/cchdo.hydro-1.0.2.5-py3-none-any.whl/cchdo/hydro/tests/test_accessors.py
@@ -0,0 +1,61 @@
+import json
+from importlib.resources import read_text
+
+import pytest
+from xarray.testing import assert_identical
+
+from ..accessors import CCHDOAccessor
+from ..exchange import FileType, read_exchange
+
+exp_stn_cast = json.loads(read_text("cchdo.hydro.tests.data", "stns_test_data.json"))
+
+
+@pytest.mark.parametrize("expocode", ["318M20130321", "320620140320", "33KI159/1"])
+@pytest.mark.parametrize("station", exp_stn_cast["stations"])
+@pytest.mark.parametrize("cast", [0, 1, 10, 100, 999, 9999])
+@pytest.mark.parametrize("profile_type", [FileType.BOTTLE, FileType.CTD])
+@pytest.mark.parametrize("profile_count", [1, 2])
+@pytest.mark.parametrize("ftype", ["cf", "exchange"])
+def test_gen_fname_machinery(
+    expocode, station, cast, profile_type, profile_count, ftype
+):
+    CCHDOAccessor._gen_fname(
+        expocode, station, cast, profile_type, profile_count, ftype
+    )
+
+
+def test_exchange_bottle_round_trip():
+    # note that the differet bottle and sampno are intentional
+    test_data = b"""BOTTLE,test
+# some comment
+EXPOCODE,STNNBR,CASTNO,SAMPNO,BTLNBR,DATE,TIME,LATITUDE,LONGITUDE,CTDPRS
+,,,,,,,,,DBAR
+TEST          ,1       ,  1,1          ,2          ,20200101,0000,        0,        0,        0
+END_DATA
+"""
+    ds = read_exchange(test_data)
+    # the magic slice removes the stamp and the newline with #
+    rt = read_exchange(ds.cchdo.to_exchange()[25:])
+    assert_identical(ds, rt)
+
+
+def test_exchange_ctd_round_trip():
+    test_data = b"""CTD,test
+# some comment
+NUMBER_HEADERS = 8
+EXPOCODE = TEST
+STNNBR = 1
+CASTNO = 1
+DATE = 20200101
+TIME = 0000
+LATITUDE = 0
+LONGITUDE = 0
+CTDPRS
+DBAR
+0
+END_DATA
+"""
+    ds = read_exchange(test_data)
+    # the magic slice removes the stamp and the newline with #
+    rt = read_exchange(ds.cchdo.to_exchange()[22:])
+    assert_identical(ds, rt)
