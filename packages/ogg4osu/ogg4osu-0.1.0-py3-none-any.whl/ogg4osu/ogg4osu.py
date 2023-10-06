01217cf7fb5c28ee78a2e0b1f3ce6971da48ffe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:20:59 +0000

    Add package ogg4osu/0.1.0/ogg4osu-0.1.0-py3-none-any.whl

---
+++ packages/ogg4osu/ogg4osu-0.1.0-py3-none-any.whl/ogg4osu/ogg4osu.py
@@ -0,0 +1,25 @@
+"""The main logic of the program."""
+from . import audio
+from . import cli
+from argparse import ArgumentParser
+from ffmpeg import Error as ffmpegError
+from pathlib import Path
+
+def main():
+    """Run ogg4osu with arguments from the command line."""
+    args = cli.parse_args()
+
+    source = Path(args.source)
+    if not args.destination:
+        destination = source.with_suffix(".ogg")
+    else:
+        destination = Path(args.destination)
+
+    try:
+        audio.convert(source, destination)
+    except ffmpegError as error:
+        stderr = error.stderr.decode().strip()
+        print(stderr)
+
+if __name__ == "__main__":
+    main()
