55636e5e670e50fddd32cd5dd8461066c84cf77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 06:13:37 +0000

    Add package ksconf-jinja-markdown/1.1.0/ksconf-jinja-markdown-1.1.0.tar.gz

---
+++ packages/ksconf-jinja-markdown/ksconf-jinja-markdown-1.1.0.tar.gz/ksconf-jinja-markdown-1.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="ksconf-jinja-markdown",
+    version="1.1.0",
+    install_requires=[
+        "commonmark>=0.9",
+        "jinja2>=3.0",
+        "ksconf>=0.13.0",
+    ],
+    entry_points={
+        "ksconf_plugin": [
+            "jinja-markdown = ksconf.plugins.jinja_markdown"
+        ]
+    },
+    packages=find_namespace_packages(include=['ksconf.plugins.*']),
+    description="Markdown rendering support for Jinja2 templates within Ksconf",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Environment :: Plugins",
+        "Topic :: Text Processing :: Markup",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords='ksconf splunk jinja markdown',
+    author="Lowell Alleman",
+    author_email="lowell.alleman@cdillc.com",
+    url="https://github.com/Kintyre/ksconf/tree/devel/plugins/jinja-markdown",
+    zip_safe=False
+)
