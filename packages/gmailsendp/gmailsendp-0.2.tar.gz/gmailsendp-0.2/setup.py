a5709839fba37e7b4493c9881534f3c9d2e7e92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:23:36 +0000

    Add package gmailsendp/0.2/gmailsendp-0.2.tar.gz

---
+++ packages/gmailsendp/gmailsendp-0.2.tar.gz/gmailsendp-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gmailsendp',
+    version='0.2',
+    description='Una librería para enviar correos electrónicos usando Gmail',
+    author='aslskks',
+    author_email='davikenat@gmail.com',
+    packages=find_packages(),
+)
