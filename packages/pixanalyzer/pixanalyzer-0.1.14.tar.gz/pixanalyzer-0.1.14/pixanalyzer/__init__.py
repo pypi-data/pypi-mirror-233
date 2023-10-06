9f6da587d06bc3f509330477210b7ef83165a077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 15:13:08 +0000

    Add package pixanalyzer/0.1.14/pixanalyzer-0.1.14.tar.gz

---
+++ packages/pixanalyzer/pixanalyzer-0.1.14.tar.gz/pixanalyzer-0.1.14/pixanalyzer/__init__.py
@@ -0,0 +1,19 @@
+# from .pixanalyzer import *
+
+# import os
+
+# current_directory = os.getcwd()
+# print(current_directory)
+
+# from pixanalyzer.main import *
+
+
+
+__copyright__ = "Copyright (C) 2023 Hayato M Yamanouchi"
+__version__ = "0.1.14"
+__license__ = "MIT"
+__author__ = "Hayato M Yamanouchi"
+__author_email__ = "haya.m.yamano.neuro@gmail.com"
+__url__ = "https://github.com/HMYamano/PixAnalyzer"
+
+__all__ = ["pixanalyzer", "pixanalyzer.libs"]
