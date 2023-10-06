edc15eea1ebfc123d9d11cb1a811c5c6f504f1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:06:49 +0000

    Add package neurum-db/0.3.2/neurum_db-0.3.2-py3-none-any.whl

---
+++ packages/neurum-db/neurum_db-0.3.2-py3-none-any.whl/neurum_db/update.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+class DB():
+    notion_token:str
+    database_id:str
+headers = {
+    "Authorization": "Bearer " + DB.notion_token,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+def update(column_name:str,cell_name:str,new_name:str):
+
+    url = f'https://api.notion.com/v1/databases/{DB.database_id}/query'
+
+    headers = {
+        "Authorization": f"Bearer {DB.notion_token}",
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "filter": {
+            "property": column_name,
+            "title": {
+                "contains": cell_name
+            }
+        }
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    pages = response.json()['results']
+
+    if len(pages) > 0:
+        page_id = pages[0]['id']
+    else:
+        print(f'No page found with the name "{cell_name}"')
+
+    url = f'https://api.notion.com/v1/pages/{page_id}'
+
+    payload = {
+        "properties": {
+            column_name: {
+                "title": [
+                    {
+                        "text": {
+                            "content": new_name
+                        }
+                    }
+                ]
+            }
+        }
+    }
+
+    requests.request("PATCH", url, headers=headers, data=json.dumps(payload))
+
