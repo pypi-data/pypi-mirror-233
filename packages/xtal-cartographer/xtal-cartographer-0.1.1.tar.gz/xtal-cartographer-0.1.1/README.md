405585e3411f389414951ac13ab30dddad484c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:17:16 +0000

    Add package xtal-cartographer/0.1.1/xtal-cartographer-0.1.1.tar.gz

---
+++ packages/xtal-cartographer/xtal-cartographer-0.1.1.tar.gz/xtal-cartographer-0.1.1/README.md
@@ -0,0 +1,19 @@
+Example Usage:
+
+```
+cartographer -i PDB.mtz -o PDB.map -intensity FWT -phase PHWT
+```
+
+
+```
+usage: cartographer [-h] [-m M] -i I -o O [-r [R]] [-intensity [INTENSITY]] [-phase [PHASE]]
+
+options:
+  -h, --help              show this help message and exit
+  -m M, -model_path M     Path to model
+  -i I, -input I          Input mtz path
+  -o O, -output O         Output map path 
+  -r [R], -resolution [R] Resolution cutoff to apply to mtz
+  -intensity [INTENSITY]  Name of intensity column in MTZ
+  -phase [PHASE]          Name of phase column in MTZ
+```
---
+++ packages/xtal-cartographer/xtal-cartographer-0.1.1.tar.gz/xtal-cartographer-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import urllib.request
+import site 
+import os
+
+def download_dependencies(): 
+    site_packages_dir = site.getsitepackages()
+    if site_packages_dir:
+        model_dir = os.path.join(site_packages_dir[0], "cartographer_models")
+        if not os.path.exists(model_dir):
+            os.mkdir(model_dir)
+        phos_model_path = os.path.join(model_dir, "phos.hdf5")
+        if not os.path.exists(phos_model_path):
+            print("Downloading the Cartographer phosphate model into ", phos_model_path, " cancel now if you do not wish for this to happen")
+            urllib.request.urlretrieve("http://www.ysbl.york.ac.uk/jsd523/phos.best.hdf5", phos_model_path)  
+
+    return [ 
+        'tensorflow',
+        'tensorflow_addons',
+        'gemmi',
+        'tqdm' ]
+
+setup(
+    name="xtal-cartographer",
+    author="Jordan Dialpuri",
+    author_email="jsd523@york.ac.uk",
+    version="0.1.1",
+    entry_points={
+        "console_scripts": [
+            'cartographer=cartographer.predict:run'
+        ]
+    },
+     install_requires=download_dependencies(),
+
+)
