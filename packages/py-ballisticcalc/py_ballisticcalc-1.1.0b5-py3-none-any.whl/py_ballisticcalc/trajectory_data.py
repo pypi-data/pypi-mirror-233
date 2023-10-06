e6d918650d8980c2085f0c76a23f4c5b0e87d744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 04:08:15 +0000

    Add package py-ballisticcalc/1.1.0b5/py_ballisticcalc-1.1.0b5-py3-none-any.whl

---
+++ packages/py-ballisticcalc/py_ballisticcalc-1.1.0b5-py3-none-any.whl/py_ballisticcalc/trajectory_data.py
@@ -0,0 +1,91 @@
+"""Implements a point of trajectory class in applicable data types"""
+from enum import Flag, auto
+from typing import NamedTuple
+
+from .settings import Settings as Set
+from .unit import Angular, Distance, Weight, Velocity, Energy, AbstractUnit, Unit
+
+__all__ = ('TrajectoryData', 'TrajFlag')
+
+
+class TrajFlag(Flag):
+    """Flags for marking trajectory row if Zero or Mach crossing
+    Also uses to set a brake points of trajectory calculation loop
+    """
+    NONE = auto()
+    ZERO = auto()
+    MACH = auto()
+
+
+class TrajectoryData(NamedTuple):
+    """
+    Represents point of trajectory in applicable data types
+
+    Attributes:
+        time (float): bullet flight time
+        distance (Distance): traveled distance
+        velocity (Velocity): velocity in current trajectory point
+        mach (float): velocity in current trajectory point in "Mach" number
+        drop (Distance):
+        drop_adj (Angular):
+        windage (Distance):
+        windage_adj (Angular):
+        angle (Angular)
+        mach float
+        energy (Energy):
+        ogw (Weight): optimal game weight
+        rtype (int): row type
+    """
+
+    time: float
+    distance: Distance
+    velocity: Velocity
+    mach: float  # velocity in Mach
+    drop: Distance
+    drop_adj: Angular  # drop_adjustment
+    windage: Distance
+    windage_adj: Angular  # windage_adjustment
+    angle: Angular  # Trajectory angle
+    energy: Energy
+    ogw: Weight
+    flag: TrajFlag = TrajFlag.NONE
+
+    def formatted(self) -> tuple:
+        """
+        :return: matrix of formatted strings for each value of trajectory in default units
+        """
+        def _fmt(v: AbstractUnit, u: Unit):
+            """simple formatter"""
+            return f"{v >> u:.{u.accuracy}f} {u.symbol}"
+
+        return (
+            f'{self.time:.2f} s',
+            _fmt(self.distance, Set.Units.distance),
+            _fmt(self.velocity, Set.Units.velocity),
+            f'{self.mach:.2f} mach',
+            _fmt(self.drop, Set.Units.drop),
+            _fmt(self.drop_adj, Set.Units.adjustment),
+            _fmt(self.windage, Set.Units.drop),
+            _fmt(self.windage_adj, Set.Units.adjustment),
+            _fmt(self.angle, Set.Units.angular),
+            _fmt(self.energy, Set.Units.energy),
+            _fmt(self.ogw, Set.Units.ogw)
+        )
+
+    def in_def_units(self) -> tuple:
+        """
+        :return: matrix of floats of the trajectory in default units
+        """
+        return (
+            self.time,
+            self.distance >> Set.Units.distance,
+            self.velocity >> Set.Units.velocity,
+            self.mach,
+            self.drop >> Set.Units.drop,
+            self.drop_adj >> Set.Units.adjustment,
+            self.windage >> Set.Units.drop,
+            self.windage_adj >> Set.Units.adjustment,
+            self.angle >> Set.Units.angular,
+            self.energy >> Set.Units.energy,
+            self.ogw >> Set.Units.ogw
+        )
