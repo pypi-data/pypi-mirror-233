1184f75865ebefd7c928041e6111d416fc4464a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:22:09 +0000

    Add package defog-data/0.1.1/defog_data-0.1.1-py3-none-any.whl

---
+++ packages/defog-data/defog_data-0.1.1-py3-none-any.whl/defog_data/metadata.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+print(f"script_dir: {script_dir}")
+
+def get_db(db_name):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, f"{db_name}/{db_name}.json")
+    with open(file_path, "r") as f:
+
+        db_schema = json.load(f)
+    return db_schema
+
+
+academic = get_db("academic")
+advising = get_db("advising")
+atis = get_db("atis")
+geography = get_db("geography")
+restaurants = get_db("restaurants")
+scholar = get_db("scholar")
+yelp = get_db("yelp")
+
+dbs = {
+    "academic": academic,
+    "advising": advising,
+    "atis": atis,
+    "geography": geography,
+    "restaurants": restaurants,
+    "scholar": scholar,
+    "yelp": yelp
+}
