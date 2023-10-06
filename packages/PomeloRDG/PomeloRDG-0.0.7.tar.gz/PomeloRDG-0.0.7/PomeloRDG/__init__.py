c48ac729893fa02817799cd0abdf3f45d187c3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:19:54 +0000

    Add package PomeloRDG/0.0.7/PomeloRDG-0.0.7.tar.gz

---
+++ packages/PomeloRDG/PomeloRDG-0.0.7.tar.gz/PomeloRDG-0.0.7/PomeloRDG/__init__.py
@@ -0,0 +1,31 @@
+"""
+Pomelo Random Data Generator.
+By PomeloABC.
+This is a tool package for everyone to make the test data quickly.
+"""
+
+# OtherLibs
+
+import random
+import os
+
+# Tools
+
+from PomeloRDG.Maths import *
+from PomeloRDG.IO import IO
+
+# DataTypes
+
+import PomeloRDG.Const as Const
+from PomeloRDG.Char import Char
+from PomeloRDG.Double import Double
+from PomeloRDG.Float import Float
+from PomeloRDG.Int import Int
+from PomeloRDG.LLong import LLong
+from PomeloRDG.ULLong import ULLong
+from PomeloRDG.String import String
+
+# DataStructs
+
+from PomeloRDG.Vector import Vector
+from PomeloRDG.Graph import Graph
