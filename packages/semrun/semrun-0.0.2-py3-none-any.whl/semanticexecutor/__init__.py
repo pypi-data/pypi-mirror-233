9097967b8868b58fba5b1617fae14127cf5343f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 14:09:01 +0000

    Add package semrun/0.0.2/semrun-0.0.2-py3-none-any.whl

---
+++ packages/semrun/semrun-0.0.2-py3-none-any.whl/semanticexecutor/__init__.py
@@ -0,0 +1,1 @@
+from .semrun import SemRun
---
+++ packages/semrun/semrun-0.0.2-py3-none-any.whl/semanticexecutor/semrun.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
+import torch
+
+class SemanticExecutor:
+    def __init__(self, function_topic_dict, model_name='thenlper/gte-small'):
+        self.functions = list(function_topic_dict.keys())
+        self.topics = list(function_topic_dict.values())
+        self.model = SentenceTransformer(model_name)
+        self.topic_embeddings = self.model.encode(self.topics)
+
+    def execute(self, user_input):
+        user_embedding = self.model.encode([user_input])
+        similarities = [cos_sim(topic_embedding, user_embedding) for topic_embedding in self.topic_embeddings]
+        max_sim_index = torch.argmax(torch.Tensor(similarities))
+        return self.functions[max_sim_index] 
