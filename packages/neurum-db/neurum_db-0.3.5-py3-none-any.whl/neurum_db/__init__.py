5ab6aeae57a4570a16cd011f9905d2038bddc1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 05:11:35 +0000

    Add package neurum-db/0.3.5/neurum_db-0.3.5-py3-none-any.whl

---
+++ packages/neurum-db/neurum_db-0.3.5-py3-none-any.whl/neurum_db/__init__.py
@@ -0,0 +1,4 @@
+from .get import get
+from .post import post
+from .update import update
+from .delete import delete
---
+++ packages/neurum-db/neurum_db-0.3.5-py3-none-any.whl/neurum_db/get.py
@@ -0,0 +1,30 @@
+import requests
+import json
+    
+
+def get(column_name:str, cell_name:str, notion_token:str, database_id:str, num_pages=None):
+    headers = {
+            "Authorization": "Bearer " + notion_token,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28",
+        }
+    """
+    If num_pages is None, get all pages, otherwise just the defined number.
+    """
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+
+    get_all = num_pages is None
+    page_size = 100 if get_all else num_pages
+
+    payload = {"page_size": page_size}
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+
+    pages = data["results"]
+    content=[]
+
+    for page in pages:
+        props=page['properties']
+        tweet=props[column_name]['title'][0]['text']['content']
+        content.append(tweet)
+        return content
