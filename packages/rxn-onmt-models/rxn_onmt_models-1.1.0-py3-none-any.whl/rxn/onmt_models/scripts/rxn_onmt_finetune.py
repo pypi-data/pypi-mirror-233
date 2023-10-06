6dd35042914d083b478d409f924ec97951f19f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-06 03:19:54 +0000

    Add package rxn-onmt-models/1.1.0/rxn_onmt_models-1.1.0-py3-none-any.whl

---
+++ packages/rxn-onmt-models/rxn_onmt_models-1.1.0-py3-none-any.whl/rxn/onmt_models/scripts/rxn_onmt_finetune.py
@@ -0,0 +1,141 @@
+import logging
+from typing import Tuple
+
+import click
+from rxn.onmt_utils import __version__ as onmt_utils_version
+from rxn.onmt_utils.model_introspection import (
+    get_model_rnn_size,
+    model_vocab_is_compatible,
+)
+from rxn.onmt_utils.model_resize import ModelResizer
+from rxn.onmt_utils.train_command import OnmtTrainCommand
+from rxn.utilities.logging import setup_console_and_file_logger
+
+from rxn.onmt_models import __version__ as onmt_models_version
+from rxn.onmt_models import defaults
+from rxn.onmt_models.training_files import ModelFiles, OnmtPreprocessedFiles
+from rxn.onmt_models.utils import log_file_name_from_time, run_command
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+@click.command(context_settings=dict(show_default=True))
+@click.option("--batch_size", default=defaults.BATCH_SIZE)
+@click.option(
+    "--data_weights",
+    type=int,
+    multiple=True,
+    help="Weights of the different data sets for training. Only needed in a multi-task setting.",
+)
+@click.option("--dropout", default=defaults.DROPOUT)
+@click.option(
+    "--keep_checkpoint",
+    type=int,
+    default=defaults.KEEP_CHECKPOINT,
+    help='How many checkpoints to keep ("-1" means "keep all").',
+)
+@click.option("--learning_rate", type=float, default=0.06)
+@click.option(
+    "--model_output_dir", type=str, required=True, help="Where to save the models"
+)
+@click.option("--no_gpu", is_flag=True, help="Run the training on CPU (slow!)")
+@click.option(
+    "--preprocess_dir",
+    type=str,
+    required=True,
+    help="Directory with OpenNMT-preprocessed files",
+)
+@click.option("--seed", default=defaults.SEED)
+@click.option(
+    "--train_from", type=str, required=True, help="Path to the model to start from"
+)
+@click.option("--train_num_steps", "--finetune_num_steps", default=100000)
+@click.option("--warmup_steps", default=defaults.WARMUP_STEPS)
+@click.option("--report_every", default=1000)
+@click.option("--save_checkpoint_steps", default=5000)
+def main(
+    batch_size: int,
+    data_weights: Tuple[int, ...],
+    dropout: float,
+    keep_checkpoint: int,
+    learning_rate: float,
+    model_output_dir: str,
+    no_gpu: bool,
+    preprocess_dir: str,
+    seed: int,
+    train_from: str,
+    train_num_steps: int,
+    warmup_steps: int,
+    report_every: int,
+    save_checkpoint_steps: int,
+) -> None:
+    """Finetune an OpenNMT model."""
+
+    # Set up paths
+    model_files = ModelFiles(model_output_dir)
+    onmt_preprocessed_files = OnmtPreprocessedFiles(preprocess_dir)
+
+    # Set up the logs
+    log_file = model_files.model_dir / log_file_name_from_time("rxn-onmt-finetune")
+    setup_console_and_file_logger(log_file)
+
+    logger.info("Fine-tuning RXN model.")
+    logger.info(f"rxn-onmt-utils version: {onmt_utils_version}. ")
+    logger.info(f"rxn-onmt-models version: {onmt_models_version}. ")
+
+    if not model_vocab_is_compatible(train_from, onmt_preprocessed_files.vocab_file):
+        # Extend the vocabulary of the base model based on the training data for
+        # finetuning, and save its updated version in the new model directory.
+        updated_base_model = model_files.model_dir / "updated_base_model.pt"
+        resizer = ModelResizer(train_from)
+        resizer.extend_vocab(onmt_preprocessed_files.vocab_file)
+        resizer.save_checkpoint(updated_base_model)
+
+        logger.info(
+            f'The model checkpoint "{train_from}" needs additional vocabulary '
+            f'tokens. The extended checkpoint was saved to "{updated_base_model}".'
+        )
+        train_from = str(updated_base_model)
+
+    # In principle, the rnn_size should not be needed for finetuning. However,
+    # when resetting the decay algorithm for the learning rate, this value
+    # is necessary - and does not get it from the model checkpoint (OpenNMT bug).
+    rnn_size = get_model_rnn_size(train_from)
+    logger.info(f"Loaded the value of rnn_size from the model: {rnn_size}.")
+
+    config_file = model_files.next_config_file()
+
+    train_cmd = OnmtTrainCommand.finetune(
+        batch_size=batch_size,
+        data=onmt_preprocessed_files.preprocess_prefix,
+        dropout=dropout,
+        keep_checkpoint=keep_checkpoint,
+        learning_rate=learning_rate,
+        rnn_size=rnn_size,
+        save_model=model_files.model_prefix,
+        seed=seed,
+        train_from=train_from,
+        train_steps=train_num_steps,
+        warmup_steps=warmup_steps,
+        no_gpu=no_gpu,
+        data_weights=data_weights,
+        report_every=report_every,
+        save_checkpoint_steps=save_checkpoint_steps,
+    )
+
+    # Write config file
+    command_and_args = train_cmd.save_to_config_cmd(config_file)
+    run_command(command_and_args)
+
+    # Actual training config file
+    command_and_args = train_cmd.execute_from_config_cmd(config_file)
+    run_command(command_and_args)
+
+    logger.info(
+        f'Fine-tuning successful. Models saved under "{str(model_files.model_dir)}".'
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/rxn-onmt-models/rxn_onmt_models-1.1.0-py3-none-any.whl/rxn/onmt_models/scripts/rxn_prepare_data.py
@@ -0,0 +1,134 @@
+import logging
+from pathlib import Path
+
+import click
+from rxn.onmt_utils import __version__ as onmt_utils_version
+from rxn.reaction_preprocessing.config import (
+    CommonConfig,
+    Config,
+    DataConfig,
+    FragmentBond,
+    InitialDataFormat,
+    PreprocessConfig,
+    RxnImportConfig,
+    SplitConfig,
+    StandardizeConfig,
+)
+from rxn.reaction_preprocessing.main import preprocess_data
+from rxn.utilities.logging import setup_console_and_file_logger
+
+from rxn.onmt_models import __version__ as onmt_models_version
+from rxn.onmt_models import defaults
+from rxn.onmt_models.training_files import RxnPreprocessingFiles
+from rxn.onmt_models.utils import log_file_name_from_time
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+@click.command(context_settings=dict(show_default=True))
+@click.option("--input_data", type=str, required=True, help="Input data TXT or CSV")
+@click.option(
+    "--import_from",
+    type=str,
+    default="txt",
+    help=(
+        'Column to import reaction SMILES from in a CSV. The default, "txt", '
+        "means the input file is a simple TXT file."
+    ),
+)
+@click.option(
+    "--output_dir",
+    type=str,
+    required=True,
+    help="Directory where to save the generated files",
+)
+@click.option(
+    "--min_reactants",
+    type=int,
+    default=2,
+    help=(
+        "Minimum number of precursors / reactants. "
+        "Reactions with fewer precursors than that will be discarded."
+    ),
+)
+@click.option(
+    "--split_seed", default=defaults.SEED, help="Random seed for splitting step"
+)
+@click.option(
+    "--fragment_bond",
+    type=click.Choice(["DOT", "TILDE"], case_sensitive=False),
+    default="DOT",
+)
+def main(
+    input_data: str,
+    import_from: str,
+    output_dir: str,
+    min_reactants: int,
+    split_seed: int,
+    fragment_bond: str,
+) -> None:
+    """Preprocess the data to generate a dataset for training transformer models.
+
+    The script will automatically generate the following files in output_dir:
+        data.imported.csv
+        data.standardized.csv
+        data.processed.csv
+        data.processed.train.csv
+        data.processed.validation.csv
+        data.processed.test.csv
+        data.processed.train.precursors_tokens
+        data.processed.train.products_tokens
+        data.processed.validation.precursors_tokens
+        data.processed.validation.products_tokens
+        data.processed.test.precursors_tokens
+        data.processed.test.products_tokens
+    """
+
+    # Running the command below fails if the paths are relative -> make them absolute
+    input_data_path = Path(input_data).resolve()
+    output_dir_path = Path(output_dir).resolve()
+
+    # make sure that the required output directory exists
+    output_dir_path.mkdir(parents=True, exist_ok=True)
+
+    # Set up the logs
+    log_file = output_dir_path / log_file_name_from_time("rxn-prepare-data")
+    setup_console_and_file_logger(log_file)
+
+    logger.info("Prepare reaction data for training with rxn-onmt-models.")
+    logger.info(f"rxn-onmt-utils version: {onmt_utils_version}. ")
+    logger.info(f"rxn-onmt-models version: {onmt_models_version}. ")
+
+    if import_from == "txt":
+        import_config = RxnImportConfig(data_format=InitialDataFormat.TXT)
+    else:
+        import_config = RxnImportConfig(
+            data_format=InitialDataFormat.CSV, input_csv_column_name=import_from
+        )
+
+    cfg = Config(
+        data=DataConfig(
+            path=str(input_data_path),
+            proc_dir=str(output_dir_path),
+            name=RxnPreprocessingFiles.FILENAME_ROOT,
+        ),
+        common=CommonConfig(fragment_bond=FragmentBond[fragment_bond]),
+        rxn_import=import_config,
+        standardize=StandardizeConfig(
+            annotation_file_paths=[], discard_unannotated_metals=False
+        ),
+        preprocess=PreprocessConfig(min_reactants=min_reactants),
+        split=SplitConfig(hash_seed=split_seed),
+    )
+
+    try:
+        logger.info("Running the data preprocessing")
+        preprocess_data(cfg)
+    except Exception as e:
+        logger.exception("Error during data preprocessing:")
+        raise SystemExit("Error during data preprocessing") from e
+
+
+if __name__ == "__main__":
+    main()
