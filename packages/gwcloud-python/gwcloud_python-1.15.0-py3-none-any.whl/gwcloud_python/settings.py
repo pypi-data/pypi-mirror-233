92fb02edab5a2931792b30d03f2395b8fb41f0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-15 23:07:42 +0000

    Add package gwcloud-python/1.15.0/gwcloud_python-1.15.0-py3-none-any.whl

---
+++ packages/gwcloud-python/gwcloud_python-1.15.0-py3-none-any.whl/gwcloud_python/settings.py
@@ -0,0 +1,6 @@
+from gwdc_python.settings import GWDC_FILE_DOWNLOAD_ENDPOINT
+
+GWCLOUD_ENDPOINT = 'https://gwcloud.org.au/bilby/graphql'
+GWCLOUD_AUTH_ENDPOINT = 'https://gwcloud.org.au/auth/graphql'
+GWCLOUD_FILE_DOWNLOAD_ENDPOINT = GWDC_FILE_DOWNLOAD_ENDPOINT
+GWCLOUD_UPLOADED_JOB_FILE_DOWNLOAD_ENDPOINT = 'https://gwcloud.org.au/bilby/file_download/?fileId='
