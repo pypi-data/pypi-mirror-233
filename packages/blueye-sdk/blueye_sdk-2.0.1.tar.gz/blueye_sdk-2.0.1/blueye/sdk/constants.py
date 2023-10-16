4817563015adbaae023a77bbf0d92c288d2c7601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 00:08:08 +0000

    Add package blueye-sdk/2.0.1/blueye_sdk-2.0.1.tar.gz

---
+++ packages/blueye-sdk/blueye_sdk-2.0.1.tar.gz/blueye_sdk-2.0.1/blueye/sdk/constants.py
@@ -0,0 +1,13 @@
+"""
+This file contains constants used by the SDK.
+"""
+
+
+class WaterDensities:
+    """
+    Various typical densities for salt water (in kilograms/liter)
+    """
+
+    fresh = 0.997
+    brackish = 1.011
+    salty = 1.025
