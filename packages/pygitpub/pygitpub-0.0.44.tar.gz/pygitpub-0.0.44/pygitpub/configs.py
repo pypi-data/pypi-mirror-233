05acabba4df689e889f3b695d44583fdcf11e21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 01:30:34 +0000

    Add package pygitpub/0.0.44/pygitpub-0.0.44.tar.gz

---
+++ packages/pygitpub/pygitpub-0.0.44.tar.gz/pygitpub-0.0.44/pygitpub/configs.py
@@ -0,0 +1,55 @@
+"""
+All configurations for pygitpub
+"""
+from pytconf import Config, ParamCreator
+
+
+class ConfigGithub(Config):
+    """
+    Paramters for your github account
+    """
+    username = ParamCreator.create_str(
+        help_string="What is your github username?",
+    )
+    token = ParamCreator.create_str(
+        help_string="What is your github token?",
+    )
+    token_name = ParamCreator.create_str(
+        help_string="What is the name of the token on github?",
+    )
+
+
+class ConfigAlgo(Config):
+    """
+    Parameters to control the algorithm
+    """
+    private = ParamCreator.create_bool(
+        help_string="Include private repos?",
+        default=True,
+    )
+    public = ParamCreator.create_bool(
+        help_string="Include public repos?",
+        default=True,
+    )
+    fork = ParamCreator.create_bool(
+        help_string="Include forks?",
+        default=False,
+    )
+    dryrun = ParamCreator.create_bool(
+        help_string="Do a try run?",
+        default=False,
+    )
+    owner_login = ParamCreator.create_str_or_none(
+        help_string="Only include repos owned by this owner (None for dont mind)",
+        default=None,
+    )
+
+
+class ConfigOutput(Config):
+    """
+    Parameters to control output
+    """
+    verbose = ParamCreator.create_bool(
+        help_string="be verbose?",
+        default=False,
+    )
