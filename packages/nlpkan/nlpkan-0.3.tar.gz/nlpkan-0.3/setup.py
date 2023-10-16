61d590d78f0bb76300ebf23c5c47e40440553167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 00:08:08 +0000

    Add package nlpkan/0.3/nlpkan-0.3.tar.gz

---
+++ packages/nlpkan/nlpkan-0.3.tar.gz/nlpkan-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'nlpkan',   
+  packages=['nlp_kan']  ,
+  version = '0.3',   
+  description = '', 
+  author = 'Chandru Vinayagam',                 
+  author_email = 'chandru.vinayagam21@gmail.com', 
+  install_requires=[]
+)
