f58ec857f1d42a25573516b0999d5919864a4391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-16 01:29:59 +0000

    Add package django-drf-otp/0.0.1/django_drf_otp-0.0.1-py3-none-any.whl

---
+++ packages/django-drf-otp/django_drf_otp-0.0.1-py3-none-any.whl/django_drf_otp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .viewsets import OTPTokenViewSet
+
+urlpatterns = [
+    path("login/", OTPTokenViewSet.as_view({"post": "login"}), name="login"),
+    path("verify-token/", OTPTokenViewSet.as_view({"post": "verify_token"}), name="verify-token"),
+]
